f91fcac6739141af4bb66a97dfdaaccdcd74f50b
Micael Karlberg <bmk@erlang.org>
2010-02-02 13:29:22 +0000

    OTP-8317 & OTP-8362.

--- lib/megaco/vsn.mk
+++ lib/megaco/vsn.mk
@@ -1,27 +1,30 @@
 #-*-makefile-*-   ; force emacs to enter makefile-mode
 
 # %CopyrightBegin%
-# 
-# Copyright Ericsson AB 2001-2009. All Rights Reserved.
-# 
+#
+# Copyright Ericsson AB 2001-2010. All Rights Reserved.
+#
 # The contents of this file are subject to the Erlang Public License,
 # Version 1.1, (the "License"); you may not use this file except in
 # compliance with the License. You should have received a copy of the
 # Erlang Public License along with this software. If not, it can be
 # retrieved online at http://www.erlang.org/.
-# 
+#
 # Software distributed under the License is distributed on an "AS IS"
 # basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
 # the License for the specific language governing rights and limitations
 # under the License.
-# 
+#
 # %CopyrightEnd%
 
-MEGACO_VSN = 3.13.0.1
-PRE_VSN    =
-APP_VSN    = "megaco-$(MEGACO_VSN)$(PRE_VSN)"
+APPLICATION = megaco
+MEGACO_VSN = 3.13.1
+PRE_VSN    =-p01
+APP_VSN    = "$(APPLICATION)-$(MEGACO_VSN)$(PRE_VSN)"
 
-TICKETS = OTP-8205 OTP-8239 OTP-8249
+TICKETS = OTP-8317 OTP-8362 
+
+TICKETS_3_13 = OTP-8205 OTP-8239 OTP-8249
 
 TICKETS_3_12 = OTP-8183 OTP-8212
 
