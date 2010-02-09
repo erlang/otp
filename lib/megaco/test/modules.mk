19fda3d8ddbd8b844024bd15689dbf45fa8e5e1e
Micael Karlberg <bmk@erlang.org>
2010-02-09 14:56:37 +0000

    Fixing spec-file(s) bugs.

--- lib/megaco/test/modules.mk
+++ lib/megaco/test/modules.mk
@@ -1,23 +1,25 @@
 #-*-makefile-*-   ; force emacs to enter makefile-mode
 
 # %CopyrightBegin%
-# 
-# Copyright Ericsson AB 2001-2009. All Rights Reserved.
-# 
+#
+# Copyright Ericsson AB 2001-2010. All Rights Reserved.
+#
 # The contents of this file are subject to the Erlang Public License,
 # Version 1.1, (the "License"); you may not use this file except in
 # compliance with the License. You should have received a copy of the
 # Erlang Public License along with this software. If not, it can be
 # retrieved online at http://www.erlang.org/.
-# 
+#
 # Software distributed under the License is distributed on an "AS IS"
 # basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
 # the License for the specific language governing rights and limitations
 # under the License.
-# 
+#
 # %CopyrightEnd%
 
 TEST_SPEC_FILE = megaco.spec
+
+COVER_SPEC_FILE = megaco.cover
 
 BEHAVIOUR_MODULES = \
 	megaco_test_generator
