695c11cc64f62c112d40c1db24195643fe7afe11
Lars G Thorsen <lars@erlang.org>
2010-02-16 09:48:04 +0000

    ** Empty commit message **

--- lib/xmerl/vsn.mk
+++ lib/xmerl/vsn.mk
@@ -1,28 +1,30 @@
 #
 # %CopyrightBegin%
-# 
-# Copyright Ericsson AB 2003-2009. All Rights Reserved.
-# 
+#
+# Copyright Ericsson AB 2003-2010. All Rights Reserved.
+#
 # The contents of this file are subject to the Erlang Public License,
 # Version 1.1, (the "License"); you may not use this file except in
 # compliance with the License. You should have received a copy of the
 # Erlang Public License along with this software. If not, it can be
 # retrieved online at http://www.erlang.org/.
-# 
+#
 # Software distributed under the License is distributed on an "AS IS"
 # basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
 # the License for the specific language governing rights and limitations
 # under the License.
-# 
+#
 # %CopyrightEnd%
 #
 
-XMERL_VSN = 1.2.3
+XMERL_VSN = 1.2.4
+
 
 TICKETS	= \
-	OTP-8251 \
-	OTP-8252 \
-	OTP-8253 
+	OTP-8343
+
+TICKETS_1.2.4 = \
+	OTP-8343
 
 TICKETS_1.2.3 = \
 	OTP-8251 \
