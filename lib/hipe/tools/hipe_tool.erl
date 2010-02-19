6328bb2d212e63c1cf6c044a7688412b4ddf6954
Kostis Sagonas <kostis@cs.ntua.gr>
2010-02-19 11:39:38 +0000

    Fix problems identified by dialyzer.

--- lib/hipe/tools/hipe_tool.erl
+++ lib/hipe/tools/hipe_tool.erl
@@ -1,20 +1,20 @@
 %% -*- erlang-indent-level: 2 -*-
 %%
 %% %CopyrightBegin%
-%% 
-%% Copyright Ericsson AB 2002-2009. All Rights Reserved.
-%% 
+%%
+%% Copyright Ericsson AB 2002-2010. All Rights Reserved.
+%%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
 %% compliance with the License. You should have received a copy of the
 %% Erlang Public License along with this software. If not, it can be
 %% retrieved online at http://www.erlang.org/.
-%% 
+%%
 %% Software distributed under the License is distributed on an "AS IS"
 %% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
 %% the License for the specific language governing rights and limitations
 %% under the License.
-%% 
+%%
 %% %CopyrightEnd%
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -506,8 +506,8 @@ get_edoc(Mod) ->
   ok.
 
 edoc(Name, Opts) ->
-  Doc = edoc:get_doc([Name, Opts]),
+  Doc = edoc:get_doc(Name, Opts),
   %% Comments = edoc:read_comments(Name, Opts),
   %% Text = edoc:forms(Forms, Comments, Name, Opts),
-  edoc:layout([Doc, Opts]),
+  edoc:layout(Doc, Opts),
   ok.
