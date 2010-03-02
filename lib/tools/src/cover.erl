0961f08a58ccdc870920ec7f690fbf23a28dfe74
Hans Bolinder <hasse@erlang.org>
2010-03-02 06:47:00 +0000

    OTP-8469 tools: race in Cover
    A race condition affecting Cover has been removed.

--- lib/tools/src/cover.erl
+++ lib/tools/src/cover.erl
@@ -1,19 +1,19 @@
 %%
 %% %CopyrightBegin%
-%% 
-%% Copyright Ericsson AB 2001-2009. All Rights Reserved.
-%% 
+%%
+%% Copyright Ericsson AB 2001-2010. All Rights Reserved.
+%%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
 %% compliance with the License. You should have received a copy of the
 %% Erlang Public License along with this software. If not, it can be
 %% retrieved online at http://www.erlang.org/.
-%% 
+%%
 %% Software distributed under the License is distributed on an "AS IS"
 %% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
 %% the License for the specific language governing rights and limitations
 %% under the License.
-%% 
+%%
 %% %CopyrightEnd%
 %%
 -module(cover).
@@ -689,6 +689,7 @@ main_process_loop(State) ->
 	      end,
 	      State#main_state.nodes),
 	    reload_originals(State#main_state.compiled),
+            unregister(?SERVER),
 	    reply(From, ok);
 
 	{From, {Request, Module}} ->
@@ -869,6 +870,7 @@ remote_process_loop(State) ->
 
 	{remote,stop} ->
 	    reload_originals(State#remote_state.compiled),
+            unregister(?SERVER),
 	    remote_reply(State#remote_state.main_node, ok);
 
 	get_status ->
