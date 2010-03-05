02112d8dba5b40cd0adb7ecb2b2ed4340594369f
Niclas Eklund <nick@erlang.org>
2010-03-05 12:29:29 +0000

    Added missing bracket

--- lib/orber/include/ifr_types.hrl
+++ lib/orber/include/ifr_types.hrl
@@ -1,20 +1,20 @@
 %%--------------------------------------------------------------------
 %%
 %% %CopyrightBegin%
-%% 
-%% Copyright Ericsson AB 1997-2009. All Rights Reserved.
-%% 
+%%
+%% Copyright Ericsson AB 1997-2010. All Rights Reserved.
+%%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
 %% compliance with the License. You should have received a copy of the
 %% Erlang Public License along with this software. If not, it can be
 %% retrieved online at http://www.erlang.org/.
-%% 
+%%
 %% Software distributed under the License is distributed on an "AS IS"
 %% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
 %% the License for the specific language governing rights and limitations
 %% under the License.
-%% 
+%%
 %% %CopyrightEnd%
 %%
 %%
@@ -54,7 +54,7 @@
 			 id = Obj#Object_type.id,
 			 defined_in = Obj#Object_type.defined_in,
 			 version = Obj#Object_type.version,
-			 type = Obj#Object_type.type}.
+			 type = Obj#Object_type.type}).
 
 -record(exceptiondescription, {name, id, defined_in, version, type}).
 
