5e454b3d8ea3bf14db3afd44c34964de21770ecb
Håkan Mattsson <hakan@erlang.org>
2010-03-15 15:08:17 +0100

    Add app and appup files to the wx application

--- lib/wx/src/wxe_master.erl
+++ lib/wx/src/wxe_master.erl
@@ -33,7 +33,6 @@
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 	 terminate/2, code_change/3]).
--compile(export_all).
 
 -record(state, {cb_port,  %% Callback port and to erlang messages goes via it.
 		users,    %% List of wx servers, needed ??
