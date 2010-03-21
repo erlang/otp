0ca1793ce70563721da505f724a8e6807750b15d
Kostis Sagonas <kostis@cs.ntua.gr>
2010-03-21 09:54:10 +0200

    stdlib: Add types and specs

--- lib/stdlib/src/c.erl
+++ lib/stdlib/src/c.erl
@@ -31,9 +31,13 @@
 -export([display_info/1]).
 -export([appcall/4]).
 
--import(lists, [reverse/1,flatten/1,sublist/3,sort/1,keysearch/3,keysort/2,
+-import(lists, [reverse/1,flatten/1,sublist/3,sort/1,keysort/2,
 		concat/1,max/1,min/1,foreach/2,foldl/3,flatmap/2]).
 -import(io, [format/1, format/2]).
+
+%%-----------------------------------------------------------------------
+
+-spec help() -> 'ok'.
 
 help() ->
     format("bt(Pid)    -- stack backtrace for a process\n"
@@ -65,7 +69,11 @@ help() ->
 %% c(FileName)
 %%  Compile a file/module.
 
+-spec c(file:name()) -> {'ok', module()} | 'error'.
+
 c(File) -> c(File, []).
+
+-spec c(file:name(), [compile:option()]) -> {'ok', module()} | 'error'.
 
 c(File, Opts0) when is_list(Opts0) ->
     Opts = [report_errors,report_warnings|Opts0],
@@ -82,6 +90,8 @@ c(File, Opt) ->
 
 %%% Obtain the 'outdir' option from the argument. Return "." if no
 %%% such option was given.
+-spec outdir([compile:option()]) -> file:filename().
+
 outdir([]) ->
     ".";
 outdir([Opt|Rest]) ->
@@ -118,8 +128,8 @@ machine_load(Mod, File, Opts) ->
 %%% loaded from some other place than current directory.
 %%% Now, loading from other than current directory is supposed to work.
 %%% so this function does nothing special.
-check_load({error, R}, _) -> {error, R};
-check_load(_, X) -> {ok, X}.
+check_load({error, _R} = Error, _) -> Error;
+check_load(_, Mod) -> {ok, Mod}.
 
 %% Compile a list of modules
 %% enables the nice unix shell cmd
@@ -127,6 +137,8 @@ check_load(_, X) -> {ok, X}.
 %% to compile files f1.erl , f2.erl ....... from a unix shell
 %% with constant c2 defined, c1=v1 (v1 must be a term!), include dir
 %% IDir, outdir ODir.
+
+-spec lc([erl_compile:cmd_line_arg()]) -> 'ok' | 'error'.
 
 lc(Args) ->
     case catch split(Args, [], []) of
@@ -145,7 +157,7 @@ lc_batch() ->
     io:format("Error: no files to compile~n"),
     halt(1).
 
--spec lc_batch([_]) -> no_return().
+-spec lc_batch([erl_compile:cmd_line_arg()]) -> no_return().
 
 lc_batch(Args) ->
     try split(Args, [], []) of
@@ -191,7 +203,12 @@ make_term(Str) ->
 	    throw(error)
     end.
 
+-spec nc(file:name()) -> {'ok', module()} | 'error'.
+
 nc(File) -> nc(File, []).
+
+-spec nc(file:name(), [compile:option()] | compile:option()) ->
+        {'ok', module} | 'error'.
 
 nc(File, Opts0) when is_list(Opts0) ->
     Opts = Opts0 ++ [report_errors, report_warnings],
@@ -215,25 +232,36 @@ nc(File, Opt) when is_atom(Opt) ->
 
 %% l(Mod)
 %%  Reload module Mod from file of same name
+-spec l(module()) -> code:load_ret().
 
 l(Mod) ->
     code:purge(Mod),
     code:load_file(Mod).
 
 %% Network version of l/1
+%%-spec nl(module()) ->
 nl(Mod) ->
     case code:get_object_code(Mod) of
 	{_Module, Bin, Fname} ->
-            rpc:eval_everywhere(code,load_binary,[Mod,Fname,Bin]);
+            rpc:eval_everywhere(code, load_binary, [Mod, Fname, Bin]);
 	Other ->
 	    Other
     end.
 
+-spec i() -> 'ok'.
+
 i() -> i(processes()).
+
+-spec ni() -> 'ok'.
+
 ni() -> i(all_procs()).
+
+-spec i([pid()]) -> 'ok'.
 
 i(Ps) ->
     i(Ps, length(Ps)).
+
+-spec i([pid()], non_neg_integer()) -> 'ok'.
 
 i(Ps, N) when N =< 100 ->
     iformat("Pid", "Initial Call", "Heap", "Reds",
@@ -275,7 +303,6 @@ paged_i(Ps, Acc, N, Page) ->
 	    paged_i([], NewAcc, 0, Page)
     end.
 
-
 choice(F) ->
     case get_line('(c)ontinue (q)uit -->', "c\n") of
 	"c\n" ->
@@ -285,7 +312,6 @@ choice(F) ->
 	_ ->
 	    choice(F)
     end.
-    
 
 get_line(P, Default) ->
     case io:get_line(P) of
@@ -305,7 +331,6 @@ mfa_string({M,F,A}) ->
 mfa_string(X) ->
     w(X).
 
-
 display_info(Pid) ->
     case pinfo(Pid) of
 	undefined -> {0,0,0,0};
@@ -317,7 +342,7 @@ display_info(Pid) ->
 		       Other ->
 			   Other
 		   end,
-	    Reds  = fetch(reductions, Info),
+	    Reds = fetch(reductions, Info),
 	    LM = length(fetch(messages, Info)),
 	    HS = fetch(heap_size, Info),
 	    SS = fetch(stack_size, Info),
@@ -364,20 +389,29 @@ pinfo(Pid) ->
     end.
 
 fetch(Key, Info) ->
-    case keysearch(Key, 1, Info) of
-	{value, {_, Val}} -> Val;
+    case lists:keyfind(Key, 1, Info) of
+	{_, Val} -> Val;
 	false -> 0
     end.
 
-pid(X,Y,Z) ->
+-spec pid(non_neg_integer(), non_neg_integer(), non_neg_integer()) -> pid().
+
+pid(X, Y, Z) ->
     list_to_pid("<" ++ integer_to_list(X) ++ "." ++
 		integer_to_list(Y) ++ "." ++
 		integer_to_list(Z) ++ ">").
 
-i(X,Y,Z) -> pinfo(pid(X,Y,Z)).
+-spec i(non_neg_integer(), non_neg_integer(), non_neg_integer()) ->
+        [{atom(), term()}].
+
+i(X, Y, Z) -> pinfo(pid(X, Y, Z)).
+
+-spec q() -> no_return().
 
 q() ->
     init:stop().
+
+-spec bt(pid()) -> 'ok' | 'undefined'.
 
 bt(Pid) ->
     case catch erlang:process_display(Pid, backtrace) of
@@ -386,6 +420,8 @@ bt(Pid) ->
 	_ ->
 	    ok
     end.
+
+-spec m() -> 'ok'.
 
 m() ->
     mformat("Module", "File"),
@@ -414,8 +450,8 @@ error(Fmt, Args) ->
 
 f_p_e(P, F) ->
     case file:path_eval(P, F) of
-	{error, enoent} ->
-	    {error, enoent};
+	{error, enoent} = Enoent ->
+	    Enoent;
 	{error, E={Line, _Mod, _Term}} ->
 	    error("file:path_eval(~p,~p): error on line ~p: ~s~n",
 		  [P, F, Line, file:format_error(E)]),
@@ -438,10 +474,11 @@ bi(I) ->
 %%
 %% Short and nice form of module info
 %%
+-spec m(module()) -> 'ok'.
 
 m(M) ->
     L = M:module_info(),
-    {value,{exports,E}} = keysearch(exports, 1, L),
+    {exports,E} = lists:keyfind(exports, 1, L),
     Time = get_compile_time(L),
     COpts = get_compile_options(L),
     format("Module ~w compiled: ",[M]), print_time(Time),
@@ -470,10 +507,10 @@ get_compile_options(L) ->
     end.
 
 get_compile_info(L, Tag) ->
-    case keysearch(compile, 1, L) of
-	{value, {compile, I}} ->
-	    case keysearch(Tag, 1, I) of
-		{value, {Tag, Val}} -> {ok,Val};
+    case lists:keyfind(compile, 1, L) of
+	{compile, I} ->
+	    case lists:keyfind(Tag, 1, I) of
+		{Tag, Val} -> {ok,Val};
 		false -> error
 	    end;
 	false -> error
@@ -523,6 +560,8 @@ month(11) -> "November";
 month(12) -> "December".
 
 %% Just because we can't eval receive statements...
+-spec flush() -> 'ok'.
+
 flush() ->
     receive
 	X ->
@@ -533,8 +572,12 @@ flush() ->
     end.
 
 %% Print formatted info about all registered names in the system
+-spec nregs() -> 'ok'.
+
 nregs() ->
     foreach(fun (N) -> print_node_regs(N) end, all_regs()).
+
+-spec regs() -> 'ok'.
 
 regs() ->
     print_node_regs({node(),registered()}).
@@ -609,6 +652,8 @@ portformat(Name, Id, Cmd) ->
 %% cd(Directory)
 %%  These are just wrappers around the file:get/set_cwd functions.
 
+-spec pwd() -> 'ok'.
+
 pwd() ->
     case file:get_cwd() of
 	{ok, Str} ->
@@ -616,6 +661,8 @@ pwd() ->
 	{error, _} ->
 	    ok = io:format("Cannot determine current directory\n")
     end.
+
+-spec cd(file:name()) -> 'ok'.
 
 cd(Dir) ->
     file:set_cwd(Dir),
@@ -625,8 +672,12 @@ cd(Dir) ->
 %% ls(Directory)
 %%  The strategy is to print in fixed width files.
 
+-spec ls() -> 'ok'.
+
 ls() ->
     ls(".").
+
+-spec ls(file:name()) -> 'ok'.
 
 ls(Dir) ->
     case file:list_dir(Dir) of
@@ -660,24 +711,31 @@ w(X) ->
 %% memory/[0,1]
 %%
 
-memory()         -> erlang:memory().
+-spec memory() -> [{atom(), non_neg_integer()}].
+
+memory() -> erlang:memory().
+
+-spec memory(atom()) -> non_neg_integer()
+          ; ([atom()]) -> [{atom(), non_neg_integer()}].
+
 memory(TypeSpec) -> erlang:memory(TypeSpec).
 
 %%
 %% Cross Reference Check
 %% 
-
+%%-spec xm(module() | file:filename()) -> xref:m/1 return
 xm(M) ->
     appcall(tools, xref, m, [M]).
 
 %%
 %% Call yecc 
 %% 
-
+%%-spec y(file:name()) -> yecc:file/2 return
 y(File) -> y(File, []).
 
+%%-spec y(file:name(), [yecc:option()]) -> yecc:file/2 return
 y(File, Opts) ->
-    appcall(parsetools, yecc, file, [File,Opts]).
+    appcall(parsetools, yecc, file, [File, Opts]).
 
 
 %%
@@ -699,4 +757,3 @@ appcall(App, M, F, Args) ->
 		    erlang:raise(error, undef, Stk)
 	    end
     end.
-
--- lib/stdlib/src/dets_sup.erl
+++ lib/stdlib/src/dets_sup.erl
@@ -22,8 +22,15 @@
 
 -export([start_link/0, init/1]).
 
+-spec start_link() -> {'ok', pid()} | 'ignore' | {'error', term()}.
+
 start_link() ->
     supervisor:start_link({local, dets_sup}, dets_sup, []).
+
+-spec init([]) ->
+        {'ok', {{'simple_one_for_one', 4, 3600},
+		[{'dets', {'dets', 'istart_link', []},
+		  'temporary', 30000, 'worker', ['dets']}]}}.
 
 init([]) ->
     SupFlags = {simple_one_for_one, 4, 3600},
