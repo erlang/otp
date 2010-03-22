835dba95f810cc925ac856171014b9dbea8c90be
Niclas Eklund <nick@erlang.org>
2010-03-22 14:22:24 +0000

    Fix usage of ip_v6_disabled and publish it

--- lib/ssh/src/ssh.erl
+++ lib/ssh/src/ssh.erl
@@ -1,19 +1,19 @@
 %%
 %% %CopyrightBegin%
-%% 
-%% Copyright Ericsson AB 2004-2009. All Rights Reserved.
-%% 
+%%
+%% Copyright Ericsson AB 2004-2010. All Rights Reserved.
+%%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
 %% compliance with the License. You should have received a copy of the
 %% Erlang Public License along with this software. If not, it can be
 %% retrieved online at http://www.erlang.org/.
-%% 
+%%
 %% Software distributed under the License is distributed on an "AS IS"
 %% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
 %% the License for the specific language governing rights and limitations
 %% under the License.
-%% 
+%%
 %% %CopyrightEnd%
 %%
 
@@ -330,10 +330,10 @@ handle_options([{nodelay, _} = Opt | Rest], SockOpts, Opts) ->
 handle_options([Opt | Rest], SockOpts, Opts) ->
     handle_options(Rest, SockOpts, [Opt | Opts]).
 
+%% Has IPv6 been disabled?
 inetopt(true) ->
-    inet6;
-
+    inet;
 inetopt(false) ->
-    inet.
+    inet6.
 
 
