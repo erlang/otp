1443a8648abdd49a653a984289e4bf6d7f062310
Ingela Anderton Andin <ingela@erlang.org>
2010-04-07 07:14:15 +0000

    To preserve coding style of this module

--- lib/ssl/src/ssl_connection.erl
+++ lib/ssl/src/ssl_connection.erl
@@ -441,8 +441,8 @@ certify(#certificate{} = Cert,
 			       Opts#ssl_options.verify,
 			       Opts#ssl_options.verify_fun) of
         {PeerCert, PublicKeyInfo} ->
-	    State1 = State#state{client_certificate_requested = false},
-	    handle_peer_cert(PeerCert, PublicKeyInfo, State1);
+	    handle_peer_cert(PeerCert, PublicKeyInfo, 
+			     State#state{client_certificate_requested = false});
 	#alert{} = Alert ->
             handle_own_alert(Alert, Version, certify_certificate, State),
             {stop, normal, State}
