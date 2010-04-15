56207406c58d9167759ba26cc5a9a6472d047a5a
Ingela Anderton Andin <ingela@erlang.org>
2010-04-15 12:05:00 +0000

    Tried to minimize sleeping

--- lib/ssl/test/ssl_to_openssl_SUITE.erl
+++ lib/ssl/test/ssl_to_openssl_SUITE.erl
@@ -163,7 +163,7 @@ erlang_client_openssl_server(Config) when is_list(Config) ->
 
     OpensslPort =  open_port({spawn, Cmd}, [stderr_to_stdout]), 
 
-    test_server:sleep(?SLEEP),
+    wait_for_openssl_server(),
 
     Client = ssl_test_lib:start_client([{node, ClientNode}, {port, Port}, 
 					{host, Hostname},
@@ -202,8 +202,6 @@ erlang_server_openssl_client(Config) when is_list(Config) ->
 			   {options, ServerOpts}]),
     Port = ssl_test_lib:inet_port(Server),
     
-    test_server:sleep(?SLEEP),
-   
     Cmd = "openssl s_client -port " ++ integer_to_list(Port)  ++ 
 	" -host localhost",
 
@@ -242,8 +240,6 @@ erlang_server_openssl_client_reuse_session(Config) when is_list(Config) ->
 			   {options, ServerOpts}]),
     Port = ssl_test_lib:inet_port(Server),
     
-    test_server:sleep(?SLEEP),
-   
     Cmd = "openssl s_client -port " ++ integer_to_list(Port)  ++ 
 	" -host localhost -reconnect",
 
@@ -288,7 +284,7 @@ erlang_client_openssl_server_renegotiate(Config) when is_list(Config) ->
 
     OpensslPort =  open_port({spawn, Cmd}, [stderr_to_stdout]), 
 
-    test_server:sleep(?SLEEP),
+    wait_for_openssl_server(),
 
     Client = ssl_test_lib:start_client([{node, ClientNode}, {port, Port}, 
 					{host, Hostname},
@@ -296,12 +292,9 @@ erlang_client_openssl_server_renegotiate(Config) when is_list(Config) ->
 					{mfa, {?MODULE, 
 					       delayed_send, [[ErlData, OpenSslData]]}},
 					{options, ClientOpts}]),
-    test_server:sleep(?SLEEP),
 
     port_command(OpensslPort, ?OPENSSL_RENEGOTIATE),
-
     test_server:sleep(?SLEEP),
-
     port_command(OpensslPort, OpenSslData),
     
     %%ssl_test_lib:check_result(Client, ok), 
@@ -347,7 +340,7 @@ erlang_client_openssl_server_no_wrap_sequence_number(Config) when is_list(Config
 
     OpensslPort =  open_port({spawn, Cmd}, [stderr_to_stdout]), 
 
-    test_server:sleep(?SLEEP),
+    wait_for_openssl_server(),
 
     Client = ssl_test_lib:start_client([{node, ClientNode}, {port, Port}, 
 					{host, Hostname},
@@ -395,8 +388,6 @@ erlang_server_openssl_client_no_wrap_sequence_number(Config) when is_list(Config
 					{options, [{renegotiate_at, N} | ServerOpts]}]),
     Port = ssl_test_lib:inet_port(Server),
     
-    test_server:sleep(?SLEEP),
-   
     Cmd = "openssl s_client -port " ++ integer_to_list(Port)  ++ 
 	" -host localhost -msg",
 
@@ -441,7 +432,7 @@ erlang_client_openssl_server_no_server_ca_cert(Config) when is_list(Config) ->
 
     OpensslPort =  open_port({spawn, Cmd}, [stderr_to_stdout]), 
 
-    test_server:sleep(?SLEEP),
+    wait_for_openssl_server(),
 
     Client = ssl_test_lib:start_client([{node, ClientNode}, {port, Port}, 
 					{host, Hostname},
@@ -483,7 +474,7 @@ ssl3_erlang_client_openssl_server(Config) when is_list(Config) ->
 
     OpensslPort =  open_port({spawn, Cmd}, [stderr_to_stdout]), 
 
-    test_server:sleep(?SLEEP),
+    wait_for_openssl_server(),
 
     Client = ssl_test_lib:start_client([{node, ClientNode}, {port, Port}, 
 					{host, Hostname},
@@ -518,8 +509,6 @@ ssl3_erlang_server_openssl_client(Config) when is_list(Config) ->
 					{options, 
 					 [{versions, [sslv3]} | ServerOpts]}]),
     Port = ssl_test_lib:inet_port(Server),
-
-    test_server:sleep(?SLEEP),
     
     Cmd = "openssl s_client -port " ++ integer_to_list(Port)  ++ 
 	" -host localhost -ssl3",
@@ -562,7 +551,7 @@ ssl3_erlang_client_openssl_server_client_cert(Config) when is_list(Config) ->
 
     OpensslPort =  open_port({spawn, Cmd}, [stderr_to_stdout]), 
 
-    test_server:sleep(?SLEEP),
+    wait_for_openssl_server(),
 
     Client = ssl_test_lib:start_client([{node, ClientNode}, {port, Port}, 
 					{host, Hostname},
@@ -604,8 +593,6 @@ ssl3_erlang_server_openssl_client_client_cert(Config) when is_list(Config) ->
 					  | ServerOpts]}]),
     Port = ssl_test_lib:inet_port(Server),
     
-    test_server:sleep(?SLEEP),
-
     CaCertFile = proplists:get_value(cacertfile, ClientOpts),
     CertFile = proplists:get_value(certfile, ClientOpts),
     KeyFile = proplists:get_value(keyfile, ClientOpts),
@@ -652,8 +639,6 @@ ssl3_erlang_server_erlang_client_client_cert(Config) when is_list(Config) ->
 					  | ServerOpts]}]),
     Port = ssl_test_lib:inet_port(Server),
     
-    test_server:sleep(?SLEEP),
-
     Client = ssl_test_lib:start_client([{node, ClientNode}, {port, Port}, 
 					{host, Hostname},
 					{from, self()}, 
@@ -695,7 +680,7 @@ tls1_erlang_client_openssl_server(Config) when is_list(Config) ->
 
     OpensslPort =  open_port({spawn, Cmd}, [stderr_to_stdout]), 
 
-    test_server:sleep(?SLEEP),
+    wait_for_openssl_server(),
     
     Client = ssl_test_lib:start_client([{node, ClientNode}, {port, Port}, 
 					{host, Hostname},
@@ -733,8 +718,6 @@ tls1_erlang_server_openssl_client(Config) when is_list(Config) ->
 					 [{versions, [tlsv1]} | ServerOpts]}]),
     Port = ssl_test_lib:inet_port(Server),
 
-    test_server:sleep(?SLEEP),
-    
     Cmd = "openssl s_client -port " ++ integer_to_list(Port)  ++ 
 	" -host localhost -tls1",
 
@@ -778,7 +761,7 @@ tls1_erlang_client_openssl_server_client_cert(Config) when is_list(Config) ->
 
     OpensslPort =  open_port({spawn, Cmd}, [stderr_to_stdout]), 
 
-    test_server:sleep(?SLEEP),
+    wait_for_openssl_server(),
 
     Client = ssl_test_lib:start_client([{node, ClientNode}, {port, Port}, 
 					{host, Hostname},
@@ -820,8 +803,6 @@ tls1_erlang_server_openssl_client_client_cert(Config) when is_list(Config) ->
 					  | ServerOpts]}]),
     Port = ssl_test_lib:inet_port(Server),
     
-    test_server:sleep(?SLEEP),
-
     CaCertFile = proplists:get_value(cacertfile, ClientOpts),
     CertFile = proplists:get_value(certfile, ClientOpts),
     KeyFile = proplists:get_value(keyfile, ClientOpts),
@@ -866,8 +847,6 @@ tls1_erlang_server_erlang_client_client_cert(Config) when is_list(Config) ->
 					  | ServerOpts]}]),
     Port = ssl_test_lib:inet_port(Server),
     
-    test_server:sleep(?SLEEP),
-
     Client = ssl_test_lib:start_client([{node, ClientNode}, {port, Port}, 
 					{host, Hostname},
 					{from, self()}, 
@@ -923,7 +902,7 @@ cipher(CipherSuite, Version, Config) ->
     
     OpenSslPort =  open_port({spawn, Cmd}, [stderr_to_stdout]), 
     
-    test_server:sleep(?SLEEP),
+    wait_for_openssl_server(),
 
     Client = ssl_test_lib:start_client([{node, ClientNode}, {port, Port}, 
 					{host, Hostname},
@@ -977,7 +956,7 @@ erlang_client_bad_openssl_server(Config) when is_list(Config) ->
 
     OpensslPort =  open_port({spawn, Cmd}, [stderr_to_stdout]), 
 
-    test_server:sleep(?SLEEP),
+    wait_for_openssl_server(),
     
     Client = ssl_test_lib:start_client([{node, ClientNode}, {port, Port}, 
 					{host, Hostname},
@@ -1078,5 +1057,14 @@ server_sent_garbage(Socket) ->
 	    {error, closed} == ssl:send(Socket, "data")
     end.
     
-
-
+wait_for_openssl_server() ->
+    receive
+     	{Port, {data, Debug}} when is_port(Port) ->
+ 	    io:format("openssl ~s~n",[Debug]),
+	    %% openssl has started make sure
+	    %% it will be in accept. Parsing
+	    %% output is too error prone. (Even 
+	    %% more so than sleep!)
+	    test_server:sleep(?SLEEP)
+    end.
+	    
