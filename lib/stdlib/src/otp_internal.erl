3995ae555503cefdfee5ed0d28727c72bdc830f1
Micael Karlberg <bmk@erlang.org>
2010-04-27 13:33:37 +0000

    OTP-8564 Update deprecation status
    Deprecated functions designated to be removed in R14 has been removed.
    Also, some new functions has been marked as deprecated (the old http client
    api module).

--- lib/stdlib/src/otp_internal.erl
+++ lib/stdlib/src/otp_internal.erl
@@ -236,32 +236,50 @@ obsolete_1(erlang, fault, 2) ->
 obsolete_1(file, rawopen, 2) ->
     {removed, "deprecated (will be removed in R13B); use file:open/2 with the raw option"};
 
-obsolete_1(httpd, start, 0) 	  -> {deprecated,{inets,start,[2,3]},"R14B"};
-obsolete_1(httpd, start, 1) 	  -> {deprecated,{inets,start,[2,3]},"R14B"};
-obsolete_1(httpd, start_link, 1)  -> {deprecated,{inets,start,[2,3]},"R14B"};
-obsolete_1(httpd, start_child, 0) -> {deprecated,{inets,start,[2,3]},"R14B"};
-obsolete_1(httpd, start_child, 1) -> {deprecated,{inets,start,[2,3]},"R14B"};
-obsolete_1(httpd, stop, 0) 	  -> {deprecated,{inets,stop,2},"R14B"};
-obsolete_1(httpd, stop, 1)        -> {deprecated,{inets,stop,2},"R14B"};
-obsolete_1(httpd, stop, 2)        -> {deprecated,{inets,stop,2},"R14B"};
-obsolete_1(httpd, stop_child, 0)  -> {deprecated,{inets,stop,2},"R14B"};
-obsolete_1(httpd, stop_child, 1)  -> {deprecated,{inets,stop,2},"R14B"};
-obsolete_1(httpd, stop_child, 2)  -> {deprecated,{inets,stop,2},"R14B"};
-obsolete_1(httpd, restart, 0) 	  -> {deprecated,{httpd,reload_config,2},"R14B"};
-obsolete_1(httpd, restart, 1) 	  -> {deprecated,{httpd,reload_config,2},"R14B"};
-obsolete_1(httpd, restart, 2) 	  -> {deprecated,{httpd,reload_config,2},"R14B"};
-obsolete_1(httpd, block, 0) 	  -> {deprecated,{httpd,reload_config,2},"R14B"};
-obsolete_1(httpd, block, 1) 	  -> {deprecated,{httpd,reload_config,2},"R14B"};
-obsolete_1(httpd, block, 2) 	  -> {deprecated,{httpd,reload_config,2},"R14B"};
-obsolete_1(httpd, block, 3) 	  -> {deprecated,{httpd,reload_config,2},"R14B"};
-obsolete_1(httpd, block, 4)	  -> {deprecated,{httpd,reload_config,2},"R14B"};
-obsolete_1(httpd, unblock, 0) 	  -> {deprecated,{httpd,reload_config,2},"R14B"};
-obsolete_1(httpd, unblock, 1)     -> {deprecated,{httpd,reload_config,2},"R14B"};
-obsolete_1(httpd, unblock, 2)     -> {deprecated,{httpd,reload_config,2},"R14B"};
+obsolete_1(http, request, 1) 	      -> {deprecated,{httpc,request,1},"R15B"};
+obsolete_1(http, request, 2) 	      -> {deprecated,{httpc,request,2},"R15B"};
+obsolete_1(http, request, 4) 	      -> {deprecated,{httpc,request,4},"R15B"};
+obsolete_1(http, request, 5) 	      -> {deprecated,{httpc,request,5},"R15B"};
+obsolete_1(http, cancel_request, 1)   -> {deprecated,{httpc,cancel_request,1},"R15B"};
+obsolete_1(http, cancel_request, 2)   -> {deprecated,{httpc,cancel_request,2},"R15B"};
+obsolete_1(http, set_option, 2)       -> {deprecated,{httpc,set_option,2},"R15B"};
+obsolete_1(http, set_option, 3)       -> {deprecated,{httpc,set_option,3},"R15B"};
+obsolete_1(http, set_options, 1)      -> {deprecated,{httpc,set_options,1},"R15B"};
+obsolete_1(http, set_options, 2)      -> {deprecated,{httpc,set_options,2},"R15B"};
+obsolete_1(http, verify_cookies, 2)   -> {deprecated,{httpc,verify_cookies,2},"R15B"};
+obsolete_1(http, verify_cookies, 3)   -> {deprecated,{httpc,verify_cookies,3},"R15B"};
+obsolete_1(http, cookie_header, 1)    -> {deprecated,{httpc,cookie_header,1},"R15B"};
+obsolete_1(http, cookie_header, 2)    -> {deprecated,{httpc,cookie_header,2},"R15B"};
+obsolete_1(http, stream_next, 1)      -> {deprecated,{httpc,stream_next,1},"R15B"};
+obsolete_1(http, default_profile, 0)  -> {deprecated,{httpc,default_profile,0},"R15B"};
+
+obsolete_1(httpd, start, 0) 	      -> {removed,{inets,start,[2,3]},"R14B"};
+obsolete_1(httpd, start, 1) 	      -> {removed,{inets,start,[2,3]},"R14B"};
+obsolete_1(httpd, start_link, 0)      -> {removed,{inets,start,[2,3]},"R14B"};
+obsolete_1(httpd, start_link, 1)      -> {removed,{inets,start,[2,3]},"R14B"};
+obsolete_1(httpd, start_child, 0)     -> {removed,{inets,start,[2,3]},"R14B"};
+obsolete_1(httpd, start_child, 1)     -> {removed,{inets,start,[2,3]},"R14B"};
+obsolete_1(httpd, stop, 0) 	      -> {removed,{inets,stop,2},"R14B"};
+obsolete_1(httpd, stop, 1)            -> {removed,{inets,stop,2},"R14B"};
+obsolete_1(httpd, stop, 2)            -> {removed,{inets,stop,2},"R14B"};
+obsolete_1(httpd, stop_child, 0)      -> {removed,{inets,stop,2},"R14B"};
+obsolete_1(httpd, stop_child, 1)      -> {removed,{inets,stop,2},"R14B"};
+obsolete_1(httpd, stop_child, 2)      -> {removed,{inets,stop,2},"R14B"};
+obsolete_1(httpd, restart, 0) 	      -> {removed,{httpd,reload_config,2},"R14B"};
+obsolete_1(httpd, restart, 1) 	      -> {removed,{httpd,reload_config,2},"R14B"};
+obsolete_1(httpd, restart, 2) 	      -> {removed,{httpd,reload_config,2},"R14B"};
+obsolete_1(httpd, block, 0) 	      -> {removed,{httpd,reload_config,2},"R14B"};
+obsolete_1(httpd, block, 1) 	      -> {removed,{httpd,reload_config,2},"R14B"};
+obsolete_1(httpd, block, 2) 	      -> {removed,{httpd,reload_config,2},"R14B"};
+obsolete_1(httpd, block, 3) 	      -> {removed,{httpd,reload_config,2},"R14B"};
+obsolete_1(httpd, block, 4)	      -> {removed,{httpd,reload_config,2},"R14B"};
+obsolete_1(httpd, unblock, 0) 	      -> {removed,{httpd,reload_config,2},"R14B"};
+obsolete_1(httpd, unblock, 1)         -> {removed,{httpd,reload_config,2},"R14B"};
+obsolete_1(httpd, unblock, 2)         -> {removed,{httpd,reload_config,2},"R14B"};
 obsolete_1(httpd_util, key1search, 2) -> {removed,{proplists,get_value,2},"R13B"};
 obsolete_1(httpd_util, key1search, 3) -> {removed,{proplists,get_value,3},"R13B"};
-obsolete_1(ftp, open, 3)          -> {deprecated,{inets,start,[2,3]},"R14B"};
-obsolete_1(ftp, force_active, 1)  -> {deprecated,{inets,start,[2,3]},"R14B"};
+obsolete_1(ftp, open, 3)              -> {removed,{inets,start,[2,3]},"R14B"};
+obsolete_1(ftp, force_active, 1)      -> {removed,{inets,start,[2,3]},"R14B"};
 
 %% Added in R12B-4.
 obsolete_1(ssh_cm, connect, A) when 1 =< A, A =< 3 ->
