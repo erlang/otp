e61ffa5c8a3aa840d2169d4ea0daa99b2644173f
Dan Gudmundsson <dgud@erlang.org>
2010-06-07 15:38:19 +0200

    Cleanup

--- lib/mnesia/examples/mnesia_meter.erl
+++ lib/mnesia/examples/mnesia_meter.erl
@@ -1,19 +1,19 @@
 %%
 %% %CopyrightBegin%
-%% 
-%% Copyright Ericsson AB 1997-2009. All Rights Reserved.
-%% 
+%%
+%% Copyright Ericsson AB 1997-2010. All Rights Reserved.
+%%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
 %% compliance with the License. You should have received a copy of the
 %% Erlang Public License along with this software. If not, it can be
 %% retrieved online at http://www.erlang.org/.
-%% 
+%%
 %% Software distributed under the License is distributed on an "AS IS"
 %% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
 %% the License for the specific language governing rights and limitations
 %% under the License.
-%% 
+%%
 %% %CopyrightEnd%
 %%
 
@@ -407,7 +407,7 @@ run(Nodes, Config, FunOverhead) ->
     stop(Nodes),
     Res.
 
-run_meter(M, Nodes, FunOverhead) when record(M, meter) ->
+run_meter(M, Nodes, FunOverhead) when is_record(M, meter) ->
     io:format(".", []),
     case catch init_records(M#meter.init, ?TIMES) of
         {atomic, ok} ->
