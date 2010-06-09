b834904a7c0793e831531f0877a102544145fec0
Micael Karlberg <bmk@erlang.org>
2010-06-09 13:08:20 +0000

    OTP-8696: Update performance chapter

--- lib/megaco/doc/src/files.mk
+++ lib/megaco/doc/src/files.mk
@@ -1,20 +1,20 @@
 #-*-makefile-*-   ; force emacs to enter makefile-mode
 
 # %CopyrightBegin%
-# 
-# Copyright Ericsson AB 2001-2009. All Rights Reserved.
-# 
+#
+# Copyright Ericsson AB 2001-2010. All Rights Reserved.
+#
 # The contents of this file are subject to the Erlang Public License,
 # Version 1.1, (the "License"); you may not use this file except in
 # compliance with the License. You should have received a copy of the
 # Erlang Public License along with this software. If not, it can be
 # retrieved online at http://www.erlang.org/.
-# 
+#
 # Software distributed under the License is distributed on an "AS IS"
 # basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
 # the License for the specific language governing rights and limitations
 # under the License.
-# 
+#
 # %CopyrightEnd%
 
 XML_APPLICATION_FILES = \
@@ -56,7 +56,7 @@ XML_CHAPTER_FILES = \
 
 BOOK_FILES = book.xml
 
-GIF_FILES = \
+IMG_FILES = \
 	single_node_config.gif \
 	distr_node_config.gif \
 	megaco_sys_arch.gif \
@@ -70,4 +70,4 @@ GIF_FILES = \
 	MG_startup_call_flow.gif \
 	call_flow.gif \
 	call_flow_cont.gif \
-        mstone1.gif
+        mstone1.jpg
