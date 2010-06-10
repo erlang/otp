7e6fe78278c203c3756ce0d6bf23a6bd6cf7bb5d
Patrik Nyblom <pan@erlang.org>
2010-06-10 15:53:19 +0200

    Teach erl_time_sup to handle timezones w/o DST on FreeBSD as on other platforms
    This change handles erlang:localtime_to_universaltime(...,true) in timezones
    without DST in a consistent way over platforms. Specifically BSD did return
    an (accurate) -1 from mktime, which blew things up. Now all platforms regard
    no DST as a DST with delta zero. Linux, SunOS, MacOS etc already did this,
    why the behaviour on BSD was updated to the slightly incorrect one instead
    of breaking backward compatibility on most other platforms.
    
    Before this change, erlang:localtime_to_universaltime({{2008, 8, 1},
    {0, 0, 0}},true) would return {1969, 12, 31}, {23, 59, 59}} when
    TZ=UTC on FreeBSD, now it mimics the behaviour of other systems and returns
    {{2008, 8, 1},{0, 0, 0}}.
    
    The problem was originally reported by Paul Guyot on erlang-bugs mailing list:
    http://www.erlang.org/pipermail/erlang-bugs/2008-November/001077.html
    
    This correction replaces earlier BSD specific open source patches for this
    problem.

--- erts/emulator/test/time_SUITE.erl
+++ erts/emulator/test/time_SUITE.erl
@@ -34,6 +34,8 @@
 	 consistency/1,
 	 now/1, now_unique/1, now_update/1, timestamp/1]).
 
+-export([local_to_univ_utc/1]).
+
 -include("test_server.hrl").
 
 -export([linear_time/1]).
@@ -53,7 +55,40 @@
 -define(dst_timezone, 2).
 
 all(suite) -> [univ_to_local, local_to_univ,
-	       bad_univ_to_local, bad_local_to_univ, consistency, now, timestamp].
+	       local_to_univ_utc,
+	       bad_univ_to_local, bad_local_to_univ,
+	       consistency, now, timestamp].
+
+local_to_univ_utc(suite) ->
+    [];
+local_to_univ_utc(doc) ->
+    ["Test that DST = true on timezones without DST is ignored"];
+local_to_univ_utc(Config) when is_list(Config) ->
+    case os:type() of
+	{unix,_} ->
+	    %% TZ variable has a meaning
+	    ?line {ok, Node} =
+		test_server:start_node(local_univ_utc,peer,
+				       [{args, "-env TZ UTC"}]),
+	    ?line {{2008,8,1},{0,0,0}} =
+		rpc:call(Node,
+			 erlang,localtime_to_universaltime,
+			 [{{2008, 8, 1}, {0, 0, 0}},
+			  false]),
+	    ?line {{2008,8,1},{0,0,0}} =
+		rpc:call(Node,
+			 erlang,localtime_to_universaltime,
+			 [{{2008, 8, 1}, {0, 0, 0}},
+			  true]),
+	    ?line [{{2008,8,1},{0,0,0}}] =
+		rpc:call(Node,
+			 calendar,local_time_to_universal_time_dst,
+			 [{{2008, 8, 1}, {0, 0, 0}}]),
+	    ?line test_server:stop_node(Node),
+	    ok;
+	_ ->
+	    {skip,"Only valid on Unix"}
+    end.
 
 
 %% Tests conversion from univeral to local time.
