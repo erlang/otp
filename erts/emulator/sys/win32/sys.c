d389ff6f96cbd855e95a7455407b6140f1601d0f
Rickard Green <rickard@erlang.org>
2010-08-17 23:05:43 +0200

    Initialize environment functionality after thread lib
    An assertion failed due to the thread library not being
    initialized when initializing an rwmutex. This was however
    harmless.

--- erts/emulator/sys/win32/sys.c
+++ erts/emulator/sys/win32/sys.c
@@ -3002,7 +3002,6 @@ erts_sys_pre_init(void)
     }
 #endif
     erts_smp_atomic_init(&sys_misc_mem_sz, 0);
-    erts_sys_env_init();
 }
 
 void noinherit_std_handle(DWORD type)
@@ -3017,6 +3016,8 @@ void noinherit_std_handle(DWORD type)
 void erl_sys_init(void)
 {
     HANDLE handle;
+
+    erts_sys_env_init();
 
     noinherit_std_handle(STD_OUTPUT_HANDLE);
     noinherit_std_handle(STD_INPUT_HANDLE);
