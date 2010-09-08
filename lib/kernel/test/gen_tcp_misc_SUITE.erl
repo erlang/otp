33caf70b63ef4cdb7ba5f3b24d7f04c596f081eb
Björn Gustavsson <bjorn@erlang.org>
2010-09-08 16:19:54 +0200

    Make gen_tcp:recv/2 consistent with ssl:recv/2
    When the HTTP packet mode has been enabled for a socket,
    the ssl and gen_tcp modules have different error indications
    when there is an error while parsing the HTTP header:
    
      ssl:recv(SSLSocket, 0) -> {ok, {http_error, _Str}}
    
      gen_tcp:recv(Socket, 0) -> {error, {http_error, _Str}}
    
    We have decided to change gen_tcp:recv/2 to behave the same
    way as ssl:recv/2. That means that there will be always be
    an ok tuple if data could be succefully read from the socket,
    and an error tuple if there was a read error at the socket level.

--- lib/kernel/test/gen_tcp_misc_SUITE.erl
+++ lib/kernel/test/gen_tcp_misc_SUITE.erl
@@ -1,19 +1,19 @@
 %%
 %% %CopyrightBegin%
-%% 
-%% Copyright Ericsson AB 1998-2009. All Rights Reserved.
-%% 
+%%
+%% Copyright Ericsson AB 1998-2010. All Rights Reserved.
+%%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
 %% compliance with the License. You should have received a copy of the
 %% Erlang Public License along with this software. If not, it can be
 %% retrieved online at http://www.erlang.org/.
-%% 
+%%
 %% Software distributed under the License is distributed on an "AS IS"
 %% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
 %% the License for the specific language governing rights and limitations
 %% under the License.
-%% 
+%%
 %% %CopyrightEnd%
 %%
 -module(gen_tcp_misc_SUITE).
@@ -957,12 +957,11 @@ http_bad_packet(Config) when is_list(Config) ->
 
 http_worker(S) ->
     case gen_tcp:recv(S, 0, 30000) of
+	{ok,{http_error,Error}} ->
+	     io:format("Http error: ~s\n", [Error]);
 	{ok,Data} ->
 	    io:format("Data: ~p\n", [Data]),
-	    http_worker(S);
-	{error,Rsn} ->
-	    io:format("Error: ~p\n", [Rsn]),
-	    ok
+	    http_worker(S)
     end.
 
 http_bad_client(Port) ->
