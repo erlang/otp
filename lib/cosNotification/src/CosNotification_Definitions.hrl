02d774d80e6f85e665b4f28b78f5e307d46e2b0d
Niclas Eklund <nick@erlang.org>
2010-09-23 11:04:44 +0200

    Added missing bracket.

--- lib/cosNotification/src/CosNotification_Definitions.hrl
+++ lib/cosNotification/src/CosNotification_Definitions.hrl
@@ -2,7 +2,7 @@
 %%
 %% %CopyrightBegin%
 %% 
-%% Copyright Ericsson AB 1999-2009. All Rights Reserved.
+%% Copyright Ericsson AB 1999-2010. All Rights Reserved.
 %% 
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
@@ -162,7 +162,7 @@
     low_val=any:create(orber_tc:long(), ?not_MinConsumerEvents), 
     high_val=any:create(orber_tc:long(), ?not_MaxConsumerEvents)
    }}
-].
+]).
 
 
 
