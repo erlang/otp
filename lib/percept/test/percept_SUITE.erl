91298c06d77197433c68b3cc1d05eb966e1370e0
Lukas Larsson <lukas@erix.ericsson.se>
2010-10-12 10:36:54 +0200

    Update percept tests to conform with common_test standard

--- lib/percept/test/percept_SUITE.erl
+++ lib/percept/test/percept_SUITE.erl
@@ -18,10 +18,10 @@
 %%
 
 -module(percept_SUITE).
--include("test_server.hrl").
+-include_lib("test_server/include/test_server.hrl").
 
 %% Test server specific exports
--export([all/1]).
+-export([all/0,groups/0,init_per_group/2,end_per_group/2]).
 -export([init_per_suite/1, end_per_suite/1]).
 -export([init_per_testcase/2, end_per_testcase/2]).
 
@@ -51,12 +51,18 @@ end_per_testcase(_Case, Config) ->
     ?t:timetrap_cancel(Dog),
     ok.
 
-all(suite) ->
-    % Test cases
-    [	webserver,
-	profile,
-	analyze,
-	analyze_dist].
+all() -> 
+[webserver, profile, analyze, analyze_dist].
+
+groups() -> 
+    [].
+
+init_per_group(_GroupName, Config) ->
+	Config.
+
+end_per_group(_GroupName, Config) ->
+	Config.
+
 
 %%----------------------------------------------------------------------
 %% Tests
