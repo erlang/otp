e6a27219093c22e6d63025078de9926590816f7c
Lukas Larsson <lukas@erix.ericsson.se>
2010-11-23 17:57:07 +0100

    Remove supurflous comments and _SUITE files

--- lib/common_test/test/ct_suite_callback_SUITE_data/scb/tests/ct_scb_empty_SUITE.erl
+++ lib/common_test/test/ct_suite_callback_SUITE_data/scb/tests/ct_scb_empty_SUITE.erl
@@ -27,84 +27,21 @@
 -include("ct.hrl").
 
 %% Test server callback functions
-%%--------------------------------------------------------------------
-%% @doc
-%% Config - [tuple()]
-%%   A list of key/value pairs, holding the test case configuration.
-%%
-%% Initiation before the whole suite
-%%
-%% Note: This function is free to add any key/value pairs to the Config
-%% variable, but should NOT alter/remove any existing entries.
-%%
-%% @spec init_per_suite(Config) -> Config
-%% @end
-%%--------------------------------------------------------------------
 init_per_suite(Config) ->
     Config.
 
-%%--------------------------------------------------------------------
-%% @doc
-%% Config - [tuple()]
-%%   A list of key/value pairs, holding the test case configuration.
-%%
-%% Cleanup after the whole suite
-%%
-%% @spec end_per_suite(Config) -> _
-%% @end
-%%--------------------------------------------------------------------
 end_per_suite(_Config) ->
     ok.
 
-%%--------------------------------------------------------------------
-%% @doc
-%% Case - atom()
-%%   Name of the test case that is about to be run.
-%% Config - [tuple()]
-%%   A list of key/value pairs, holding the test case configuration.
-%%
-%% Initiation before each test case
-%%
-%% Note: This function is free to add any key/value pairs to the Config
-%% variable, but should NOT alter/remove any existing entries.
-%% Initiation before each test case
-%%
-%% @spec init_per_testcase(TestCase, Config) -> Config
-%% @end
-%%--------------------------------------------------------------------
 init_per_testcase(_TestCase, Config) ->
     Config.
 
-%%--------------------------------------------------------------------
-%% @doc
-%% Case - atom()
-%%   Name of the test case that is about to be run.
-%% Config - [tuple()]
-%%   A list of key/value pairs, holding the test case configuration.
-%%
-%% Cleanup after each test case
-%%
-%% @spec end_per_testcase(TestCase, Config) -> _
-%% @end
-%%--------------------------------------------------------------------
 end_per_testcase(_TestCase, _Config) ->
     ok.
 
-%%--------------------------------------------------------------------
-%% @doc
-%% TestCases - [Case]
-%% Case - atom()
-%%   Name of a test case.
-%%
-%% Returns a list of all test cases in this test suite
-%%
-%% @spec all() -> TestCases
-%% @end
-%%--------------------------------------------------------------------
 all() ->
     [test_case].
 
 %% Test cases starts here.
-%%--------------------------------------------------------------------
 test_case(Config) when is_list(Config) ->
     ok.
