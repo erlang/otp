dd99864e18829150951285d3819096e04ebda3de
Micael Karlberg <bmk@erlang.org>
2011-02-17 12:02:54 +0100

    No auto-import of erlang:error/1,2.

--- lib/snmp/test/snmp_appup_test.erl
+++ lib/snmp/test/snmp_appup_test.erl
@@ -1,7 +1,7 @@
 %% 
 %% %CopyrightBegin%
 %% 
-%% Copyright Ericsson AB 2003-2009. All Rights Reserved.
+%% Copyright Ericsson AB 2003-2011. All Rights Reserved.
 %% 
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
@@ -29,6 +29,8 @@
 	 appup_file/1
 
 	]).
+
+-compile({no_auto_import, [error/1]}).
 
 -include("test_server.hrl").
 -include("snmp_test_lib.hrl").
