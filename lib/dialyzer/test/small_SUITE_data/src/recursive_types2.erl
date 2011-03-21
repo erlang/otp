f78931dbd401f39a88c375c205fabf2c5bd83581
Stavros Aronis <aronisstav@gmail.com>
2011-03-21 18:26:26 +0200

    Update small/recursive_types2 source

--- lib/dialyzer/test/small_SUITE_data/src/recursive_types2.erl
+++ lib/dialyzer/test/small_SUITE_data/src/recursive_types2.erl
@@ -2,9 +2,9 @@
 
 -export([test/0]).
 
--type tree() :: 'nil' | {non_neg_integer(), child(), child()}.
+-type tree() :: 'nil' | {non_neg_integer(), subtree(), subtree()}.
 
--type child() :: tree().
+-type subtree() :: tree().
 
 -spec test() -> {42, tree(), tree()}.
 
