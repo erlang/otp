6b09d2edaa362cfa506dd9cebe2de8cc76d92b73
Piotr Dorobisz <piotr.dorobisz@erlang-solutions.com>
2011-03-28 08:59:27 +0200

    new tests for ttb module

---
+++ lib/observer/test/client.erl
@@ -0,0 +1,29 @@
+-module(client).
+-compile(export_all).
+
+init(Node) ->
+    io:format(user, "Initing~p~n", [Node]),
+    application:start(runtime_tools),
+    net_kernel:connect_node(Node).
+
+init() ->
+    init(server_node()).
+
+restart() ->
+    init:restart().
+
+server_node() ->
+    {ok,HostName} = inet:gethostname(),
+    list_to_atom("server@" ++ HostName).
+
+get() ->
+    erlang:send({server,server_node()}, {get,self()}),
+    receive Data -> Data
+    after 1000   -> no_reply
+    end.
+
+put(Thing) ->
+    erlang:send({server,server_node()}, {put,self(),Thing}),
+    receive ok -> ok
+    after 1000 -> no_reply
+    end.
