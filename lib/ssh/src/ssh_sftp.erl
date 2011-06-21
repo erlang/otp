b331676b2fa755d767fc51b65ed6d99e1c20d7a2
Niclas Eklund <nick@erlang.org>
2011-06-21 16:23:36 +0200

    OTP-9386 - Calling ssh_sftp:stop_channel/1 resulted in that the trap_exit flag was set to true for the invoking process.

--- lib/ssh/src/ssh_sftp.erl
+++ lib/ssh/src/ssh_sftp.erl
@@ -130,9 +130,9 @@ start_channel(Host, Port, Opts) ->
     end.
 
 stop_channel(Pid) ->
-    case process_info(Pid, [trap_exit]) of
-	[{trap_exit, Bool}] ->
-	    process_flag(trap_exit, true),
+    case is_process_alive(Pid) of
+	true ->
+	    OldValue = process_flag(trap_exit, true),
 	    link(Pid),
 	    exit(Pid, ssh_sftp_stop_channel),
 	    receive 
@@ -145,9 +145,9 @@ stop_channel(Pid) ->
 			    ok
 		    end
 	    end,
-	    process_flag(trap_exit, Bool),
+	    process_flag(trap_exit, OldValue),
 	    ok;
-	undefined ->
+	false ->
 	    ok
     end.
 
