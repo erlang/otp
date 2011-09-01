b8d4a363e3f0a48cc698e987b2cc13328f1a55d4
Björn Gustavsson <bjorn@erlang.org>
2011-09-01 09:27:01 +0200

    Make the check_old_code/1 BIF auto-imported
    check_old_code/1 was documented to be auto-imported, but in fact
    was not auto-imported. Since check_process_code/2 and the other
    code loading BIFs are now auto-imported, for consistency auto-import
    check_old_code/1 too.

--- lib/stdlib/src/erl_internal.erl
+++ lib/stdlib/src/erl_internal.erl
@@ -262,6 +262,7 @@ bif(bitsize, 1) -> true;
 bif(bit_size, 1) -> true;
 bif(bitstring_to_list, 1) -> true;
 bif(byte_size, 1) -> true;
+bif(check_old_code, 1) -> true;
 bif(check_process_code, 2) -> true;
 bif(concat_binary, 1) -> true;
 bif(date, 0) -> true;
