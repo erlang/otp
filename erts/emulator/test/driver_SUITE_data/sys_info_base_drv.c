deca218163838f88ade8f62eca60d8d67db00f0d
Raimo Niskanen <raimo@erlang.org>
2011-12-07 17:33:11 +0100

    driver_SUITE.erl: Fix sys info drivers

--- erts/emulator/test/driver_SUITE_data/sys_info_base_drv.c
+++ erts/emulator/test/driver_SUITE_data/sys_info_base_drv.c
@@ -3,34 +3,35 @@
  * compliance with the License. You should have received a copy of the
  * Erlang Public License along with this software. If not, it can be
  * retrieved via the world wide web at http://www.erlang.org/.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
  * the License for the specific language governing rights and limitations
  * under the License.
- * 
+ *
  * The Initial Developer of the Original Code is Ericsson Utvecklings AB.
  * Portions created by Ericsson are Copyright 1999, Ericsson Utvecklings
  * AB. All Rights Reserved.''
- * 
+ *
  *     $Id$
  */
 
 /*
  * Author: Rickard Green
  *
- * Description: Driver that fakes driver version 1.1 and tests
+ * Description: Driver that fakes driver version 2.0 and tests
  *              driver_system_info().
  *
  */
 
 #include "sys_info_drv_impl.h"
 
-#define SYS_INFO_DRV_MAJOR_VSN		1
-#define SYS_INFO_DRV_MINOR_VSN		1
-#define SYS_INFO_DRV_NAME_STR		"sys_info_1_1_drv"
-#define SYS_INFO_DRV_NAME		sys_info_1_1_drv
-#define SYS_INFO_DRV_LAST_FIELD		scheduler_threads
+#define SYS_INFO_DRV_MAJOR_VSN		2
+#define SYS_INFO_DRV_MINOR_VSN		0
+#define SYS_INFO_DRV_NAME_STR		"sys_info_base_drv"
+#define SYS_INFO_DRV_NAME		sys_info_base_drv
+#define SYS_INFO_DRV_LAST_FIELD		smp_support
+#define ERL_DRV_SYS_INFO_SIZE		sizeof(ErlDrvSysInfo)
 
 #define SYS_INFO_DRV_RES_FORMAT "ok: "	\
   "drv_drv_vsn=%d.%d "			\
@@ -41,7 +42,6 @@
   "smp=%s "				\
   "async_thrs=%d "			\
   "sched_thrs=%d"
-
 
 static size_t
 sys_info_drv_max_res_len(ErlDrvSysInfo *sip)
@@ -76,5 +76,3 @@ sys_info_drv_sprintf_sys_info(ErlDrvSysInfo *sip, char *str)
 }
 
 #include "sys_info_drv_impl.c"
-
-
