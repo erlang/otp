83d38d0dda488ecb7090f3070a5b2569be5a555e
Raimo Niskanen <raimo@erlang.org>
2012-03-15 11:22:59 +0100

    stdlib: Stop working around erlang:halt not flushing

--- lib/stdlib/src/escript.erl
+++ lib/stdlib/src/escript.erl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 2007-2011. All Rights Reserved.
+%% Copyright Ericsson AB 2007-2012. All Rights Reserved.
 %%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
@@ -848,17 +848,7 @@ fatal(Str) ->
     throw(Str).
 
 my_halt(Reason) ->
-    case process_info(group_leader(), status) of
-        {_,waiting} ->
-            %% Now all output data is down in the driver.
-            %% Give the driver some extra time before halting.
-            receive after 1 -> ok end,
-            halt(Reason);
-        _ ->
-            %% Probably still processing I/O requests.
-            erlang:yield(),
-            my_halt(Reason)
-    end.
+    erlang:halt(Reason).
 
 hidden_apply(App, M, F, Args) ->
     try
