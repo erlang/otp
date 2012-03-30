ce4431b33d18c04f4166def737b2aed582e4426a
Björn-Egil Dahlberg <egil@erlang.org>
2012-03-30 14:46:02 +0200

    Update copyright years

--- lib/runtime_tools/examples/spawn-exit.d
+++ lib/runtime_tools/examples/spawn-exit.d
@@ -2,7 +2,7 @@
 /*
  * %CopyrightBegin%
  *
- * Copyright Scott Lystig Fritchie 2011. All Rights Reserved.
+ * Copyright Scott Lystig Fritchie 2011-2012. All Rights Reserved.
  *
  * The contents of this file are subject to the Erlang Public License,
  * Version 1.1, (the "License"); you may not use this file except in
