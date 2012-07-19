a157483f119594be71e2a8882bc20ed9e8c2a8b8
Anders Svensson <anders@erlang.org>
2012-07-19 17:52:38 +0200

    Fix counter typo

--- lib/diameter/src/base/diameter_service.erl
+++ lib/diameter/src/base/diameter_service.erl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 2010-2011. All Rights Reserved.
+%% Copyright Ericsson AB 2010-2012. All Rights Reserved.
 %%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
@@ -2218,7 +2218,7 @@ a(#diameter_packet{errors = []}
            packet = P}
   = Req) ->
     try
-        incr(in, Pkt, TPid)
+        incr(recv, Pkt, TPid)
     of
         _ ->
             cb(Req, handle_answer, [Pkt, msg(P), SvcName, {TPid, Caps}])
