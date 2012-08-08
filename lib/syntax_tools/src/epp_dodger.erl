f333cd0ef64e75cf82a0086e14d49d711b67a8b6
Richard Carlsson <richardc@klarna.com>
2012-08-08 16:10:07 +0300

    fix bad format of error in epp_dodger:parse_file/3

--- lib/syntax_tools/src/epp_dodger.erl
+++ lib/syntax_tools/src/epp_dodger.erl
@@ -88,7 +88,7 @@
 %% This is a so-called Erlang I/O ErrorInfo structure; see the {@link
 %% //stdlib/io} module for details.
 
--type errorinfo() :: term(). % {integer(), atom(), term()}.
+-type errorinfo() :: {integer(), atom(), term()}.
 
 -type option() :: atom() | {atom(), term()}.
 
@@ -208,8 +208,8 @@ do_parse_file(DefEncoding, File, Parser, Options) ->
             try Parser(Dev, 1, Options)
             after ok = file:close(Dev)
 	    end;
-        {error, _} = Error ->
-            Error
+        {error, Error} ->
+            {error, {0, file, Error}}  % defer to file:format_error/1
     end.
 
 find_invalid_unicode([H|T]) ->
