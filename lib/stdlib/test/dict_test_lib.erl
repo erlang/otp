306a239a150e6258b8d4b96764638551a8d9c0e0
Björn Gustavsson <bjorn@erlang.org>
2012-11-07 16:13:26 +0100

    dict_SUITE: Eliminate use of parameterized modules
    Since the support for parameterized modules will be removed in R16,
    we should not use them in test cases.

--- lib/stdlib/test/dict_test_lib.erl
+++ lib/stdlib/test/dict_test_lib.erl
@@ -17,67 +17,48 @@
 %% %CopyrightEnd%
 %%
 
--module(dict_test_lib, [Mod,Equal]).
+-module(dict_test_lib).
 
--export([module/0,equal/2,empty/0,size/1,to_list/1,from_list/1,
-	 enter/3,delete/2,lookup/2]).
+-export([new/2]).
 
-module() ->
-    Mod.
+new(Mod, Eq) ->
+    fun (enter, {K,V,D}) -> enter(Mod, K, V, D);
+	(empty, []) -> empty(Mod);
+	(equal, {D1,D2}) -> Eq(D1, D2);
+	(from_list, L) -> from_list(Mod, L);
+	(module, []) -> Mod;
+	(size, D) -> Mod:size(D);
+	(to_list, D) -> to_list(Mod, D)
+    end.
 
-equal(X, Y) ->
-    Equal(X, Y).
-
-empty() ->
+empty(Mod) ->
     case erlang:function_exported(Mod, new, 0) of
 	false -> Mod:empty();
 	true -> Mod:new()
     end.
 
-size(S) ->
-    Mod:size(S).
+to_list(Mod, D) ->
+    Mod:to_list(D).
 
-to_list(S) ->
-    Mod:to_list(S).
-
-from_list(S) ->
+from_list(Mod, L) ->
     case erlang:function_exported(Mod, from_orddict, 1) of
 	false ->
-	    Mod:from_list(S);
+	    Mod:from_list(L);
 	true ->
 	    %% The gb_trees module has no from_list/1 function.
 	    %%
 	    %% The keys in S are not unique. To make sure
 	    %% that we pick the same key/value pairs as
 	    %% dict/orddict, first convert the list to an orddict.
-	    Orddict = orddict:from_list(S),
+	    Orddict = orddict:from_list(L),
 	    Mod:from_orddict(Orddict)
     end.
 
 %% Store new value into dictionary or update previous value in dictionary.
-enter(Key, Val, Dict) ->
+enter(Mod, Key, Val, Dict) ->
     case erlang:function_exported(Mod, store, 3) of
 	false ->
 	    Mod:enter(Key, Val, Dict);
 	true ->
 	    Mod:store(Key, Val, Dict)
-    end.
-
-%% Delete an EXISTING key.
-delete(Key, Dict) ->
-    case erlang:function_exported(Mod, delete, 2) of
-	true -> Mod:delete(Key, Dict);
-	false -> Mod:erase(Key, Dict)
-    end.
-
-%% -> none | {value,Value}
-lookup(Key, Dict) ->
-    case erlang:function_exported(Mod, lookup, 2) of
-	false ->
-	    case Mod:find(Key, Dict) of
-		error -> none;
-		{ok,Value} -> {value,Value}
-	    end;
-	true ->
-	    Mod:lookup(Key, Dict)
     end.
