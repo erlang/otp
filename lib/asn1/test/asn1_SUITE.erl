9a0754b4d051eb75547da790fadef962b67a512e
Björn Gustavsson <bjorn@erlang.org>
2013-01-07 14:37:00 +0100

    Fix a bug for multiple extension addition groups
    Encoding would fail.

--- lib/asn1/test/asn1_SUITE.erl
+++ lib/asn1/test/asn1_SUITE.erl
@@ -1096,6 +1096,7 @@ testExtensionAdditionGroup(Config, Rule, Opts) ->
                                  [debug_info]),
     extensionAdditionGroup:run([Rule|Opts]),
     extensionAdditionGroup:run2([Rule|Opts]),
+    extensionAdditionGroup:run3(),
     asn1_test_lib:compile("EUTRA-RRC-Definitions", Config, [Rule, {record_name_prefix, "RRC-"}|Opts]),
     extensionAdditionGroup:run3([Rule|Opts]).
 
