24522845d50866782a939f3c8fbafa99e70d9f44
Björn Gustavsson <bjorn@erlang.org>
2013-02-06 13:12:19 +0100

    testX420: Pass Options to the ASN.1 compiler
    When the caller passed the 'der' option, it was ignored.

--- lib/asn1/test/testX420.erl
+++ lib/asn1/test/testX420.erl
@@ -37,7 +37,7 @@ compile_loop(Erule, [Spec|Specs], Options, Config)
   when Erule == ber; Erule == ber_bin; Erule == ber_bin_v2; Erule == per ->
     CaseDir = ?config(case_dir, Config),
     asn1_test_lib:compile(filename:join([x420, Spec]), Config,
-                          [Erule, {i, CaseDir}]),
+                          [Erule, {i, CaseDir} | Options]),
     compile_loop(Erule, Specs, Options, Config);
 compile_loop(_Erule, _Specs, _Options, _Config) ->
     ok.
