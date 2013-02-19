5c11ccb5522a03371b175ab1ef78e15114a38758
Anders Svensson <anders@erlang.org>
2013-02-19 18:19:32 +0100

    Remove cruft from doc Makefile

--- lib/diameter/doc/src/files.mk
+++ lib/diameter/doc/src/files.mk
@@ -2,7 +2,7 @@
 
 # %CopyrightBegin%
 #
-# Copyright Ericsson AB 2010-2012. All Rights Reserved.
+# Copyright Ericsson AB 2010-2013. All Rights Reserved.
 #
 # The contents of this file are subject to the Erlang Public License,
 # Version 1.1, (the "License"); you may not use this file except in
@@ -38,7 +38,9 @@ XML_REF4_FILES = \
 XML_PART_FILES = \
 	user_man.xml
 
-XML_EXTRA_FILES = 
+XML_EXTRA_FILES = \
+	seealso.ent \
+	diameter_soc_rfc6733.xml
 
 XML_CHAPTER_FILES = \
 	diameter_intro.xml \
@@ -50,5 +52,4 @@ XML_CHAPTER_FILES = \
 BOOK_FILES = \
 	book.xml
 
-GIF_FILES = \
-	notes.gif
+GIF_FILES =
