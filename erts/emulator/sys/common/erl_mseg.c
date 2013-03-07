1c8524a1a45de864a0c2b2e4a544c7e37501d3be
YAMAMOTO Takashi <yamt@mwd.biglobe.ne.jp>
2013-03-07 11:57:44 +0900

    compilation fix for NetBSD
    spells __NetBSD__ correctly.

--- erts/emulator/sys/common/erl_mseg.c
+++ erts/emulator/sys/common/erl_mseg.c
@@ -439,7 +439,7 @@ mseg_destroy(ErtsMsegAllctr_t *ma, MemKind* mk, void *seg, Uint size) {
 }
 
 #if HAVE_MSEG_RECREATE
-#if defined(__NetBsd__)
+#if defined(__NetBSD__)
 #define MREMAP_FLAGS  (0)
 #else
 #define MREMAP_FLAGS  (MREMAP_MAYMOVE)
