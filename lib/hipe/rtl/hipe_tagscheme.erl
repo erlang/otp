ff25b3525d273469fe9b113b35ba68c3c0aff7a4
Kostis Sagonas <kostis@it.uu.se>
2013-03-10 16:04:10 +0100

    Update a comment

--- lib/hipe/rtl/hipe_tagscheme.erl
+++ lib/hipe/rtl/hipe_tagscheme.erl
@@ -2,7 +2,7 @@
 %%
 %% %CopyrightBegin%
 %% 
-%% Copyright Ericsson AB 2001-2011. All Rights Reserved.
+%% Copyright Ericsson AB 2001-2013. All Rights Reserved.
 %% 
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
@@ -20,7 +20,7 @@
 %%========================================================================
 %%
 %% Filename : hipe_tagscheme.erl
-%% Note     : This is specific to Erlang 5.* (i.e. R9 to R13).
+%% Note     : This is specific to Erlang 5.* (i.e. starting with R9).
 %%
 %% Modifications:
 %%  020904: Happi - added support for external pids and ports.
