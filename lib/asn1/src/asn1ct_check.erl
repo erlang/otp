45e356fb7b5d381a6e3c9ec83bf20fe9cefe8e4b
Björn Gustavsson <bjorn@erlang.org>
2013-04-02 08:13:32 +0200

    Don't lose the extension mark for object set parameters
    When an object set is an actual parameter, the extension marker
    for the object set could get lost.

--- lib/asn1/src/asn1ct_check.erl
+++ lib/asn1/src/asn1ct_check.erl
@@ -4026,8 +4026,8 @@ categorize(S,value,Type,Value) ->
     [#valuedef{type=Type,value=Value,module=S#state.mname}].
 
 
-parse_objectset({valueset,T=#type{}}) ->
-    [T];
+parse_objectset({valueset,#type{def=#'Externaltypereference'{}=Ref}}) ->
+    Ref;
 parse_objectset({valueset,Set}) ->
     Set;
 parse_objectset(#type{def=Ref}) when is_record(Ref,'Externaltypereference') ->
