erlang/otp

commit f0a36c79af84c044ba04af3602506624a5ee8799

Don't hardcode common dictionary
Instead, use whatever dictionary a transport has configured as
supporting application id 0. This is to support the updated RFC 6733
dictionaries (which bring with them updated records) and also to be able
to transparently support any changed semantics (eg. 5xxx in
answer-message).

lib/diameter/src/base/diameter_capx.erl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 2010-2012. All Rights Reserved.
+%% Copyright Ericsson AB 2010-2013. All Rights Reserved.
 %%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
@@ -47,14 +47,13 @@
 
 -module(diameter_capx).
 
--export([build_CER/1,
-         recv_CER/2,
-         recv_CEA/2,
+-export([build_CER/2,
+         recv_CER/3,
+         recv_CEA/3,
          make_caps/2]).
 
 -include_lib("diameter/include/diameter.hrl").
 -include("diameter_internal.hrl").
--include("diameter_gen_base_rfc3588.hrl").
 
 -define(SUCCESS,    2001).  %% DIAMETER_SUCCESS
 -define(NOAPP,      5010).  %% DIAMETER_NO_COMMON_APPLICATION
@@ -67,27 +66,31 @@
 
 -type tried(T) :: {ok, T} | {error, {term(), list()}}.
 
--spec build_CER(#diameter_caps{})
-   -> tried(#diameter_base_CER{}).
+-spec build_CER(#diameter_caps{}, module())
+   -> tried(CER)
+ when CER :: tuple().
 
-build_CER(Caps) ->
-    try_it([fun bCER/1, Caps]).
+build_CER(Caps, Dict) ->
+    try_it([fun bCER/2, Caps, Dict]).
 
--spec recv_CER(#diameter_base_CER{}, #diameter_service{})
+-spec recv_CER(CER, #diameter_service{}, module())
    -> tried({[diameter:'Unsigned32'()],
              #diameter_caps{},
-             #diameter_base_CEA{}}).
+             CEA})
+ when CER :: tuple(),
+      CEA :: tuple().
 
-recv_CER(CER, Svc) ->
-    try_it([fun rCER/2, CER, Svc]).
+recv_CER(CER, Svc, Dict) ->
+    try_it([fun rCER/3, CER, Svc, Dict]).
 
--spec recv_CEA(#diameter_base_CEA{}, #diameter_service{})
+-spec recv_CEA(CEA, #diameter_service{}, module())
    -> tried({[diameter:'Unsigned32'()],
              [diameter:'Unsigned32'()],
-             #diameter_caps{}}).
+             #diameter_caps{}})
+ when CEA :: tuple().
 
-recv_CEA(CEA, Svc) ->
-    try_it([fun rCEA/2, CEA, Svc]).
+recv_CEA(CEA, Svc, Dict) ->
+    try_it([fun rCEA/3, CEA, Svc, Dict]).
 
 make_caps(Caps, Opts) ->
     try_it([fun mk_caps/2, Caps, Opts]).
@@ -161,16 +164,17 @@ ipaddr(A) ->
             ?THROW(T)
     end.
 
-%% bCER/1
+%% bCER/2
 %%
 %% Build a CER record to send to a remote peer.
 
 %% Use the fact that diameter_caps has the same field names as CER.
-bCER(#diameter_caps{} = Rec) ->
-    #diameter_base_CER{}
-        = list_to_tuple([diameter_base_CER | tl(tuple_to_list(Rec))]).
+bCER(#diameter_caps{} = Rec, Dict) ->
+    Values = lists:zip(Dict:'#info-'(diameter_base_CER, fields),
+                       tl(tuple_to_list(Rec))),
+    Dict:'#new-'(diameter_base_CER, Values).
 
-%% rCER/2
+%% rCER/3
 %%
 %% Build a CEA record to send to a remote peer in response to an
 %% incoming CER. RFC 3588 gives no guidance on what should be sent
@@ -214,12 +218,9 @@ bCER(#diameter_caps{} = Rec) ->
 %%   TLS                               1
 %%      This node supports TLS security, as defined by [TLS].
 
-rCER(CER, #diameter_service{capabilities = LCaps} = Svc) ->
-    #diameter_base_CEA{}
-        = CEA
-        = cea_from_cer(bCER(LCaps)),
-
-    RCaps = capx_to_caps(CER),
+rCER(CER, #diameter_service{capabilities = LCaps} = Svc, Dict) ->
+    CEA = cea_from_cer(bCER(LCaps, Dict), Dict),
+    RCaps = capx_to_caps(CER, Dict),
     SApps = common_applications(LCaps, RCaps, Svc),
 
     {SApps,
@@ -227,17 +228,18 @@ rCER(CER, #diameter_service{capabilities = LCaps} = Svc) ->
      build_CEA(SApps,
                LCaps,
                RCaps,
-               CEA#diameter_base_CEA{'Result-Code' = ?SUCCESS})}.
+               Dict,
+               Dict:'#set-'({'Result-Code', ?SUCCESS}, CEA))}.
 
-build_CEA([], _, _, CEA) ->
-    CEA#diameter_base_CEA{'Result-Code' = ?NOAPP};
+build_CEA([], _, _, Dict, CEA) ->
+    Dict:'#set-'({'Result-Code', ?NOAPP}, CEA);
 
-build_CEA(_, LCaps, RCaps, CEA) ->
+build_CEA(_, LCaps, RCaps, Dict, CEA) ->
     case common_security(LCaps, RCaps) of
         [] ->
-            CEA#diameter_base_CEA{'Result-Code' = ?NOSECURITY};
+            Dict:'#set-'({'Result-Code', ?NOSECURITY}, CEA);
         [_] = IS ->
-            CEA#diameter_base_CEA{'Inband-Security-Id' = IS}
+            Dict:'#set-'({'Inband-Security-Id', IS}, CEA)
     end.
 
 %% common_security/2
@@ -275,46 +277,49 @@ cs(LS, RS) ->
 %% practice something there may be a need for more synchronization
 %% than notification by way of an event subscription offers.
 
-%% cea_from_cer/1
+%% cea_from_cer/2
 
 %% CER is a subset of CEA, the latter adding Result-Code and a few
 %% more AVP's.
-cea_from_cer(#diameter_base_CER{} = CER) ->
-    lists:foldl(fun(F,A) -> to_cea(CER, F, A) end,
-                #diameter_base_CEA{},
-                record_info(fields, diameter_base_CER)).
+cea_from_cer(CER, Dict) ->
+    lists:foldl(fun(F,A) -> to_cea(CER, F, A, Dict) end,
+                Dict:'#new-'(diameter_base_CEA),
+                Dict:'#info-'(diameter_base_CER, fields)).
 
-to_cea(CER, Field, CEA) ->
-    try ?BASE:'#get-'(Field, CER) of
-        V -> ?BASE:'#set-'({Field, V}, CEA)
+to_cea(CER, Field, CEA, Dict) ->
+    try Dict:'#get-'(Field, CER) of
+        V -> Dict:'#set-'({Field, V}, CEA)
     catch
         error: _ -> CEA
     end.
         
-%% rCEA/2
+%% rCEA/3
 
-rCEA(CEA, #diameter_service{capabilities = LCaps} = Svc) ->
-    RCaps = capx_to_caps(CEA),
+rCEA(CEA, #diameter_service{capabilities = LCaps} = Svc, Dict) ->
+    RCaps = capx_to_caps(CEA, Dict),
     SApps = common_applications(LCaps, RCaps, Svc),
     IS = common_security(LCaps, RCaps),
 
     {SApps, IS, RCaps}.
 
-%% capx_to_caps/1
+%% capx_to_caps/2
 
-capx_to_caps(#diameter_base_CEA{'Origin-Host' = OH,
-                                'Origin-Realm' = OR,
-                                'Host-IP-Address' = IP,
-                                'Vendor-Id' = VId,
-                                'Product-Name' = PN,
-                                'Origin-State-Id' = OSI,
-                                'Supported-Vendor-Id' = SV,
-                                'Auth-Application-Id' = Auth,
-                                'Inband-Security-Id' = IS,
-                                'Acct-Application-Id' = Acct,
-                                'Vendor-Specific-Application-Id' = VSA,
-                                'Firmware-Revision' = FR,
-                                'AVP' = X}) ->
+capx_to_caps(CEX, Dict) ->
+    [OH, OR, IP, VId, PN, OSI, SV, Auth, IS, Acct, VSA, FR, X]
+        = Dict:'#get-'(['Origin-Host',
+                        'Origin-Realm',
+                        'Host-IP-Address',
+                        'Vendor-Id',
+                        'Product-Name',
+                        'Origin-State-Id',
+                        'Supported-Vendor-Id',
+                        'Auth-Application-Id',
+                        'Inband-Security-Id',
+                        'Acct-Application-Id',
+                        'Vendor-Specific-Application-Id',
+                        'Firmware-Revision',
+                        'AVP'],
+                       CEX),
     #diameter_caps{origin_host = OH,
                    origin_realm = OR,
                    vendor_id = VId,
@@ -327,10 +332,7 @@ capx_to_caps(#diameter_base_CEA{'Origin-Host' = OH,
                    acct_application_id = Acct,
                    vendor_specific_application_id = VSA,
                    firmware_revision = FR,
-                   avp = X};
-
-capx_to_caps(#diameter_base_CER{} = CER) ->
-    capx_to_caps(cea_from_cer(CER)).
+                   avp = X}.
 
 %% ---------------------------------------------------------------------------
 %% ---------------------------------------------------------------------------
@@ -365,13 +367,12 @@ app_union(#diameter_caps{auth_application_id = U,
                          vendor_specific_application_id = V}) ->
     set_list(U ++ C ++ lists:flatmap(fun vsa_apps/1, V)).
 
-vsa_apps(#'diameter_base_Vendor-Specific-Application-Id'
-         {'Auth-Application-Id' = U,
-          'Acct-Application-Id' = C}) ->
-    U ++ C;
-vsa_apps(L) ->
-    Rec = ?BASE:'#new-'('diameter_base_Vendor-Specific-Application-Id', L),
-    vsa_apps(Rec).
+vsa_apps([_ | [_,_] = Ids]) ->
+    lists:append(Ids);
+vsa_apps(Rec)
+  when is_tuple(Rec) ->
+    [_|T] = tuple_to_list(Rec),
+    vsa_apps(T).
 
 %% It's a configuration error for a locally advertised application not
 %% to be represented in Apps. Don't just match on lists:keyfind/3 in

lib/diameter/include/diameter.hrl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 2010-2012. All Rights Reserved.
+%% Copyright Ericsson AB 2010-2013. All Rights Reserved.
 %%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
@@ -25,7 +25,11 @@
 -define(DIAMETER_APP_ID_ACCOUNTING, 3).
 -define(DIAMETER_APP_ID_RELAY,      16#FFFFFFFF).
 
-%% Corresponding dictionaries:
+%% Corresponding dictionaries. These macros are deprecated now that
+%% there is an RFC6733 whose dictionaries are not strictly backwards
+%% compatible. The RFC 6733 common and accounting dictionaries are
+%% diameter_gen_base_rfc6733 and diameter_gen_acct_rfc6733
+%% respectively.
 -define(DIAMETER_DICT_COMMON,       diameter_gen_base_rfc3588).
 -define(DIAMETER_DICT_ACCOUNTING,   diameter_gen_base_accounting).
 -define(DIAMETER_DICT_RELAY,        diameter_gen_relay).

lib/diameter/src/base/diameter_codec.erl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 2010-2012. All Rights Reserved.
+%% Copyright Ericsson AB 2010-2013. All Rights Reserved.
 %%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
@@ -26,7 +26,7 @@
          decode_header/1,
          sequence_numbers/1,
          hop_by_hop_id/2,
-         msg_name/1,
+         msg_name/2,
          msg_id/1]).
 
 %% Towards generated encoders (from diameter_gen.hrl).
@@ -99,13 +99,13 @@ e(_, #diameter_packet{msg = [#diameter_header{} = Hdr | As]} = Pkt) ->
                                 Eid:32,
                                 Avps/binary>>};
 
-e(Mod0, #diameter_packet{header = Hdr, msg = Msg} = Pkt) ->
+e(Mod, #diameter_packet{header = Hdr, msg = Msg} = Pkt) ->
     #diameter_header{version = Vsn,
                      hop_by_hop_id = Hid,
                      end_to_end_id = Eid}
         = Hdr,
 
-    {Mod, MsgName} = rec2msg(Mod0, Msg),
+    MsgName = rec2msg(Mod, Msg),
     {Code, Flags0, Aid} = msg_header(Mod, MsgName, Hdr),
     Flags = make_flags(Flags0, Hdr),
 
@@ -192,11 +192,11 @@ encode_avps(Avps) ->
 %% msg_header/3
 
 msg_header(Mod, 'answer-message' = MsgName, Header) ->
-    ?BASE = Mod,
+    0 = Mod:id(),  %% assert
     #diameter_header{application_id = Aid,
                      cmd_code = Code}
         = Header,
-    {-1, Flags, ?DIAMETER_APP_ID_COMMON} = ?BASE:msg_header(MsgName),
+    {-1, Flags, ?DIAMETER_APP_ID_COMMON} = Mod:msg_header(MsgName),
     {Code, Flags, Aid};
 
 msg_header(Mod, MsgName, _) ->
@@ -204,22 +204,12 @@ msg_header(Mod, MsgName, _) ->
 
 %% rec2msg/2
 
-rec2msg(_, ['answer-message' = M | _]) ->
-    {?BASE, M};
-
-rec2msg(Mod, [MsgName|_])
-  when is_atom(MsgName) ->
-    {Mod, MsgName};
+rec2msg(_, [Name|_])
+  when is_atom(Name) ->
+    Name;
 
 rec2msg(Mod, Rec) ->
-    R = element(1, Rec),
-    A = 'answer-message',
-    case ?BASE:msg2rec(A) of
-        R ->
-            {?BASE, A};
-        _ ->
-            {Mod, Mod:rec2msg(R)}
-    end.
+    Mod:rec2msg(element(1, Rec)).
 
 %%% ---------------------------------------------------------------------------
 %%% # decode/2
@@ -243,20 +233,19 @@ decode(?APP_ID_RELAY, _, #diameter_packet{} = Pkt) ->
     end;
 
 %% Otherwise decode using the dictionary.
-decode(_, Mod, #diameter_packet{header = Hdr} = Pkt)
-  when is_atom(Mod) ->
+decode(_, Mod, #diameter_packet{header = Hdr} = Pkt) ->
     #diameter_header{cmd_code = CmdCode,
                      is_request = IsRequest,
                      is_error = IsError}
         = Hdr,
 
-    {M, MsgName} = if IsError andalso not IsRequest ->
-                           {?BASE, 'answer-message'};
-                      true ->
-                           {Mod, Mod:msg_name(CmdCode, IsRequest)}
-                   end,
+    MsgName = if IsError andalso not IsRequest ->
+                      'answer-message';
+                 true ->
+                      Mod:msg_name(CmdCode, IsRequest)
+              end,
 
-    decode_avps(MsgName, M, Pkt, collect_avps(Pkt));
+    decode_avps(MsgName, Mod, Pkt, collect_avps(Pkt));
 
 decode(Id, Mod, Bin)
   when is_bitstring(Bin) ->
@@ -360,15 +349,15 @@ hop_by_hop_id(Id, <<H:12/binary, _:32, T/binary>>) ->
     <<H/binary, Id:32, T/binary>>.
 
 %%% ---------------------------------------------------------------------------
-%%% # msg_name/1
+%%% # msg_name/2
 %%% ---------------------------------------------------------------------------
 
-msg_name(#diameter_header{application_id = ?APP_ID_COMMON,
-                          cmd_code = C,
-                          is_request = R}) ->
-    ?BASE:msg_name(C,R);
+msg_name(Dict0, #diameter_header{application_id = ?APP_ID_COMMON,
+                                 cmd_code = C,
+                                 is_request = R}) ->
+    Dict0:msg_name(C,R);
 
-msg_name(Hdr) ->
+msg_name(_, Hdr) ->
     msg_id(Hdr).
 
 %% Note that messages in different applications could have the same

lib/diameter/src/base/diameter_config.erl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 2010-2012. All Rights Reserved.
+%% Copyright Ericsson AB 2010-2013. All Rights Reserved.
 %%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
@@ -113,14 +113,21 @@
 
 -define(VALUES(Rec), tl(tuple_to_list(Rec))).
 
+%% The RFC 3588 common dictionary is used to validate capabilities
+%% configuration. That a given transport may use the RFC 6733
+%% dictionary is of no consequence.
+-define(BASE, diameter_gen_base_rfc3588).
+
 %%% The return values below assume the server diameter_config is started.
 %%% The functions will exit if it isn't.
 
 %% --------------------------------------------------------------------------
-%% # start_service(SvcName, Opts)
-%%
-%% Output: ok | {error, Reason}
+%% # start_service/2
 %% --------------------------------------------------------------------------
+
+-spec start_service(diameter:service_name(), [diameter:service_opt()])
+   -> ok
+    | {error, term()}.
 
 start_service(SvcName, Opts)
   when is_list(Opts)  ->
@@ -134,21 +141,22 @@ start_rc(timeout) ->
     {error, application_not_started}.
 
 %% --------------------------------------------------------------------------
-%% # stop_service(SvcName)
-%%
-%% Output: ok
+%% # stop_service/1
 %% --------------------------------------------------------------------------
+
+-spec stop_service(diameter:service_name())
+   -> ok.
 
 stop_service(SvcName) ->
     sync(SvcName, {stop_service, SvcName}).
 
 %% --------------------------------------------------------------------------
-%% # add_transport(SvcName, {Type, Opts})
-%%
-%% Input:  Type = connect | listen
-%%
-%% Output: {ok, Ref} | {error, Reason}
+%% # add_transport/2
 %% --------------------------------------------------------------------------
+
+-spec add_transport(diameter:service_name(), {connect|listen, [diameter:transport_opt()]})
+   -> {ok, diameter:transport_ref()}
+    | {error, term()}.
 
 add_transport(SvcName, {T, Opts})
   when is_list(Opts), (T == connect orelse T == listen) ->
@@ -170,6 +178,10 @@ add_transport(SvcName, {T, Opts})
 %%
 %% Output: ok | {error, Reason}
 %% --------------------------------------------------------------------------
+
+-spec remove_transport(diameter:service_name(), diameter:transport_pred())
+   -> ok
+    | {error, term()}.
 
 remove_transport(SvcName, Pred) ->
     try
@@ -473,6 +485,10 @@ stop(SvcName) ->
 
 %% add/3
 
+%% Can't check for a single common dictionary since a transport may
+%% restrict applications so that that there's one while the service
+%% has many.
+
 add(SvcName, Type, Opts) ->
     %% Ensure usable capabilities. diameter_service:merge_service/2
     %% depends on this.
@@ -545,7 +561,7 @@ make_config(SvcName, Opts) ->
     [] == Apps andalso ?THROW(no_apps),
 
     %% Use the fact that diameter_caps has the same field names as CER.
-    Fields = diameter_gen_base_rfc3588:'#info-'(diameter_base_CER) -- ['AVP'],
+    Fields = ?BASE:'#info-'(diameter_base_CER) -- ['AVP'],
 
     COpts = [T || {K,_} = T <- Opts, lists:member(K, Fields)],
     Caps = make_caps(#diameter_caps{}, COpts),
@@ -629,7 +645,8 @@ make_caps(Caps, Opts) ->
 
 %% Validate types by encoding a CER.
 encode_CER(Opts) ->
-    {ok, CER} = diameter_capx:build_CER(make_caps(?EXAMPLE_CAPS, Opts)),
+    {ok, CER} = diameter_capx:build_CER(make_caps(?EXAMPLE_CAPS, Opts),
+                                        ?BASE),
 
     Hdr = #diameter_header{version = ?DIAMETER_VERSION,
                            end_to_end_id = 0,

lib/diameter/src/base/diameter_internal.hrl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 2010-2011. All Rights Reserved.
+%% Copyright Ericsson AB 2010-2013. All Rights Reserved.
 %%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
@@ -57,8 +57,6 @@
 %% 3588, 2.4:
 -define(APP_ID_COMMON, 0).
 -define(APP_ID_RELAY, 16#FFFFFFFF).
-
--define(BASE, diameter_gen_base_rfc3588).
 
 %%% ---------------------------------------------------------
 

commit c2d5c74d231b62cc511f3927201ac6578844b578

Fix list-valued Vendor-Specific-Application-Id config
Commit f0a36c79 broke the handling of such configuration, resulting in a
function clause error in diameter_capx if the list was not of length 3,
and faulty extraction of application id's otherwise.  Only record-valued
config was properly interpreted.

lib/diameter/src/base/diameter_capx.erl
@@ -419,12 +419,13 @@ app_union(#diameter_caps{auth_application_id = U,
                          vendor_specific_application_id = V}) ->
     set_list(U ++ C ++ lists:flatmap(fun vsa_apps/1, V)).
 
-vsa_apps([_ | [_,_] = Ids]) ->
-    lists:append(Ids);
+vsa_apps(Vals)
+  when is_list(Vals) ->
+    lists:flatmap(fun({'Vendor-Id', _}) -> []; ({_, Ids}) -> Ids end, Vals);
 vsa_apps(Rec)
   when is_tuple(Rec) ->
-    [_|T] = tuple_to_list(Rec),
-    vsa_apps(T).
+    [_Name, _VendorId | Idss] = tuple_to_list(Rec),
+    lists:append(Idss).
 
 %% It's a configuration error for a locally advertised application not
 %% to be represented in Apps. Don't just match on lists:keyfind/3 in

lib/diameter/test/diameter_capx_SUITE.erl
@@ -27,6 +27,8 @@
 -export([suite/0,
          all/0,
          groups/0,
+         init_per_suite/1,
+         end_per_suite/1,
          init_per_group/2,
          end_per_group/2,
          init_per_testcase/2,
@@ -56,6 +58,11 @@
          peer_down/4]).
 
 -include("diameter.hrl").
+-include("diameter_gen_base_rfc3588.hrl").
+%% Use only the Vendor-Specific-Application-Id record from the base
+%% include, to test the independence of capabilities configuration
+%% from the different definitions of Vendor-Id in RFC's 3588 and RFC
+%% 6733.
 
 %% ===========================================================================
 
@@ -68,6 +75,11 @@
 
 -define(REALM, "erlang.org").
 -define(HOST(Name), Name ++ "." ++ ?REALM).
+
+%% Application id's that are never agreed upon at capabilities
+%% exchange. Testcase no_common_application references them in order
+%% to exercise Vendor-Specific-Application-Id handling.
+-define(NOAPPS, [1111, 2222, 3333, 4444]).
 
 %% Config for diameter:start_service/2.
 -define(SERVICE,
@@ -83,7 +95,10 @@
             || {A,D} <- [{base3588, diameter_gen_base_rfc3588},
                          {acct3588, diameter_gen_base_accounting},
                          {base6733, diameter_gen_base_rfc6733},
-                         {acct6733, diameter_gen_acct_rfc6733}]]]).
+                         {acct6733, diameter_gen_acct_rfc6733}]]]
+        ++ [{application, [{dictionary, dict(N)},
+                           {module, not_really}]}
+            || N <- ?NOAPPS]).
 
 -define(A, list_to_atom).
 -define(L, atom_to_list).
@@ -115,6 +130,16 @@ all() -> [start,
 groups() ->
     Tc = lists:flatmap(fun tc/1, tc()),
     [{D, [], Tc} || D <- ?DICTS].
+
+init_per_suite(Config) ->
+    lists:foreach(fun load_dict/1, ?NOAPPS),
+    Config.
+
+end_per_suite(_Config) ->
+    [] = [Mod || N <- ?NOAPPS,
+                 Mod <- [dict(N)],
+                 false <- [code:delete(Mod)]],
+    ok.
 
 %% Generate a unique hostname for each testcase so that watchdogs
 %% don't prevent a connection from being brought up immediately.
@@ -160,7 +185,7 @@ start(_Config) ->
     ok = diameter:start().
 
 %% Ensure that both integer and list-valued vendor id's can be
-%% configured in a 'Vendor-Specific-Application-Id, the arity having
+%% configured in a Vendor-Specific-Application-Id, the arity having
 %% changed between RFC 3588 and RFC 6733.
 vendor_id(_Config) ->
     [] = ?util:run([[fun vid/1, V] || V <- [1, [1], [1,2], x]]).
@@ -188,13 +213,13 @@ add_listeners(Config) ->
     Acct = [listen(?SERVER,
                    [{capabilities, [{'Origin-Host', ?HOST(H)},
                                     {'Auth-Application-Id', []}]},
-                    {applications, [A]},
+                    {applications, [A | noapps()]},
                     {capabilities_cb, [fun server_capx/3, acct]}])
             || {A,H} <- [{acct3588, "acct3588-srv"},
                          {acct6733, "acct6733-srv"}]],
     Base = [listen(?SERVER,
                    [{capabilities, [{'Origin-Host', ?HOST(H)}]},
-                    {applications, A},
+                    {applications, A ++ noapps()},
                     {capabilities_cb, [fun server_capx/3, base]}])
             || {A,H} <- [{[base3588, acct3588], "base3588-srv"},
                          {[base6733, acct6733], "base6733-srv"}]],
@@ -224,15 +249,33 @@ stop(_Config) ->
 %% DIAMETER_NO_COMMON_APPLICATION = 5010.
 
 s_no_common_application(Config) ->
-    server_closed(Config, fun no_common_application/1, 5010).
+    Vs = [[{'Vendor-Id', 111},
+           {'Auth-Application-Id', [1111]}],
+          #'diameter_base_Vendor-Specific-Application-Id'
+           {'Vendor-Id' = [222],
+            'Acct-Application-Id' = [2222]}],
+    server_closed(Config,
+                  fun(C) -> no_common_application(C,Vs) end,
+                  5010).
 
 c_no_common_application(Config) ->
-    client_closed(Config, "acct-srv", fun no_common_application/1, 5010).
+    Vs = [#'diameter_base_Vendor-Specific-Application-Id'
+          {'Vendor-Id' = 333,
+           'Auth-Application-Id' = [3333]},
+          [{'Vendor-Id', [444]},
+           {'Acct-Application-Id', [4444]}]],
+    client_closed(Config,
+                  "acct-srv",
+                  fun(C) -> no_common_application(C,Vs) end,
+                  5010).
 
-no_common_application(Config) ->
+no_common_application(Config, Vs) ->
     [Common, _Acct] = apps(Config),
-    connect(Config, acct, [{capabilities, [{'Acct-Application-Id', []}]},
-                           {applications, [Common]}]).
+    connect(Config,
+            acct,
+            [{capabilities, [{'Acct-Application-Id', []},
+                             {'Vendor-Specific-Application-Id', Vs}]},
+             {applications, [Common | noapps()]}]).
 
 %% ====================
 %% Ask the base server to speak accounting with an unknown security
@@ -323,6 +366,25 @@ client_reject(Config) ->
                            {applications, apps(Config)}]).
 
 %% ===========================================================================
+
+noapps() ->
+    lists:map(fun dict/1, ?NOAPPS).
+
+dict(N) ->
+    ?A(?L(?MODULE) ++ "_" ++ integer_to_list(N)).
+
+%% Compile and load minimal dictionary modules. These actually have to
+%% exists since diameter will call their id/0 to extract application
+%% id's, failing with app_not_configured if it can't.
+load_dict(N) ->
+    Mod = dict(N),
+    Forms = [{attribute, 1, module, Mod},
+             {attribute, 2, compile, [export_all]},
+             {function, 3, id, 0,
+              [{clause, 4, [], [], [{integer, 4, N}]}]}],
+    {ok, Mod, Bin, []} = compile:forms(Forms, [return]),
+    {module, Mod} = code:load_binary(Mod, Mod, Bin),
+    N = Mod:id().
 
 %% server_closed/3
 