de853c60333c6e95379b69ee81bd5fff1dca95de
Björn Gustavsson <bjorn@erlang.org>
2013-08-16 14:04:45 +0200

    Teach erlc to handle UTF-8 file names
    The 'erlc' program passes options to the 'erl' program using
    the '-s' option. The '-s' option causes all options to be converted
    to atoms, which implies that UTF-8 file names may not be given on
    the command line.
    
    We could solve just the UTF-8 problem by using '-run' and change
    the erl_compile module to expect strings instead of atoms, but since
    that is an incompatible change, we should take the opportunity to
    make more incompatible changes while we are at it.
    
    Specifically, when 'erlc' was first written, there was no way to pass
    command line arguments starting with '-' to Erlang, so 'erlc' did all
    parsing of arguments and translated options to atoms starting with a
    '@' character (for example, -I was translated to @i). Since then,
    the '-extra' option has been introduced which allows us to pass
    anything to Erlang at the end of the command line.
    
    Therefore, while at it, do the minimum of necessary command line
    parsing in the 'erlc' program (e.g. the '-smp' option), passing the
    command line essentially unchanged to 'erl' using the '-extra' option,
    and rewrite the option parsing in Erlang.

--- erts/etc/common/erlc.c
+++ erts/etc/common/erlc.c
@@ -60,7 +60,6 @@ static int eargc;		/* Number of arguments in eargv. */
 #define PUSH2(s, t) PUSH(s); PUSH(t)
 #define PUSH3(s, t, u) PUSH2(s, t); PUSH(u)
 
-static char* output_type = NULL; /* Type of output file. */
 #ifdef __WIN32__
 static int pause_after_execution = 0;
 #endif
@@ -71,7 +70,6 @@ static int pause_after_execution = 0;
 
 static char* process_opt(int* pArgc, char*** pArgv, int offset);
 static void error(char* format, ...);
-static void usage(void);
 static char* emalloc(size_t size);
 static char* strsave(char* string);
 static void push_words(char* src);
@@ -218,6 +216,7 @@ int main(int argc, char** argv)
     PUSH2("-mode", "minimal");
     PUSH2("-boot", "start_clean");
     PUSH3("-s", "erl_compile", "compile_cmdline");
+    PUSH("-extra");
 
     /*
      * Push standard arguments to Erlang.
@@ -227,154 +226,31 @@ int main(int argc, char** argv)
      * Parse all command line switches.
      */
 
-    while (argc > 1 && (argv[1][0] == '-' || argv[1][0] == '+')) {
+    while (argc > 1) {
 
 	/*
 	 * Options starting with '+' are passed on to Erlang.
 	 */
 
-	if (argv[1][0] == '+') {
-	    PUSH2("@option", argv[1]+1);
-	} else {
-	    /*
-	     * Interpret options starting with '-'.
-	     */
-	    
+	switch (argv[1][0]) {
+	case '+':
+	    PUSH(argv[1]);
+	    break;
+	case '-':
 	    switch (argv[1][1]) {
-	    case 'b':
-		output_type = process_opt(&argc, &argv, 0);
-		PUSH2("@output_type", output_type);
-		break;
-	    case 'c':		/* Allowed for compatibility with 'erl'. */
-		if (strcmp(argv[1], "-compile") != 0)
-		    goto error;
-		break;
 	    case 'd':
-		debug = 1;
-		break;
-	    case 'D':
-		{
-		    char* def = process_opt(&argc, &argv, 0);
-		    char* equals;
-		    
-		    def = strsave(def);	/* Do not clobber original. */
-		    if ((equals = strchr(def, '=')) == NULL) {
-			PUSH2("@d", def);
-		    } else {
-			*equals = '\0';
-			equals++;
-			PUSH3("@dv", def, equals);
-		    }
+		if (argv[1][2] == '\0') {
+		    debug = 1;
+		} else {
+		    PUSH(argv[1]);
 		}
-		break;
-	    case 'I':
-		PUSH2("@i", process_opt(&argc, &argv, 0));
-		break;
-	    case 'M':
-		{
-		    char *buf, *key, *val;
-		    size_t buf_len;
-
-		    if (argv[1][2] == '\0') { /* -M */
-			/* Push the following options:
-			 *   o  'makedep'
-			 *   o  {makedep_output, standard_io}
-			 */
-			buf = strsave("makedep");
-			PUSH2("@option", buf);
-
-			key = "makedep_output";
-			val = "standard_io";
-			buf_len = 1 + strlen(key) + 1 + strlen(val) + 1 + 1;
-			buf = emalloc(buf_len);
-			snprintf(buf, buf_len, "{%s,%s}", key, val);
-			PUSH2("@option", buf);
-		    } else if (argv[1][3] == '\0') {
-			switch(argv[1][2]) {
-			case 'D': /* -MD */
-			    /* Push the following options:
-			     *   o  'makedep'
-			     */
-			    buf = strsave("makedep");
-			    PUSH2("@option", buf);
-			    break;
-			case 'F': /* -MF <file> */
-			    /* Push the following options:
-			     *   o  'makedep'
-			     *   o  {makedep_output, <file>}
-			     */
-			    buf = strsave("makedep");
-			    PUSH2("@option", buf);
-
-			    key = "makedep_output";
-			    val = process_opt(&argc, &argv, 1);
-			    buf_len = 1 + strlen(key) + 2 + strlen(val) + 2 + 1;
-			    buf = emalloc(buf_len);
-			    snprintf(buf, buf_len, "{%s,\"%s\"}", key, val);
-			    PUSH2("@option", buf);
-			    break;
-			case 'T': /* -MT <target> */
-			    /* Push the following options:
-			     *   o  {makedep_target, <target>}
-			     */
-			    key = "makedep_target";
-			    val = process_opt(&argc, &argv, 1);
-			    buf_len = 1 + strlen(key) + 2 + strlen(val) + 2 + 1;
-			    buf = emalloc(buf_len);
-			    snprintf(buf, buf_len, "{%s,\"%s\"}", key, val);
-			    PUSH2("@option", buf);
-			    break;
-			case 'Q': /* -MQ <target> */
-			    /* Push the following options:
-			     *   o  {makedep_target, <target>}
-			     *   o  makedep_quote_target
-			     */
-			    key = "makedep_target";
-			    val = process_opt(&argc, &argv, 1);
-			    buf_len = 1 + strlen(key) + 2 + strlen(val) + 2 + 1;
-			    buf = emalloc(buf_len);
-			    snprintf(buf, buf_len, "{%s,\"%s\"}", key, val);
-			    PUSH2("@option", buf);
-
-			    buf = strsave("makedep_quote_target");
-			    PUSH2("@option", buf);
-			    break;
-			case 'G': /* -MG */
-			    /* Push the following options:
-			     *   o  makedep_add_missing
-			     */
-			    buf = strsave("makedep_add_missing");
-			    PUSH2("@option", buf);
-			    break;
-			case 'P': /* -MP */
-			    /* Push the following options:
-			     *   o  makedep_phony
-			     */
-			    buf = strsave("makedep_phony");
-			    PUSH2("@option", buf);
-			    break;
-			default:
-			    goto error;
-			}
-		    }
-		}
-		break;
-	    case 'o':
-		PUSH2("@outdir", process_opt(&argc, &argv, 0));
-		break;
-	    case 'O':
-		PUSH("@optimize");
-		if (argv[1][2] == '\0')
-		    PUSH("1");
-		else
-		    PUSH(argv[1]+2);
 		break;
 	    case 'p':
 		{
 		    int c = argv[1][2];
 		    
 		    if (c != 'a' && c != 'z') {
-			goto error;
+			PUSH(argv[1]);
 #ifdef __WIN32__
 		    } else if (strcmp(argv[1], "-pause") == 0) {
 			pause_after_execution = 1;
@@ -395,78 +271,18 @@ int main(int argc, char** argv)
 		if (strcmp(argv[1], "-smp") == 0) {
 		    UNSHIFT(argv[1]);
 		} else {
-		    goto error;
+		    PUSH(argv[1]);
 		}
 		break;
-	    case 'v':		/* Verbose. */
-		PUSH2("@verbose", "true");
-		break;
-	    case 'V':
-		/** XXX Version perhaps, but of what? **/
-		break;
-	    case 'W':		/* Enable warnings. */
-		if (strcmp(argv[1]+2, "all") == 0) {
-		    PUSH2("@warn", "999");
-		} else if (strcmp(argv[1]+2, "error") == 0) {
-		    PUSH2("@option", "warnings_as_errors");
-		} else if (isdigit((int)argv[1][2])) {
-		    PUSH2("@warn", argv[1]+2);
-		} else {
-		    PUSH2("@warn", "1");
-		}
-		break;
-	    case 'E':
-	    case 'S':
-	    case 'P':
-		{
-		    char* buf;
-
-		    /* 
-		     * From the given upper-case letter, construct
-		     * a quoted atom.  This is a convenience for the
-		     * Erlang compiler, to avoid fighting with the shell's
-		     * quoting.
-		     */
-
-		    buf = emalloc(4);
-		    buf[0] = '\'';
-		    buf[1] = argv[1][1];
-		    buf[2] = '\'';
-		    buf[3] = '\0';
-
-		    PUSH2("@option", buf);
-		}
-		break;
-
-	    case '-':
-		goto no_more_options;
-
 	    default:
-	    error:
-		usage();
+		PUSH(argv[1]);
 		break;
 	    }
+	    break;
+	default:
+	    PUSH(argv[1]);
+	    break;
 	}
-	argc--, argv++;
-    }
-
- no_more_options:
-
-    if (argc <= 1) {
-	/*
-	 * To avoid starting an Erlang system unless absolutely needed
-	 * exit if no files were specified on the command line.
-	 */
-	exit(0);
-    }
-
-    /*
-     * The rest of the command line must be filenames.  Simply push them.
-     */
-
-    PUSH("@files");
-    while (argc > 1) {
-	PUSH(argv[1]);
 	argc--, argv++;
     }
 
@@ -646,53 +462,6 @@ run_erlang(char* progname, char** argv)
     error("Error %d executing \'%s\'.", errno, progname);
     return 2;
 #endif
-}
-
-static void
-usage(void)
-{
-    static struct {
-	char* name;
-	char* desc;
-    } options[] = {
-	{"-b type", "type of output file (e.g. jam or beam)"},
-	{"-d", "turn on debugging of erlc itself"},
-	{"-Dname", "define name"},
-	{"-Dname=value", "define name to have value"},
-	{"-help", "shows this help text"},
-	{"-I path", "where to search for include files"},
-	{"-M", "generate a rule for make(1) describing the dependencies"},
-	{"-MF file", "write the dependencies to 'file'"},
-	{"-MT target", "change the target of the rule emitted by dependency "
-		"generation"},
-	{"-MQ target", "same as -MT but quote characters special to make(1)"},
-	{"-MG", "consider missing headers as generated files and add them to "
-		"the dependencies"},
-	{"-MP", "add a phony target for each dependency"},
-	{"-MD", "same as -M -MT file (with default 'file')"},
-	{"-o name", "name output directory or file"},
-	{"-pa path", "add path to the front of Erlang's code path"},
-	{"-pz path", "add path to the end of Erlang's code path"},
-	{"-smp", "compile using SMP emulator"},
-	{"-v", "verbose compiler output"},
-	{"-Werror", "make all warnings into errors"},
-	{"-W0", "disable warnings"},
-	{"-Wnumber", "set warning level to number"},
-	{"-Wall", "enable all warnings"},
-	{"-W", "enable warnings (default; same as -W1)"},
-	{"-E", "generate listing of expanded code (Erlang compiler)"},
-	{"-S", "generate assembly listing (Erlang compiler)"},
-	{"-P", "generate listing of preprocessed code (Erlang compiler)"},
-	{"+term", "pass the Erlang term unchanged to the compiler"},
-    };
-    int i;
-
-    fprintf(stderr, "Usage:\terlc [options] file.ext ...\n");
-    fprintf(stderr, "Options:\n");
-    for (i = 0; i < sizeof(options)/sizeof(options[0]); i++) {
-	fprintf(stderr, "%-14s %s\n", options[i].name, options[i].desc);
-    }
-    exit(1);
 }
 
 static void
