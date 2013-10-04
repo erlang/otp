b179331cfb49ce67cda3e66c91f42fd29ed50116
Leo Correa <lcorr005@gmail.com>
2013-10-04 16:40:09 -0400

    Typo fix ambigous -> ambiguous

--- lib/compiler/src/beam_validator.erl
+++ lib/compiler/src/beam_validator.erl
@@ -530,7 +530,7 @@ valfun_2(I, #vst{current=#st{ct=[[Fail]|_]}}=Vst) when is_integer(Fail) ->
     %% Update branched state
     valfun_3(I, branch_state(Fail, Vst));
 valfun_2(_, _) ->
-    error(ambigous_catch_try_state).
+    error(ambiguous_catch_try_state).
 
 %% Handle the remaining floating point instructions here.
 %% Floating point.
