1101fcb3e61634f1be92e1b9ba9fad5a11b8554a
Björn Gustavsson <bjorn@erlang.org>
2014-01-16 12:16:39 +0100

    Add the 'rle' zstrategy

--- erts/preloaded/src/zlib.erl
+++ erts/preloaded/src/zlib.erl
@@ -47,6 +47,7 @@
 %% compresssion strategy
 -define(Z_FILTERED,            1).
 -define(Z_HUFFMAN_ONLY,        2).
+-define(Z_RLE,                 3).
 -define(Z_DEFAULT_STRATEGY,    0).
 
 %% deflate compression method
@@ -125,7 +126,7 @@
 -type zmethod()     :: 'deflated'.
 -type zwindowbits() :: -15..-9 | 9..47.
 -type zmemlevel()   :: 1..9.
--type zstrategy()   :: 'default' | 'filtered' | 'huffman_only'.
+-type zstrategy()   :: 'default' | 'filtered' | 'huffman_only' | 'rle'.
 
 %%------------------------------------------------------------------------
 
@@ -486,6 +487,7 @@ arg_level(_) -> erlang:error(badarg).
      
 arg_strategy(filtered) ->     ?Z_FILTERED;
 arg_strategy(huffman_only) -> ?Z_HUFFMAN_ONLY;
+arg_strategy(rle) -> ?Z_RLE;
 arg_strategy(default) ->      ?Z_DEFAULT_STRATEGY;
 arg_strategy(_) -> erlang:error(badarg).
 
