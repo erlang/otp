erlang/otp

commit 8a147a73651713efebb9ac973f618a6d66685eca

Don't make gzio.c dependent on the zutil.h header file
gzio.c is our own replacement for zlib's gzopen() etc
(based on a version of gzio.c that was included in an old version
of zlib).

Unfortunately, gzio.c still depends on the *internal* zlib header file
zutil.h which is not supposed to be used outside of the zlib source
code. The dependencies are the use of the gzFile typedef and the
F_OPEN() macro.

Instead of gzFile, define and use our own ErtsGzFile.

To get rid of the F_OPEN() macro, call open() of _wfopen() directly.

erts/emulator/drivers/common/gzio.c
@@ -77,7 +77,7 @@ typedef struct gz_stream {
 					    *  this structure. */
 } gz_stream;
 
-local gzFile gz_open      OF((const char *path, const char *mode));
+local ErtsGzFile gz_open      OF((const char *path, const char *mode));
 local int    get_byte     OF((gz_stream *s));
 local void   check_header OF((gz_stream *s));
 local int    destroy      OF((gz_stream *s));
@@ -144,7 +144,7 @@ local uLong  getLong      OF((gz_stream *s));
    can be checked to distinguish the two cases (if errno is zero, the
    zlib error is Z_MEM_ERROR).
 */
-local gzFile gz_open (path, mode)
+local ErtsGzFile gz_open (path, mode)
     const char *path;
     const char *mode;
 {
@@ -179,7 +179,7 @@ local gzFile gz_open (path, mode)
 
     s->path = (char*)ALLOC(FILENAME_BYTELEN(path)+FILENAME_CHARSIZE);
     if (s->path == NULL) {
-        return s->destroy(s), (gzFile)Z_NULL;
+        return s->destroy(s), (ErtsGzFile)Z_NULL;
     }
     FILENAME_COPY(s->path, path); /* do this early for debugging */
 
@@ -197,7 +197,7 @@ local gzFile gz_open (path, mode)
     } while (*p++ && m < fmode + sizeof(fmode) - 1);
     *m = '\0';
     if (s->mode == '\0')
-	return s->destroy(s), (gzFile)Z_NULL;
+	return s->destroy(s), (ErtsGzFile)Z_NULL;
     
     if (s->mode == 'w') {
         err = deflateInit2(&(s->stream), level,
@@ -207,7 +207,7 @@ local gzFile gz_open (path, mode)
         s->stream.next_out = s->outbuf = (Byte*)ALLOC(Z_BUFSIZE);
 
         if (err != Z_OK || s->outbuf == Z_NULL) {
-            return s->destroy(s), (gzFile)Z_NULL;
+            return s->destroy(s), (ErtsGzFile)Z_NULL;
         }
     } else {
 	/*
@@ -221,7 +221,7 @@ local gzFile gz_open (path, mode)
         s->stream.next_in  = s->inbuf = (Byte*)ALLOC(Z_BUFSIZE);
 
         if (err != Z_OK || s->inbuf == Z_NULL) {
-            return s->destroy(s), (gzFile)Z_NULL;
+            return s->destroy(s), (ErtsGzFile)Z_NULL;
         }
     }
     s->stream.avail_out = Z_BUFSIZE;
@@ -229,17 +229,16 @@ local gzFile gz_open (path, mode)
     errno = 0;
 #if defined(FILENAMES_16BIT)
     {
-	char wfmode[160];
-	int i=0,j;
-	for(j=0;fmode[j] != '\0';++j) {
-	    wfmode[i++]=fmode[j];
-	    wfmode[i++]='\0';
+	WCHAR wfmode[80];
+	int i = 0;
+	int j;
+	for(j = 0; fmode[j] != '\0'; ++j) {
+	    wfmode[i++] = (WCHAR) fmode[j];
 	}
-	wfmode[i++] = '\0';
-	wfmode[i++] = '\0';
-	s->file = F_OPEN(path, wfmode);
+	wfmode[i++] = L'\0';
+	s->file = _wfopen((WCHAR *)path, wfmode);
 	if (s->file == NULL) {
-	    return s->destroy(s), (gzFile)Z_NULL;
+	    return s->destroy(s), (ErtsGzFile)Z_NULL;
 	}
     }
 #elif defined(UNIX)
@@ -249,18 +248,18 @@ local gzFile gz_open (path, mode)
 	s->file = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0666);
     }
     if (s->file == -1) {
-        return s->destroy(s), (gzFile)Z_NULL;
+        return s->destroy(s), (ErtsGzFile)Z_NULL;
     }
 #else
-    s->file = F_OPEN(path, fmode);
+    s->file = fopen(path, fmode);
     if (s->file == NULL) {
-        return s->destroy(s), (gzFile)Z_NULL;
+        return s->destroy(s), (ErtsGzFile)Z_NULL;
     }
 #endif
     if (s->mode == 'r') {
 	check_header(s); /* skip the .gz header */
     }
-    return (gzFile)s;
+    return (ErtsGzFile)s;
 }
 
 /* ===========================================================================
@@ -296,7 +295,7 @@ local int gz_rewind (gz_stream *s)
 /* ===========================================================================
      Opens a gzip (.gz) file for reading or writing.
 */
-gzFile erts_gzopen (path, mode)
+ErtsGzFile erts_gzopen (path, mode)
     const char *path;
     const char *mode;
 {
@@ -447,7 +446,7 @@ local int destroy (s)
    gzread returns the number of bytes actually read (0 for end of file).
 */
 int
-erts_gzread(gzFile file, voidp buf, unsigned len)
+erts_gzread(ErtsGzFile file, voidp buf, unsigned len)
 {
     gz_stream *s = (gz_stream*)file;
     Bytef *start = buf; /* starting point for crc computation */
@@ -557,7 +556,7 @@ erts_gzread(gzFile file, voidp buf, unsigned len)
    gzwrite returns the number of bytes actually written (0 in case of error).
 */
 int
-erts_gzwrite(gzFile file, voidp buf, unsigned len)
+erts_gzwrite(ErtsGzFile file, voidp buf, unsigned len)
 {
     gz_stream *s = (gz_stream*)file;
 
@@ -593,7 +592,7 @@ erts_gzwrite(gzFile file, voidp buf, unsigned len)
  */
 
 int
-erts_gzseek(gzFile file, int offset, int whence)
+erts_gzseek(ErtsGzFile file, int offset, int whence)
 {
     int pos;
     gz_stream* s = (gz_stream *) file;
@@ -655,7 +654,7 @@ erts_gzseek(gzFile file, int offset, int whence)
    degrade compression.
 */
 int
-erts_gzflush(gzFile file, int flush)
+erts_gzflush(ErtsGzFile file, int flush)
 {
     uInt len;
     int done = 0;
@@ -714,7 +713,7 @@ local uLong getLong (s)
    and deallocates all the (de)compression state.
 */
 int
-erts_gzclose(gzFile file)
+erts_gzclose(ErtsGzFile file)
 {
     int err;
     gz_stream *s = (gz_stream*)file;
@@ -723,9 +722,9 @@ erts_gzclose(gzFile file)
 
     if (s->mode == 'w') {
         err = erts_gzflush (file, Z_FINISH);
-        if (err != Z_OK) return s->destroy(file);
+        if (err != Z_OK) return s->destroy(s);
     }
-    return s->destroy(file);
+    return s->destroy(s);
 }
 
 

erts/emulator/drivers/common/efile_drv.c
@@ -111,7 +111,6 @@
 #include "erl_driver.h"
 #include "erl_efile.h"
 #include "erl_threads.h"
-#include "zlib.h"
 #include "gzio.h"
 #include "dtrace-wrapper.h" 
 #include <ctype.h>
@@ -818,7 +817,7 @@ file_start(ErlDrvPort port, char* command)
 
 static void do_close(int flags, SWord fd) {
     if (flags & EFILE_COMPRESSED) {
-	erts_gzclose((gzFile)(fd));
+	erts_gzclose((ErtsGzFile)(fd));
     } else {
 	efile_closefile((int) fd);
     }
@@ -1136,7 +1135,7 @@ static void invoke_read(void *data)
     }
     read_size = size;
     if (d->flags & EFILE_COMPRESSED) {
-	read_size = erts_gzread((gzFile)d->fd, 
+	read_size = erts_gzread((ErtsGzFile)d->fd,
 				d->c.read.binp->orig_bytes + d->c.read.bin_offset,
 				size);
 	status = (read_size != (size_t) -1);
@@ -1209,7 +1208,7 @@ static void invoke_read_line(void *data)
 	    size = need - d->c.read_line.read_size;
 	}
 	if (d->flags & EFILE_COMPRESSED) {
-	    read_size = erts_gzread((gzFile)d->fd, 
+	    read_size = erts_gzread((ErtsGzFile)d->fd,
 				    d->c.read_line.binp->orig_bytes + 
 				    d->c.read_line.read_offset + d->c.read_line.read_size,
 				    size);
@@ -1250,7 +1249,7 @@ static void invoke_read_line(void *data)
 		    d->c.read_line.read_size -= too_much;
 		    ASSERT(d->c.read_line.read_size >= 0);
 		    if (d->flags & EFILE_COMPRESSED) {
-			Sint64 location = erts_gzseek((gzFile)d->fd, 
+			Sint64 location = erts_gzseek((ErtsGzFile)d->fd,
 						      -((Sint64) too_much), EFILE_SEEK_CUR);
 			if (location == -1) {
 			    d->result_ok = 0;
@@ -1535,7 +1534,7 @@ static void invoke_writev(void *data) {
 		     */
 		    errno = EINVAL; 
 		    if (! (status = 
-			   erts_gzwrite((gzFile)d->fd, 
+			   erts_gzwrite((ErtsGzFile)d->fd,
 					iov[i].iov_base,
 					iov[i].iov_len)) == iov[i].iov_len) {
 			d->errInfo.posix_errno =
@@ -1797,7 +1796,7 @@ static void invoke_lseek(void *data)
 	    d->errInfo.posix_errno = EINVAL;
 	    status = 0;
 	} else {
-	    d->c.lseek.location = erts_gzseek((gzFile)d->fd, 
+	    d->c.lseek.location = erts_gzseek((ErtsGzFile)d->fd,
 					      offset, d->c.lseek.origin);
 	    if (d->c.lseek.location == -1) {
 		d->errInfo.posix_errno = errno;
@@ -1885,7 +1884,7 @@ static void invoke_open(void *data)
 	    if (status || (d->errInfo.posix_errno != EISDIR)) {
 		mode = (d->flags & EFILE_MODE_READ) ? "rb" : "wb";
 		d->fd = (SWord) erts_gzopen(d->b, mode);
-		if ((gzFile)d->fd) {
+		if ((ErtsGzFile)d->fd) {
 		    status = 1;
 		} else {
 		    if (errno == 0) {

erts/emulator/drivers/common/gzio.h
@@ -17,11 +17,15 @@
  * %CopyrightEnd%
  */
 
-gzFile erts_gzopen (const char *path, const char *mode);
-int erts_gzread(gzFile file, voidp buf, unsigned len);
-int erts_gzwrite(gzFile file, voidp buf, unsigned len);
-int erts_gzseek(gzFile, int, int);
-int erts_gzflush(gzFile file, int flush);
-int erts_gzclose(gzFile file);
+#include "zlib.h"
+
+typedef struct erts_gzFile* ErtsGzFile;
+
+ErtsGzFile erts_gzopen (const char *path, const char *mode);
+int erts_gzread(ErtsGzFile file, voidp buf, unsigned len);
+int erts_gzwrite(ErtsGzFile file, voidp buf, unsigned len);
+int erts_gzseek(ErtsGzFile, int, int);
+int erts_gzflush(ErtsGzFile file, int flush);
+int erts_gzclose(ErtsGzFile file);
 ErlDrvBinary* erts_gzinflate_buffer(char*, uLong);
 ErlDrvBinary* erts_gzdeflate_buffer(char*, uLong);

erts/emulator/drivers/common/gzio_zutil.h
@@ -23,12 +23,6 @@
  * that may change or not exist at all.
  */
 
-#ifndef HAVE_LIBZ
-/* Use our "real" copy of zutil.h if we don't use shared zlib */
-#include "zutil.h"
-
-#else /* HAVE_LIBZ: Shared zlib is used */
-
 #define local static
 #define DEF_MEM_LEVEL 8
 #define zmemcpy sys_memcpy
@@ -76,7 +70,4 @@
 #ifndef OS_CODE
 #  define OS_CODE  0x03  /* assume Unix */
 #endif
-
-
-#endif /* HAVE_LIBZ */
 

commit 21f2d57e5f7919d468fe89e38c52f978e15c1dfc

gzio.c: Remove unnecessary usage of the OF() macro
Daniel Goertzen reported that commit 8a147a7365 broke building
of Erlang/OTP on Gentoo Linux because the macro OF() was missing.
Apparently, on Gentoo the OF() macro in zconf.h has been renamed
to _Z_OF() (to avoid polluting the global namespace).

Don't use the OF() macro in gzio.c since it no longer serves any
useful purpose (it provided compatibility with pre-ANSI/ISO C
compilers, but the rest of Erlang/OTP requires an ANSI/ISO C
compiler anyway).

erts/emulator/drivers/common/gzio.c
@@ -73,15 +73,15 @@ typedef struct gz_stream {
     int      transparent; /* 1 if input file is not a .gz file */
     char     mode;    /* 'w' or 'r' */
     int      position; /* Position (for seek) */
-    int (*destroy)OF((struct gz_stream*)); /* Function to destroy
+    int (*destroy)(struct gz_stream*); /* Function to destroy
 					    *  this structure. */
 } gz_stream;
 
-local ErtsGzFile gz_open      OF((const char *path, const char *mode));
-local int    get_byte     OF((gz_stream *s));
-local void   check_header OF((gz_stream *s));
-local int    destroy      OF((gz_stream *s));
-local uLong  getLong      OF((gz_stream *s));
+local ErtsGzFile gz_open      (const char *path, const char *mode);
+local int    get_byte     (gz_stream *s);
+local void   check_header (gz_stream *s);
+local int    destroy      (gz_stream *s);
+local uLong  getLong      (gz_stream *s);
 
 #ifdef UNIX
 /*