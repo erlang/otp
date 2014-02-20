5605ed452429d3ec037e86708da357ffff46d5bc
Hans Bolinder <hasse@erlang.org>
2014-02-20 13:28:18 +0100

    Correct the contract of inet:ntoa/1
    Thanks to Max Treskin.

--- lib/kernel/src/inet.erl
+++ lib/kernel/src/inet.erl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 1997-2013. All Rights Reserved.
+%% Copyright Ericsson AB 1997-2014. All Rights Reserved.
 %%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
@@ -597,8 +597,7 @@ getservbyname(Name, Protocol) when is_atom(Name) ->
 	Error -> Error
     end.
 
--spec ntoa(IpAddress) ->
-	{ok, Address} | {error, einval} when
+-spec ntoa(IpAddress) -> Address | {error, einval} when
       Address :: string(),
       IpAddress :: ip_address().
 ntoa(Addr) ->
