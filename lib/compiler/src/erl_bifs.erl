2890335ec4ecee6888d184a6dc73c7fc227c0566
Björn-Egil Dahlberg <egil@erlang.org>
2014-03-06 19:07:10 +0100

    compiler: Guard BIF is_map/1 is pure

--- lib/compiler/src/erl_bifs.erl
+++ lib/compiler/src/erl_bifs.erl
@@ -91,6 +91,7 @@ is_pure(erlang, is_float, 1) -> true;
 is_pure(erlang, is_function, 1) -> true;
 is_pure(erlang, is_integer, 1) -> true;
 is_pure(erlang, is_list, 1) -> true;
+is_pure(erlang, is_map, 1) -> true;
 is_pure(erlang, is_number, 1) -> true;
 is_pure(erlang, is_pid, 1) -> true;
 is_pure(erlang, is_port, 1) -> true;
