a7d0e35b834f9b0d9538c75d06dfe3d698a83ded
Dan Gudmundsson <dgud@erlang.org>
2014-03-07 15:54:52 +0100

    observer: Add simple test and add debug hooks in app
    Add hooks in observer so that we can do some basic testing on it

--- lib/observer/src/observer.erl
+++ lib/observer/src/observer.erl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 2011. All Rights Reserved.
+%% Copyright Ericsson AB 2011-2014. All Rights Reserved.
 %%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
@@ -18,8 +18,11 @@
 
 -module(observer).
 
--export([start/0]).
+-export([start/0, stop/0]).
 
 
 start() ->
     observer_wx:start().
+
+stop() ->
+    observer_wx:stop().
