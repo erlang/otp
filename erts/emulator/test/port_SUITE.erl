d59a203a4e79c8ef3d4470e0039e1d4dc99cbce5
Sverker Eriksson <sverker@erlang.org>
2014-03-26 21:33:31 +0100

    erts: Reduce runtime of test port_SUITE:close_deaf_port
    as it fails with timeout sometimes.

--- erts/emulator/test/port_SUITE.erl
+++ erts/emulator/test/port_SUITE.erl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 1997-2013. All Rights Reserved.
+%% Copyright Ericsson AB 1997-2014. All Rights Reserved.
 %%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
@@ -2318,7 +2318,7 @@ close_deaf_port(Config) when is_list(Config) ->
     test_server:timetrap_cancel(Dog),
     Res.
 
-close_deaf_port_1(1000, _) ->
+close_deaf_port_1(200, _) ->
     ok;
 close_deaf_port_1(N, Cmd) ->
     Timeout = integer_to_list(random:uniform(5*1000)),
