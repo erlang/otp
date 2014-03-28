918f0a26da2ad559bb674e19fce952c2564b9b45
Kostis Sagonas <kostis@it.uu.se>
2014-03-28 17:47:10 +0100

    Reinstate native code compilation of cerl and erl_types

--- lib/dialyzer/src/dialyzer_cl.erl
+++ lib/dialyzer/src/dialyzer_cl.erl
@@ -504,9 +504,7 @@ hipe_compile(Files, #options{erlang_mode = ErlangMode} = Options) ->
 	_ ->
 	  Mods = [lists, dict, digraph, digraph_utils, ets,
 		  gb_sets, gb_trees, ordsets, sets, sofs,
-		  %cerl,      % uses maps instructions
-		  %erl_types, % uses maps instructions
-		  cerl_trees, erl_bif_types,
+		  cerl, erl_types, cerl_trees, erl_bif_types,
 		  dialyzer_analysis_callgraph, dialyzer, dialyzer_behaviours,
 		  dialyzer_codeserver, dialyzer_contracts,
 		  dialyzer_coordinator, dialyzer_dataflow, dialyzer_dep,
