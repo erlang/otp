2fb83e99c6db6fa9537222b20af5f678c75dd584
Dan Gudmundsson <dgud@erlang.org>
2014-04-16 16:30:42 +0200

    observer: Fix range check
    Avoids wxWidgets assert if the table is empty

--- lib/observer/src/observer_tv_table.erl
+++ lib/observer/src/observer_tv_table.erl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 2011-2013. All Rights Reserved.
+%% Copyright Ericsson AB 2011-2014. All Rights Reserved.
 %%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
@@ -409,7 +409,7 @@ handle_info({refresh, Min, Min}, State = #state{grid=Grid}) ->
     wxListCtrl:refreshItem(Grid, Min), %% Avoid assert in wx below if Max is 0
     {noreply, State};
 handle_info({refresh, Min, Max}, State = #state{grid=Grid}) ->
-    wxListCtrl:refreshItems(Grid, Min, Max),
+    Max > 0 andalso wxListCtrl:refreshItems(Grid, Min, Max),
     {noreply, State};
 
 handle_info(refresh_interval, State = #state{pid=Pid}) ->
