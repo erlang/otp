erlang/otp

commit adb5dc0090bc419e2c4c1250653badbddeb6263b

Add ETHR_FORCE_INLINE define to hide compiler specific directives
Some win32 specific code does not compile with gcc (mingw-w64) since
'__forceinline' is not supported by gcc. This can be avoided by
defining a new macro ETHR_FORCE_INLINE similar to ETHR_INLINE.

erts/include/internal/ethread.h
@@ -54,13 +54,16 @@
 #undef ETHR_INLINE
 #if defined(__GNUC__)
 #  define ETHR_INLINE __inline__
+#  define ETHR_FORCE_INLINE __inline__ __attribute__((__always_inline__))
 #elif defined(__WIN32__)
 #  define ETHR_INLINE __forceinline
+#  define ETHR_FORCE_INLINE __forceinline
 #endif
 #if defined(ETHR_DEBUG) || !defined(ETHR_INLINE) || ETHR_XCHK \
     || (defined(__GNUC__) && defined(ERTS_MIXED_CYGWIN_VC))
 #  undef ETHR_INLINE
 #  define ETHR_INLINE 
+#  define ETHR_FORCE_INLINE
 #  undef ETHR_TRY_INLINE_FUNCS
 #endif
 

erts/lib_src/common/erl_misc_utils.c
@@ -25,6 +25,7 @@
 #  include <windows.h>
 #endif
 
+#include "ethread.h"
 #include "erl_misc_utils.h"
 
 #if defined(__WIN32__)
@@ -191,7 +192,7 @@ struct erts_cpu_info_t_ {
 
 #if defined(__WIN32__)
 
-static __forceinline int
+static ETHR_FORCE_INLINE int
 get_proc_affinity(erts_cpu_info_t *cpuinfo, cpu_set_t *cpuset)
 {
     DWORD_PTR pamask;
@@ -206,7 +207,7 @@ get_proc_affinity(erts_cpu_info_t *cpuinfo, cpu_set_t *cpuset)
     }
 }
 
-static __forceinline int
+static ETHR_FORCE_INLINE int
 set_thr_affinity(cpu_set_t *set)
 {
     if (*set == (cpu_set_t) 0)
@@ -1157,7 +1158,7 @@ read_topology(erts_cpu_info_t *cpuinfo)
 #define ERTS_MU_RELATION_CACHE                2 /* RelationCache */
 #define ERTS_MU_RELATION_PROCESSOR_PACKAGE    3 /* RelationProcessorPackage */
 
-static __forceinline int
+static ETHR_FORCE_INLINE int
 rel_cmp_val(int r)
 {
     switch (r) {

erts/include/internal/win/ethr_membar.h
@@ -63,13 +63,13 @@ do {							\
 #pragma intrinsic(_mm_sfence)
 #pragma intrinsic(_mm_lfence)
 
-static __forceinline void
+static ETHR_FORCE_INLINE void
 ethr_cfence__(void)
 {
     _ReadWriteBarrier();
 }
 
-static __forceinline void
+static ETHR_FORCE_INLINE void
 ethr_mfence__(void)
 {
 #if ETHR_SIZEOF_PTR == 4
@@ -80,7 +80,7 @@ ethr_mfence__(void)
 	_mm_mfence();
 }
 
-static __forceinline void
+static ETHR_FORCE_INLINE void
 ethr_sfence__(void)
 {
 #if ETHR_SIZEOF_PTR == 4
@@ -91,7 +91,7 @@ ethr_sfence__(void)
 	_mm_sfence();
 }
 
-static __forceinline void
+static ETHR_FORCE_INLINE void
 ethr_lfence__(void)
 {
 #if ETHR_SIZEOF_PTR == 4

erts/lib_src/common/ethr_mutex.c
@@ -1433,7 +1433,7 @@ void LeaveCriticalSection(CRITICAL_SECTION *cs)
 #define ETHR_CND_WAIT__ ((ethr_sint32_t) 0x11dead11)
 #define ETHR_CND_WAKEUP__ ((ethr_sint32_t) 0x11beef11)
 
-static __forceinline void
+static ETHR_FORCE_INLINE void
 cond_wakeup(ethr_ts_event *tse)
 {
     ETHR_ASSERT(ethr_atomic32_read(&tse->uaflgs) == ETHR_CND_WAIT__);
@@ -1574,7 +1574,7 @@ ethr_cond_wait(ethr_cond *cnd, ethr_mutex *mtx)
     return 0;
 }
 
-static __forceinline void
+static ETHR_FORCE_INLINE void
 posix_compliant_mtx_enqueue(ethr_mutex *mtx,
 			    ethr_ts_event *tse_start,
 			    ethr_ts_event *tse_end)
@@ -1614,7 +1614,7 @@ posix_compliant_mtx_enqueue(ethr_mutex *mtx,
     }
 }
 
-static __forceinline void
+static ETHR_FORCE_INLINE void
 enqueue_cond_wakeups(ethr_ts_event *queue, int posix_compliant)
 {
     if (queue) {

commit 50169c34511242987e6f2a047c79a0f7e1b7cfcc

erts: Separate ethread inlining from ethread.h
The commit adb5dc0090bc419e2c4c1250653badbddeb6263b (ETHR_FORCE_INLINE)
broke some platforms without adequate thread support.

erts/include/internal/ethread.h
@@ -31,6 +31,7 @@
 #endif
 
 #include <stdlib.h>
+#include "ethread_inline.h"
 #include "erl_errno.h"
 
 #if defined(DEBUG)
@@ -51,31 +52,6 @@
 #  endif
 #endif
 
-#if !defined(__GNUC__)
-#  define ETHR_AT_LEAST_GCC_VSN__(MAJ, MIN, PL) 0
-#elif !defined(__GNUC_MINOR__)
-#  define ETHR_AT_LEAST_GCC_VSN__(MAJ, MIN, PL) \
-  ((__GNUC__ << 24) >= (((MAJ) << 24) | ((MIN) << 12) | (PL)))
-#elif !defined(__GNUC_PATCHLEVEL__)
-#  define ETHR_AT_LEAST_GCC_VSN__(MAJ, MIN, PL) \
-  (((__GNUC__ << 24) | (__GNUC_MINOR__ << 12)) >= (((MAJ) << 24) | ((MIN) << 12) | (PL)))
-#else
-#  define ETHR_AT_LEAST_GCC_VSN__(MAJ, MIN, PL) \
-  (((__GNUC__ << 24) | (__GNUC_MINOR__ << 12) | __GNUC_PATCHLEVEL__) >= (((MAJ) << 24) | ((MIN) << 12) | (PL)))
-#endif
-
-#undef ETHR_INLINE
-#if defined(__GNUC__)
-#  define ETHR_INLINE __inline__
-#  if ETHR_AT_LEAST_GCC_VSN__(3, 1, 1)
-#    define ETHR_FORCE_INLINE __inline__ __attribute__((__always_inline__))
-#  else
-#    define ETHR_FORCE_INLINE __inline__
-#  endif
-#elif defined(__WIN32__)
-#  define ETHR_INLINE __forceinline
-#  define ETHR_FORCE_INLINE __forceinline
-#endif
 #if defined(ETHR_DEBUG) || !defined(ETHR_INLINE) || ETHR_XCHK \
     || (defined(__GNUC__) && defined(ERTS_MIXED_CYGWIN_VC))
 #  undef ETHR_INLINE

erts/include/internal/ethread_inline.h
@@ -1,0 +1,49 @@
+/*
+ * %CopyrightBegin%
+ *
+ * Copyright Ericsson AB 2004-2014. All Rights Reserved.
+ *
+ * The contents of this file are subject to the Erlang Public License,
+ * Version 1.1, (the "License"); you may not use this file except in
+ * compliance with the License. You should have received a copy of the
+ * Erlang Public License along with this software. If not, it can be
+ * retrieved online at http://www.erlang.org/.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * %CopyrightEnd%
+ */
+
+#ifndef ETHREAD_INLINE_H__
+#define ETHREAD_INLINE_H__
+
+#if !defined(__GNUC__)
+#  define ETHR_AT_LEAST_GCC_VSN__(MAJ, MIN, PL) 0
+#elif !defined(__GNUC_MINOR__)
+#  define ETHR_AT_LEAST_GCC_VSN__(MAJ, MIN, PL) \
+  ((__GNUC__ << 24) >= (((MAJ) << 24) | ((MIN) << 12) | (PL)))
+#elif !defined(__GNUC_PATCHLEVEL__)
+#  define ETHR_AT_LEAST_GCC_VSN__(MAJ, MIN, PL) \
+  (((__GNUC__ << 24) | (__GNUC_MINOR__ << 12)) >= (((MAJ) << 24) | ((MIN) << 12) | (PL)))
+#else
+#  define ETHR_AT_LEAST_GCC_VSN__(MAJ, MIN, PL) \
+  (((__GNUC__ << 24) | (__GNUC_MINOR__ << 12) | __GNUC_PATCHLEVEL__) >= (((MAJ) << 24) | ((MIN) << 12) | (PL)))
+#endif
+
+#undef ETHR_INLINE
+#if defined(__GNUC__)
+#  define ETHR_INLINE __inline__
+#  if ETHR_AT_LEAST_GCC_VSN__(3, 1, 1)
+#    define ETHR_FORCE_INLINE __inline__ __attribute__((__always_inline__))
+#  else
+#    define ETHR_FORCE_INLINE __inline__
+#  endif
+#elif defined(__WIN32__)
+#  define ETHR_INLINE __forceinline
+#  define ETHR_FORCE_INLINE __forceinline
+#endif
+
+#endif /* #ifndef ETHREAD_INLINE_H__ */

erts/lib_src/Makefile.in
@@ -465,6 +465,7 @@ RELEASE_LIBS=$(ERTS_LIBS)
 INTERNAL_RELEASE_INCLUDES= \
 	$(ERTS_INCL_INT)/README \
 	$(ERTS_INCL_INT)/ethread.h \
+	$(ERTS_INCL_INT)/ethread_inline.h \
 	$(ERTS_INCL_INT)/ethr_mutex.h \
 	$(ERTS_INCL_INT)/ethr_optimized_fallbacks.h \
 	$(ERTS_INCL_INT)/ethr_atomics.h \

erts/lib_src/common/erl_misc_utils.c
@@ -25,7 +25,7 @@
 #  include <windows.h>
 #endif
 
-#include "ethread.h"
+#include "ethread_inline.h"
 #include "erl_misc_utils.h"
 
 #if defined(__WIN32__)