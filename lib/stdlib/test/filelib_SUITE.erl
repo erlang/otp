erlang/otp

commit f11aabdc9fec593c31e6c4f3fa25c1707e9c35df

filelib:wildcard() should show broken symlinks
filelib:wildcard("broken_symlink") would return an empty list if
"broken_symlink" is a symlink that does not point to an existing
file.

lib/stdlib/src/filelib.erl
@@ -488,7 +488,7 @@ badpattern(Reason) ->
     error({badpattern,Reason}).
 
 eval_read_file_info(File, file) ->
-    file:read_file_info(File);
+    file:read_link_info(File);
 eval_read_file_info(File, erl_prim_loader) ->
     case erl_prim_loader:read_file_info(File) of
 	error -> {error, erl_prim_loader};

lib/stdlib/test/filelib_SUITE.erl
@@ -23,7 +23,7 @@
 	 init_per_group/2,end_per_group/2,
 	 init_per_testcase/2,end_per_testcase/2,
 	 wildcard_one/1,wildcard_two/1,wildcard_errors/1,
-	 fold_files/1,otp_5960/1,ensure_dir_eexist/1]).
+	 fold_files/1,otp_5960/1,ensure_dir_eexist/1,symlinks/1]).
 
 -import(lists, [foreach/2]).
 
@@ -43,7 +43,7 @@ suite() -> [{ct_hooks,[ts_install_cth]}].
 
 all() -> 
     [wildcard_one, wildcard_two, wildcard_errors,
-     fold_files, otp_5960, ensure_dir_eexist].
+     fold_files, otp_5960, ensure_dir_eexist, symlinks].
 
 groups() -> 
     [].
@@ -366,3 +366,39 @@ ensure_dir_eexist(Config) when is_list(Config) ->
     ?line {error, eexist} = filelib:ensure_dir(NeedFile),
     ?line {error, eexist} = filelib:ensure_dir(NeedFileB),
     ok.
+
+symlinks(Config) when is_list(Config) ->
+    PrivDir = ?config(priv_dir, Config),
+    Dir = filename:join(PrivDir, ?MODULE_STRING++"_symlinks"),
+    SubDir = filename:join(Dir, "sub"),
+    AFile = filename:join(SubDir, "a_file"),
+    Alias = filename:join(Dir, "symlink"),
+    ok = file:make_dir(Dir),
+    ok = file:make_dir(SubDir),
+    ok = file:write_file(AFile, "not that big\n"),
+    case file:make_symlink(AFile, Alias) of
+	{error, enotsup} ->
+	    {skip, "Links not supported on this platform"};
+	{error, eperm} ->
+	    {win32,_} = os:type(),
+	    {skip, "Windows user not privileged to create symlinks"};
+	ok ->
+	    ["sub","symlink"] =
+		basenames(Dir, filelib:wildcard(filename:join(Dir, "*"))),
+	    ["symlink"] =
+		basenames(Dir, filelib:wildcard(filename:join(Dir, "symlink"))),
+	    ok = file:delete(AFile),
+	    %% The symlink should still be visible even when its target
+	    %% has been deleted.
+	    ["sub","symlink"] =
+		basenames(Dir, filelib:wildcard(filename:join(Dir, "*"))),
+	    ["symlink"] =
+		basenames(Dir, filelib:wildcard(filename:join(Dir, "symlink"))),
+	    ok
+    end.
+
+basenames(Dir, Files) ->
+    [begin
+	 Dir = filename:dirname(F),
+	 filename:basename(F)
+     end || F <- Files].

commit de2fb97f15ac98aa1d1c5533aacca378334f4778

Fix handling of broken symlinks in filelib
This fixes a bug introduced in f11aabdc9fec593c31e6c4f3fa25c1707e9c35df where
filelib:eval_read_file_info/2 was made to use file:read_link_info/1 to never
follow symlinks. This fixed wildcard/1 but broke every other function using
eval_read_file_info/2.

Reported-by: Louis-Philippe Gauthier
Reported-by: Danil Zagoskin

erts/doc/src/erl_prim_loader.xml
@@ -148,6 +148,22 @@
       </desc>
     </func>
     <func>
+      <name name="read_link_info" arity="1"/>
+      <fsummary>Get information about a link or file</fsummary>
+      <desc>
+        <p>This function works like
+          <seealso marker="#read_file_info/1">read_file_info/1</seealso>
+          except that if <c><anno>Filename</anno></c> is a symbolic link,
+          information about the link will be returned in the <c>file_info</c>
+          record and the <c>type</c> field of the record will be set to
+          <c>symlink</c>.</p>
+        <p>If <c><anno>Filename</anno></c> is not a symbolic link, this function
+          returns exactly the same result as <c>read_file_info/1</c>.
+          On platforms that do not support symbolic links, this function
+          is always equivalent to <c>read_file_info/1</c>.</p>
+      </desc>
+    </func>
+    <func>
       <name name="set_path" arity="1"/>
       <fsummary>Set the path of the loader</fsummary>
       <desc>

erts/preloaded/src/erl_prim_loader.erl
@@ -42,11 +42,11 @@
 
 %% Public
 -export([start/3, set_path/1, get_path/0, get_file/1, get_files/2,
-         list_dir/1, read_file_info/1, get_cwd/0, get_cwd/1]).
+         list_dir/1, read_file_info/1, read_link_info/1, get_cwd/0, get_cwd/1]).
 
 %% Used by erl_boot_server
 -export([prim_init/0, prim_get_file/2, prim_list_dir/2,
-         prim_read_file_info/2, prim_get_cwd/2]).
+         prim_read_file_info/3, prim_get_cwd/2]).
 
 %% Used by escript and code
 -export([set_primary_archive/4, release_archives/0]).
@@ -223,6 +223,12 @@ list_dir(Dir) ->
 read_file_info(File) ->
     check_file_result(read_file_info, File, request({read_file_info,File})).
 
+-spec read_link_info(Filename) -> {'ok', FileInfo} | 'error' when
+      Filename :: string(),
+      FileInfo :: file:file_info().
+read_link_info(File) ->
+    check_file_result(read_link_info, File, request({read_link_info,File})).
+
 -spec get_cwd() -> {'ok', string()} | 'error'.
 get_cwd() ->
     check_file_result(get_cwd, [], request({get_cwd,[]})).
@@ -325,6 +331,9 @@ loop(State, Parent, Paths) ->
                     {read_file_info,File} ->
                         {Res,State1} = handle_read_file_info(State, File),
                         {Res,State1,Paths};
+                    {read_link_info,File} ->
+                        {Res,State1} = handle_read_link_info(State, File),
+                        {Res,State1,Paths};
                     {get_cwd,[]} ->
                         {Res,State1} = handle_get_cwd(State, []),
                         {Res,State1,Paths};
@@ -387,9 +396,14 @@ handle_list_dir(State = #state{loader = inet}, Dir) ->
     ?SAFE2(inet_list_dir(State, Dir), State).
 
 handle_read_file_info(State = #state{loader = efile}, File) ->
-    ?SAFE2(efile_read_file_info(State, File), State);
+    ?SAFE2(efile_read_file_info(State, File, true), State);
 handle_read_file_info(State = #state{loader = inet}, File) ->
     ?SAFE2(inet_read_file_info(State, File), State).
+
+handle_read_link_info(State = #state{loader = efile}, File) ->
+    ?SAFE2(efile_read_file_info(State, File, false), State);
+handle_read_link_info(State = #state{loader = inet}, File) ->
+    ?SAFE2(inet_read_link_info(State, File), State).
 
 handle_get_cwd(State = #state{loader = efile}, Drive) ->
     ?SAFE2(efile_get_cwd(State, Drive), State);
@@ -514,8 +528,8 @@ efile_list_dir(#state{prim_state = PS} = State, Dir) ->
     {Res, PS2} = prim_list_dir(PS, Dir),
     {Res, State#state{prim_state = PS2}}.
 
-efile_read_file_info(#state{prim_state = PS} = State, File) ->
-    {Res, PS2} = prim_read_file_info(PS, File),
+efile_read_file_info(#state{prim_state = PS} = State, File, FollowLinks) ->
+    {Res, PS2} = prim_read_file_info(PS, File, FollowLinks),
     {Res, State#state{prim_state = PS2}}.
 
 efile_get_cwd(#state{prim_state = PS} = State, Drive) ->
@@ -717,6 +731,10 @@ inet_list_dir(State, Dir) ->
 %% -> {{ok,Info},State} | {{error,Reason},State}
 inet_read_file_info(State, File) ->
     inet_send_and_rcv({read_file_info,File}, read_file_info, State).
+
+%% -> {{ok,Info},State} | {{error,Reason},State}
+inet_read_link_info(State, File) ->
+    inet_send_and_rcv({read_link_info,File}, read_link_info, State).
 
 %% -> {{ok,Cwd},State} | {{error,Reason},State}
 inet_get_cwd(State, []) ->
@@ -951,16 +969,18 @@ prim_list_dir(PS, Dir) ->
     debug(PS, {return, Res2}),
     {Res2, PS3}.
 
--spec prim_read_file_info(prim_state(), file:filename()) ->
+-spec prim_read_file_info(prim_state(), file:filename(), boolean()) ->
 	{{'ok', #file_info{}}, prim_state()}
       | {{'error', term()}, prim_state()}.
-prim_read_file_info(PS, File) ->
+prim_read_file_info(PS, File, FollowLinks) ->
     debug(PS, {read_file_info, File}),
     {Res2, PS2} =
         case name_split(PS#prim_state.primary_archive, File) of
             {file, PrimFile} ->
-                Res = prim_file:read_file_info(PrimFile),
-                {Res, PS};
+                case FollowLinks of
+                    true -> {prim_file:read_file_info(PrimFile), PS};
+                    false -> {prim_file:read_link_info(PrimFile), PS}
+                end;
             {archive, ArchiveFile, []} ->
                 %% Fake top directory
                 debug(PS, {archive_read_file_info, ArchiveFile}),

lib/kernel/src/erl_boot_server.erl
@@ -341,9 +341,13 @@ handle_command(S, PS, Msg) ->
 	    send_file_result(S, list_dir, Res),
 	    PS2;
 	{read_file_info,File} ->
-	    {Res, PS2} = erl_prim_loader:prim_read_file_info(PS, File),
+	    {Res, PS2} = erl_prim_loader:prim_read_file_info(PS, File, true),
 	    send_file_result(S, read_file_info, Res),
 	    PS2;
+        {read_link_info,File} ->
+            {Res, PS2} = erl_prim_loader:prim_read_file_info(PS, File, false),
+            send_file_result(S, read_link_info, Res),
+            PS2;
 	get_cwd ->
 	    {Res, PS2} = erl_prim_loader:prim_get_cwd(PS, []),
 	    send_file_result(S, get_cwd, Res),

lib/stdlib/src/filelib.erl
@@ -265,7 +265,7 @@ do_wildcard(Pattern, Cwd, Mod) ->
     lists:sort(Files).
 
 do_wildcard_1({exists,File}, Mod) ->
-    case eval_read_file_info(File, Mod) of
+    case eval_read_link_info(File, Mod) of
 	{ok,_} -> [File];
 	_ -> []
     end;
@@ -488,7 +488,7 @@ badpattern(Reason) ->
     error({badpattern,Reason}).
 
 eval_read_file_info(File, file) ->
-    file:read_link_info(File);
+    file:read_file_info(File);
 eval_read_file_info(File, erl_prim_loader) ->
     case erl_prim_loader:read_file_info(File) of
 	error -> {error, erl_prim_loader};
@@ -496,6 +496,16 @@ eval_read_file_info(File, erl_prim_loader) ->
     end;
 eval_read_file_info(File, Mod) ->
     Mod:read_file_info(File).
+
+eval_read_link_info(File, file) ->
+    file:read_link_info(File);
+eval_read_link_info(File, erl_prim_loader) ->
+    case erl_prim_loader:read_link_info(File) of
+        error -> {error, erl_prim_loader};
+        Res-> Res
+    end;
+eval_read_link_info(File, Mod) ->
+    Mod:read_link_info(File).
 
 eval_list_dir(Dir, file) ->
     file:list_dir(Dir);

lib/stdlib/test/filelib_SUITE.erl
@@ -23,7 +23,8 @@
 	 init_per_group/2,end_per_group/2,
 	 init_per_testcase/2,end_per_testcase/2,
 	 wildcard_one/1,wildcard_two/1,wildcard_errors/1,
-	 fold_files/1,otp_5960/1,ensure_dir_eexist/1,symlinks/1]).
+	 fold_files/1,otp_5960/1,ensure_dir_eexist/1,ensure_dir_symlink/1,
+     symlinks/1]).
 
 -import(lists, [foreach/2]).
 
@@ -43,7 +44,7 @@ suite() -> [{ct_hooks,[ts_install_cth]}].
 
 all() -> 
     [wildcard_one, wildcard_two, wildcard_errors,
-     fold_files, otp_5960, ensure_dir_eexist, symlinks].
+     fold_files, otp_5960, ensure_dir_eexist, ensure_dir_symlink, symlinks].
 
 groups() -> 
     [].
@@ -366,6 +367,26 @@ ensure_dir_eexist(Config) when is_list(Config) ->
     ?line {error, eexist} = filelib:ensure_dir(NeedFile),
     ?line {error, eexist} = filelib:ensure_dir(NeedFileB),
     ok.
+
+ensure_dir_symlink(Config) when is_list(Config) ->
+    PrivDir = ?config(priv_dir, Config),
+    Dir = filename:join(PrivDir, "ensure_dir_symlink"),
+    Name = filename:join(Dir, "same_name_as_file_and_dir"),
+    ok = filelib:ensure_dir(Name),
+    ok = file:write_file(Name, <<"some string\n">>),
+    %% With a symlink to the directory.
+    Symlink = filename:join(PrivDir, "ensure_dir_symlink_link"),
+    case file:make_symlink(Dir, Symlink) of
+        {error,enotsup} ->
+            {skip,"Symlinks not supported on this platform"};
+        {error,eperm} ->
+            {win32,_} = os:type(),
+            {skip,"Windows user not privileged to create symlinks"};
+        ok ->
+            SymlinkedName = filename:join(Symlink, "same_name_as_file_and_dir"),
+            ok = filelib:ensure_dir(SymlinkedName)
+    end.
+
 
 symlinks(Config) when is_list(Config) ->
     PrivDir = ?config(priv_dir, Config),