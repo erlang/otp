erlang/otp

commit f11aabdc9fec593c31e6c4f3fa25c1707e9c35df

filelib:wildcard() should show broken symlinks
filelib:wildcard("broken_symlink") would return an empty list if
"broken_symlink" is a symlink that does not point to an existing
file.

lib/stdlib/test/filelib_SUITE.erl
@@ -23,7 +23,7 @@
 	 init_per_group/2,end_per_group/2,
 	 init_per_testcase/2,end_per_testcase/2,
 	 wildcard_one/1,wildcard_two/1,wildcard_errors/1,
-	 fold_files/1,otp_5960/1,ensure_dir_eexist/1]).
+	 fold_files/1,otp_5960/1,ensure_dir_eexist/1,symlinks/1]).
 
 -import(lists, [foreach/2]).
 
@@ -43,7 +43,7 @@ suite() -> [{ct_hooks,[ts_install_cth]}].
 
 all() -> 
     [wildcard_one, wildcard_two, wildcard_errors,
-     fold_files, otp_5960, ensure_dir_eexist].
+     fold_files, otp_5960, ensure_dir_eexist, symlinks].
 
 groups() -> 
     [].
@@ -366,3 +366,39 @@ ensure_dir_eexist(Config) when is_list(Config) ->
     ?line {error, eexist} = filelib:ensure_dir(NeedFile),
     ?line {error, eexist} = filelib:ensure_dir(NeedFileB),
     ok.
+
+symlinks(Config) when is_list(Config) ->
+    PrivDir = ?config(priv_dir, Config),
+    Dir = filename:join(PrivDir, ?MODULE_STRING++"_symlinks"),
+    SubDir = filename:join(Dir, "sub"),
+    AFile = filename:join(SubDir, "a_file"),
+    Alias = filename:join(Dir, "symlink"),
+    ok = file:make_dir(Dir),
+    ok = file:make_dir(SubDir),
+    ok = file:write_file(AFile, "not that big\n"),
+    case file:make_symlink(AFile, Alias) of
+	{error, enotsup} ->
+	    {skip, "Links not supported on this platform"};
+	{error, eperm} ->
+	    {win32,_} = os:type(),
+	    {skip, "Windows user not privileged to create symlinks"};
+	ok ->
+	    ["sub","symlink"] =
+		basenames(Dir, filelib:wildcard(filename:join(Dir, "*"))),
+	    ["symlink"] =
+		basenames(Dir, filelib:wildcard(filename:join(Dir, "symlink"))),
+	    ok = file:delete(AFile),
+	    %% The symlink should still be visible even when its target
+	    %% has been deleted.
+	    ["sub","symlink"] =
+		basenames(Dir, filelib:wildcard(filename:join(Dir, "*"))),
+	    ["symlink"] =
+		basenames(Dir, filelib:wildcard(filename:join(Dir, "symlink"))),
+	    ok
+    end.
+
+basenames(Dir, Files) ->
+    [begin
+	 Dir = filename:dirname(F),
+	 filename:basename(F)
+     end || F <- Files].

lib/stdlib/src/filelib.erl
@@ -488,7 +488,7 @@ badpattern(Reason) ->
     error({badpattern,Reason}).
 
 eval_read_file_info(File, file) ->
-    file:read_file_info(File);
+    file:read_link_info(File);
 eval_read_file_info(File, erl_prim_loader) ->
     case erl_prim_loader:read_file_info(File) of
 	error -> {error, erl_prim_loader};

commit 9dc807376b0dff43a19af5ef4301333f16cc11c1

Add tests of filelib and symlinks
This commit complements 769996c0cd4cb68c157e39de725642f209ed7423 and
adds tests for the following problems introduced by
f11aabdc9fec593c31e6c4f3fa25c1707e9c35df:

Given the symlink 'dirlink' pointing to an existing dir, then
- filelib:is_regular("dirlink") -> false
- filelib:is_dir("dirlink") -> false

Given the symlink 'filelink' pointing to an existing file, then
- filelib:is_regular("filelink") -> false
- filelib:is_file("filelink") -> false
- filelib:last_modified("filelink") and filelib:filesize("filelink")
  return the info of the link and not the file

Given the symlink "broken_symlink" pointing to a non-existing file, then
- filelib:wildcard("broken_symlink",File) -> []

lib/kernel/test/erl_prim_loader_SUITE.erl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 1996-2013. All Rights Reserved.
+%% Copyright Ericsson AB 1996-2014. All Rights Reserved.
 %%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
@@ -328,6 +328,30 @@ file_requests(Config) when is_list(Config) ->
     {ok,Info} = file:read_file_info(code:which(test_server)),
     ?line {ok,Info} = rpc:call(Node, erl_prim_loader, read_file_info,
 			       [code:which(test_server)]),
+
+    PrivDir = ?config(priv_dir,Config),
+    Dir = filename:join(PrivDir,?MODULE_STRING++"_file_requests"),
+    ok = file:make_dir(Dir),
+    Alias = filename:join(Dir,"symlink"),
+    case file:make_symlink(code:which(test_server), Alias) of
+	{error, enotsup} ->
+	    %% Links not supported on this platform
+	    ok;
+	{error, eperm} ->
+	    {win32,_} = os:type(),
+	    %% Windows user not privileged to create symlinks"
+	    ok;
+	ok ->
+	    %% Reading file info for link should return file info for
+	    %% link target
+	    {ok,Info} = rpc:call(Node, erl_prim_loader, read_file_info,
+				 [Alias]),
+	    #file_info{type=regular} = Info,
+	    {ok,#file_info{type=symlink}} =
+		rpc:call(Node, erl_prim_loader, read_link_info,
+			 [Alias])
+    end,
+
     {ok,Cwd} = file:get_cwd(),
     ?line {ok,Cwd} = rpc:call(Node, erl_prim_loader, get_cwd, []),
     case file:get_cwd("C:") of

lib/stdlib/test/filelib_SUITE.erl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 2005-2013. All Rights Reserved.
+%% Copyright Ericsson AB 2005-2014. All Rights Reserved.
 %%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
@@ -24,7 +24,7 @@
 	 init_per_testcase/2,end_per_testcase/2,
 	 wildcard_one/1,wildcard_two/1,wildcard_errors/1,
 	 fold_files/1,otp_5960/1,ensure_dir_eexist/1,ensure_dir_symlink/1,
-     symlinks/1]).
+	 wildcard_symlink/1, is_file_symlink/1, file_props_symlink/1]).
 
 -import(lists, [foreach/2]).
 
@@ -44,7 +44,8 @@ suite() -> [{ct_hooks,[ts_install_cth]}].
 
 all() -> 
     [wildcard_one, wildcard_two, wildcard_errors,
-     fold_files, otp_5960, ensure_dir_eexist, ensure_dir_symlink, symlinks].
+     fold_files, otp_5960, ensure_dir_eexist, ensure_dir_symlink,
+     wildcard_symlink, is_file_symlink, file_props_symlink].
 
 groups() -> 
     [].
@@ -387,10 +388,9 @@ ensure_dir_symlink(Config) when is_list(Config) ->
             ok = filelib:ensure_dir(SymlinkedName)
     end.
 
-
-symlinks(Config) when is_list(Config) ->
+wildcard_symlink(Config) when is_list(Config) ->
     PrivDir = ?config(priv_dir, Config),
-    Dir = filename:join(PrivDir, ?MODULE_STRING++"_symlinks"),
+    Dir = filename:join(PrivDir, ?MODULE_STRING++"_wildcard_symlink"),
     SubDir = filename:join(Dir, "sub"),
     AFile = filename:join(SubDir, "a_file"),
     Alias = filename:join(Dir, "symlink"),
@@ -408,6 +408,18 @@ symlinks(Config) when is_list(Config) ->
 		basenames(Dir, filelib:wildcard(filename:join(Dir, "*"))),
 	    ["symlink"] =
 		basenames(Dir, filelib:wildcard(filename:join(Dir, "symlink"))),
+	    ["sub","symlink"] =
+		basenames(Dir, filelib:wildcard(filename:join(Dir, "*"),
+						erl_prim_loader)),
+	    ["symlink"] =
+		basenames(Dir, filelib:wildcard(filename:join(Dir, "symlink"),
+						erl_prim_loader)),
+	    ["sub","symlink"] =
+		basenames(Dir, filelib:wildcard(filename:join(Dir, "*"),
+						prim_file)),
+	    ["symlink"] =
+		basenames(Dir, filelib:wildcard(filename:join(Dir, "symlink"),
+						prim_file)),
 	    ok = file:delete(AFile),
 	    %% The symlink should still be visible even when its target
 	    %% has been deleted.
@@ -415,6 +427,18 @@ symlinks(Config) when is_list(Config) ->
 		basenames(Dir, filelib:wildcard(filename:join(Dir, "*"))),
 	    ["symlink"] =
 		basenames(Dir, filelib:wildcard(filename:join(Dir, "symlink"))),
+	    ["sub","symlink"] =
+		basenames(Dir, filelib:wildcard(filename:join(Dir, "*"),
+						erl_prim_loader)),
+	    ["symlink"] =
+		basenames(Dir, filelib:wildcard(filename:join(Dir, "symlink"),
+						erl_prim_loader)),
+	    ["sub","symlink"] =
+		basenames(Dir, filelib:wildcard(filename:join(Dir, "*"),
+						prim_file)),
+	    ["symlink"] =
+		basenames(Dir, filelib:wildcard(filename:join(Dir, "symlink"),
+						prim_file)),
 	    ok
     end.
 
@@ -423,3 +447,60 @@ basenames(Dir, Files) ->
 	 Dir = filename:dirname(F),
 	 filename:basename(F)
      end || F <- Files].
+
+is_file_symlink(Config) ->
+    PrivDir = ?config(priv_dir, Config),
+    Dir = filename:join(PrivDir, ?MODULE_STRING++"_is_file_symlink"),
+    SubDir = filename:join(Dir, "sub"),
+    AFile = filename:join(SubDir, "a_file"),
+    DirAlias = filename:join(Dir, "dir_symlink"),
+    FileAlias = filename:join(Dir, "file_symlink"),
+    ok = file:make_dir(Dir),
+    ok = file:make_dir(SubDir),
+    ok = file:write_file(AFile, "not that big\n"),
+    case file:make_symlink(SubDir, DirAlias) of
+	{error, enotsup} ->
+	    {skip, "Links not supported on this platform"};
+	{error, eperm} ->
+	    {win32,_} = os:type(),
+	    {skip, "Windows user not privileged to create symlinks"};
+	ok ->
+	    true = filelib:is_dir(DirAlias),
+	    true = filelib:is_dir(DirAlias, erl_prim_loader),
+	    true = filelib:is_dir(DirAlias, prim_file),
+	    true = filelib:is_file(DirAlias),
+	    true = filelib:is_file(DirAlias, erl_prim_loader),
+	    true = filelib:is_file(DirAlias, prim_file),
+	    ok = file:make_symlink(AFile,FileAlias),
+	    true = filelib:is_file(FileAlias),
+	    true = filelib:is_file(FileAlias, erl_prim_loader),
+	    true = filelib:is_file(FileAlias, prim_file),
+	    true = filelib:is_regular(FileAlias),
+	    true = filelib:is_regular(FileAlias, erl_prim_loader),
+	    true = filelib:is_regular(FileAlias, prim_file),
+	    ok
+    end.
+
+file_props_symlink(Config) ->
+    PrivDir = ?config(priv_dir, Config),
+    Dir = filename:join(PrivDir, ?MODULE_STRING++"_file_props_symlink"),
+    AFile = filename:join(Dir, "a_file"),
+    Alias = filename:join(Dir, "symlink"),
+    ok = file:make_dir(Dir),
+    ok = file:write_file(AFile, "not that big\n"),
+    case file:make_symlink(AFile, Alias) of
+	{error, enotsup} ->
+	    {skip, "Links not supported on this platform"};
+	{error, eperm} ->
+	    {win32,_} = os:type(),
+	    {skip, "Windows user not privileged to create symlinks"};
+	ok ->
+	    {_,_} = LastMod = filelib:last_modified(AFile),
+	    LastMod = filelib:last_modified(Alias),
+	    LastMod = filelib:last_modified(Alias, erl_prim_loader),
+	    LastMod = filelib:last_modified(Alias, prim_file),
+	    FileSize = filelib:file_size(AFile),
+	    FileSize = filelib:file_size(Alias),
+	    FileSize = filelib:file_size(Alias, erl_prim_loader),
+	    FileSize = filelib:file_size(Alias, prim_file)
+    end.