fafff9077b05ccb7bd958d374e78d2ffa78be829
Olivier Girondel <olivier@biniou.info>
2014-09-11 19:01:33 +0200

    Revert "Add a fallback if log2() is not available"
    This reverts commit 152c41487d78bc94a758d676c94223ee9f88e616.

--- lib/stdlib/src/math.erl
+++ lib/stdlib/src/math.erl
@@ -94,8 +94,8 @@ log(_) ->
 
 -spec log2(X) -> float() when
       X :: number().
-log2(X) ->
-    log(X) / log(2).
+log2(_) ->
+    erlang:nif_error(undef).
 
 -spec log10(X) -> float() when
       X :: number().
