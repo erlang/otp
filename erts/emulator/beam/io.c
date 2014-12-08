01cc99b35c00be86d832693776ee8ed880b59882
Sverker Eriksson <sverker@erlang.org>
2014-12-08 20:36:28 +0100

    erts: Make key argument constant for erl_drv_{get|put}env
    This should be a harmless and compatible API change.

--- erts/emulator/beam/io.c
+++ erts/emulator/beam/io.c
@@ -7596,15 +7596,15 @@ int null_func(void)
 }
 
 int
-erl_drv_putenv(char *key, char *value)
+erl_drv_putenv(const char *key, char *value)
 {
-    return erts_sys_putenv_raw(key, value);
+    return erts_sys_putenv_raw((char*)key, value);
 }
 
 int
-erl_drv_getenv(char *key, char *value, size_t *value_size)
+erl_drv_getenv(const char *key, char *value, size_t *value_size)
 {
-    return erts_sys_getenv_raw(key, value, value_size);
+    return erts_sys_getenv_raw((char*)key, value, value_size);
 }
 
 /* get heart_port
