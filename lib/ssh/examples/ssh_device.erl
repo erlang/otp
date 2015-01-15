718da22060ddba9b129bedea64abc537148253eb
Linus.yuan <yuan_shijiang@163.com>
2015-01-15 11:27:12 +0800

    modified code style, tidy up with erl_tidy

--- lib/ssh/examples/ssh_device.erl
+++ lib/ssh/examples/ssh_device.erl
@@ -22,38 +22,50 @@
 -module(ssh_device).
 
 %% api
--export(compile_all).
+-export([ssh_device/5]).
 
 ssh_device(H, P, U, Pass, Cmd) ->
     ssh:start(),
-    case ssh:connect(H, P,[{user, U}, {password, Pass},{silently_accept_hosts, true},{quiet_mode,true}]) of
-    	{ok, Conn} ->
-    		{ok, ChannelId} = ssh_connection:session_channel(Conn,infinity),
-    		ssh_connection:exec(Conn, ChannelId, Cmd,infinity),
-    		Init_rep = <<>>,
-    		wait_for_response(Conn,H,Init_rep),
-    		ssh:close(Conn);
-    	{error,nxdomain}->
-    	{ok,Try1}=ssh:connect(vnet:q_ip(H), P,[{user, U}, {password, Pass},{silently_accept_hosts, true},{quiet_mode,true}]),
-    		{ok, ChannelId1} = ssh_connection:session_channel(Try1,infinity),
-    		ssh_connection:exec(Try1, ChannelId1, Cmd,infinity),
-    		Init_rep2 = <<>>,
-    		wait_for_response(Try1,H,Init_rep2),
-    		ssh:close(Try1)
+    case ssh:connect(H, P,
+		     [{user, U}, {password, Pass},
+		      {silently_accept_hosts, true}, {quiet_mode, true}])
+	of
+      {ok, Conn} ->
+	  {ok, ChannelId} = ssh_connection:session_channel(Conn,
+							   infinity),
+	  ssh_connection:exec(Conn, ChannelId, Cmd, infinity),
+	  Init_rep = <<>>,
+	  wait_for_response(Conn, H, Init_rep),
+	  ssh:close(Conn);
+      {error, nxdomain} ->
+	  {ok, Try1} = ssh:connect(vnet:q_ip(H), P,
+				   [{user, U}, {password, Pass},
+				    {silently_accept_hosts, true},
+				    {quiet_mode, true}]),
+	  {ok, ChannelId1} = ssh_connection:session_channel(Try1,
+							    infinity),
+	  ssh_connection:exec(Try1, ChannelId1, Cmd, infinity),
+	  Init_rep2 = <<>>,
+	  wait_for_response(Try1, H, Init_rep2),
+	  ssh:close(Try1)
     end.
 
-wait_for_response(Conn,H,Acc) ->
+%%--------------------------------------------------------------------
+%%% Internal application API
+%%--------------------------------------------------------------------
+wait_for_response(Conn, H, Acc) ->
     receive
       {ssh_cm, Conn, Msg} ->
-          case Msg of
-            {closed, _ChannelId} -> 
-            	%file:write_file(string:concat("log\\",H), Acc, [append]),
-            	io:format([">>>>>>>>>>>>>>>>>>> ",H," <<<<<<<<<<<<<<<<<\r\n",Acc,"\r\n"]);
-            {data, _, _, A} ->
-            	Acc2= <<Acc/binary,A/binary>>,
-                wait_for_response(Conn,H,Acc2);
-            _ -> 
-            	%io:format("~p", [Msg]),
-            	wait_for_response(Conn,H,Acc)
-          end
+	  case Msg of
+	    {closed, _ChannelId} ->
+		%file:write_file(string:concat("log\\",H), Acc, [append]),
+		io:format([">>>>>>>>>>>>>>>>>>> ", H,
+			   " <<<<<<<<<<<<<<<<<\r\n", Acc, "\r\n"]);
+	    {data, _, _, A} ->
+		Acc2 = <<Acc/binary, A/binary>>,
+		wait_for_response(Conn, H, Acc2);
+	    _ ->
+		%io:format("~p", [Msg]),
+		wait_for_response(Conn, H, Acc)
+	  end
     end.
