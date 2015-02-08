e6d19a18b7c1057c2b4493f8db822c82bb0dbe0d
Anders Svensson <anders@erlang.org>
2015-02-09 00:36:56 +0100

    Use new time api in implementation
    In particular, deal with the deprecation of erlang:now/0 in OTP 18. Be
    backwards compatible with older releases: the new api is only used when
    available.
    
    The test suites have not been modified.

--- lib/diameter/src/base/diameter_sync.erl
+++ lib/diameter/src/base/diameter_sync.erl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 2010-2011. All Rights Reserved.
+%% Copyright Ericsson AB 2010-2015. All Rights Reserved.
 %%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
@@ -26,6 +26,9 @@
 
 -module(diameter_sync).
 -behaviour(gen_server).
+
+-compile({no_auto_import, [now/0]}).
+-import(diameter_lib, [now/0]).
 
 -export([call/4, call/5,
          cast/4, cast/5,
--- lib/diameter/src/base/diameter_watchdog.erl
+++ lib/diameter/src/base/diameter_watchdog.erl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 2010-2014. All Rights Reserved.
+%% Copyright Ericsson AB 2010-2015. All Rights Reserved.
 %%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
@@ -122,7 +122,8 @@ i({Ack, T, Pid, {RecvData,
                  = Svc}}) ->
     erlang:monitor(process, Pid),
     wait(Ack, Pid),
-    random:seed(now()),
+    {_, Seed} = diameter_lib:seed(),
+    random:seed(Seed),
     putr(restart, {T, Opts, Svc}),  %% save seeing it in trace
     putr(dwr, dwr(Caps)),           %%
     {_,_} = Mask = proplists:get_value(sequence, SvcOpts),
