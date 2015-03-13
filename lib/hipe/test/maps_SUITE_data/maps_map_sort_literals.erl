5388095c2d33b5d6a2df2a6b94cdf98e951290e2
Björn-Egil Dahlberg <egil@erlang.org>
2015-03-13 15:26:53 +0100

    hipe: Fix maps sort order testcase

--- lib/hipe/test/maps_SUITE_data/maps_map_sort_literals.erl
+++ lib/hipe/test/maps_SUITE_data/maps_map_sort_literals.erl
@@ -10,23 +10,25 @@ test() ->
     false = #{ c => 1, b => 1, a => 1} < id(#{ c => 1, a => 1}),
 
     %% key order
-    true  = id(#{ a => 1 }) < id(#{ b => 1}),
-    false = id(#{ b => 1 }) < id(#{ a => 1}),
-    true  = id(#{ a => 1, b => 1, c => 1 }) < id(#{ b => 1, c => 1, d => 1}),
-    true  = id(#{ b => 1, c => 1, d => 1 }) > id(#{ a => 1, b => 1, c => 1}),
-    true  = id(#{ c => 1, b => 1, a => 1 }) < id(#{ b => 1, c => 1, d => 1}),
-    true  = id(#{ "a" => 1 }) < id(#{ <<"a">> => 1}),
-    false = id(#{ <<"a">> => 1 }) < id(#{ "a" => 1}),
-    false = id(#{ 1 => 1 }) < id(#{ 1.0 => 1}),
-    false = id(#{ 1.0 => 1 }) < id(#{ 1 => 1}),
+    true  = #{ a => 1 } < id(#{ b => 1}),
+    false = #{ b => 1 } < id(#{ a => 1}),
+    true  = #{ a => 1, b => 1, c => 1 } < id(#{ b => 1, c => 1, d => 1}),
+    true  = #{ b => 1, c => 1, d => 1 } > id(#{ a => 1, b => 1, c => 1}),
+    true  = #{ c => 1, b => 1, a => 1 } < id(#{ b => 1, c => 1, d => 1}),
+    true  = #{ "a" => 1 } < id(#{ <<"a">> => 1}),
+    false = #{ <<"a">> => 1 } < id(#{ "a" => 1}),
+    true  = #{ 1 => 1 } < id(#{ 1.0 => 1}),
+    false = #{ 1.0 => 1 } < id(#{ 1 => 1}),
 
     %% value order
-    true  = id(#{ a => 1 }) < id(#{ a => 2}),
-    false = id(#{ a => 2 }) < id(#{ a => 1}),
-    false = id(#{ a => 2, b => 1 }) < id(#{ a => 1, b => 3}),
-    true  = id(#{ a => 1, b => 1 }) < id(#{ a => 1, b => 3}),
+    true  = #{ a => 1 } < id(#{ a => 2}),
+    false = #{ a => 2 } < id(#{ a => 1}),
+    false = #{ a => 2, b => 1 } < id(#{ a => 1, b => 3}),
+    true  = #{ a => 1, b => 1 } < id(#{ a => 1, b => 3}),
+    false = #{ a => 1 } < id(#{ a => 1.0}),
+    false = #{ a => 1.0 } < id(#{ a => 1}),
 
-    true  = id(#{ "a" => "hi", b => 134 }) == id(#{ b => 134,"a" => "hi"}),
+    true  = #{ "a" => "hi", b => 134 } == id(#{ b => 134,"a" => "hi"}),
 
     %% lists:sort
 
@@ -34,7 +36,6 @@ test() ->
     [#{1:=ok},#{a:=ok},#{"a":=ok},#{<<"a">>:=ok}] = lists:sort([#{"a"=>ok},#{a=>ok},#{1=>ok},#{<<"a">>=>ok}]),
     [#{1:=3},#{a:=2},#{"a":=1},#{<<"a">>:=4}] = lists:sort(SortVs),
     [#{1:=3},#{a:=2},#{"a":=1},#{<<"a">>:=4}] = lists:sort(lists:reverse(SortVs)),
-
     ok.
 
 %% Use this function to avoid compile-time evaluation of an expression.
