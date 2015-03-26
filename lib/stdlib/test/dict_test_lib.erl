dfb2c4ba8517e8b2206034c66c5ba99b332a9892
Kirill Kinduk <evilbluebeaver@gmail.com>
2015-03-26 16:42:48 +0300

    add tests and doc for gb_trees:iterate_from/2

--- lib/stdlib/test/dict_test_lib.erl
+++ lib/stdlib/test/dict_test_lib.erl
@@ -1,19 +1,19 @@
 %%
 %% %CopyrightBegin%
-%% 
+%%
 %% Copyright Ericsson AB 2008-2013. All Rights Reserved.
-%% 
+%%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
 %% compliance with the License. You should have received a copy of the
 %% Erlang Public License along with this software. If not, it can be
 %% retrieved online at http://www.erlang.org/.
-%% 
+%%
 %% Software distributed under the License is distributed on an "AS IS"
 %% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
 %% the License for the specific language governing rights and limitations
 %% under the License.
-%% 
+%%
 %% %CopyrightEnd%
 %%
 
@@ -29,7 +29,10 @@ new(Mod, Eq) ->
 	(module, []) -> Mod;
 	(size, D) -> Mod:size(D);
 	(is_empty, D) -> Mod:is_empty(D);
+    (iterate_from, {S, D}) -> iterate_from(Mod, S, D);
+    (iterate, D) -> iterate(Mod, D);
 	(to_list, D) -> to_list(Mod, D)
+    % (to_list_ordered, D) -> to_list_ordered(Mod, D)
     end.
 
 empty(Mod) ->
@@ -40,6 +43,13 @@ empty(Mod) ->
 
 to_list(Mod, D) ->
     Mod:to_list(D).
+
+% to_list_ordered(Mod, D) ->
+    % L = Mod:to_list(D),
+    % case Mod of
+        % dict -> lists:sort(L);
+        % _ -> L
+    % end.
 
 from_list(Mod, L) ->
     case erlang:function_exported(Mod, from_orddict, 1) of
@@ -54,6 +64,36 @@ from_list(Mod, L) ->
 	    Orddict = orddict:from_list(L),
 	    Mod:from_orddict(Orddict)
     end.
+
+iterate(Mod, Dict) ->
+    case erlang:function_exported(Mod, iterator, 1) of
+        true ->
+            lists:reverse(iterate_tree(Mod, Dict));
+        false ->
+            Mod:to_list(Dict)
+    end.
+
+iterate_from(Mod, Start, Dict) ->
+    case erlang:function_exported(Mod, iterator_from, 2) of
+        true ->
+            lists:reverse(iterate_tree(Mod, Start, Dict));
+        false ->
+            Filter = fun({K, _}) -> K >= Start end,
+            lists:filter(Filter, Mod:to_list(Dict))
+    end.
+
+iterate_tree(Mod, Tree) ->
+    I = Mod:iterator(Tree),
+    iterate_tree_1(Mod, Mod:next(I), []).
+
+iterate_tree(Mod, Start, Tree) ->
+    I = Mod:iterator_from(Start, Tree),
+    iterate_tree_1(Mod, Mod:next(I), []).
+
+iterate_tree_1(_, none, R) ->
+    R;
+iterate_tree_1(Mod, {K, V, I}, R) ->
+    iterate_tree_1(Mod, Mod:next(I), [{K, V} | R]).
 
 %% Store new value into dictionary or update previous value in dictionary.
 enter(Mod, Key, Val, Dict) ->
