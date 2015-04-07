e3f21d4911117b80e80ea4a07f3532ad39ede16b
Sverker Eriksson <sverker@erlang.org>
2015-04-07 21:28:05 +0200

    erts: Fix bug in map_from_list when keys clash in both value and hash
    Subtle bug in qsort callback. Cast from Sint to int does not retain sign.

--- erts/emulator/beam/erl_map.c
+++ erts/emulator/beam/erl_map.c
@@ -860,7 +860,12 @@ static Eterm hashmap_from_chunked_array(ErtsHeapFactory *factory, hxnode_t *hxns
 #undef HALLOC_EXTRA
 
 static int hxnodecmpkey(hxnode_t *a, hxnode_t *b) {
-    return CMP_TERM(CAR(list_val(a->val)), CAR(list_val(b->val)));
+    Sint c = CMP_TERM(CAR(list_val(a->val)), CAR(list_val(b->val)));
+#if ERTS_SIZEOF_ETERM <= SIZEOF_INT
+    return c;
+#else
+    return c > 0 ? 1 : (c < 0 ? -1 : 0);
+#endif
 }
 
 static int hxnodecmp(hxnode_t *a, hxnode_t *b) {
