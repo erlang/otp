erlang/otp

commit 49e8b11ce3620e686e3388ee7d8a0f445b5f853a

Fix counting error with unknown application id
Statistics could be accumulated on a key like {{23,275,0}, recv} even
though 23 was not the application id of the dictionary in question.

Missed in commits df19c272 and 7816ab2f.

lib/diameter/src/base/diameter_traffic.erl
@@ -1116,18 +1116,14 @@ msg_id(#diameter_packet{header = H}, Dict) ->
 %% there are 2^32 (application ids) * 2^24 (command codes) = 2^56
 %% pairs for an attacker to choose from.
 msg_id(Hdr, Dict) ->
-    {_ApplId, Code, R} = Id = diameter_codec:msg_id(Hdr),
-    case Dict:msg_name(Code, 0 == R) of
-        '' ->
-            unknown(Dict:id(), R);
-        _ ->
-            Id
+    {Aid, Code, R} = Id = diameter_codec:msg_id(Hdr),
+    if Aid == ?APP_ID_RELAY ->
+            {relay, R};
+       true ->
+            choose(Aid /= Dict:id() orelse '' == Dict:msg_name(Code, 0 == R),
+                   unknown,
+                   Id)
     end.
-
-unknown(?APP_ID_RELAY, R) ->
-    {relay, R};
-unknown(_, _) ->
-    unknown.
 
 %% No E-bit: can't be 3xxx.
 is_result(RC, false, _Dict0) ->

commit 3e7ec99afb2469cff4e776de5b6d16909315ff88

Fix broken relay counters
Commit 49e8b11c broke the counting of relayed message, causing them to
be accumulated as unknown messages.

lib/diameter/src/base/diameter_traffic.erl
@@ -1081,6 +1081,9 @@ incr_result(Dir, Pkt, TPid, {Dict, AppDict, Dict0}) ->
         = Pkt,
 
     Id = msg_id(Hdr, AppDict),
+    %% Could be {relay, 0}, in which case the R-bit is redundant since
+    %% only answers are being counted. Let it be however, so that the
+    %% same tuple is in both send/recv and result code counters.
 
     %% Count incoming decode errors.
     recv /= Dir orelse [] == Es orelse incr_error(Dir, Id, TPid, AppDict),
@@ -1107,10 +1110,11 @@ msg_id(#diameter_packet{header = H}, Dict) ->
 %% pairs for an attacker to choose from.
 msg_id(Hdr, Dict) ->
     {Aid, Code, R} = Id = diameter_codec:msg_id(Hdr),
-    if Aid == ?APP_ID_RELAY ->
+    case Dict:id() of
+        ?APP_ID_RELAY ->
             {relay, R};
-       true ->
-            choose(Aid /= Dict:id() orelse '' == Dict:msg_name(Code, 0 == R),
+        A ->
+            choose(A /= Aid orelse '' == Dict:msg_name(Code, 0 == R),
                    unknown,
                    Id)
     end.