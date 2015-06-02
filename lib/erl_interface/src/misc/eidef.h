20dc34bfc200758f6a837d193e042d1e4653562f
Björn-Egil Dahlberg <egil@erlang.org>
2015-06-02 14:55:39 +0200

    erl_interface: Move isfinite definition to header

--- lib/erl_interface/src/misc/eidef.h
+++ lib/erl_interface/src/misc/eidef.h
@@ -41,6 +41,27 @@
 typedef int socklen_t;
 #endif
 
+#ifdef USE_ISINF_ISNAN		/* simulate finite() */
+#  define isfinite(f) (!isinf(f) && !isnan(f))
+#  define HAVE_ISFINITE
+#elif defined(__GNUC__) && defined(HAVE_FINITE)
+/* We use finite in gcc as it emits assembler instead of
+   the function call that isfinite emits. The assembler is
+   significantly faster. */
+#  ifdef isfinite
+#     undef isfinite
+#  endif
+#  define isfinite finite
+#  ifndef HAVE_ISFINITE
+#    define HAVE_ISFINITE
+#  endif
+#elif defined(isfinite) && !defined(HAVE_ISFINITE)
+#  define HAVE_ISFINITE
+#elif !defined(HAVE_ISFINITE) && defined(HAVE_FINITE)
+#  define isfinite finite
+#  define HAVE_ISFINITE
+#endif
+
 typedef unsigned char  uint8;	/* FIXME use configure */
 typedef unsigned short uint16;
 typedef unsigned int   uint32;
