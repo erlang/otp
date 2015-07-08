94053c7afced676466b3109d20c991dff1b6c1b8
Richard Carlsson <richardc@klarna.com>
2015-07-08 22:14:57 +0200

    Correct copyright on stdlib files

--- lib/stdlib/src/gb_sets.erl
+++ lib/stdlib/src/gb_sets.erl
@@ -1,8 +1,3 @@
-%%
-%% %CopyrightBegin%
-%% 
-%% Copyright Ericsson AB 2001-2015. All Rights Reserved.
-%% 
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
 %% You may obtain a copy of the License at
@@ -14,8 +9,6 @@
 %% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
-%% 
-%% %CopyrightEnd%
 %%
 %% =====================================================================
 %% Ordered Sets implemented as General Balanced Trees
