8b02979590d5a7b5f5ce8b4a0410abbf822ccf6d
Richard Carlsson <richardc@klarna.com>
2015-07-08 21:39:51 +0200

    Correct copyright on remaining hipe files

--- lib/hipe/icode/hipe_icode_ebb.erl
+++ lib/hipe/icode/hipe_icode_ebb.erl
@@ -1,8 +1,3 @@
-%%
-%% %CopyrightBegin%
-%% 
-%% Copyright Ericsson AB 2001-2016. All Rights Reserved.
-%% 
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
 %% You may obtain a copy of the License at
@@ -14,14 +9,12 @@
 %% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
-%% 
-%% %CopyrightEnd%
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%
 %% Icode version of extended basic blocks.
 %%
- 
+
 -module(hipe_icode_ebb).
 
 -define(CFG, hipe_icode_cfg).
--- lib/hipe/rtl/hipe_rtl_ssa.erl
+++ lib/hipe/rtl/hipe_rtl_ssa.erl
@@ -1,9 +1,5 @@
 %% -*- erlang-indent-level: 2 -*-
 %%
-%% %CopyrightBegin%
-%% 
-%% Copyright Ericsson AB 2004-2016. All Rights Reserved.
-%% 
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
 %% You may obtain a copy of the License at
@@ -15,8 +11,6 @@
 %% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
-%% 
-%% %CopyrightEnd%
 %%
 %%----------------------------------------------------------------------
 %% File    : hipe_rtl_ssa.erl
@@ -37,7 +31,7 @@
 
 -include("hipe_rtl.hrl").
 -include("../ssa/hipe_ssa.inc").
-     
+
 %%----------------------------------------------------------------------
 %% Auxiliary operations which seriously differ between Icode and RTL.
 %%----------------------------------------------------------------------
