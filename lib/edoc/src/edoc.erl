bf51ff030c8b798c4f9697d768093c3052f52367
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2015-08-06 14:27:23 +0300

    Fix FSF mail address

--- lib/edoc/src/edoc.erl
+++ lib/edoc/src/edoc.erl
@@ -11,7 +11,7 @@
 %%
 %% You should have received a copy of the GNU Lesser General Public
 %% License along with this library; if not, write to the Free Software
-%% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+%% Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
 %% USA
 %%
 %% @copyright 2001-2007 Richard Carlsson
