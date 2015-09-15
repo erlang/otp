erlang/otp

commit 541e87f3597523cb7d71f75fe0381fddb2af0ee0

debugger: Optimize evaluation of new map creation
Make sure that we recognize map literals at load time, as we
do for lists and tuples. Also use maps:from_list/1 to build
a new map instead of building it up from scratch inserting one
key/value pair at the time.

lib/debugger/src/dbg_iload.erl
@@ -290,8 +290,7 @@ gexpr({tuple,Anno,Es0}) ->
     Es1 = gexpr_list(Es0),
     {tuple,ln(Anno),Es1};
 gexpr({map,Anno,Fs0}) ->
-    Fs1 = map_fields(Fs0, fun gexpr/1),
-    {map,ln(Anno),Fs1};
+    new_map(Fs0, Anno, fun gexpr/1);
 gexpr({map,Anno,E0,Fs0}) ->
     E1 = gexpr(E0),
     Fs1 = map_fields(Fs0, fun gexpr/1),
@@ -358,9 +357,8 @@ expr({cons,Anno,H0,T0}, _Lc) ->
 expr({tuple,Anno,Es0}, _Lc) ->
     Es1 = expr_list(Es0),
     {tuple,ln(Anno),Es1};
-expr({map,Anno,Fs0}, _Lc) ->
-    Fs1 = map_fields(Fs0),
-    {map,ln(Anno),Fs1};
+expr({map,Anno,Fs}, _Lc) ->
+    new_map(Fs, Anno, fun (E) -> expr(E, false) end);
 expr({map,Anno,E0,Fs0}, _Lc) ->
     E1 = expr(E0, false),
     Fs1 = map_fields(Fs0),
@@ -537,6 +535,24 @@ icr_clauses([], _) -> [].
 fun_clauses([{clause,A,H,G,B}|Cs]) ->
     [{clause,ln(A),head(H),guard(G),exprs(B, true)}|fun_clauses(Cs)];
 fun_clauses([]) -> [].
+
+
+new_map(Fs0, Anno, F) ->
+    Line = ln(Anno),
+    Fs1 = map_fields(Fs0, F),
+    Fs2 = [{ln(A),K,V} || {map_field_assoc,A,K,V} <- Fs1],
+    try
+	{value,Line,map_literal(Fs2, #{})}
+    catch
+	throw:not_literal ->
+	    {map,Line,Fs2}
+    end.
+
+map_literal([{_,{value,_,K},{value,_,V}}|T], M) ->
+    map_literal(T, maps:put(K, V, M));
+map_literal([_|_], _) ->
+    throw(not_literal);
+map_literal([], M) -> M.
 
 map_fields(Fs) ->
     map_fields(Fs, fun (E) -> expr(E, false) end).

lib/debugger/src/dbg_ieval.erl
@@ -650,11 +650,10 @@ expr({tuple,Line,Es0}, Bs0, Ieval) ->
     {value,list_to_tuple(Vs),Bs};
 
 %% Map
-expr({map,Line,Fs0}, Bs0, Ieval) ->
-    {Fs,Bs} = eval_map_fields(Fs0, Bs0, Ieval#ieval{line=Line,top=false}),
-    Value = lists:foldl(fun ({map_assoc,K,V}, Mi) -> maps:put(K,V,Mi) end,
-                        #{}, Fs),
-    {value,Value,Bs};
+expr({map,Line,Fs}, Bs0, Ieval) ->
+    {Map,Bs} = eval_new_map_fields(Fs, Bs0, Ieval#ieval{line=Line,top=false},
+				   fun expr/3),
+    {value,Map,Bs};
 expr({map,Line,E0,Fs0}, Bs0, Ieval0) ->
     Ieval = Ieval0#ieval{line=Line,top=false},
     {value,E,Bs1} = expr(E0, Bs0, Ieval),
@@ -1473,11 +1472,13 @@ guard_expr({cons,_,H0,T0}, Bs) ->
 guard_expr({tuple,_,Es0}, Bs) ->
     {values,Es} = guard_exprs(Es0, Bs),
     {value,list_to_tuple(Es)};
-guard_expr({map,_,Fs0}, Bs) ->
-    Fs = eval_map_fields_guard(Fs0, Bs),
-    Value = lists:foldl(fun ({map_assoc,K,V}, Mi) -> maps:put(K,V,Mi) end,
-                        #{}, Fs),
-    {value,Value};
+guard_expr({map,_,Fs}, Bs0) ->
+    F = fun (G0, B0, _) ->
+		{value,G} = guard_expr(G0, B0),
+		{value,G,B0}
+	end,
+    {Map,_} = eval_new_map_fields(Fs, Bs0, #ieval{top=false}, F),
+    {value,Map};
 guard_expr({map,_,E0,Fs0}, Bs) ->
     {value,E} = guard_expr(E0, Bs),
     Fs = eval_map_fields_guard(Fs0, Bs),
@@ -1525,6 +1526,17 @@ eval_map_fields([{map_field_exact,Line,K0,V0}|Fs], Bs0, Ieval0, F, Acc) ->
     eval_map_fields(Fs, Bs2, Ieval0, F, [{map_exact,K,V}|Acc]);
 eval_map_fields([], Bs, _Ieval, _F, Acc) ->
     {lists:reverse(Acc),Bs}.
+
+eval_new_map_fields(Fs, Bs0, Ieval, F) ->
+    eval_new_map_fields(Fs, Bs0, Ieval, F, []).
+
+eval_new_map_fields([{Line,K0,V0}|Fs], Bs0, Ieval0, F, Acc) ->
+    Ieval = Ieval0#ieval{line=Line},
+    {value,K,Bs1} = F(K0, Bs0, Ieval),
+    {value,V,Bs2} = F(V0, Bs1, Ieval),
+    eval_new_map_fields(Fs, Bs2, Ieval0, F, [{K,V}|Acc]);
+eval_new_map_fields([], Bs, _, _, Acc) ->
+    {maps:from_list(lists:reverse(Acc)),Bs}.
 
 %% match(Pattern,Term,Bs) -> {match,Bs} | nomatch
 match(Pat, Term, Bs) ->

commit 7fd49429e85a1fe2c24b76bd5d3d8dfddc908822

debugger: Fix a bug related to the use of the erl_anno module
The (harmless) bug was introduced in 541e87f.

lib/debugger/src/dbg_iload.erl
@@ -541,7 +541,7 @@ fun_clauses([]) -> [].
 new_map(Fs0, Anno, F) ->
     Line = ln(Anno),
     Fs1 = map_fields(Fs0, F),
-    Fs2 = [{ln(A),K,V} || {map_field_assoc,A,K,V} <- Fs1],
+    Fs2 = [{L,K,V} || {map_field_assoc,L,K,V} <- Fs1],
     try
 	{value,Line,map_literal(Fs2, #{})}
     catch