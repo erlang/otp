erlang/otp

commit 42cf8b8ca9daec40a3595034a15f9f4be1cc854b

Fix problem not recognizing 'macro' tuple from epp_dodger
OTP-12740

lib/test_server/src/erl2html2.erl
@@ -170,7 +170,11 @@ get_line(Anno) ->
 %%% Find the line number of the last expression in the function
 find_clause_lines([{clause,CL,_Params,_Op,Exprs}], CLs) -> % last clause
     try tuple_to_list(lists:last(Exprs)) of
-	[_Type,ExprLine | _] ->
+	[_Type,ExprLine | _] when is_integer(ExprLine) ->
+	    {lists:reverse([{clause,get_line(CL)}|CLs]), get_line(ExprLine)};
+	[tree,_ | Exprs1] ->
+	    find_clause_lines([{clause,CL,undefined,undefined,Exprs1}], CLs);
+	[macro,{_var,ExprLine,_MACRO} | _] when is_integer(ExprLine) ->
 	    {lists:reverse([{clause,get_line(CL)}|CLs]), get_line(ExprLine)};
 	_ ->
 	    {lists:reverse([{clause,get_line(CL)}|CLs]), get_line(CL)}

lib/test_server/test/erl2html2_SUITE.erl
@@ -130,15 +130,7 @@ groups() ->
 %% @end
 %%--------------------------------------------------------------------
 all() ->
-    [m1].
-
-%%--------------------------------------------------------------------
-%% @spec TestCase() -> Info
-%% Info = [tuple()]
-%% @end
-%%--------------------------------------------------------------------
-m1() ->
-    [].
+    [macros_defined, macros_undefined].
 
 %%--------------------------------------------------------------------
 %% @spec TestCase(Config0) ->
@@ -149,19 +141,29 @@ m1() ->
 %% Comment = term()
 %% @end
 %%--------------------------------------------------------------------
-m1(Config) ->
-    {Src,Dst} = convert_module("m1",Config),
+macros_defined(Config) ->
+    %% let erl2html2 use epp as parser
+    DataDir = ?config(data_dir,Config),
+    InclDir = filename:join(DataDir, "include"),
+    {Src,Dst} = convert_module("m1",[InclDir],Config),
     {true,L} = check_line_numbers(Src,Dst),
-    ok = check_link_targets(Src,Dst,L,[{baz,0}]),
+    ok = check_link_targets(Src,Dst,L,[{baz,0}],[]),
     ok.
 
-convert_module(Mod,Config) ->
+macros_undefined(Config) ->
+    %% let erl2html2 use epp_dodger as parser
+    {Src,Dst} = convert_module("m1",[],Config),
+    {true,L} = check_line_numbers(Src,Dst),
+    ok = check_link_targets(Src,Dst,L,[{baz,0}],[{quux,0}]),
+    ok.
+
+convert_module(Mod,InclDirs,Config) ->
     DataDir = ?config(data_dir,Config),
     PrivDir = ?config(priv_dir,Config),
     Src = filename:join(DataDir,Mod++".erl"),
     Dst = filename:join(PrivDir,Mod++".erl.html"),
     io:format("<a href=\"~s\">~s</a>\n",[Src,filename:basename(Src)]),
-    ok = erl2html2:convert(Src, Dst, [], "<html><body>"),
+    ok = erl2html2:convert(Src, Dst, InclDirs, "<html><body>"),
     io:format("<a href=\"~s\">~s</a>\n",[Dst,filename:basename(Dst)]),
     {Src,Dst}.
 
@@ -229,14 +231,17 @@ check_line_number(Last,Line,OrigLine) ->
 %% function.
 %% The test module has -compile(export_all), so all functions are
 %% found by listing the exported ones.
-check_link_targets(Src,Dst,L,RmFncs) ->
+check_link_targets(Src,Dst,L,RmFncs,ShouldRemain) ->
     Mod = list_to_atom(filename:basename(filename:rootname(Src))),
     Exports = Mod:module_info(exports)--[{module_info,0},{module_info,1}|RmFncs],
     LastExprFuncs = [Func || {Func,_A} <- Exports],
-    {ok,{[],[],L},_} =
+    {ok,{FAs,Fs,L},_} =
 	xmerl_sax_parser:file(Dst,
 			      [{event_fun,fun sax_event/3},
-			       {event_state,{Exports,LastExprFuncs,0}}]),
+			       {event_state,{Exports,LastExprFuncs,0}}]),    
+    true = (length(FAs) == length(ShouldRemain)),
+    [] = [FA || FA <- FAs, not lists:member(FA,ShouldRemain)],
+    [] = [F || F <- Fs, not lists:keymember(F,1,ShouldRemain)],
     ok.
 
 sax_event(Event,_Loc,State) ->

lib/test_server/test/erl2html2_SUITE_data/include/header3.hrl
@@ -1,0 +1,1 @@
+-define(EPP_SWITCH, on).

lib/test_server/test/erl2html2_SUITE_data/m1.erl
@@ -7,8 +7,14 @@
 
 -include("header1.hrl").
 -include("header2.hrl").
+-include("header3.hrl").
 
 -define(MACRO1,value).
+
+%% This macro is used to select parser in erl2html2.
+%% If EPP_SWITCH is defined epp is used, else epp_dodger.
+epp_switch() ->
+    ?EPP_SWITCH.
 
 %%% Comment
 foo(x) ->

commit 5ba36a3857eb2673d28fd33ec4655da41ee78d3d

test_server: Fix a bug related to the use of the erl_anno module
The (harmless) bug was introduced in 42cf8b8.

lib/test_server/src/erl2html2.erl
@@ -170,22 +170,31 @@ get_line(Anno) ->
 %%%-----------------------------------------------------------------
 %%% Find the line number of the last expression in the function
 find_clause_lines([{clause,CL,_Params,_Op,Exprs}], CLs) -> % last clause
-    try tuple_to_list(lists:last(Exprs)) of
-	[_Type,ExprLine | _] when is_integer(ExprLine) ->
-	    {lists:reverse([{clause,get_line(CL)}|CLs]), get_line(ExprLine)};
-	[tree,_ | Exprs1] ->
+    case classify_exprs(Exprs) of
+        {anno, Anno} ->
+	    {lists:reverse([{clause,get_line(CL)}|CLs]), get_line(Anno)};
+        {tree, Exprs1} ->
 	    find_clause_lines([{clause,CL,undefined,undefined,Exprs1}], CLs);
-	[macro,{_var,ExprLine,_MACRO} | _] when is_integer(ExprLine) ->
-	    {lists:reverse([{clause,get_line(CL)}|CLs]), get_line(ExprLine)};
-	_ ->
-	    {lists:reverse([{clause,get_line(CL)}|CLs]), get_line(CL)}
-    catch
-	_:_ ->
+        unknown ->
 	    {lists:reverse([{clause,get_line(CL)}|CLs]), get_line(CL)}
     end;
-
 find_clause_lines([{clause,CL,_Params,_Op,_Exprs} | Cs], CLs) ->
     find_clause_lines(Cs, [{clause,get_line(CL)}|CLs]).
+
+classify_exprs(Exprs) ->
+    case tuple_to_list(lists:last(Exprs)) of
+        [macro,{_var,Anno,_MACRO} | _] ->
+            {anno, Anno};
+        [T,ExprAnno | Exprs1] ->
+            case erl_anno:is_anno(ExprAnno) of
+                true ->
+                    {anno, ExprAnno};
+                false when T =:= tree ->
+                    {tree, Exprs1};
+                false ->
+                    unknown
+            end
+    end.
 
 %%%-----------------------------------------------------------------
 %%% Add a link target for each line and one for each function definition.