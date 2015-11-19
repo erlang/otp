471d2408de06f3c93507769ce0eb0a9f42c3d119
Björn Gustavsson <bjorn@erlang.org>
2015-11-19 10:17:28 +0100

    erl_prim_loader: Remove code for handling OSE

--- erts/preloaded/src/erl_prim_loader.erl
+++ erts/preloaded/src/erl_prim_loader.erl
@@ -1405,8 +1405,6 @@ absname_vr([Drive, $\: | NameRest], _) ->
 %% Assumes normalized name
 pathtype(Name) when is_list(Name) -> 
     case erlang:system_info(os_type) of
-	{ose, _}  ->
-	    unix_pathtype(Name);
 	{unix, _}  -> 
 	    unix_pathtype(Name);
 	{win32, _} ->
