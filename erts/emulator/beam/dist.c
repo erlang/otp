d86548e01746aa14074b4df4c157f6d06f3be697
John Eckersberg <jeckersb@redhat.com>
2015-12-16 11:03:42 -0500

    Add patch to crash dump on large distribution
    https://bugzilla.redhat.com/show_bug.cgi?id=1291822
    https://bugzilla.redhat.com/show_bug.cgi?id=1291855
    https://bugzilla.redhat.com/show_bug.cgi?id=1291856
    https://bugzilla.redhat.com/show_bug.cgi?id=1291857

--- erts/emulator/beam/dist.c
+++ erts/emulator/beam/dist.c
@@ -1963,7 +1963,7 @@ dist_port_command(Port *prt, ErtsDistOutputBuf *obuf)
     ERTS_SMP_LC_ASSERT(erts_lc_is_port_locked(prt));
 
     if (size > (Uint) INT_MAX)
-	erts_exit(ERTS_ABORT_EXIT,
+	erts_exit(ERTS_DUMP_EXIT,
 		 "Absurdly large distribution output data buffer "
 		 "(%beu bytes) passed.\n",
 		 size);
@@ -2003,7 +2003,7 @@ dist_port_commandv(Port *prt, ErtsDistOutputBuf *obuf)
     ERTS_SMP_LC_ASSERT(erts_lc_is_port_locked(prt));
 
     if (size > (Uint) INT_MAX)
-	erts_exit(ERTS_ABORT_EXIT,
+	erts_exit(ERTS_DUMP_EXIT,
 		 "Absurdly large distribution output data buffer "
 		 "(%beu bytes) passed.\n",
 		 size);
