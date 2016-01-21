cf22986055ce9c23886c96643e64aebb4b60a51d
Hans Bolinder <hasse@erlang.org>
2016-01-21 16:30:03 +0100

    compiler: Let module_info(attributes) skip more attributes
    'callback' and 'optional_callbacks' are no longer wild attributes.

--- lib/compiler/src/v3_kernel.erl
+++ lib/compiler/src/v3_kernel.erl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 1999-2015. All Rights Reserved.
+%% Copyright Ericsson AB 1999-2016. All Rights Reserved.
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -147,6 +147,7 @@ include_attribute(callback) -> false;
 include_attribute(opaque) -> false;
 include_attribute(export_type) -> false;
 include_attribute(record) -> false;
+include_attribute(optional_callbacks) -> false;
 include_attribute(_) -> true.
 
 function({#c_var{name={F,Arity}=FA},Body}, St0) ->
