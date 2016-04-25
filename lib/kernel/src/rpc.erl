0c2954dd7fa1923a26d7da05e366bca63d5fe53e
Zandra <zandra@erlang.org>
2016-04-25 09:05:30 +0200

    use maps instead of gb_trees to optimize the rpc server

--- lib/kernel/src/rpc.erl
+++ lib/kernel/src/rpc.erl
@@ -63,7 +63,7 @@
 
 %%------------------------------------------------------------------------
 
--type state() :: gb_trees:tree(pid(), {pid(), reference()}).
+-type state() :: map().
 
 %%------------------------------------------------------------------------
 
@@ -91,7 +91,7 @@ stop(Rpc) ->
 
 init([]) ->
     process_flag(trap_exit, true),
-    {ok, gb_trees:empty()}.
+    {ok, maps:new()}.
 
 -spec handle_call(term(), term(), state()) ->
         {'noreply', state()} |
@@ -131,14 +131,14 @@ handle_cast(_, S) ->
 -spec handle_info(term(), state()) -> {'noreply', state()}.
 
 handle_info({'DOWN', _, process, Caller, normal}, S) ->
-    {noreply, gb_trees:delete(Caller, S)};
+    {noreply, maps:remove(Caller, S)};
 handle_info({'DOWN', _, process, Caller, Reason}, S) ->
-    case gb_trees:lookup(Caller, S) of
-        {value, To} ->
+    case maps:get(Caller, S, undefined) of
+	undefined ->
+	    {noreply, S};
+	{_, _} = To ->
 	    gen_server:reply(To, {badrpc, {'EXIT', Reason}}),
-	    {noreply, gb_trees:delete(Caller, S)};
-	none ->
-	    {noreply, S}
+	    {noreply, maps:remove(Caller, S)}
     end;
 handle_info({From, {sbcast, Name, Msg}}, S) ->
     _ = case catch Name ! Msg of  %% use catch to get the printout
@@ -192,7 +192,7 @@ handle_call_call(Mod, Fun, Args, Gleader, To, S) ->
 		      end,
 		  gen_server:reply(To, Reply)
 	  end),
-    {noreply, gb_trees:insert(Caller, To, S)}.
+    {noreply, maps:put(Caller, To, S)}.
 
 
 %% RPC aid functions ....
