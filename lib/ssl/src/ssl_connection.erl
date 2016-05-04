e99bc5a149eaaaacb81a84a2d2608f9893242e5d
Andreas Schultz <aschultz@tpip.net>
2016-05-04 17:41:49 +0200

    dtls: replace tls_record with RecordCB in connection_info
    Conflicts:
    	lib/ssl/src/ssl_connection.erl

--- lib/ssl/src/ssl_connection.erl
+++ lib/ssl/src/ssl_connection.erl
@@ -1054,8 +1054,11 @@ format_status(terminate, [_, StateName, State]) ->
 %%--------------------------------------------------------------------
 connection_info(#state{sni_hostname = SNIHostname, 
 		       session = #session{cipher_suite = CipherSuite}, 
-		       negotiated_version = Version, ssl_options = Opts}) ->
-    [{protocol, tls_record:protocol_version(Version)}, 
+		       protocol_cb = Connection,
+		       negotiated_version =  {_,_} = Version, 
+		       ssl_options = Opts}) ->
+    RecordCB = record_cb(Connection),
+    [{protocol, RecordCB:protocol_version(Version)},
      {cipher_suite, ssl_cipher:erl_suite_definition(CipherSuite)}, 
      {sni_hostname, SNIHostname}] ++ ssl_options_list(Opts).
 
