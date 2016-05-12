cbee76c388d08100856a92f14850bb3302a51207
Sverker Eriksson <sverker@erlang.org>
2016-05-12 19:20:41 +0200

    erts: Remove compiler warning
    'hx' may be used uninitialized

--- erts/emulator/beam/beam_load.c
+++ erts/emulator/beam/beam_load.c
@@ -4368,6 +4368,7 @@ gen_get_map_elements(LoaderState* stp, GenOpArg Fail, GenOpArg Src,
     int good_hash;
 #endif
 
+    ERTS_UNDEF(hx, 0);
     ASSERT(Size.type == TAG_u);
 
     NEW_GENOP(stp, op);
