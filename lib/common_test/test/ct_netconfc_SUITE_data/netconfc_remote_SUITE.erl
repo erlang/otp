e3800f7cbf158463ec958f585672bb3d8ca8b130
Siri Hansen <siri@erlang.org>
2016-05-26 12:18:45 +0200

    Don't generate ssh key files in netconf client test
    Generating the dsa files can be very slow on some machines. Use
    hardcoded files instead.

--- lib/common_test/test/ct_netconfc_SUITE_data/netconfc_remote_SUITE.erl
+++ lib/common_test/test/ct_netconfc_SUITE_data/netconfc_remote_SUITE.erl
@@ -51,7 +51,7 @@ init_per_testcase(Case, Config) ->
     stop_node(Case),
     Config.
 
-end_per_testcase(Case, Config) ->
+end_per_testcase(Case, _Config) ->
     stop_node(Case),
     ok.
 
@@ -61,16 +61,13 @@ stop_node(Case) ->
     rpc:call(Node,erlang,halt,[]).
 
 
-init_per_suite() ->
-    [{timetrap,2*?default_timeout}]. % making dsa files can be slow
 init_per_suite(Config) ->
     case ssh:start() of
 	Ok when Ok==ok; Ok=={error,{already_started,ssh}} ->
 	    ct:log("SSH started locally",[]),
-	    {ok, _} =  netconfc_test_lib:get_id_keys(Config),
-	    netconfc_test_lib:make_dsa_files(Config),
-	    ct:log("dsa files created",[]),
-	    Config;
+	    SshDir = filename:join(filename:dirname(code:which(?MODULE)),
+				   "ssh_dir"),
+	    [{ssh_dir,SshDir}|Config];
 	Other ->
 	    ct:log("could not start ssh locally: ~p",[Other]),
 	    {skip, "SSH could not be started locally!"}
@@ -79,7 +76,6 @@ init_per_suite(Config) ->
 end_per_suite(Config) ->
     ssh:stop(),
     crypto:stop(),
-    netconfc_test_lib:remove_id_keys(Config),
     Config.
 
 %% This test case is related to seq12645
@@ -93,7 +89,7 @@ remote_crash(Config) ->
     case rpc:call(Node,ssh,start,[]) of
 	Ok when Ok==ok; Ok=={error,{already_started,ssh}} ->
 	    ct:log("SSH started remote",[]),
-	    Server = rpc:call(Node,?NS,start,[?config(data_dir,Config)]),
+	    ns(Node,start,[?config(ssh_dir,Config)]),
 	    ct:log("netconf server started remote",[]),
 	    remote_crash(Node,Config);
 	Other ->
@@ -102,8 +98,7 @@ remote_crash(Config) ->
     end.
 
 remote_crash(Node,Config) ->
-    DataDir = ?config(data_dir,Config),
-    {ok,Client} = open_success(Node,DataDir),
+    {ok,Client} = open_success(Node,?config(ssh_dir,Config)),
 
     ns(Node,expect_reply,[{'create-subscription',[stream]},ok]),
     ?ok = ct_netconfc:create_subscription(Client),
