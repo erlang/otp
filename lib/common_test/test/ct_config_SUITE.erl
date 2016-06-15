bd5066b6dbb2ed4a5b96f84f29feedc1481481b7
Siri Hansen <siri@erlang.org>
2016-06-15 15:26:30 +0200

    [ct test] Skip system time related tests on TimeWarpingOS
    Tests using system time can be skipped on test hosts set up to do a
    lot of time warps.

--- lib/common_test/test/ct_config_SUITE.erl
+++ lib/common_test/test/ct_config_SUITE.erl
@@ -113,10 +113,14 @@ userconfig_static(Config) when is_list(Config) ->
              ["config_static_SUITE"]).
 
 userconfig_dynamic(Config) when is_list(Config) ->
-    run_test(config_dynamic_SUITE,
-	     Config,
-	     {userconfig, {config_driver, "config_server"}},
-             ["config_dynamic_SUITE"]).
+    case skip_dynamic() of
+	true -> {skip,"TimeWarpingOS"};
+	false ->
+	    run_test(config_dynamic_SUITE,
+		     Config,
+		     {userconfig, {config_driver, "config_server"}},
+		     ["config_dynamic_SUITE"])
+    end.
 
 testspec_legacy(Config) when is_list(Config) ->
     DataDir = ?config(data_dir, Config),
@@ -147,16 +151,20 @@ testspec_static(Config) when is_list(Config) ->
     file:delete(filename:join(ConfigDir, "spec_static.spec")).
 
 testspec_dynamic(Config) when is_list(Config) ->
-    DataDir = ?config(data_dir, Config),
-    ConfigDir = ?config(config_dir, Config),
-    make_spec(DataDir, ConfigDir, "spec_dynamic.spec",
-	      [config_dynamic_SUITE],
-	      [{userconfig, {config_driver, "config_server"}}]),
-    run_test(config_dynamic_SUITE,
-	     Config,
-	     {spec, filename:join(ConfigDir, "spec_dynamic.spec")},
-             []),
-    file:delete(filename:join(ConfigDir, "spec_dynamic.spec")).
+    case skip_dynamic() of
+	true -> {skip,"TimeWarpingOS"};
+	false ->
+	    DataDir = ?config(data_dir, Config),
+	    ConfigDir = ?config(config_dir, Config),
+	    make_spec(DataDir, ConfigDir, "spec_dynamic.spec",
+		      [config_dynamic_SUITE],
+		      [{userconfig, {config_driver, "config_server"}}]),
+	    run_test(config_dynamic_SUITE,
+		     Config,
+		     {spec, filename:join(ConfigDir, "spec_dynamic.spec")},
+		     []),
+	    file:delete(filename:join(ConfigDir, "spec_dynamic.spec"))
+    end.
 
 
 
@@ -197,6 +205,23 @@ setup_env(Test, Config, CTConfig) ->
 
 reformat_events(Events, EH) ->
     ct_test_support:reformat(Events, EH).
+
+
+%%%-----------------------------------------------------------------
+%%% Test related to 'localtime' will often fail if the test host is
+%%% time warping, so let's just skip the 'dynamic' tests then.
+skip_dynamic() ->
+    case os:getenv("TS_EXTRA_PLATFORM_LABEL") of
+	TSExtraPlatformLabel when is_list(TSExtraPlatformLabel) ->
+	    case string:str(TSExtraPlatformLabel,"TimeWarpingOS") of
+		0 -> false;
+		_ -> true
+	    end;
+	_ ->
+	    false
+    end.
+
+
 
 %%%-----------------------------------------------------------------
 %%% TEST EVENTS
