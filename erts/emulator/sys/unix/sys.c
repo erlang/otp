ca7578aca806a6eb3e816c918120db78f485f5cb
Rickard Green <rickard@erlang.org>
2016-09-26 15:05:21 +0200

    Fix initialization dependencies between time, lock-check and lcnt

--- erts/emulator/sys/unix/sys.c
+++ erts/emulator/sys/unix/sys.c
@@ -559,6 +559,10 @@ erts_sys_pre_init(void)
 
     report_exit_list = NULL;
 
+#ifdef ERTS_ENABLE_LOCK_CHECK
+    erts_lc_init();
+#endif
+
 #ifdef ERTS_ENABLE_LOCK_COUNT
     erts_lcnt_init();
 #endif
