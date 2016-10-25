e21a6c9ec713219b0aa2b2a4224f97e713d7337f
Guilherme Andrade <g@gandrade.net>
2016-10-25 22:05:10 +0100

    Add math:fmod/2 BIF
    Returns the (floating point) remainder of first argument divided
    by second argument.

--- erts/emulator/beam/erl_math.c
+++ erts/emulator/beam/erl_math.c
@@ -256,3 +256,8 @@ BIF_RETTYPE math_floor_1(BIF_ALIST_1)
 {
     return math_call_1(BIF_P, floor, BIF_ARG_1);
 }
+
+BIF_RETTYPE math_fmod_2(BIF_ALIST_2)
+{
+    return math_call_2(BIF_P, fmod, BIF_ARG_1, BIF_ARG_2);
+}
