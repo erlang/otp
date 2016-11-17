4d7f8c2ab1a7b8c204d932166837b58d08de6506
Sverker Eriksson <sverker@erlang.org>
2016-11-17 16:49:30 +0100

    erts: Add cbprintf for Callback Printing

--- erts/include/internal/erl_printf.h
+++ erts/include/internal/erl_printf.h
@@ -41,12 +41,18 @@ struct erts_dsprintf_buf_t_ {
 
 #define ERTS_DSPRINTF_BUF_INITER(GFUNC) {NULL, 0, 0, (GFUNC)}
 
+typedef int (*fmtfn_t)(void*, char*, size_t);
+
+int erts_write_fd(void *vfdp, char* buf, size_t len);
+int erts_write_ds(void *vdsbufp, char* buf, size_t len);
+
 int erts_printf(const char *, ...);
 int erts_fprintf(FILE *, const char *, ...);
 int erts_fdprintf(int, const char *, ...);
 int erts_sprintf(char *, const char *, ...);
 int erts_snprintf(char *, size_t, const char *, ...);
 int erts_dsprintf(erts_dsprintf_buf_t *, const char *, ...);
+int erts_cbprintf(fmtfn_t, void*, const char*, ...);
 
 int erts_vprintf(const char *, va_list);
 int erts_vfprintf(FILE *, const char *, va_list);
@@ -54,5 +60,6 @@ int erts_vfdprintf(int, const char *, va_list);
 int erts_vsprintf(char *, const char *, va_list);
 int erts_vsnprintf(char *, size_t, const char *, va_list);
 int erts_vdsprintf(erts_dsprintf_buf_t *, const char *, va_list);
+int erts_vcbprintf(fmtfn_t, void*, const char*, va_list);
 
 #endif /* #ifndef ERL_PRINTF_H_ */
