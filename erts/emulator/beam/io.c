erlang/otp

commit 3671dd2d426208cf0639530b294da083b5e75acd

erts: Emasculate binaries that are scheduled in port_control

erts/emulator/beam/io.c
@@ -4884,10 +4884,16 @@ erts_port_control(Process* c_p,
 	ASSERT(!tmp_alloced);
 	if (*ebinp == HEADER_SUB_BIN)
 	    ebinp = binary_val(((ErlSubBin *) ebinp)->orig);
+
 	if (*ebinp != HEADER_PROC_BIN)
 	    copy = 1;
 	else {
-	    binp = ((ProcBin *) ebinp)->val;
+            ProcBin *pb = (ProcBin *) ebinp;
+
+            if (pb->flags)
+                erts_emasculate_writable_binary(pb);
+
+	    binp = pb->val;
 	    ASSERT(bufp <= bufp + size);
 	    ASSERT(binp->orig_bytes <= bufp
 		   && bufp + size <= binp->orig_bytes + binp->orig_size);

commit cfe4b5c4aaa0d86f9d98095515ae6582a053cc47

erts: Fix emasculation of binaries in port_control
This fixes a bug introduced in 3671dd2d

erts/emulator/beam/io.c
@@ -4889,9 +4889,17 @@ erts_port_control(Process* c_p,
 	    copy = 1;
 	else {
             ProcBin *pb = (ProcBin *) ebinp;
+            int offset = bufp - pb->val->orig_bytes;
 
-            if (pb->flags)
+	    ASSERT(pb->val->orig_bytes <= bufp
+		   && bufp + size <= pb->val->orig_bytes + pb->val->orig_size);
+
+            if (pb->flags) {
                 erts_emasculate_writable_binary(pb);
+
+                /* The procbin may have been reallocated, so update bufp */
+                bufp = pb->val->orig_bytes + offset;
+            }
 
 	    binp = pb->val;
 	    ASSERT(bufp <= bufp + size);