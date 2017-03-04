58632af80fd43955ec58c021e5c0d04caa1840de
Peer Stritzinger <peer@stritzinger.com>
2017-03-04 15:08:41 +0100

    erts_allctr_wrappers should be declared extern in header

--- erts/emulator/beam/erl_alloc.h
+++ erts/emulator/beam/erl_alloc.h
@@ -146,7 +146,7 @@ typedef struct ErtsAllocatorWrapper_t_ {
     void (*unlock)(void);
     struct ErtsAllocatorWrapper_t_* next;
 }ErtsAllocatorWrapper_t;
-ErtsAllocatorWrapper_t *erts_allctr_wrappers;
+extern ErtsAllocatorWrapper_t *erts_allctr_wrappers;
 extern int erts_allctr_wrapper_prelocked;
 extern erts_tsd_key_t erts_allctr_prelock_tsd_key;
 void erts_allctr_wrapper_prelock_init(ErtsAllocatorWrapper_t* wrapper);
