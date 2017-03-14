erlang/otp

commit ce431409d0daba5630e36173dcb751f0ab65e5bb

Refactor code generation options
Most options to the code generation pass are passed through the
process dictionary. At the same time, an Erule or Erules argument
is passed to most code generation functions. The Erule argument
is only an atom indicating the encoding rules ('ber', 'per', or
'uper').

Introduce a new record #gen{} to contain code generation options
and parameters. Pass it as the Erule argument (renaming it to Gen
in functions that we will have to touch anyway). In this commit,
eliminate the use of the variable 'encoding_options' in the process
dictionary.

lib/asn1/src/asn1ct_gen_per.erl
@@ -113,11 +113,7 @@ gen_encode_prim(Erules, D) ->
     Value = {var,atom_to_list(asn1ct_gen:mk_var(asn1ct_name:curr(val)))},
     gen_encode_prim(Erules, D, Value).
 
-gen_encode_prim(Erules, #type{}=D, Value) ->
-    Aligned = case Erules of
-		  uper -> false;
-		  per -> true
-	      end,
+gen_encode_prim(#gen{erule=per,aligned=Aligned}, #type{}=D, Value) ->
     Imm = gen_encode_prim_imm(Value, D, Aligned),
     asn1ct_imm:enc_cg(Imm, Aligned).
 
@@ -284,11 +280,7 @@ gen_dec_external(Ext, BytesVar) ->
 	      _ -> [{asis,Mod},":"]
 	  end,{asis,dec_func(Type)},"(",BytesVar,")"]).
 
-gen_dec_imm(Erule, #type{def=Name,constraint=C}) ->
-    Aligned = case Erule of
-		  uper -> false;
-		  per -> true
-	      end,
+gen_dec_imm(#gen{erule=per,aligned=Aligned}, #type{def=Name,constraint=C}) ->
     gen_dec_imm_1(Name, C, Aligned).
 
 gen_dec_imm_1('ASN1_OPEN_TYPE', Constraint, Aligned) ->

lib/asn1/src/asn1_records.hrl
@@ -96,6 +96,16 @@
 	 error_context				%Top-level thingie (contains line numbers)
 	}).
 
+%% Code generation parameters and options.
+-record(gen,
+        {erule=ber :: 'ber' | 'per',
+         der=false :: boolean(),
+         aligned=false :: boolean(),
+         rec_prefix="" :: string(),
+         macro_prefix="" :: string(),
+         options=[] :: [any()]
+        }).
+
 %% state record used by back-end at partial decode
 %% active is set to 'yes' when a partial decode function is generated.
 %% prefix is set to 'dec-inc-' or 'dec-partial-' is for

lib/asn1/src/asn1ct.erl
@@ -838,33 +838,47 @@ generate({M,GenTOrV}, OutFile, EncodingRule, Options) ->
     debug_on(Options),
     setup_bit_string_format(Options),
     setup_legacy_erlang_types(Options),
-    put(encoding_options,Options),
     asn1ct_table:new(check_functions),
 
+    Gen = init_gen_record(EncodingRule, Options),
+
     %% create decoding function names and taglists for partial decode
-    case (catch specialized_decode_prepare(EncodingRule,M,GenTOrV,Options)) of
-        {error, Reason}  -> warning("Error in configuration file: ~n~p~n",
-                                    [Reason], Options,
-                                    "Error in configuration file");
-        _                -> ok
+    try
+        specialized_decode_prepare(Gen, M)
+    catch
+        throw:{error, Reason} ->
+            warning("Error in configuration file: ~n~p~n",
+                    [Reason], Options,
+                    "Error in configuration file")
     end,
 
-    Result = 
-	case (catch asn1ct_gen:pgen(OutFile,EncodingRule,
-				   M#module.name,GenTOrV,Options)) of
-	    {'EXIT',Reason2} ->
-		error("~p~n",[Reason2],Options),
-		{error,Reason2};
-	    _ ->
-		ok
-	end,
+    Res = case catch asn1ct_gen:pgen(OutFile, Gen, M#module.name, GenTOrV) of
+              {'EXIT',Reason2} ->
+                  error("~p~n",[Reason2],Options),
+                  {error,Reason2};
+              _ ->
+                  ok
+          end,
     debug_off(Options),
-    erase(encoding_options),
     cleanup_bit_string_format(),
     erase(tlv_format), % used in ber
     erase(class_default_type),% used in ber
     asn1ct_table:delete(check_functions),
-    Result.
+    Res.
+
+init_gen_record(EncodingRule, Options) ->
+    Erule = case EncodingRule of
+                uper -> per;
+                _ -> EncodingRule
+            end,
+    Der = proplists:get_bool(der, Options),
+    Aligned = EncodingRule =:= per,
+    RecPrefix = proplists:get_value(record_name_prefix, Options, ""),
+    MacroPrefix = proplists:get_value(macro_name_prefix, Options, ""),
+    #gen{erule=Erule,der=Der,aligned=Aligned,
+         rec_prefix=RecPrefix,macro_prefix=MacroPrefix,
+         options=Options}.
+
 
 setup_legacy_erlang_types(Opts) ->
     F = case lists:member(legacy_erlang_types, Opts) of
@@ -997,9 +1011,8 @@ input_file_type(File) ->
 		    end
 	    end;
 	".asn1config" ->
-	    case read_config_file(File,asn1_module) of
+	    case read_config_file_info(File, asn1_module) of
 		{ok,Asn1Module} -> 
-%		    put(asn1_config_file,File),
 		    input_file_type(Asn1Module);
 		Error ->
 		    Error
@@ -1370,25 +1383,26 @@ prepare_bytes(Bytes) -> list_to_binary(Bytes).
 vsn() ->
     ?vsn.
 
-specialized_decode_prepare(Erule,M,TsAndVs,Options) ->
-    case lists:member(asn1config,Options) of
+specialized_decode_prepare(#gen{erule=ber,options=Options}=Gen, M) ->
+    case lists:member(asn1config, Options) of
 	true ->
-	    partial_decode_prepare(Erule,M,TsAndVs,Options);
-	_ ->
+	    special_decode_prepare_1(Gen, M);
+	false ->
 	    ok
-    end.
+    end;
+specialized_decode_prepare(_, _) ->
+    ok.
+
 %% Reads the configuration file if it exists and stores information
 %% about partial decode and incomplete decode
-partial_decode_prepare(ber,M,TsAndVs,Options) when is_tuple(TsAndVs) ->
+special_decode_prepare_1(#gen{options=Options}=Gen, M) ->
     %% read configure file
-
-    ModName =
-	case lists:keysearch(asn1config,1,Options) of
-	    {value,{_,MName}} -> MName;
-	    _ -> M#module.name
-	end,
+    ModName = case lists:keyfind(asn1config, 1, Options) of
+                  {_,MName} -> MName;
+                  false -> M#module.name
+              end,
 %%    io:format("ModName: ~p~nM#module.name: ~p~n~n",[ModName,M#module.name]),
-    case read_config_file(ModName) of
+    case read_config_file(Gen, ModName) of
         no_config_file ->
             ok;
         CfgList ->
@@ -1407,11 +1421,7 @@ partial_decode_prepare(ber,M,TsAndVs,Options) when is_tuple(TsAndVs) ->
             Part_inc_tlv_tags = tlv_tags(CommandList2),
             save_config(partial_incomplete_decode,Part_inc_tlv_tags),
             save_gen_state(exclusive_decode,ExclusiveDecode,Part_inc_tlv_tags)
-    end;
-partial_decode_prepare(_,_,_,_) ->
-    ok.
-
-
+    end.
 
 %% create_partial_inc_decode_gen_info/2
 %%
@@ -1863,46 +1873,38 @@ tlv_tag1(<<0:1,PartialTag:7>>,Acc) ->
 tlv_tag1(<<1:1,PartialTag:7,Buffer/binary>>,Acc) ->
     tlv_tag1(Buffer,(Acc bsl 7) bor PartialTag).
     
-%% reads the content from the configuration file and returns the
-%% selected part choosen by InfoType. Assumes that the config file
+%% Reads the content from the configuration file and returns the
+%% selected part chosen by InfoType. Assumes that the config file
 %% content is an Erlang term.
-read_config_file(ModuleName,InfoType) when is_atom(InfoType) ->
-    CfgList = read_config_file(ModuleName),
-    get_config_info(CfgList,InfoType).
+read_config_file_info(ModuleName, InfoType) when is_atom(InfoType) ->
+    Name = ensure_ext(ModuleName, ".asn1config"),
+    CfgList = read_config_file0(Name, []),
+    get_config_info(CfgList, InfoType).
 
+read_config_file(#gen{options=Options}, ModuleName) ->
+    Name = ensure_ext(ModuleName, ".asn1config"),
+    Includes = [I || {i,I} <- Options],
+    read_config_file0(Name, ["."|Includes]).
 
-read_config_file(ModuleName) ->
-    case file:consult(lists:concat([ModuleName,'.asn1config'])) of
+read_config_file0(Name, [D|Dirs]) ->
+    case file:consult(filename:join(D, Name)) of
 	{ok,CfgList} ->
 	    CfgList;
 	{error,enoent} ->
-	    Options = get(encoding_options),
-	    Includes = [I || {i,I} <- Options],
-	    read_config_file1(ModuleName,Includes);
+            read_config_file0(Name, Dirs);
 	{error,Reason} ->
 	    Error = "error reading asn1 config file: " ++
 		file:format_error(Reason),
 	    throw({error,Error})
-    end.
-read_config_file1(ModuleName,[]) ->
-    case filename:extension(ModuleName) of
-	".asn1config" ->
-            no_config_file;
-	_ ->
-	    read_config_file(lists:concat([ModuleName,".asn1config"]))
     end;
-read_config_file1(ModuleName,[H|T]) ->
-%    File = filename:join([H,lists:concat([ModuleName,'.asn1config'])]),
-    File = filename:join([H,ModuleName]),
-    case file:consult(File) of
-	{ok,CfgList} ->
-	    CfgList;
-	{error,enoent} ->
-	    read_config_file1(ModuleName,T);
-	{error,Reason} ->
-	    Error = "error reading asn1 config file: " ++
-		file:format_error(Reason),
-	    throw({error,Error})
+read_config_file0(_, []) ->
+    no_config_file.
+
+ensure_ext(ModuleName, Ext) ->
+    Name = filename:join([ModuleName]),
+    case filename:extension(Name) of
+        Ext -> Name;
+        _ -> Name ++ Ext
     end.
     
 get_config_info(CfgList,InfoType) ->
@@ -2382,8 +2384,10 @@ format_error({write_error,File,Reason}) ->
     io_lib:format("writing output file ~s failed: ~s",
 		  [File,file:format_error(Reason)]).
 
-is_error(S) when is_record(S, state) ->
-    is_error(S#state.options);
+is_error(#state{options=Opts}) ->
+    is_error(Opts);
+is_error(#gen{options=Opts}) ->
+    is_error(Opts);
 is_error(O) ->
     lists:member(errors, O) orelse is_verbose(O).
 
@@ -2392,8 +2396,10 @@ is_warning(S) when is_record(S, state) ->
 is_warning(O) ->
     lists:member(warnings, O) orelse is_verbose(O).
 
-is_verbose(S) when is_record(S, state) ->
-    is_verbose(S#state.options);
+is_verbose(#state{options=Opts}) ->
+    is_verbose(Opts);
+is_verbose(#gen{options=Opts}) ->
+    is_verbose(Opts);
 is_verbose(O) ->
     lists:member(verbose, O).
 

lib/asn1/src/asn1ct_constructed_ber_bin_v2.erl
@@ -32,7 +32,7 @@
 
 -include("asn1_records.hrl").
 
--import(asn1ct_gen, [emit/1,demit/1,get_record_name_prefix/0]).
+-import(asn1ct_gen, [emit/1,demit/1,get_record_name_prefix/1]).
 
 -define(ASN1CT_GEN_BER,asn1ct_gen_ber_bin_v2).
 
@@ -225,7 +225,7 @@ gen_decode_sequence(Erules,Typename,D) when is_record(D,type) ->
 	    _ ->
 		{false,false}
 	end,
-    RecordName = lists:concat([get_record_name_prefix(),
+    RecordName = lists:concat([get_record_name_prefix(Erules),
 			       asn1ct_gen:list2rname(Typename)]),
     case gen_dec_sequence_call(Erules,Typename,CompList2,Ext,DecObjInf) of
 	no_terms -> % an empty sequence	    
@@ -405,7 +405,7 @@ gen_decode_set(Erules,Typename,D) when is_record(D,type) ->
 	    asn1ct_name:new(tlv)
 
     end,
-    RecordName = lists:concat([get_record_name_prefix(),
+    RecordName = lists:concat([get_record_name_prefix(Erules),
 			       asn1ct_gen:list2rname(Typename)]),
     case gen_dec_sequence_call(Erules,Typename,CompList,Ext,DecObjInf) of
 	no_terms -> % an empty sequence	    
@@ -504,10 +504,8 @@ gen_decode_sof(Erules,TypeName,_InnerTypeName,D) when is_record(D,type) ->
     emit([" || ",{curr,v}," <- ",{curr,tlv},"].",nl,nl,nl]).
     
 
-gen_encode_sof_components(Erules,Typename,SeqOrSetOf,Cont) 
-  when is_record(Cont,type)->
-
-    {Objfun,Objfun_novar,EncObj} = 
+gen_encode_sof_components(Gen, Typename, SeqOrSetOf, #type{}=Cont) ->
+    {Objfun,Objfun_novar,EncObj} =
 	case Cont#type.tablecinf of
 	    [{objfun,_}|_R] ->
 		{", ObjFun",", _",{no_attr,"ObjFun"}};
@@ -517,20 +515,19 @@ gen_encode_sof_components(Erules,Typename,SeqOrSetOf,Cont)
     emit(["'enc_",asn1ct_gen:list2name(Typename),
 	  "_components'([]",Objfun_novar,", AccBytes, AccLen) -> ",nl]),
 
-    case catch lists:member(der,get(encoding_options)) of
-	true when SeqOrSetOf=='SET OF'->
+    case {Gen,SeqOrSetOf} of
+        {#gen{der=true},'SET OF'} ->
 	    asn1ct_func:need({ber,dynamicsort_SETOF,1}),
 	    emit([indent(3),
 		  "{dynamicsort_SETOF(AccBytes),AccLen};",nl,nl]);
-	_ ->
+	{_,_} ->
 	    emit([indent(3),"{lists:reverse(AccBytes),AccLen};",nl,nl])
     end,
     emit(["'enc_",asn1ct_gen:list2name(Typename),
 	  "_components'([H|T]",Objfun,",AccBytes, AccLen) ->",nl]),
     TypeNameSuffix = asn1ct_gen:constructed_suffix(SeqOrSetOf,Cont#type.def),
-    gen_enc_line(Erules,Typename,TypeNameSuffix,Cont,"H",3,
-%		 mandatory,"{EncBytes,EncLen} = ",EncObj),
-		 mandatory,EncObj),
+    gen_enc_line(Gen, Typename, TypeNameSuffix, Cont, "H", 3,
+		 mandatory, EncObj),
     emit([",",nl]),
     emit([indent(3),"'enc_",asn1ct_gen:list2name(Typename),
 	  "_components'(T",Objfun,","]), 
@@ -1035,26 +1032,26 @@ gen_optormand_case('OPTIONAL', Erules, _TopType, _Cname, _Type, Element) ->
     emit([indent(9),"asn1_NOVALUE -> {",
 	  empty_lb(Erules),",0};",nl]),
     emit([indent(9),"_ ->",nl,indent(12)]);
-gen_optormand_case({'DEFAULT',DefaultValue}, Erules, _TopType,
+gen_optormand_case({'DEFAULT',DefaultValue}, Gen, _TopType,
 		   _Cname, Type, Element) ->
     CurrMod = get(currmod),
-    case catch lists:member(der,get(encoding_options)) of
-	true ->
+    case Gen of
+        #gen{erule=ber,der=true} ->
 	    asn1ct_gen_check:emit(Type, DefaultValue, Element);
-	_ ->
+	#gen{erule=ber,der=false} ->
 	    emit([" case ",Element," of",nl]),
 	    emit([indent(9),"asn1_DEFAULT -> {",
-		  empty_lb(Erules),
+		  empty_lb(Gen),
 		  ",0};",nl]),
 	    case DefaultValue of 
 		#'Externalvaluereference'{module=CurrMod,
 					  value=V} ->
 		    emit([indent(9),"?",{asis,V}," -> {",
-			  empty_lb(Erules),",0};",nl]);
+			  empty_lb(Gen),",0};",nl]);
 		_ ->
 		    emit([indent(9),{asis,
 				     DefaultValue}," -> {",
-			  empty_lb(Erules),",0};",nl])
+			  empty_lb(Gen),",0};",nl])
 	    end,
 	    emit([indent(9),"_ ->",nl,indent(12)])
     end.
@@ -1429,7 +1426,7 @@ mkfuncname(TopType,Cname,WhatKind,Prefix,Suffix) ->
 	    {F, "?MODULE", F}
     end.
 
-empty_lb(ber) ->
+empty_lb(#gen{erule=ber}) ->
     "<<>>".
 
 value_match(Index,Value) when is_atom(Value) ->

lib/asn1/src/asn1ct_constructed_per.erl
@@ -32,17 +32,26 @@
 -include("asn1_records.hrl").
 %-compile(export_all).
 
--import(asn1ct_gen, [emit/1,demit/1,get_record_name_prefix/0]).
--import(asn1ct_func, [call/3]).
+-import(asn1ct_gen, [emit/1,demit/1,get_record_name_prefix/1]).
+
+-type type_name() :: any().
 
 %% ENCODE GENERATOR FOR SEQUENCE TYPE  ** **********
 
 
-gen_encode_set(Erules,TypeName,D) ->
-    gen_encode_constructed(Erules,TypeName,D).
+-spec gen_encode_set(Gen, TypeName, #type{}) -> 'ok' when
+      Gen :: #gen{},
+      TypeName :: type_name().
 
-gen_encode_sequence(Erules,TypeName,D) ->
-    gen_encode_constructed(Erules,TypeName,D).
+gen_encode_set(Gen, TypeName, D) ->
+    gen_encode_constructed(Gen, TypeName, D).
+
+-spec gen_encode_sequence(Gen, TypeName, #type{}) -> 'ok' when
+      Gen :: #gen{},
+      TypeName :: type_name().
+
+gen_encode_sequence(Gen, TypeName, D) ->
+    gen_encode_constructed(Gen, TypeName, D).
 
 gen_encode_constructed(Erule, Typename, #type{}=D) ->
     asn1ct_name:start(),
@@ -303,7 +312,7 @@ gen_dec_constructed_imm(Erule, Typename, #type{}=D) ->
 				       DecObjInf, Ext, length(Optionals)),
     EmitObjSets = gen_dec_objsets_fun(Erule, ObjSetInfo),
     EmitPack = fun(_) ->
-                       gen_dec_pack(Typename, CompList)
+                       gen_dec_pack(Erule, Typename, CompList)
                end,
     RestGroup = {group,[{safe,EmitObjSets},{safe,EmitPack}]},
     [EmitExt,EmitOpt|EmitComp++[RestGroup]].
@@ -327,8 +336,8 @@ gen_dec_objsets_fun(Erule, ObjSetInfo) ->
             end
     end.
 
-gen_dec_pack(Typename, CompList) ->
-    RecordName = record_name(Typename),
+gen_dec_pack(Gen, Typename, CompList) ->
+    RecordName = record_name(Gen, Typename),
     case Typename of
 	['EXTERNAL'] ->
 	    emit({"   OldFormat={'",RecordName,
@@ -356,10 +365,10 @@ gen_dec_pack(Typename, CompList) ->
 %%  group. Such fake sequences never appear as a top type, and their
 %%  name always start with "ExtAddGroup".
 
-record_name(Typename0) ->
+record_name(Gen, Typename0) ->
     [TopType|Typename1] = lists:reverse(Typename0),
     Typename = filter_ext_add_groups(Typename1, [TopType]),
-    lists:concat([get_record_name_prefix(),
+    lists:concat([get_record_name_prefix(Gen),
 		  asn1ct_gen:list2rname(Typename)]).
 
 filter_ext_add_groups([H|T], Acc) when is_atom(H) ->
@@ -588,8 +597,7 @@ do_gen_decode_sof(Erules, Typename, SeqOrSetOf, D) ->
     emit([",",nl,
 	  {asis,F},"(",Num,", ",Buf,ObjFun,", [])"]).
 
-is_aligned(per) -> true;
-is_aligned(uper) -> false.
+is_aligned(#gen{erule=per,aligned=Aligned}) -> Aligned.
 
 gen_decode_length(Constraint, Erule) ->
     emit(["%% Length with constraint ",{asis,Constraint},nl]),
@@ -1089,27 +1097,31 @@ gen_dec_components_call(Erule, TopType, {Root,ExtList},
 			DecInfObj, Ext, NumberOfOptionals) ->
     gen_dec_components_call(Erule,TopType,{Root,ExtList,[]},
 			    DecInfObj,Ext,NumberOfOptionals);
-gen_dec_components_call(Erule,TopType,CL={Root1,ExtList,Root2},
-			DecInfObj,Ext,NumberOfOptionals) ->
+gen_dec_components_call(Gen, TopType, {Root1,ExtList,Root2}=CL,
+			DecInfObj, Ext, NumberOfOptionals) ->
     %% The type has extensionmarker
     OptTable = create_optionality_table(Root1++Root2),
     Init = {ignore,fun(_) -> {[],[]} end},
     {EmitRoot,Tpos} =
-	gen_dec_comp_calls(Root1++Root2, Erule, TopType, OptTable,
+	gen_dec_comp_calls(Root1++Root2, Gen, TopType, OptTable,
 			   DecInfObj, noext, NumberOfOptionals,
 			   1, []),
-    EmitGetExt = gen_dec_get_extension(Erule),
+    EmitGetExt = gen_dec_get_extension(Gen),
     {extgrouppos,ExtGroupPosLen}  = extgroup_pos_and_length(CL),
     NewExtList = wrap_extensionAdditionGroups(ExtList, ExtGroupPosLen),
-    {EmitExts,_} = gen_dec_comp_calls(NewExtList, Erule, TopType, OptTable,
+    {EmitExts,_} = gen_dec_comp_calls(NewExtList, Gen, TopType, OptTable,
 				      DecInfObj, Ext, NumberOfOptionals,
 				      Tpos, []),
     NumExtsToSkip = ext_length(ExtList),
     Finish =
 	fun(St) ->
 		emit([{next,bytes},"= "]),
-		call(Erule, skipextensions,
-		     [{curr,bytes},NumExtsToSkip+1,"Extensions"]),
+                Mod = case Gen of
+                          #gen{erule=per,aligned=false} -> uper;
+                          #gen{erule=per,aligned=true} -> per
+                      end,
+		asn1ct_func:call(Mod, skipextensions,
+                                 [{curr,bytes},NumExtsToSkip+1,"Extensions"]),
 		asn1ct_name:new(bytes),
 		St
 	end,

commit 1831c36cc0d7c98917ae3bd274071c9c450a9c02

asn1ct_gen_per: Fix broken dialyzer suppression function
ce431409d0daba broke generation of dialyzer suppressions
for per and uper.

While we are it, add type tests to asn1ct_func:is_used/1
to avoid similar problems in the future.

lib/asn1/src/asn1ct_func.erl
@@ -65,7 +65,7 @@ generate(Fd) ->
     Funcs = sofs:to_external(Funcs0),
     ok = file:write(Fd, Funcs).
 
-is_used({_,_,_}=MFA) ->
+is_used({M,F,A}=MFA) when is_atom(M), is_atom(F), is_integer(A) ->
     req({is_used,MFA}).
 
 

lib/asn1/src/asn1ct_gen_per.erl
@@ -42,8 +42,12 @@
 %% Generate ENCODING ******************************
 %%****************************************x
 
-dialyzer_suppressions(Erules) ->
-    case asn1ct_func:is_used({Erules,complete,1}) of
+dialyzer_suppressions(#gen{erule=per,aligned=Aligned}) ->
+    Mod = case Aligned of
+              false -> uper;
+              true -> per
+          end,
+    case asn1ct_func:is_used({Mod,complete,1}) of
 	false ->
 	    ok;
 	true ->