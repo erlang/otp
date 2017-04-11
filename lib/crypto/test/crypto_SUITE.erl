f1dc990550c01ad7373c4c4850f7d5b0cb924296
Hans Nilsson <hans@erlang.org>
2017-04-11 12:00:42 +0200

    crypto: Increase prime to pass with FIPS-enabled cryptolib
    Fix for problem introduced with OTP-14140

--- lib/crypto/test/crypto_SUITE.erl
+++ lib/crypto/test/crypto_SUITE.erl
@@ -1135,7 +1135,7 @@ group_config(rsa = Type, Config) ->
                   rsa_oaep(),
                   no_padding()
                  ],
-    Generate = [{rsa, 2048, 3}, {rsa, 3072, 65537}],
+    Generate = [{rsa, 2048, 17}, {rsa, 3072, 65537}],
     [{sign_verify, SignVerify}, {pub_priv_encrypt, PubPrivEnc}, {generate, Generate} | Config];
 group_config(dss = Type, Config) ->
     Msg = dss_plain(),
