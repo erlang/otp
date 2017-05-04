457254ab726855850fd6690135b19bddd660283f
Siri Hansen <siri@erlang.org>
2017-05-04 13:36:16 +0200

    Add typer to excluded applications in upgrade_SUITE
    When removing typer as application, it was also removed from the list
    of applications to exclude from upgrade tests. However, since typer
    still exist in older OTP releases, it must remain in the exclude list
    as long as these releases are used in the test.

--- erts/test/upgrade_SUITE.erl
+++ erts/test/upgrade_SUITE.erl
@@ -39,7 +39,7 @@
 %% - dialyzer requires hipe (in the .app file)
 %% - erl_interface, jinterface support no upgrade
 -define(appup_exclude, 
-	[dialyzer,hipe,erl_interface,jinterface,ose]).
+	[dialyzer,hipe,typer,erl_interface,jinterface,ose]).
 
 init_per_suite(Config) ->
     %% Check that a real release is running, not e.g. cerl
