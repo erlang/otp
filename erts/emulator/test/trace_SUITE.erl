98fde55efd0f8c3f5013329cf3d1acfe7516ab38
Björn Gustavsson <bjorn@erlang.org>
2017-05-30 14:25:34 +0200

    receive_SUITE: Remove ?line macros

--- erts/emulator/test/trace_SUITE.erl
+++ erts/emulator/test/trace_SUITE.erl
@@ -184,10 +184,10 @@ receive_trace(Config) when is_list(Config) ->
     {'EXIT', Intruder, {badarg, _}} = receive_first(),
 
     %% Untrace the process; we should not receive anything.
-    ?line 1 = erlang:trace(Receiver, false, ['receive']),
-    ?line Receiver ! {hello, there},
-    ?line Receiver ! any_garbage,
-    ?line receive_nothing(),
+    1 = erlang:trace(Receiver, false, ['receive']),
+    Receiver ! {hello, there},
+    Receiver ! any_garbage,
+    receive_nothing(),
 
     %% Verify restrictions in matchspec for 'receive'
     F3 = fun (Pat) -> {'EXIT', {badarg,_}} = (catch erlang:trace_pattern('receive', Pat, [])) end,
