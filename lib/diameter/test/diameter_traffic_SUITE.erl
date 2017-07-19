1339269fefcbd688b3eb4bde2bb6c55fe2e1b17d
Anders Svensson <anders@erlang.org>
2017-07-19 14:48:12 +0200

    Increase init_per_group timetrap in traffic suite
    Connection establishment is slow on some test hosts.

--- lib/diameter/test/diameter_traffic_SUITE.erl
+++ lib/diameter/test/diameter_traffic_SUITE.erl
@@ -30,6 +30,7 @@
          init_per_suite/0,
          init_per_suite/1,
          end_per_suite/1,
+         init_per_group/1,
          init_per_group/2,
          end_per_group/2,
          init_per_testcase/2,
@@ -313,6 +314,9 @@ end_per_suite(_Config) ->
     ok.
 
 %% --------------------
+
+init_per_group(_) ->
+    [{timetrap, {seconds, 30}}].
 
 init_per_group(Name, Config)
   when Name == shuffle;
