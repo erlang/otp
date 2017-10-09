7769c7a29806cd2c7a9bbff76b2fe32f5401e9ac
John Högberg <john@erlang.org>
2017-10-09 20:56:25 +0200

    Remove efile_SUITE:async_dist
    This test is irrelevant as the new implementation doesn't use async
    threads.

--- erts/emulator/test/efile_SUITE.erl
+++ erts/emulator/test/efile_SUITE.erl
@@ -19,12 +19,9 @@
 
 -module(efile_SUITE).
 -export([all/0, suite/0]).
--export([async_dist/1,
-         iter_max_files/1,
-         proc_zero_sized_files/1
-        ]).
+-export([iter_max_files/1, proc_zero_sized_files/1]).
 
--export([do_iter_max_files/2, do_async_dist/1]).
+-export([do_iter_max_files/2]).
 
 -include_lib("common_test/include/ct.hrl").
 -include_lib("stdlib/include/assert.hrl").
@@ -32,84 +29,7 @@
 suite() -> [{ct_hooks,[ts_install_cth]}].
 
 all() -> 
-    [iter_max_files, async_dist, proc_zero_sized_files].
-
-do_async_dist(Dir) ->
-    X = 100,
-    AT = erlang:system_info(thread_pool_size),
-    Keys = file_keys(Dir,AT*X,[],[]),
-    Tab = ets:new(x,[ordered_set]),
-    [ ets:insert(Tab,{N,0}) || N <- lists:seq(0,AT-1) ],
-    [ ets:update_counter(Tab,(N rem AT),1) || N <- Keys ],
-    Res = [ V || {_,V} <- ets:tab2list(Tab) ],
-    ets:delete(Tab),
-    {Res, sdev(Res)/X}.
-
-sdev(List) ->
-    Len = length(List),
-    Mean = lists:sum(List)/Len,
-    math:sqrt(lists:sum([ (X - Mean) * (X - Mean) || X <- List ]) / Len).
-
-file_keys(_,0,FdList,FnList) ->
-    [ file:close(FD) || FD <- FdList ],
-    [ file:delete(FN) || FN <- FnList ],
-    [];
-file_keys(Dir,Num,FdList,FnList) ->
-    Name = "dummy"++integer_to_list(Num),
-    FN = filename:join([Dir,Name]),
-    case file:open(FN,[write,raw]) of
-        {ok,FD} ->
-            {file_descriptor,prim_file,{Port,_}} = FD,
-            <<X:32/integer-big>> = 
-            iolist_to_binary(erlang:port_control(Port,$K,[])),
-            [X | file_keys(Dir,Num-1,[FD|FdList],[FN|FnList])];
-        {error,_} ->
-            % Try freeing up FD's if there are any
-            case FdList of 
-                [] ->
-                    exit({cannot_open_file,FN});
-                _ ->
-                    [ file:close(FD) || FD <- FdList ],
-                    [ file:delete(F) || F <- FnList ],
-                    file_keys(Dir,Num,[],[])
-            end
-    end.
-
-%% Check that the distribution of files over async threads is fair
-async_dist(Config) when is_list(Config) ->
-    DataDir = proplists:get_value(data_dir,Config),
-    Dir = filename:dirname(code:which(?MODULE)),
-    AsyncSizes = [7,10,100,255,256,64,63,65],
-    Max = 0.5,
-
-    lists:foreach(fun(Size) ->
-                          {ok,Node} = 
-                          test_server:start_node
-                          (test_iter_max_files,slave,
-                           [{args,
-                             "+A "++integer_to_list(Size)++
-                             " -pa " ++ Dir}]),
-                          {Distr,SD} = rpc:call(Node,?MODULE,do_async_dist,
-                                                [DataDir]),
-                          test_server:stop_node(Node),
-                          if
-                              SD > Max ->
-                                  io:format("Bad async queue distribution for "
-                                            "~p async threads:~n"
-                                            "    Standard deviation is ~p~n"
-                                            "    Key distribution:~n    ~lp~n",
-                                            [Size,SD,Distr]),
-                                  exit({bad_async_dist,Size,SD,Distr});
-                              true ->
-                                  io:format("OK async queue distribution for "
-                                            "~p async threads:~n"
-                                            "    Standard deviation is ~p~n"
-                                            "    Key distribution:~n    ~lp~n",
-                                            [Size,SD,Distr]),
-                                  ok
-                          end
-                  end, AsyncSizes),
-    ok.
+    [iter_max_files, proc_zero_sized_files].
 
 %%
 %% Open as many files as possible. Do this several times and check 
