erlang/otp

commit e1310b8f92a14809dccfca1100a4bf22921991b4

observer: Improve handling of Unicode

lib/observer/src/cdv_bin_cb.erl
@@ -38,6 +38,7 @@ init_bin_page(Parent,{Type,Bin}) ->
       [{"Format \~p",cdv_html_wx,{Type,format_bin_fun("~p",Bin)}},
        {"Format \~tp",cdv_html_wx,{Type,format_bin_fun("~tp",Bin)}},
        {"Format \~w",cdv_html_wx,{Type,format_bin_fun("~w",Bin)}},
+       {"Format \~tw",cdv_html_wx,{Type,format_bin_fun("~tw",Bin)}},
        {"Format \~s",cdv_html_wx,{Type,format_bin_fun("~s",Bin)}},
        {"Format \~ts",cdv_html_wx,{Type,format_bin_fun("~ts",Bin)}},
        {"Hex",cdv_html_wx,{Type,hex_binary_fun(Bin)}},
@@ -56,7 +57,7 @@ format_bin_fun(Format,Bin) ->
 binary_to_term_fun(Bin) ->
     fun() ->
 	    try binary_to_term(Bin) of
-		Term -> plain_html(io_lib:format("~p",[Term]))
+		Term -> plain_html(io_lib:format("~tp",[Term]))
 	    catch error:badarg ->
 		    Warning = "This binary can not be converted to an Erlang term",
 		    observer_html_lib:warning(Warning)

lib/observer/src/observer_html_lib.erl
@@ -142,13 +142,13 @@ dict_table(Tab,{Key0,Value0}, Even) ->
     tr(color(Even), [td("VALIGN=center",pre(Key)), td(pre(Value))]).
 
 proc_state(Tab,{Key0,Value0}, Even) ->
-    Key = lists:flatten(io_lib:format("~s",[Key0])),
+    Key = lists:flatten(io_lib:format("~ts",[Key0])),
     Value = all_or_expand(Tab,Value0),
     tr(color(Even), [td("VALIGN=center",Key), td(pre(Value))]).
 
 all_or_expand(Tab,Term) ->
-    Preview = io_lib:format("~P",[Term,8]),
-    Check = io_lib:format("~P",[Term,100]),
+    Preview = io_lib:format("~tP",[Term,8]),
+    Check = io_lib:format("~tP",[Term,100]),
     Exp = Preview=/=Check,
     all_or_expand(Tab,Term,Preview,Exp).
 all_or_expand(_Tab,Term,Str,false)
@@ -166,13 +166,8 @@ all_or_expand(Tab,Term,Preview,true)
 	  "Click to expand above term")];
 all_or_expand(Tab,Bin,_PreviewStr,_Expand)
   when is_binary(Bin) ->
-    Size = byte_size(Bin),
-    PrevSize = min(Size, 10) * 8,
-    <<Preview:PrevSize, _/binary>> = Bin,
-    Hash = erlang:phash2(Bin),
-    Key = {Preview, Size, Hash},
-    ets:insert(Tab,{Key,Bin}),
-    Term = io_lib:format("~p", [['#OBSBin',Preview,Size,Hash]]),
+    OBSBin = observer_lib:make_obsbin(Bin,Tab),
+    Term = io_lib:format("~tp", [OBSBin]),
     href_proc_port(lists:flatten(Term), true).
 
 color(true) -> io_lib:format("BGCOLOR=\"#~2.16.0B~2.16.0B~2.16.0B\"", tuple_to_list(?BG_EVEN));
@@ -339,9 +334,11 @@ href_proc_bin(From, T, Acc, LTB) ->
     BinStr =
 	case string:tokens(OffsetSizePos,",.| \n") of
 	    [Offset,SizeStr,Pos] when From =:= cdv ->
-		Id = {list_to_integer(Offset),10,list_to_integer(Pos)},
+                Size = list_to_integer(SizeStr),
+                PreviewSize = min(Size,10),
+		Id = {list_to_integer(Offset),PreviewSize,list_to_integer(Pos)},
 		{ok,PreviewBin} = crashdump_viewer:expand_binary(Id),
-		PreviewStr = preview_string(list_to_integer(SizeStr), PreviewBin),
+		PreviewStr = preview_string(Size, PreviewBin),
 		if LTB ->
 			href("TARGET=\"expanded\"",
 			     ["#Binary?offset="++Offset++
@@ -351,14 +348,14 @@ href_proc_bin(From, T, Acc, LTB) ->
 		   true ->
 			PreviewStr
 		end;
-	    [Preview,SizeStr,Md5] when From =:= obs ->
+	    [PreviewIntStr,SizeStr,Md5] when From =:= obs ->
 		Size = list_to_integer(SizeStr),
-		PrevSize =  min(Size, 10) * 8,
-		PreviewStr = preview_string(Size,
-					    <<(list_to_integer(Preview)):PrevSize>>),
+                PreviewInt = list_to_integer(PreviewIntStr),
+		PrevSize = (trunc(math:log2(PreviewInt)/8)+1)*8,
+		PreviewStr = preview_string(Size,<<PreviewInt:PrevSize>>),
 		if LTB ->
 			href("TARGET=\"expanded\"",
-			     ["#OBSBinary?key1="++Preview++
+			     ["#OBSBinary?key1="++PreviewIntStr++
 				  "&key2="++SizeStr++
 				  "&key3="++Md5],
 			     PreviewStr);
@@ -372,14 +369,14 @@ href_proc_bin(From, T, Acc, LTB) ->
 
 preview_string(Size, PreviewBin) when Size > 10 ->
     ["&lt;&lt;",
-     remove_lgt(io_lib:format("~p",[PreviewBin])),
+     remove_lgt(io_lib:format("~tp",[PreviewBin])),
      "...(",
      observer_lib:to_str({bytes,Size}),
      ")",
      "&gt;&gt"];
 preview_string(_, PreviewBin) ->
     ["&lt;&lt;",
-     remove_lgt(io_lib:format("~p",[PreviewBin])),
+     remove_lgt(io_lib:format("~tp",[PreviewBin])),
      "&gt;&gt"].
 
 remove_lgt(Deep) ->

lib/observer/src/observer_lib.erl
@@ -30,7 +30,8 @@
 	 create_attrs/0,
 	 set_listctrl_col_size/2,
 	 create_status_bar/1,
-	 html_window/1, html_window/2
+	 html_window/1, html_window/2,
+         make_obsbin/2
 	]).
 
 -include_lib("wx/include/wx.hrl").
@@ -300,7 +301,7 @@ to_str(Float) when is_float(Float) ->
 to_str({trunc, Float}) when is_float(Float) ->
     float_to_list(Float, [{decimals,0}]);
 to_str(Term) ->
-    io_lib:format("~w", [Term]).
+    io_lib:format("~tw", [Term]).
 
 create_menus([], _MenuBar, _Type) -> ok;
 create_menus(Menus, MenuBar, Type) ->
@@ -520,7 +521,7 @@ link_entry2(Panel,{Target,Str},Cursor) ->
     TC.
 
 to_link(RegName={Name, Node}) when is_atom(Name), is_atom(Node) ->
-    Str = io_lib:format("{~p,~p}", [Name, Node]),
+    Str = io_lib:format("{~tp,~p}", [Name, Node]),
     {RegName, Str};
 to_link(TI = {_Target, _Identifier}) ->
     TI;
@@ -641,11 +642,11 @@ parse_string(Str) ->
 	Tokens = case erl_scan:string(Str, 1, [text]) of
 		     {ok, Ts, _} -> Ts;
 		     {error, {_SLine, SMod, SError}, _} ->
-			 throw(io_lib:format("~s", [SMod:format_error(SError)]))
+			 throw(io_lib:format("~ts", [SMod:format_error(SError)]))
 		 end,
 	case lib:extended_parse_term(Tokens) of
 	    {error, {_PLine, PMod, PError}} ->
-		throw(io_lib:format("~s", [PMod:format_error(PError)]));
+		throw(io_lib:format("~ts", [PMod:format_error(PError)]));
 	    Res -> Res
 	end
     catch
@@ -769,3 +770,26 @@ update_progress_text(PD,Text) ->
     end.
 finish_progress(PD) ->
     wxProgressDialog:destroy(PD).
+
+make_obsbin(Bin,Tab) ->
+    Size = byte_size(Bin),
+    Preview =
+        try
+            %% The binary might be a unicode string, in which case we
+            %% don't want to split it in the middle of a grapheme
+            %% cluster - thus trying string:length and slice.
+            PL1 = min(string:length(Bin), 10),
+            PB1 = string:slice(Bin,0,PL1),
+            PS1 = byte_size(PB1) * 8,
+            <<P1:PS1>> = PB1,
+            P1
+        catch _:_ ->
+                %% Probably not a string, so just split anywhere
+                PS2 = min(Size, 10) * 8,
+                <<P2:PS2, _/binary>> = Bin,
+                P2
+        end,
+    Hash = erlang:phash2(Bin),
+    Key = {Preview, Size, Hash},
+    ets:insert(Tab, {Key,Bin}),
+    ['#OBSBin',Preview,Size,Hash].

lib/observer/src/cdv_atom_cb.erl
@@ -42,7 +42,7 @@ get_info(_) ->
     {Info,TW}.
 
 format({Bin,q}) when is_binary(Bin) ->
-    [$'|binary_to_list(Bin)];
+    [$'|lists:flatten(io_lib:format("~ts",[Bin]))];
 format({Bin,nq}) when is_binary(Bin) ->
     lists:flatten(io_lib:format("~ts",[Bin]));
 format(D) ->

lib/observer/src/cdv_detail_wx.erl
@@ -133,7 +133,7 @@ handle_event(Event, _State) ->
     error({unhandled_event, Event}).
 
 handle_info(_Info, State) ->
-    %% io:format("~p: ~p, Handle info: ~p~n", [?MODULE, ?LINE, _Info]),
+    %% io:format("~p: ~p, Handle info: ~tp~n", [?MODULE, ?LINE, _Info]),
     {noreply, State}.
 
 handle_call(Call, From, _State) ->

lib/observer/src/cdv_html_wx.erl
@@ -62,7 +62,7 @@ handle_info(active, State) ->
     {noreply, State};
 
 handle_info(Info, State) ->
-    io:format("~p:~p: Unhandled info: ~p~n", [?MODULE, ?LINE, Info]),
+    io:format("~p:~p: Unhandled info: ~tp~n", [?MODULE, ?LINE, Info]),
     {noreply, State}.
 
 terminate(_Reason, _State) ->
@@ -72,7 +72,7 @@ code_change(_, _, State) ->
     {ok, State}.
 
 handle_call(Msg, _From, State) ->
-    io:format("~p~p: Unhandled Call ~p~n",[?MODULE, ?LINE, Msg]),
+    io:format("~p~p: Unhandled Call ~tp~n",[?MODULE, ?LINE, Msg]),
     {reply, ok, State}.
 
 handle_cast({detail_win_closed, Id},#state{expand_wins=Opened0}=State) ->
@@ -80,7 +80,7 @@ handle_cast({detail_win_closed, Id},#state{expand_wins=Opened0}=State) ->
     {noreply, State#state{expand_wins=Opened}};
 
 handle_cast(Msg, State) ->
-    io:format("~p~p: Unhandled cast ~p~n",[?MODULE, ?LINE, Msg]),
+    io:format("~p~p: Unhandled cast ~tp~n",[?MODULE, ?LINE, Msg]),
     {noreply, State}.
 
 handle_event(#wx{event=#wxHtmlLink{type=command_html_link_clicked,
@@ -118,7 +118,7 @@ handle_event(#wx{event=#wxHtmlLink{type=command_html_link_clicked,
     {noreply, NewState};
 
 handle_event(Event, State) ->
-    io:format("~p:~p: Unhandled event ~p\n", [?MODULE,?LINE,Event]),
+    io:format("~p:~p: Unhandled event ~tp\n", [?MODULE,?LINE,Event]),
     {noreply, State}.
 
 %%%-----------------------------------------------------------------

lib/observer/src/cdv_info_wx.erl
@@ -65,7 +65,7 @@ handle_info(active, State) ->
     {noreply, State};
 
 handle_info(Info, State) ->
-    io:format("~p:~p: Unhandled info: ~p~n", [?MODULE, ?LINE, Info]),
+    io:format("~p:~p: Unhandled info: ~tp~n", [?MODULE, ?LINE, Info]),
     {noreply, State}.
 
 terminate(_Reason, _State) ->
@@ -88,11 +88,11 @@ handle_call(new_dump, _From, #state{callback=Callback,panel=Panel,
     {reply, ok, State#state{fpanel=NewFPanel,trunc_warn=TW}};
 
 handle_call(Msg, _From, State) ->
-    io:format("~p~p: Unhandled Call ~p~n",[?MODULE, ?LINE, Msg]),
+    io:format("~p~p: Unhandled Call ~tp~n",[?MODULE, ?LINE, Msg]),
     {reply, ok, State}.
 
 handle_cast(Msg, State) ->
-    io:format("~p~p: Unhandled cast ~p~n",[?MODULE, ?LINE, Msg]),
+    io:format("~p~p: Unhandled cast ~tp~n",[?MODULE, ?LINE, Msg]),
     {noreply, State}.
 
 handle_event(#wx{event=#wxMouse{type=left_down},userData=Target}, State) ->
@@ -108,7 +108,7 @@ handle_event(#wx{obj=Obj,event=#wxMouse{type=leave_window}},State) ->
     {noreply, State};
 
 handle_event(Event, State) ->
-    io:format("~p:~p: Unhandled event ~p\n", [?MODULE,?LINE,Event]),
+    io:format("~p:~p: Unhandled event ~tp\n", [?MODULE,?LINE,Event]),
     {noreply, State}.
 
 %%%-----------------------------------------------------------------

lib/observer/src/cdv_multi_wx.erl
@@ -94,7 +94,7 @@ handle_info(active, State) ->
     {noreply, NewState};
 
 handle_info(Info, State) ->
-    io:format("~p:~p: Unhandled info: ~p~n", [?MODULE, ?LINE, Info]),
+    io:format("~p:~p: Unhandled info: ~tp~n", [?MODULE, ?LINE, Info]),
     {noreply, State}.
 
 terminate(_Reason, _State) ->
@@ -112,11 +112,11 @@ handle_call(new_dump, _From, State) ->
     {reply, ok, NewState};
 
 handle_call(Msg, _From, State) ->
-    io:format("~p:~p: Unhandled Call ~p~n",[?MODULE, ?LINE, Msg]),
+    io:format("~p:~p: Unhandled Call ~tp~n",[?MODULE, ?LINE, Msg]),
     {reply, ok, State}.
 
 handle_cast(Msg, State) ->
-    io:format("~p:~p: Unhandled cast ~p~n",[?MODULE, ?LINE, Msg]),
+    io:format("~p:~p: Unhandled cast ~tp~n",[?MODULE, ?LINE, Msg]),
     {noreply, State}.
 
 handle_event(#wx{event=#wxCommand{type=command_listbox_selected,
@@ -136,7 +136,7 @@ handle_event(#wx{event=#wxCommand{type=command_listbox_selected,
     {noreply,NewState};
 
 handle_event(Event, State) ->
-    io:format("~p:~p: Unhandled event ~p\n", [?MODULE,?LINE,Event]),
+    io:format("~p:~p: Unhandled event ~tp\n", [?MODULE,?LINE,Event]),
     {noreply, State}.
 
 %%%%%%%%%%%%%%%%%%%%%%% Internal %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%

commit 24c79977c7974d5415b6d4f7085a651ce1048e6a

[observer] Don't crash on empty binary
Commit e1310b8f introduced a bug which caused the observer tool (and
crashdump_viewer) to crash when trying to display an empty binary. The
same change would also cause leading zeros in a binary to disappear in
the preview. The bug is now corrected.

lib/observer/src/cdv_bin_cb.erl
@@ -71,6 +71,8 @@ hex_binary_fun(Bin) ->
 	    plain_html(io_lib:format("~s",[S]))
     end.
 
+format_hex(<<>>,_) ->
+    [];
 format_hex(<<B1:4,B2:4>>,_) ->
     [integer_to_list(B1,16),integer_to_list(B2,16)];
 format_hex(<<B1:4,B2:4,Bin/binary>>,0) ->

lib/observer/src/observer_html_lib.erl
@@ -355,11 +355,11 @@ href_proc_bin(From, T, Acc, LTB) ->
                                 PreviewStr
                         end
                 end;
-	    [PreviewIntStr,SizeStr,Md5] when From =:= obs ->
+	    [PreviewIntStr,PreviewBitSizeStr,SizeStr,Md5] when From =:= obs ->
 		Size = list_to_integer(SizeStr),
                 PreviewInt = list_to_integer(PreviewIntStr),
-		PrevSize = (trunc(math:log2(PreviewInt)/8)+1)*8,
-		PreviewStr = preview_string(Size,<<PreviewInt:PrevSize>>),
+                PreviewBitSize = list_to_integer(PreviewBitSizeStr),
+		PreviewStr = preview_string(Size,<<PreviewInt:PreviewBitSize>>),
 		if LTB ->
 			href("TARGET=\"expanded\"",
 			     ["#OBSBinary?key1="++PreviewIntStr++

lib/observer/src/observer_lib.erl
@@ -810,7 +810,7 @@ progress_dialog_destroy({Dialog,_,_}) ->
 
 make_obsbin(Bin,Tab) ->
     Size = byte_size(Bin),
-    Preview =
+    {Preview,PreviewBitSize} =
         try
             %% The binary might be a unicode string, in which case we
             %% don't want to split it in the middle of a grapheme
@@ -819,14 +819,14 @@ make_obsbin(Bin,Tab) ->
             PB1 = string:slice(Bin,0,PL1),
             PS1 = byte_size(PB1) * 8,
             <<P1:PS1>> = PB1,
-            P1
+            {P1,PS1}
         catch _:_ ->
                 %% Probably not a string, so just split anywhere
                 PS2 = min(Size, 10) * 8,
                 <<P2:PS2, _/binary>> = Bin,
-                P2
+                {P2,PS2}
         end,
     Hash = erlang:phash2(Bin),
     Key = {Preview, Size, Hash},
     ets:insert(Tab, {Key,Bin}),
-    ['#OBSBin',Preview,Size,Hash].
+    ['#OBSBin',Preview,PreviewBitSize,Size,Hash].