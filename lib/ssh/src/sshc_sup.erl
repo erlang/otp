95d5f3022d6fa5aa74205c214b31f9b747530265
Hans Nilsson <hans@erlang.org>
2018-02-26 12:39:13 +0100

    ssh: Removed unused sshc_sup:stop_child/1

--- lib/ssh/src/sshc_sup.erl
+++ lib/ssh/src/sshc_sup.erl
@@ -27,7 +27,7 @@
 
 -behaviour(supervisor).
 
--export([start_link/0, start_child/1, stop_child/1]).
+-export([start_link/0, start_child/1]).
 
 %% Supervisor callback
 -export([init/1]).
@@ -42,13 +42,6 @@ start_link() ->
 
 start_child(Args) ->
     supervisor:start_child(?MODULE, Args).
-
-stop_child(Client) ->
-    spawn(fun() -> 
-		  ClientSup = whereis(?SSHC_SUP),
-		  supervisor:terminate_child(ClientSup, Client)
-	  end),
-    ok.
 
 %%%=========================================================================
 %%%  Supervisor callback
