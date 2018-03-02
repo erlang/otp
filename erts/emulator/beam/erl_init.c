erlang/otp

commit 22cde2bda706c0bd8574f0a1301170c80b5f4340

erts: Remove eager check io
It is not longer relevant when using the poll thread

erts/emulator/beam/erl_init.c
@@ -1748,22 +1748,9 @@ erl_start(int argc, char **argv)
 		    erts_usage();
 		}
 	    }
-	    else if (has_prefix("ecio", sub_param)) {
-		arg = get_arg(sub_param+4, argv[i+1], &i);
-#ifndef __OSE__
-		if (sys_strcmp("true", arg) == 0)
-		    erts_eager_check_io = 1;
-		else
-#endif
-		if (sys_strcmp("false", arg) == 0)
-		    erts_eager_check_io = 0;
-		else {
-		    erts_fprintf(stderr,
-				 "bad schedule eager check I/O value '%s'\n",
-				 arg);
-		    erts_usage();
-		}
-	    }
+            else if (has_prefix("ecio", sub_param)) {
+                /* ignore argument, eager check io no longer used */
+            }
 	    else if (has_prefix("pp", sub_param)) {
 		arg = get_arg(sub_param+2, argv[i+1], &i);
 		if (sys_strcmp(arg, "true") == 0)

erts/doc/src/erl.xml
@@ -1296,25 +1296,6 @@
               <seealso marker="erlang#system_info_cpu_topology">
               <c>erlang:system_info(cpu_topology)</c></seealso>.</p>
           </item>
-          <tag><marker id="+secio"/><c>+secio true|false</c></tag>
-          <item>
-            <p>Enables or disables eager check I/O scheduling. Defaults
-              to <c>true</c>. The default was changed from <c>false</c>
-              as from ERTS 7.0. The behavior before this
-              flag was introduced corresponds to <c>+secio false</c>.</p>
-            <p>The flag effects when schedulers will check for I/O
-              operations possible to execute, and when such I/O operations
-              will execute. As the parameter name implies,
-              schedulers are more eager to check for I/O when
-              <c>true</c> is passed. This, however, also implies that
-              execution of outstanding I/O operation is not
-              prioritized to the same extent as when <c>false</c> is
-              passed.</p>
-            <p><seealso marker="erlang#system_info_eager_check_io">
-              <c>erlang:system_info(eager_check_io)</c></seealso>
-              returns the value of this parameter used when starting
-              the virtual machine.</p>
-          </item>
           <tag><marker id="+sfwi"/><c>+sfwi Interval</c></tag>
           <item>
             <p>Sets scheduler-forced wakeup interval. All run queues are

erts/doc/src/erlang.xml
@@ -8023,15 +8023,6 @@ ok
               The return value will always be <c>false</c>, as the
               <c>elib_malloc</c> allocator has been removed.</p>
           </item>
-          <tag><marker id="system_info_eager_check_io"/>
-            <c>eager_check_io</c></tag>
-          <item>
-            <p>Returns the value of command-line flag
-              <seealso marker="erl#+secio"><c>+secio</c></seealso> in
-              <c>erl(1)</c>, which is either <c>true</c> or <c>false</c>.
-              For information about the different values, see the
-              documentation of the command-line flag.</p>
-          </item>
           <tag><c>ets_limit</c></tag>
           <item>
             <p>Returns the maximum number of ETS tables allowed. This

erts/emulator/beam/erl_bif_info.c
@@ -2843,7 +2843,7 @@ BIF_RETTYPE system_info_1(BIF_ALIST_1)
 	BIF_RET(am_disabled);
     }
     else if (ERTS_IS_ATOM_STR("eager_check_io",BIF_ARG_1)) {
-	BIF_RET(erts_eager_check_io ? am_true : am_false);
+	BIF_RET(am_true);
     }
     else if (ERTS_IS_ATOM_STR("literal_test",BIF_ARG_1)) {
 #ifdef ERTS_HAVE_IS_IN_LITERAL_RANGE

erts/emulator/beam/erl_process.c
@@ -174,7 +174,6 @@ extern BeamInstr beam_exit[];
 extern BeamInstr beam_continue_exit[];
 
 int ERTS_WRITE_UNLIKELY(erts_default_spo_flags) = SPO_ON_HEAP_MSGQ;
-int ERTS_WRITE_UNLIKELY(erts_eager_check_io) = 1;
 int ERTS_WRITE_UNLIKELY(erts_sched_compact_load);
 int ERTS_WRITE_UNLIKELY(erts_sched_balance_util) = 0;
 Uint ERTS_WRITE_UNLIKELY(erts_no_schedulers);

erts/emulator/beam/erl_process.h
@@ -105,7 +105,6 @@ struct saved_calls {
 };
 
 extern Export exp_send, exp_receive, exp_timeout;
-extern int erts_eager_check_io;
 extern int erts_sched_compact_load;
 extern int erts_sched_balance_util;
 extern Uint erts_no_schedulers;

commit 45a1da4e9dfdb912ea0bdff69d772618d8ce460f

erts: Correctly ignore +secio true
This is a fix for a bug introduced in 22cde2bda

erts/emulator/beam/erl_init.c
@@ -1746,6 +1746,7 @@ erl_start(int argc, char **argv)
 	    }
             else if (has_prefix("ecio", sub_param)) {
                 /* ignore argument, eager check io no longer used */
+                arg = get_arg(sub_param+4, argv[i+1], &i);
             }
 	    else if (has_prefix("pp", sub_param)) {
 		arg = get_arg(sub_param+2, argv[i+1], &i);