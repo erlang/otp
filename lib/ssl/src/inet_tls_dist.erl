erlang/otp

commit 662f3c7ba50ff8ec13d86171bcfc61fd3da9deed

epmd: allow alternative to dns resolving for nodename
This makes it possible to create a custom integration with a
key-value store for example. The key would then point to the
actual address. You would have to write your own epmd module
to make use of that feature.

lib/ssl/src/inet_tls_dist.erl
@@ -518,51 +518,16 @@ gen_setup(Driver, Node, Type, MyNode, LongOrShortNames, SetupTime) ->
 
 do_setup(Driver, Kernel, Node, Type, MyNode, LongOrShortNames, SetupTime) ->
     {Name, Address} = split_node(Driver, Node, LongOrShortNames),
-    case Driver:getaddr(Address) of
+    ErlEpmd = net_kernel:epmd_module(),
+    {ARMod, ARFun} = get_address_resolver(ErlEpmd, Driver),
+    Timer = trace(dist_util:start_timer(SetupTime)),
+    case ARMod:ARFun(Address) of
+    {ok, Ip, TcpPort, Version} ->
+        do_setup_connect(Driver, Kernel, Node, Address, Ip, TcpPort, Version, Type, MyNode, Timer);
 	{ok, Ip} ->
-            Timer = trace(dist_util:start_timer(SetupTime)),
-	    ErlEpmd = net_kernel:epmd_module(),
 	    case ErlEpmd:port_please(Name, Ip) of
 		{port, TcpPort, Version} ->
-                    Opts =
-                        trace(
-                          connect_options(
-                            %%
-                            %% Use verify_server/3 to verify that
-                            %% the server's certificate is for Node
-                            %%
-                            setup_verify_server(
-                              get_ssl_options(client), Node))),
-		    dist_util:reset_timer(Timer),
-                    case ssl:connect(
-                           Address, TcpPort,
-                           [binary, {active, false}, {packet, 4},
-                            Driver:family(), nodelay()] ++ Opts,
-                           net_kernel:connecttime()) of
-			{ok, #sslsocket{pid = DistCtrl} = SslSocket} ->
-                            _ = monitor_pid(DistCtrl),
-                            ok = ssl:controlling_process(SslSocket, self()),
-                            HSData0 = hs_data_common(SslSocket),
-			    HSData =
-                                HSData0#hs_data{
-                                  kernel_pid = Kernel,
-                                  other_node = Node,
-                                  this_node = MyNode,
-                                  socket = DistCtrl,
-                                  timer = Timer,
-                                  this_flags = 0,
-                                  other_version = Version,
-                                  request_type = Type},
-                            link(DistCtrl),
-			    dist_util:handshake_we_started(trace(HSData));
-			Other ->
-			    %% Other Node may have closed since 
-			    %% port_please !
-			    ?shutdown2(
-                               Node,
-                               trace(
-                                 {ssl_connect_failed, Ip, TcpPort, Other}))
-		    end;
+                do_setup_connect(Driver, Kernel, Node, Address, Ip, TcpPort, Version, Type, MyNode, Timer);
 		Other ->
 		    ?shutdown2(
                        Node,
@@ -573,6 +538,47 @@ do_setup(Driver, Kernel, Node, Type, MyNode, LongOrShortNames, SetupTime) ->
 	    ?shutdown2(
                Node,
                trace({getaddr_failed, Driver, Address, Other}))
+    end.
+
+do_setup_connect(Driver, Kernel, Node, Address, Ip, TcpPort, Version, Type, MyNode, Timer) ->
+    Opts =
+        trace(
+            connect_options(
+            %%
+            %% Use verify_server/3 to verify that
+            %% the server's certificate is for Node
+            %%
+            setup_verify_server(
+                get_ssl_options(client), Node))),
+    dist_util:reset_timer(Timer),
+    case ssl:connect(
+        Address, TcpPort,
+        [binary, {active, false}, {packet, 4},
+            Driver:family(), nodelay()] ++ Opts,
+        net_kernel:connecttime()) of
+    {ok, #sslsocket{pid = DistCtrl} = SslSocket} ->
+            _ = monitor_pid(DistCtrl),
+            ok = ssl:controlling_process(SslSocket, self()),
+            HSData0 = hs_data_common(SslSocket),
+        HSData =
+                HSData0#hs_data{
+                kernel_pid = Kernel,
+                other_node = Node,
+                this_node = MyNode,
+                socket = DistCtrl,
+                timer = Timer,
+                this_flags = 0,
+                other_version = Version,
+                request_type = Type},
+            link(DistCtrl),
+    dist_util:handshake_we_started(trace(HSData));
+    Other ->
+    %% Other Node may have closed since
+    %% port_please !
+    ?shutdown2(
+            Node,
+            trace(
+                {ssl_connect_failed, Ip, TcpPort, Other}))
     end.
 
 close(Socket) ->
@@ -642,6 +648,16 @@ verify_server(PeerCert, valid_peer, {CertNodesFun,Node} = S) ->
             end
     end.
 
+
+%% ------------------------------------------------------------
+%% Determine if EPMD module supports address resolving. Default
+%% is to use inet_tcp:getaddr/2.
+%% ------------------------------------------------------------
+get_address_resolver(EpmdModule, Driver) ->
+    case erlang:function_exported(EpmdModule, address_please, 3) of
+        true -> {EpmdModule, address_please};
+        _    -> {Driver, getaddr}
+    end.
 
 %% ------------------------------------------------------------
 %% Do only accept new connection attempts from nodes at our

erts/doc/src/Makefile
@@ -74,6 +74,7 @@ XML_CHAPTER_FILES = \
 	match_spec.xml \
 	crash_dump.xml \
 	alt_dist.xml \
+	alt_disco.xml \
 	driver.xml \
 	absform.xml \
 	inet_cfg.xml \

erts/doc/src/alt_disco.xml
@@ -1,0 +1,93 @@
+<?xml version="1.0" encoding="utf-8" ?>
+<!DOCTYPE chapter SYSTEM "chapter.dtd">
+
+<chapter>
+  <header>
+    <copyright>
+      <year>2018</year><year>2018</year>
+      <holder>Ericsson AB. All Rights Reserved.</holder>
+    </copyright>
+    <legalnotice>
+      Licensed under the Apache License, Version 2.0 (the "License");
+      you may not use this file except in compliance with the License.
+      You may obtain a copy of the License at
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
+      Unless required by applicable law or agreed to in writing, software
+      distributed under the License is distributed on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      See the License for the specific language governing permissions and
+      limitations under the License.
+
+    </legalnotice>
+
+    <title>How to Implement an Alternative Service Discovery for Erlang Distribution
+    </title>
+    <prepared>Timmo Verlaan</prepared>
+    <responsible></responsible>
+    <docno></docno>
+    <approved></approved>
+    <checked></checked>
+    <date>2018-04-25</date>
+    <rev>PA1</rev>
+    <file>alt_disco.xml</file>
+  </header>
+  <p>
+    This section describes how to implement an alternative discovery mechanism
+    for Erlang distribution. Discovery is normally done using DNS and the
+    Erlang Port Mapper Daemon (EPMD) for port discovery.
+  </p>
+
+  <note><p>
+    Support for alternative service discovery mechanisms was added in Erlang/OTP
+    21.
+  </p></note>
+
+
+  <section>
+    <title>Introduction</title>
+    <p>To implement your own service discovery module you have to write your own
+    EPMD module. The <seealso marker="kernel:erl_epmd">EPMD module</seealso> is
+    responsible for providing the location of another node. The distribution
+    modules (<c>inet_tcp_dist</c>/<c>inet_tls_dist</c>) call the EPMD module to
+    get the IP address and port of the other node. The EPMD module that is part
+    of Erlang/OTP will resolve the hostname using DNS and uses the EPMD unix
+    process to get the port of another node. The EPMD unix process does this by
+    connecting to the other node on a well-known port, port 4369.</p>
+  </section>
+
+  <section>
+    <title>Discovery module</title>
+    <p>The discovery module needs to implement the same API as the regular
+    <seealso marker="kernel:erl_epmd">EPMD module</seealso>. However, instead of
+    communicating with EPMD you can connect to any service to find out
+    connection details of other nodes. A discovery module is enabled
+    by setting <seealso marker="erts:erl#epmd_module">-epmd_module</seealso>
+    when starting erlang. The discovery module must implement the following
+    callbacks:</p>
+
+    <taglist>
+      <tag><seealso marker="kernel:erl_epmd#start_link/0">start_link/0</seealso></tag>
+      <item>Start any processes needed by the discovery module.</item>
+      <tag><seealso marker="kernel:erl_epmd#names/1">names/1</seealso></tag>
+      <item>Return node names held by the registrar for the given host.</item>
+      <tag><seealso marker="kernel:erl_epmd#register_node/2">register_node/2</seealso></tag>
+      <item>Register the given node name with the registrar.</item>
+      <tag><seealso marker="kernel:erl_epmd#port_please/3">port_please/3</seealso></tag>
+      <item>Return the distribution port used by the given node.</item>
+    </taglist>
+
+    <p>The discovery module may implement the following callback:</p>
+
+    <taglist>
+      <tag><seealso marker="kernel:erl_epmd#address_please/3">address_please/3</seealso></tag>
+      <item><p>Return the address of the given node.
+        If not implemented, <seealso marker="kernel:inet#gethostbyname/1">
+        inet:gethostbyname/1</seealso> will be used instead</p>
+        <p>This callback may also return the port of the given node. In that case
+        <seealso marker="kernel:erl_epmd#port_please/3">port_please/3</seealso>
+        may be omitted.</p></item>
+    </taglist>
+  </section>
+</chapter>

erts/doc/src/part.xml
@@ -37,6 +37,7 @@
   <xi:include href="match_spec.xml"/>
   <xi:include href="crash_dump.xml"/>
   <xi:include href="alt_dist.xml"/>
+  <xi:include href="alt_disco.xml"/>
   <xi:include href="absform.xml"/>
   <xi:include href="tty.xml"/>
   <xi:include href="driver.xml"/>

erts/emulator/test/distribution_SUITE.erl
@@ -73,7 +73,7 @@
          dist_evil_parallel_receiver/0]).
 
 %% epmd_module exports
--export([start_link/0, register_node/2, register_node/3, port_please/2]).
+-export([start_link/0, register_node/2, register_node/3, port_please/2, address_please/3]).
 
 suite() ->
     [{ct_hooks,[ts_install_cth]},
@@ -2085,6 +2085,11 @@ port_please(_Name, _Ip) ->
 	    Version = 5,
 	    {port, Port, Version}
     end.
+
+address_please(_Name, _Address, _AddressFamily) ->
+    %% Use localhost.
+    IP = {127,0,0,1},
+    {ok, IP}.
 
 %%% Utilities
 

lib/kernel/doc/src/Makefile
@@ -42,6 +42,7 @@ XML_REF3_FILES = application.xml \
 	disk_log.xml \
 	erl_boot_server.xml \
 	erl_ddll.xml \
+	erl_epmd.xml \
 	erl_prim_loader_stub.xml \
 	erlang_stub.xml \
 	error_handler.xml \

lib/kernel/doc/src/erl_epmd.xml
@@ -1,0 +1,104 @@
+<?xml version="1.0" encoding="utf-8" ?>
+<!DOCTYPE erlref SYSTEM "erlref.dtd">
+
+<erlref>
+  <header>
+    <copyright>
+      <year>2018</year><year>2018</year>
+      <holder>Ericsson AB. All Rights Reserved.</holder>
+    </copyright>
+    <legalnotice>
+      Licensed under the Apache License, Version 2.0 (the "License");
+      you may not use this file except in compliance with the License.
+      You may obtain a copy of the License at
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
+      Unless required by applicable law or agreed to in writing, software
+      distributed under the License is distributed on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      See the License for the specific language governing permissions and
+      limitations under the License.
+
+    </legalnotice>
+
+    <title>erl_epmd</title>
+    <prepared>Timmo Verlaan</prepared>
+    <docno>1</docno>
+    <date>2018-02-19</date>
+    <rev>A</rev>
+  </header>
+  <module>erl_epmd</module>
+  <modulesummary>
+    Erlang interface towards epmd
+  </modulesummary>
+  <description>
+    <p>This module communicates with the EPMD daemon, see <seealso
+    marker="erts:epmd">epmd</seealso>. To implement your own epmd module please
+    see <seealso marker="erts:alt_disco">ERTS User's Guide: How to Implement an
+    Alternative Service Discovery for Erlang Distribution</seealso></p>
+  </description>
+
+  <funcs>
+    <func>
+      <name name="start_link" arity="0"/>
+      <fsummary>Callback for erl_distribution supervisor.</fsummary>
+      <desc>
+        <p>This function is invoked as this module is added as a child of the
+        <c>erl_distribution</c> supervisor.</p>
+      </desc>
+    </func>
+
+    <func>
+      <name name="register_node" arity="2"/>
+      <name name="register_node" arity="3"/>
+      <fsummary>Registers the node with <c>epmd</c>.</fsummary>
+      <desc>
+        <p>Registers the node with <c>epmd</c> and tells epmd what port will be
+        used for the current node. It returns a creation number. This number is
+        incremented on each register to help with identifying if a node is
+        reconnecting to epmd.</p>
+      </desc>
+    </func>
+
+    <func>
+      <name name="port_please" arity="2"/>
+      <name name="port_please" arity="3"/>
+      <fsummary>Returns the port number for a given node.</fsummary>
+      <desc>
+        <p>Requests the distribution port for the given node of an EPMD
+        instance. Together with the port it returns a distribution protocol
+        version which has been 5 since Erlang/OTP R6.</p>
+      </desc>
+    </func>
+
+    <func>
+      <name name="address_please" arity="3"/>
+      <fsummary>Returns address and port.</fsummary>
+      <desc>
+        <p>Called by the distribution module. Resolves the <c>Host</c> to an IP
+        address.</p>
+        <p>Another epmd module may return port and distribution protocol version
+        as well.</p>
+      </desc>
+    </func>
+
+    <func>
+      <name name="names" arity="1"/>
+      <fsummary>Names of Erlang nodes at a host.</fsummary>
+      <desc>
+        <p>Called by <seealso marker="net_adm"><c>net_adm:names/0</c></seealso>.
+        <c>Host</c> defaults to the localhost. Returns the names and associated
+        port numbers of the Erlang nodes that <c>epmd</c> registered at the
+        specified host. Returns <c>{error, address}</c> if <c>epmd</c> is not
+        operational.</p>
+        <p><em>Example:</em></p>
+        <pre>
+(arne@dunn)1> <input>erl_epmd:names(localhost).</input>
+{ok,[{"arne",40262}]}</pre>
+      </desc>
+    </func>
+  </funcs>
+
+</erlref>
+

lib/kernel/doc/src/ref_man.xml
@@ -38,6 +38,7 @@
   <xi:include href="disk_log.xml"/>
   <xi:include href="erl_boot_server.xml"/>
   <xi:include href="erl_ddll.xml"/>
+  <xi:include href="erl_epmd.xml"/>
   <xi:include href="erl_prim_loader_stub.xml"/>
   <xi:include href="erlang_stub.xml"/>
   <xi:include href="error_handler.xml"/>

commit 8d6a40fd09b3ff7d42786f21b1b252776ebbe1b0

ssl: Fix usage of erl_epmd in tls dist
Fixes: 662f3c7ba50ff8ec13d8

lib/ssl/src/inet_tls_dist.erl
@@ -521,7 +521,7 @@ do_setup(Driver, Kernel, Node, Type, MyNode, LongOrShortNames, SetupTime) ->
     ErlEpmd = net_kernel:epmd_module(),
     {ARMod, ARFun} = get_address_resolver(ErlEpmd, Driver),
     Timer = trace(dist_util:start_timer(SetupTime)),
-    case ARMod:ARFun(Address) of
+    case ARMod:ARFun(Name,Address,Driver:family()) of
     {ok, Ip, TcpPort, Version} ->
         do_setup_connect(Driver, Kernel, Node, Address, Ip, TcpPort, Version, Type, MyNode, Timer);
 	{ok, Ip} ->