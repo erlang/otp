7786776b6dc68e3837fa2a384185e2a05f7f6e46
Lukas Larsson <lukas@erlang.org>
2018-05-21 11:26:19 +0200

    stdlib: Fix io:put_chars/2 error
    Before this fix an error in io:put_chars/2 would signal
    a fault in io:put_chars/3 because of the way that raise is
    used and there is no such function.

--- lib/stdlib/src/io.erl
+++ lib/stdlib/src/io.erl
@@ -86,7 +86,16 @@ put_chars(Chars) ->
       CharData :: unicode:chardata().
 
 put_chars(Io, Chars) ->
-    o_request(Io, {put_chars,unicode,Chars}, put_chars).
+    put_chars(Io, unicode, Chars).
+
+%% This function is here to make the erlang:raise in o_request actually raise to
+%% a valid function.
+-spec put_chars(IoDevice, Encoding, CharData) -> 'ok' when
+      IoDevice :: device(),
+      Encoding :: unicode,
+      CharData :: unicode:chardata().
+put_chars(Io, Encoding, Chars) ->
+    o_request(Io, {put_chars,Encoding,Chars}, put_chars).
 
 -spec nl() -> 'ok'.
 
