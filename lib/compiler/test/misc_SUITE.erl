c6f5c65cae48bf08d08a79d790717eabc0884678
Björn Gustavsson <bjorn@erlang.org>
2018-11-04 11:39:20 +0100

    Cover some code in beam_peep
    Some lines in beam_peep were no longer covered when the sharing optimization
    was added to beam_ssa_opt. Also remove some code from beam_peep that no
    longer seems possible to cover.

--- lib/compiler/test/misc_SUITE.erl
+++ lib/compiler/test/misc_SUITE.erl
@@ -260,7 +260,7 @@ silly_coverage(Config) when is_list(Config) ->
 		 [{function,foo,0,2,
 		   [{label,1},
 		    {func_info,{atom,?MODULE},{atom,foo},0},
-		    {label,2},{select,op,r,{f,2},[{f,2}]}]}],
+		    {label,2},{select,select_val,r,{f,2},[{f,2}]}]}],
 		 2},
     expect_error(fun() -> beam_peep:module(PeepInput, []) end),
 
