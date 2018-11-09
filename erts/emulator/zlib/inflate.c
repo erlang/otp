eb6a5440322df7d7ce0461b11a2fa677a6e3f990
Per Hedeland <per@tail-f.com>
2018-11-09 11:58:06 +0100

    Tail-f: zlib: Apply fixes for zlib vulnerabilities
    Vulnerabilities and corresponding commits from https://github.com/madler/zlib:
    
    1. https://nvd.nist.gov/vuln/detail/CVE-2016-9841
    
    commit 9aaec95e82117c1cb0f9624264c3618fc380cecb
    Commit:     Mark Adler <madler@alumni.caltech.edu>
    CommitDate: Wed Sep 21 22:51:15 2016 -0700
    
        Use post-increment only in inffast.c.
    
        An old inffast.c optimization turns out to not be optimal anymore
        with modern compilers, and furthermore was not compliant with the
        C standard, for which decrementing a pointer before its allocated
        memory is undefined. Per the recommendation of a security audit of
        the zlib code by Trail of Bits and TrustInSoft, in support of the
        Mozilla Foundation, this "optimization" was removed, in order to
        avoid the possibility of undefined behavior.
    
    2. https://nvd.nist.gov/vuln/detail/CVE-2016-9842
    
    commit e54e1299404101a5a9d0cf5e45512b543967f958
    Commit:     Mark Adler <madler@alumni.caltech.edu>
    CommitDate: Sat Sep 5 17:45:55 2015 -0700
    
        Avoid shifts of negative values inflateMark().
    
        The C standard says that bit shifts of negative integers is
        undefined.  This casts to unsigned values to assure a known
        result.
    
    3. https://nvd.nist.gov/vuln/detail/CVE-2016-9843
    
    commit d1d577490c15a0c6862473d7576352a9f18ef811
    Commit:     Mark Adler <madler@alumni.caltech.edu>
    CommitDate: Wed Sep 28 20:48:38 2016 -0700
    
        Avoid pre-decrement of pointer in big-endian CRC calculation.
    
        There was a small optimization for PowerPCs to pre-increment a
        pointer when accessing a word, instead of post-incrementing. This
        required prefacing the loop with a decrement of the pointer,
        possibly pointing before the object passed. This is not compliant
        with the C standard, for which decrementing a pointer before its
        allocated memory is undefined. When tested on a modern PowerPC
        with a modern compiler, the optimization no longer has any effect.
        Due to all that, and per the recommendation of a security audit of
        the zlib code by Trail of Bits and TrustInSoft, in support of the
        Mozilla Foundation, this "optimization" was removed, in order to
        avoid the possibility of undefined behavior.

--- erts/emulator/zlib/inflate.c
+++ erts/emulator/zlib/inflate.c
@@ -1544,7 +1544,7 @@ z_streamp strm;
     struct inflate_state FAR *state;
 
     if (inflateStateCheck(strm))
-        return -(1L << 16);
+        return (long)(((unsigned long)0 - 1) << 16);
     state = (struct inflate_state FAR *)strm->state;
     return (long)(((unsigned long)((long)state->back)) << 16) +
         (state->mode == COPY ? state->length :
