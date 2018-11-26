7cb434ca64c08269c1d4837e0133d1ef2e20bb6e
Hans Nilsson <hans@erlang.org>
2018-11-26 11:36:46 +0100

    crypto: Fix valgrind error in dh_generate/dh_compute

--- lib/crypto/c_src/crypto.c
+++ lib/crypto/c_src/crypto.c
@@ -4234,9 +4234,9 @@ static ERL_NIF_TERM evp_compute_key_nif(ErlNifEnv* env, int argc, const ERL_NIF_
 {
 #ifdef HAVE_ED_CURVE_DH
     int type;
-    EVP_PKEY_CTX *ctx;
+    EVP_PKEY_CTX *ctx = NULL;
     ErlNifBinary peer_bin, my_bin, key_bin;
-    EVP_PKEY *peer_key, *my_key;
+    EVP_PKEY *peer_key = NULL, *my_key = NULL;
     size_t max_size;
 
     if (argv[0] == atom_x25519) type = EVP_PKEY_X25519;
@@ -4244,41 +4244,43 @@ static ERL_NIF_TERM evp_compute_key_nif(ErlNifEnv* env, int argc, const ERL_NIF_
     else return enif_make_badarg(env);
 
     if (!enif_inspect_binary(env, argv[1], &peer_bin) ||
-        !enif_inspect_binary(env, argv[2], &my_bin)) {
-	return enif_make_badarg(env);
-    }
+        !enif_inspect_binary(env, argv[2], &my_bin)) 
+        goto return_badarg;
 
     if (!(my_key = EVP_PKEY_new_raw_private_key(type, NULL, my_bin.data, my_bin.size)) ||
-        !(ctx = EVP_PKEY_CTX_new(my_key, NULL))) {
-	return enif_make_badarg(env);
-    }
+        !(ctx = EVP_PKEY_CTX_new(my_key, NULL))) 
+        goto return_badarg;
 
-    if (!EVP_PKEY_derive_init(ctx)) {
-	return enif_make_badarg(env);
-    }
+    if (!EVP_PKEY_derive_init(ctx)) 
+        goto return_badarg;
 
     if (!(peer_key = EVP_PKEY_new_raw_public_key(type, NULL, peer_bin.data, peer_bin.size)) ||
-        !EVP_PKEY_derive_set_peer(ctx, peer_key)) {
-	return enif_make_badarg(env);
-    }
+        !EVP_PKEY_derive_set_peer(ctx, peer_key)) 
+        goto return_badarg;
 
-    if (!EVP_PKEY_derive(ctx, NULL, &max_size)) {
-	return enif_make_badarg(env);
-    }
+    if (!EVP_PKEY_derive(ctx, NULL, &max_size)) 
+        goto return_badarg;
 
     if (!enif_alloc_binary(max_size, &key_bin) ||
-        !EVP_PKEY_derive(ctx, key_bin.data, &key_bin.size)) {
-	return enif_make_badarg(env);
-    }
+        !EVP_PKEY_derive(ctx, key_bin.data, &key_bin.size)) 
+        goto return_badarg;
 
     if (key_bin.size < max_size) {
         size_t actual_size = key_bin.size;
-        if (!enif_realloc_binary(&key_bin, actual_size)) {
-            return enif_make_badarg(env);
-        }
+        if (!enif_realloc_binary(&key_bin, actual_size)) 
+            goto return_badarg;
     }
 
+    EVP_PKEY_free(my_key);
+    EVP_PKEY_free(peer_key);
+    EVP_PKEY_CTX_free(ctx);
     return enif_make_binary(env, &key_bin);
+
+return_badarg:
+    if (my_key)   EVP_PKEY_free(my_key);
+    if (peer_key) EVP_PKEY_free(peer_key);
+    if (ctx)      EVP_PKEY_CTX_free(ctx);
+    return enif_make_badarg(env);
 #else
     return atom_notsup;
 #endif
@@ -4289,7 +4291,7 @@ static ERL_NIF_TERM evp_generate_key_nif(ErlNifEnv* env, int argc, const ERL_NIF
 {
 #ifdef HAVE_ED_CURVE_DH
     int type;
-    EVP_PKEY_CTX *ctx;
+    EVP_PKEY_CTX *ctx = NULL;
     EVP_PKEY *pkey = NULL;
     ERL_NIF_TERM ret_pub, ret_prv;
     size_t key_len;
@@ -4300,22 +4302,30 @@ static ERL_NIF_TERM evp_generate_key_nif(ErlNifEnv* env, int argc, const ERL_NIF
 
     if (!(ctx = EVP_PKEY_CTX_new_id(type, NULL))) return enif_make_badarg(env);
 
-    if (!EVP_PKEY_keygen_init(ctx)) return atom_error;
-    if (!EVP_PKEY_keygen(ctx, &pkey)) return atom_error;
+    if (!EVP_PKEY_keygen_init(ctx)) goto return_error;
+    if (!EVP_PKEY_keygen(ctx, &pkey)) goto return_error;
 
-    if (!EVP_PKEY_get_raw_public_key(pkey, NULL, &key_len)) return atom_error;
+    if (!EVP_PKEY_get_raw_public_key(pkey, NULL, &key_len)) goto return_error;
     if (!EVP_PKEY_get_raw_public_key(pkey,
                                      enif_make_new_binary(env, key_len, &ret_pub),
                                      &key_len))
-        return atom_error;
+        goto return_error;
 
-    if (!EVP_PKEY_get_raw_private_key(pkey, NULL, &key_len)) return atom_error;
+    if (!EVP_PKEY_get_raw_private_key(pkey, NULL, &key_len)) goto return_error;
     if (!EVP_PKEY_get_raw_private_key(pkey,
                                       enif_make_new_binary(env, key_len, &ret_prv),
                                       &key_len))
-        return atom_error;
+        goto return_error;
 
+    EVP_PKEY_free(pkey);
+    EVP_PKEY_CTX_free(ctx);
     return enif_make_tuple2(env, ret_pub, ret_prv);
+
+return_error:
+    if (pkey) EVP_PKEY_free(pkey);
+    if (ctx)  EVP_PKEY_CTX_free(ctx);
+    return atom_error;
+
 #else
     return atom_notsup;
 #endif
@@ -4811,6 +4821,7 @@ printf("\r\n");
             EVP_MD_CTX_free(mdctx);
             goto badarg;
         }
+        EVP_MD_CTX_free(mdctx);
 #else
         goto badarg;    
 #endif
