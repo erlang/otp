aef4b2076a33aaebf9e555eca10ead1b89a70d60
Doug Hogan <hogand@users.noreply.github.com>
2018-12-20 01:56:21 -0800

    Move all atoms to a new file and add common header

---
+++ lib/crypto/c_src/common.h
@@ -0,0 +1,16 @@
+#ifndef E_COMMON_H__
+#define E_COMMON_H__ 1
+
+#ifdef __WIN32__
+#  include <windows.h>
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <erl_nif.h>
+#include "openssl_config.h"
+#include "atoms.h"
+
+#endif /* E_COMMON_H__ */
