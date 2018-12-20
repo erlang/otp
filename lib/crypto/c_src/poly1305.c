a4bd9e958946a6cad3207b5a91daa2ceac2483e5
Doug Hogan <hogand@users.noreply.github.com>
2018-12-20 11:39:55 -0800

    Add Ericsson AB copyright header to all new files
    Using the same copyright header as crypto.c

--- lib/crypto/c_src/poly1305.c
+++ lib/crypto/c_src/poly1305.c
@@ -1,3 +1,23 @@
+/*
+ * %CopyrightBegin%
+ *
+ * Copyright Ericsson AB 2010-2018. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * %CopyrightEnd%
+ */
+
 #include "poly1305.h"
 
 /* For OpenSSL >= 1.1.1 the hmac_nif and cmac_nif could be integrated into poly1305 (with 'type' as parameter) */
