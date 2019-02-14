730a71a030c30bf233b25cf4aaae47574ea066b8
Ingela Anderton Andin <ingela@erlang.org>
2019-02-14 13:29:57 +0100

    ssl: Remove duplicate record_cb handling

--- lib/ssl/src/ssl.erl
+++ lib/ssl/src/ssl.erl
@@ -1202,7 +1202,6 @@ handle_options(Opts0, Role, Host) ->
 	handle_verify_options(Opts, CaCerts),
     
     CertFile = handle_option(certfile, Opts, <<>>),
-    RecordCb = record_cb(Opts),
     
     Versions = case handle_option(versions, Opts, []) of
 		   [] ->
