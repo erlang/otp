0d213a841529ff47bff71dc0d81e7a38a1ea35f0
Erlang/OTP <otp@erlang.org>
2019-02-14 12:34:47 +0100

    Prepare release

--- erts/vsn.mk
+++ erts/vsn.mk
@@ -18,7 +18,7 @@
 # %CopyrightEnd%
 # 
 
-VSN = 9.3.3.8
+VSN = 9.3.3.9
 
 # Port number 4365 in 4.2
 # Port number 4366 in 4.3
--- lib/diameter/vsn.mk
+++ lib/diameter/vsn.mk
@@ -17,5 +17,5 @@
 # %CopyrightEnd%
 
 APPLICATION  = diameter
-DIAMETER_VSN = 2.1.4
+DIAMETER_VSN = 2.1.4.1
 APP_VSN      = $(APPLICATION)-$(DIAMETER_VSN)$(PRE_VSN)
