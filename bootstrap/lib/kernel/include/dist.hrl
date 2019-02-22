1af28d672de91a12bea2c6332a14f040fbf4d63a
Björn Gustavsson <bjorn@erlang.org>
2019-02-22 11:35:53 +0100

    Update primary bootstrap

--- bootstrap/lib/kernel/include/dist.hrl
+++ bootstrap/lib/kernel/include/dist.hrl
@@ -42,6 +42,9 @@
 -define(DFLAG_BIG_CREATION, 16#40000).
 -define(DFLAG_SEND_SENDER, 16#80000).
 -define(DFLAG_BIG_SEQTRACE_LABELS, 16#100000).
+%% -define(DFLAG_NO_MAGIC, 16#200000). %% Used internally only
+-define(DFLAG_EXIT_PAYLOAD, 16#400000).
+-define(DFLAG_FRAGMENTS, 16#800000).
 
 %% Also update dflag2str() in ../src/dist_util.erl
 %% when adding flags...
