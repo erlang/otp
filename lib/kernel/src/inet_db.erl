erlang/otp

commit 7c63a98c28d477f73633e954bc287404d4889680

Set early enough start time
Adhering to the review in GitHub PR #2066:
The start time should be set so the resolver file can get
re-read as soon as possible to not get the whole timeout time
before detecting that the resolver file has been created.

lib/kernel/src/inet_db.erl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 1997-2016. All Rights Reserved.
+%% Copyright Ericsson AB 1997-2019. All Rights Reserved.
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -1224,8 +1224,9 @@ handle_set_file(Option, Fname, TagTm, TagInfo, ParseFun, From,
 			    _ -> <<>>
 			end;
 		    _ ->
-			ets:insert(Db, {TagTm, times()}),
-			<<>>
+                        ets:insert(Db, {TagInfo, undefined}),
+                        TimeZero = - (?RES_FILE_UPDATE_TM + 1), % Early enough
+                        ets:insert(Db, {TagTm, TimeZero})
 		end,
 	    handle_set_file(ParseFun, Bin, From, State);
 	false -> {reply,error,State}

commit f92fc026fe6f34bc869a5166a2726f890986929a

kernel: Fix setting of non-existing file as inet_db resolv_conf
This was broken in 7c63a98c28d477.

lib/kernel/src/inet_db.erl
@@ -1226,7 +1226,8 @@ handle_set_file(Option, Fname, TagTm, TagInfo, ParseFun, From,
 		    _ ->
                         ets:insert(Db, {TagInfo, undefined}),
                         TimeZero = - (?RES_FILE_UPDATE_TM + 1), % Early enough
-                        ets:insert(Db, {TagTm, TimeZero})
+                        ets:insert(Db, {TagTm, TimeZero}),
+                        <<>>
 		end,
 	    handle_set_file(ParseFun, Bin, From, State);
 	false -> {reply,error,State}

lib/kernel/test/inet_res_SUITE.erl
@@ -46,15 +46,16 @@
 %% a temporary local nameserver BIND 8 or 9 that must be installed
 %% on your machine.
 %%
-%% For example, on Ubuntu 14.04, as root:
+%% For example, on Ubuntu 16.04 / 18.04, as root:
 %%     apt-get install bind9
 %% Now, that is not enough since Apparmor will not allow
 %% the nameserver daemon /usr/sbin/named to read from the test directory.
 %% Assuming that you run tests in /ldisk/daily_build, and still on
-%% Ubuntu 14.04, make /usr/apparmor.d/local/usr.sbin.named contain:
+%% Ubuntu 14.04, make /etc/apparmor.d/local/usr.sbin.named contain:
 %%     /ldisk/daily_build/** r,
 %% And yes; the trailing comma must be there...
-
+%% And yes; create the file if it does not exist.
+%% And yes; restart the apparmor daemon using "service apparmor restart"
 
 
 suite() ->