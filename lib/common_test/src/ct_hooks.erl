erlang/otp

commit acd87425acf7705328b01aa8f0a804207ffe4790

[ct] Add post_groups/2 and post_all/3 hook functions

lib/common_test/src/ct_hooks.erl
@@ -26,6 +26,8 @@
 
 %% API Exports
 -export([init/1]).
+-export([groups/2]).
+-export([all/2]).
 -export([init_tc/3]).
 -export([end_tc/5]).
 -export([terminate/1]).
@@ -41,7 +43,8 @@
 					opts = [],
 					prio = ctfirst }]).
 
--record(ct_hook_config, {id, module, prio, scope, opts = [], state = []}).
+-record(ct_hook_config, {id, module, prio, scope, opts = [],
+                         state = [], groups = []}).
 
 %% -------------------------------------------------------------------------
 %% API Functions
@@ -53,6 +56,47 @@
 init(Opts) ->
     call(get_builtin_hooks(Opts) ++ get_new_hooks(Opts, undefined),
 	 ok, init, []).
+
+%% Call the post_groups/2 hook callback
+groups(Mod, Groups) ->
+    Info = try proplists:get_value(ct_hooks, Mod:suite(), []) of
+               CTHooks when is_list(CTHooks) ->
+                   [{?config_name,CTHooks}];
+               CTHook when is_atom(CTHook) ->
+                   [{?config_name,[CTHook]}]
+           catch _:_ ->
+                   %% since this might be the first time Mod:suite()
+                   %% is called, and it might just fail or return
+                   %% something bad, we allow any failure here - it
+                   %% will be catched later if there is something
+                   %% really wrong.
+                   [{?config_name,[]}]
+           end,
+    case call(fun call_generic/3, Info ++ [{'$ct_groups',Groups}], [post_groups, Mod]) of
+        [{'$ct_groups',NewGroups}] ->
+            NewGroups;
+        Other ->
+            Other
+    end.
+
+%% Call the post_all/3 hook callback
+all(Mod, Tests) ->
+    Info = try proplists:get_value(ct_hooks, Mod:suite(), []) of
+               CTHooks when is_list(CTHooks) ->
+                   [{?config_name,CTHooks}];
+               CTHook when is_atom(CTHook) ->
+                   [{?config_name,[CTHook]}]
+           catch _:_ ->
+                   %% just allow any failure here - it will be catched
+                   %% later if there is something really wrong.
+                   [{?config_name,[]}]
+           end,
+    case call(fun call_generic/3, Info ++ [{'$ct_all',Tests}], [post_all, Mod]) of
+        [{'$ct_all',NewTests}] ->
+            NewTests;
+        Other ->
+            Other
+    end.
 
 %% @doc Called after all suites are done.
 -spec terminate(Hooks :: term()) ->
@@ -88,6 +132,7 @@ init_tc(Mod, init_per_suite, Config) ->
 		   [{?config_name,[]}]
 	   end,
     call(fun call_generic/3, Config ++ Info, [pre_init_per_suite, Mod]);
+
 init_tc(Mod, end_per_suite, Config) ->
     call(fun call_generic/3, Config, [pre_end_per_suite, Mod]);
 init_tc(Mod, {init_per_group, GroupName, Properties}, Config) ->
@@ -163,7 +208,7 @@ call_id(#ct_hook_config{ module = Mod, opts = Opts} = Hook, Config, Scope) ->
     {Config, Hook#ct_hook_config{ id = Id, scope = scope(Scope)}}.
 	
 call_init(#ct_hook_config{ module = Mod, opts = Opts, id = Id, prio = P} = Hook,
-	  Config,_Meta) ->
+	  Config, _Meta) ->
     case Mod:init(Id, Opts) of
 	{ok, NewState} when P =:= undefined ->
 	    {Config, Hook#ct_hook_config{ state = NewState, prio = 0 } };
@@ -193,6 +238,18 @@ call_generic(Hook, Value, Meta) ->
 
 call_generic_fallback(Hook, Value, Meta) ->
     do_call_generic(Hook, Value, Meta, true).
+
+do_call_generic(#ct_hook_config{ module = Mod} = Hook,
+                [{'$ct_groups',Groups}], [post_groups | Args], Fallback) ->
+    NewGroups = catch_apply(Mod, post_groups, Args ++ [Groups],
+                            Groups, Fallback),
+    {[{'$ct_groups',NewGroups}], Hook#ct_hook_config{ groups = NewGroups } };
+
+do_call_generic(#ct_hook_config{ module = Mod, groups = Groups} = Hook,
+                [{'$ct_all',Tests}], [post_all | Args], Fallback) ->
+    NewTests = catch_apply(Mod, post_all, Args ++ [Tests, Groups],
+                           Tests, Fallback),
+    {[{'$ct_all',NewTests}], Hook};
 
 do_call_generic(#ct_hook_config{ module = Mod, state = State} = Hook,
                 Value, [Function | Args], Fallback) ->
@@ -228,6 +285,12 @@ call([{Hook, call_id, NextFun} | Rest], Config, Meta, Hooks) ->
 		     Rest ++ [{NewId, call_init}]};
 		ExistingHook when is_tuple(ExistingHook) ->
 		    {Hooks, Rest};
+                _ when hd(Meta)=:=post_groups; hd(Meta)=:=post_all ->
+                    %% If CTH is started because of a call from
+                    %% groups/2 or all/2, CTH:init/1 must not be
+                    %% called (the suite scope should be used).
+                    {Hooks ++ [NewHook],
+		     Rest ++ [{NewId,NextFun}]};
 		_ ->
 		    {Hooks ++ [NewHook],
 		     Rest ++ [{NewId, call_init}, {NewId,NextFun}]}
@@ -237,8 +300,8 @@ call([{Hook, call_id, NextFun} | Rest], Config, Meta, Hooks) ->
 	    Trace = erlang:get_stacktrace(),
 	    ct_logs:log("Suite Hook","Failed to start a CTH: ~tp:~tp",
 			[Error,{Reason,Trace}]),
-	    call([], {fail,"Failed to start CTH"
-		      ", see the CT Log for details"}, Meta, Hooks)
+	    call([], {fail,"Failed to start CTH, "
+		      "see the CT Log for details"}, Meta, Hooks)
     end;
 call([{HookId, call_init} | Rest], Config, Meta, Hooks) ->
     call([{HookId, fun call_init/3} | Rest], Config, Meta, Hooks);
@@ -278,6 +341,10 @@ scope([pre_init_per_suite, SuiteName|_]) ->
     [post_end_per_suite, SuiteName];
 scope([post_init_per_suite, SuiteName|_]) ->
     [post_end_per_suite, SuiteName];
+scope([post_groups, SuiteName|_]) ->
+    [post_groups, SuiteName];
+scope([post_all, SuiteName|_]) ->
+    [post_all, SuiteName];
 scope(init) ->
     none.
 
@@ -364,6 +431,7 @@ resort(Calls,Hooks,[F|_R]) when F == pre_end_per_testcase;
 				F == pre_end_per_suite;
 				F == post_end_per_suite ->
     lists:reverse(resort(Calls,Hooks));
+
 resort(Calls,Hooks,_Meta) ->
     resort(Calls,Hooks).
     

lib/common_test/test/ct_hooks_SUITE.erl
@@ -73,11 +73,15 @@ all() ->
 all(suite) ->
     lists:reverse(
       [
+       crash_groups, crash_all, bad_return_groups, bad_return_all,
+       illegal_values_groups, illegal_values_all, alter_groups, alter_all,
+       alter_all_to_skip, alter_all_from_skip,
        one_cth, two_cth, faulty_cth_no_init, faulty_cth_id_no_init,
        faulty_cth_exit_in_init, faulty_cth_exit_in_id,
        faulty_cth_exit_in_init_scope_suite, minimal_cth,
        minimal_and_maximal_cth, faulty_cth_undef,
        scope_per_suite_cth, scope_per_group_cth, scope_suite_cth,
+       scope_suite_group_only_cth,
        scope_per_suite_state_cth, scope_per_group_state_cth,
        scope_suite_state_cth,
        fail_pre_suite_cth, double_fail_pre_suite_cth,
@@ -151,6 +155,11 @@ scope_per_suite_cth(Config) when is_list(Config) ->
 scope_suite_cth(Config) when is_list(Config) ->
     do_test(scope_suite_cth, "ct_scope_suite_cth_SUITE.erl",
 	    [],Config).
+
+scope_suite_group_only_cth(Config) when is_list(Config) ->
+    do_test(scope_suite_group_only_cth,
+            "ct_scope_suite_group_only_cth_SUITE.erl",
+	    [],Config,ok,2,[{group,g1}]).
 
 scope_per_group_cth(Config) when is_list(Config) ->
     do_test(scope_per_group_cth, "ct_scope_per_group_cth_SUITE.erl",
@@ -299,9 +308,73 @@ repeat_force_stop(Config) ->
             [{force_stop,skip_rest},{duration,"000009"}]).
 
 %% Test that expected callbacks, and only those, are called when a test
-%% are fails due to clash in config alias names
+%% fails due to clash in config alias names
 config_clash(Config) ->
     do_test(config_clash, "config_clash_SUITE.erl", [skip_cth], Config).
+
+%% Test post_groups and post_all hook callbacks, introduced by OTP-14746
+alter_groups(Config) ->
+    CfgFile = gen_config(?FUNCTION_NAME,
+                         [{post_groups_return,[{new_group,[tc1,tc2]}]},
+                          {post_all_return,[{group,new_group}]}],Config),
+    do_test(?FUNCTION_NAME, "all_and_groups_SUITE.erl", [all_and_groups_cth],
+            Config, ok, 2, [{config,CfgFile}]).
+
+alter_all(Config) ->
+    CfgFile = gen_config(?FUNCTION_NAME,[{post_all_return,[tc2]}],Config),
+    do_test(?FUNCTION_NAME, "all_and_groups_SUITE.erl", [all_and_groups_cth],
+            Config, ok, 2, [{config,CfgFile}]).
+
+alter_all_from_skip(Config) ->
+    CfgFile = gen_config(?FUNCTION_NAME,[{all_return,{skip,"skipped by all/0"}},
+                                         {post_all_return,[tc2]}],Config),
+    do_test(?FUNCTION_NAME, "all_and_groups_SUITE.erl", [all_and_groups_cth],
+            Config, ok, 2, [{config,CfgFile}]).
+
+alter_all_to_skip(Config) ->
+    CfgFile = gen_config(?FUNCTION_NAME,
+                         [{post_all_return,{skip,"skipped by post_all/3"}}],
+                         Config),
+    do_test(?FUNCTION_NAME, "all_and_groups_SUITE.erl", [all_and_groups_cth],
+            Config, ok, 2, [{config,CfgFile}]).
+
+bad_return_groups(Config) ->
+    CfgFile = gen_config(?FUNCTION_NAME,[{post_groups_return,not_a_list}],
+                         Config),
+    do_test(?FUNCTION_NAME, "all_and_groups_SUITE.erl", [all_and_groups_cth],
+            Config, ok, 2, [{config,CfgFile}]).
+
+bad_return_all(Config) ->
+    CfgFile = gen_config(?FUNCTION_NAME,[{post_all_return,not_a_list}],
+                         Config),
+    do_test(?FUNCTION_NAME, "all_and_groups_SUITE.erl", [all_and_groups_cth],
+            Config, ok, 2, [{config,CfgFile}]).
+
+illegal_values_groups(Config) ->
+    CfgFile = gen_config(?FUNCTION_NAME,
+                         [{post_groups_return,[{new_group,[this_test_does_not_exist]},
+                                          this_is_not_a_group_def]}],
+                         Config),
+    do_test(?FUNCTION_NAME, "all_and_groups_SUITE.erl", [all_and_groups_cth],
+            Config, ok, 2, [{config,CfgFile}]).
+
+illegal_values_all(Config) ->
+    CfgFile = gen_config(?FUNCTION_NAME,
+                         [{post_all_return,[{group,this_group_does_not_exist},
+                                       {this_is_not_a_valid_term}]}],
+                         Config),
+    do_test(?FUNCTION_NAME, "all_and_groups_SUITE.erl", [all_and_groups_cth],
+            Config, ok, 2, [{config,CfgFile}]).
+
+crash_groups(Config) ->
+    CfgFile = gen_config(?FUNCTION_NAME,[{post_groups_return,crash}],Config),
+    do_test(?FUNCTION_NAME, "all_and_groups_SUITE.erl", [all_and_groups_cth],
+            Config, ok, 2, [{config,CfgFile}]).
+
+crash_all(Config) ->
+    CfgFile = gen_config(?FUNCTION_NAME,[{post_all_return,crash}],Config),
+    do_test(?FUNCTION_NAME, "all_and_groups_SUITE.erl", [all_and_groups_cth],
+            Config, ok, 2, [{config,CfgFile}]).
 
 %%%-----------------------------------------------------------------
 %%% HELP FUNCTIONS
@@ -322,6 +395,7 @@ do_test(Tag, {WhatTag,Wildcard}, CTHs, Config, Res, EC, ExtraOpts) ->
                filename:join([DataDir,"cth/tests",Wildcard])),
     {Opts,ERPid} =
         setup([{WhatTag,Files},{ct_hooks,CTHs},{label,Tag}|ExtraOpts], Config),
+
     Res = ct_test_support:run(Opts, Config),
     Events = ct_test_support:get_events(ERPid, Config),
 
@@ -347,6 +421,13 @@ reformat(Events, EH) ->
 %reformat(Events, _EH) ->
 %    Events.
 
+gen_config(Name,KeyVals,Config) ->
+    PrivDir = ?config(priv_dir,Config),
+    File = filename:join(PrivDir,atom_to_list(Name)++".cfg"),
+    ok = file:write_file(File,[io_lib:format("~p.~n",[{Key,Value}])
+                               || {Key,Value} <- KeyVals]),
+    File.
+
 %%%-----------------------------------------------------------------
 %%% TEST EVENTS
 %%%-----------------------------------------------------------------
@@ -365,13 +446,16 @@ test_events(one_empty_cth) ->
      {?eh,test_start,{'DEF',{'START_TIME','LOGDIR'}}},
      {?eh,cth,{empty_cth,id,[[]]}},
      {?eh,cth,{empty_cth,init,[{'_','_','_'},[]]}},
+     %% check that post_groups and post_all comes after init when hook
+     %% is installed with start flag/option.
+     {?eh,cth,{empty_cth,post_groups,[ct_cth_empty_SUITE,[]]}},
+     {?eh,cth,{empty_cth,post_all,[ct_cth_empty_SUITE,[test_case],[]]}},
      {?eh,tc_start,{ct_cth_empty_SUITE,init_per_suite}},
      {?eh,cth,{empty_cth,pre_init_per_suite,
 	       [ct_cth_empty_SUITE,'$proplist',[]]}},
      {?eh,cth,{empty_cth,post_init_per_suite,
 	       [ct_cth_empty_SUITE,'$proplist','$proplist',[]]}},
      {?eh,tc_done,{ct_cth_empty_SUITE,init_per_suite,ok}},
-
      {?eh,tc_start,{ct_cth_empty_SUITE,test_case}},
      {?eh,cth,{empty_cth,pre_init_per_testcase,[ct_cth_empty_SUITE,test_case,'$proplist',[]]}},
      {?eh,cth,{empty_cth,post_init_per_testcase,[ct_cth_empty_SUITE,test_case,'$proplist','_',[]]}},
@@ -580,6 +664,10 @@ test_events(scope_suite_cth) ->
     [
      {?eh,start_logging,{'DEF','RUNDIR'}},
      {?eh,test_start,{'DEF',{'START_TIME','LOGDIR'}}},
+     %% check that post_groups and post_all comes before init when hook
+     %% is installed in suite/0
+     {?eh,cth,{'_',post_groups,['_',[]]}},
+     {?eh,cth,{'_',post_all,['_','_',[]]}},
      {?eh,tc_start,{ct_scope_suite_cth_SUITE,init_per_suite}},
      {?eh,cth,{'_',id,[[]]}},
      {?eh,cth,{'_',init,['_',[]]}},
@@ -597,6 +685,34 @@ test_events(scope_suite_cth) ->
      {?eh,cth,{'_',post_end_per_suite,[ct_scope_suite_cth_SUITE,'$proplist','_',[]]}},
      {?eh,cth,{'_',terminate,[[]]}},
      {?eh,tc_done,{ct_scope_suite_cth_SUITE,end_per_suite,ok}},
+     {?eh,test_done,{'DEF','STOP_TIME'}},
+     {?eh,stop_logging,[]}
+    ];
+
+test_events(scope_suite_group_only_cth) ->
+    Suite = ct_scope_suite_group_only_cth_SUITE,
+    CTH = empty_cth,
+    [
+     {?eh,start_logging,{'DEF','RUNDIR'}},
+     {?eh,test_start,{'DEF',{'START_TIME','LOGDIR'}}},
+     {?eh,start_info,{1,1,1}},
+     %% check that post_groups and post_all comes before init when hook
+     %% is installed in suite/0
+     {?eh,cth,{CTH,post_groups,['_',['_']]}},
+     {negative,
+      {?eh,cth,{CTH,post_all,['_','_','_']}},
+      {?eh,tc_start,{Suite,init_per_suite}}},
+     {?eh,cth,{CTH,id,[[]]}},
+     {?eh,cth,{CTH,init,['_',[]]}},
+     {?eh,cth,{CTH,pre_init_per_suite,[Suite,'$proplist',mystate]}},
+     {?eh,cth,{CTH,post_init_per_suite,[Suite,'$proplist','$proplist',mystate]}},
+     {?eh,tc_done,{Suite,init_per_suite,ok}},
+
+     {?eh,tc_start,{Suite,end_per_suite}},
+     {?eh,cth,{CTH,pre_end_per_suite,[Suite,'$proplist',mystate]}},
+     {?eh,cth,{CTH,post_end_per_suite,[Suite,'$proplist','_',mystate]}},
+     {?eh,cth,{CTH,terminate,[mystate]}},
+     {?eh,tc_done,{Suite,end_per_suite,ok}},
      {?eh,test_done,{'DEF','STOP_TIME'}},
      {?eh,stop_logging,[]}
     ];
@@ -660,6 +776,8 @@ test_events(scope_suite_state_cth) ->
     [
      {?eh,start_logging,{'DEF','RUNDIR'}},
      {?eh,test_start,{'DEF',{'START_TIME','LOGDIR'}}},
+     {?eh,cth,{'_',post_groups,['_',[]]}},
+     {?eh,cth,{'_',post_all,['_','_',[]]}},
      {?eh,tc_start,{ct_scope_suite_state_cth_SUITE,init_per_suite}},
      {?eh,cth,{'_',id,[[test]]}},
      {?eh,cth,{'_',init,['_',[test]]}},
@@ -2307,6 +2425,229 @@ test_events(config_clash) ->
     ],
     %% Make sure no 'cth_error' events are received!
     [{negative,{?eh,cth_error,'_'},E} || E <- Events];
+
+test_events(alter_groups) ->
+    [
+     {?eh,start_logging,{'DEF','RUNDIR'}},
+     {?eh,test_start,{'DEF',{'START_TIME','LOGDIR'}}},
+     {?eh,cth,{empty_cth,id,[[]]}},
+     {?eh,cth,{empty_cth,init,[{'_','_','_'},[]]}},
+     {?eh,cth,{empty_cth,post_groups,[all_and_groups_SUITE,
+                                      [{new_group,[tc1,tc2]}]]}},
+     {?eh,cth,{empty_cth,post_all,[all_and_groups_SUITE,[{group,new_group}],
+                                   [{new_group,[tc1,tc2]}]]}},
+     {?eh,start_info,{1,1,2}},
+     {?eh,cth,{empty_cth,post_groups,[all_and_groups_SUITE,
+                                      [{new_group,[tc1,tc2]}]]}},
+     {?eh,cth,{empty_cth,post_all,[all_and_groups_SUITE,[{group,new_group}],
+                                   [{new_group,[tc1,tc2]}]]}},
+     {?eh,tc_start,{all_and_groups_SUITE,{init_per_group,new_group,[]}}},
+     {?eh,tc_done,{all_and_groups_SUITE,
+                   {init_per_group,new_group,'$proplist'},ok}},
+     {?eh,tc_start,{all_and_groups_SUITE,tc1}},
+     {?eh,tc_done,{all_and_groups_SUITE,tc1,ok}},
+     {?eh,tc_start,{all_and_groups_SUITE,tc2}},
+     {?eh,tc_done,{all_and_groups_SUITE,tc2,ok}},
+     {?eh,tc_start,{all_and_groups_SUITE,{end_per_group,new_group,[]}}},
+     {?eh,tc_done,{all_and_groups_SUITE,
+                   {end_per_group,new_group,'$proplist'},ok}},
+     {?eh,test_done,{'DEF','STOP_TIME'}},
+     {?eh,cth,{empty_cth,terminate,[[]]}},
+     {?eh,stop_logging,[]}
+    ];
+
+test_events(alter_all) ->
+    [
+     {?eh,start_logging,{'DEF','RUNDIR'}},
+     {?eh,test_start,{'DEF',{'START_TIME','LOGDIR'}}},
+     {?eh,cth,{empty_cth,id,[[]]}},
+     {?eh,cth,{empty_cth,init,[{'_','_','_'},[]]}},
+     {?eh,cth,{empty_cth,post_groups,[all_and_groups_SUITE,
+                                      [{test_group,[tc1]}]]}},
+     {?eh,cth,{empty_cth,post_all,[all_and_groups_SUITE,[tc2],
+                                   [{test_group,[tc1]}]]}},
+     {?eh,start_info,{1,1,1}},
+     {?eh,cth,{empty_cth,post_groups,[all_and_groups_SUITE,'_']}},
+     {?eh,cth,{empty_cth,post_all,[all_and_groups_SUITE,[tc2],'_']}},
+     {?eh,tc_start,{all_and_groups_SUITE,tc2}},
+     {?eh,tc_done,{all_and_groups_SUITE,tc2,ok}},
+     {?eh,test_done,{'DEF','STOP_TIME'}},
+     {?eh,cth,{empty_cth,terminate,[[]]}},
+     {?eh,stop_logging,[]}
+    ];
+
+test_events(alter_all_from_skip) ->
+    [
+     {?eh,start_logging,{'DEF','RUNDIR'}},
+     {?eh,test_start,{'DEF',{'START_TIME','LOGDIR'}}},
+     {?eh,cth,{empty_cth,id,[[]]}},
+     {?eh,cth,{empty_cth,init,[{'_','_','_'},[]]}},
+     {?eh,cth,{empty_cth,post_groups,[all_and_groups_SUITE,
+                                      [{test_group,[tc1]}]]}},
+     {?eh,cth,{empty_cth,post_all,[all_and_groups_SUITE,[tc2],
+                                   [{test_group,[tc1]}]]}},
+     {?eh,start_info,{1,1,1}},
+     {?eh,cth,{empty_cth,post_groups,[all_and_groups_SUITE,'_']}},
+     {?eh,cth,{empty_cth,post_all,[all_and_groups_SUITE,[tc2],'_']}},
+     {?eh,tc_start,{all_and_groups_SUITE,tc2}},
+     {?eh,tc_done,{all_and_groups_SUITE,tc2,ok}},
+     {?eh,test_done,{'DEF','STOP_TIME'}},
+     {?eh,cth,{empty_cth,terminate,[[]]}},
+     {?eh,stop_logging,[]}
+    ];
+
+test_events(alter_all_to_skip) ->
+    [
+     {?eh,start_logging,{'DEF','RUNDIR'}},
+     {?eh,test_start,{'DEF',{'START_TIME','LOGDIR'}}},
+     {?eh,cth,{empty_cth,id,[[]]}},
+     {?eh,cth,{empty_cth,init,[{'_','_','_'},[]]}},
+     {?eh,cth,{empty_cth,post_groups,[all_and_groups_SUITE,
+                                      [{test_group,[tc1]}]]}},
+     {?eh,cth,{empty_cth,post_all,[all_and_groups_SUITE,
+                                   {skip,"skipped by post_all/3"},
+                                   [{test_group,[tc1]}]]}},
+     {?eh,start_info,{1,1,0}},
+     {?eh,cth,{empty_cth,post_groups,[all_and_groups_SUITE,'_']}},
+     {?eh,cth,{empty_cth,post_all,[all_and_groups_SUITE,
+                                   {skip,"skipped by post_all/3"},
+                                   '_']}},
+     {?eh,tc_user_skip,{all_and_groups_SUITE,all,"skipped by post_all/3"}},
+     {?eh,cth,{'_',on_tc_skip,[all_and_groups_SUITE,all,
+                               {tc_user_skip,"skipped by post_all/3"},
+                               []]}},
+     {?eh,test_done,{'DEF','STOP_TIME'}},
+     {?eh,cth,{empty_cth,terminate,[[]]}},
+     {?eh,stop_logging,[]}
+    ];
+
+test_events(illegal_values_groups) ->
+    [
+     {?eh,start_logging,{'DEF','RUNDIR'}},
+     {?eh,test_start,{'DEF',{'START_TIME','LOGDIR'}}},
+     {?eh,cth,{empty_cth,id,[[]]}},
+     {?eh,cth,{empty_cth,init,[{'_','_','_'},[]]}},
+     {?eh,cth,{empty_cth,post_groups,
+               [all_and_groups_SUITE,
+                [{new_group,[this_test_does_not_exist]},
+                 this_is_not_a_group_def]]}},
+     {?eh,start_info,{1,0,0}},
+     {?eh,cth,{empty_cth,post_groups,
+               [all_and_groups_SUITE,
+                [{new_group,[this_test_does_not_exist]},
+                 this_is_not_a_group_def]]}},
+     {?eh,tc_start,{ct_framework,error_in_suite}},
+     {?eh,tc_done,{ct_framework,error_in_suite,{failed,{error,'_'}}}},
+     {?eh,test_done,{'DEF','STOP_TIME'}},
+     {?eh,cth,{empty_cth,terminate,[[]]}},
+     {?eh,stop_logging,[]}
+    ];
+
+test_events(illegal_values_all) ->
+    [
+     {?eh,start_logging,{'DEF','RUNDIR'}},
+     {?eh,test_start,{'DEF',{'START_TIME','LOGDIR'}}},
+     {?eh,cth,{empty_cth,id,[[]]}},
+     {?eh,cth,{empty_cth,init,[{'_','_','_'},[]]}},
+     {?eh,cth,{empty_cth,post_groups,[all_and_groups_SUITE,'_']}},
+     {?eh,cth,{empty_cth,post_all,
+               [all_and_groups_SUITE,
+                [{group,this_group_does_not_exist},
+                 {this_is_not_a_valid_term}],'_']}},
+     {?eh,start_info,{1,0,0}},
+     {?eh,cth,{empty_cth,post_groups,[all_and_groups_SUITE,'_']}},
+     {?eh,cth,{empty_cth,post_all,
+               [all_and_groups_SUITE,
+                [{group,this_group_does_not_exist},
+                 {this_is_not_a_valid_term}],'_']}},
+     {?eh,tc_start,{ct_framework,error_in_suite}},
+     {?eh,tc_done,
+      {ct_framework,error_in_suite,
+       {failed,
+        {error,'Invalid reference to group this_group_does_not_exist in all_and_groups_SUITE:all/0'}}}},
+     {?eh,test_done,{'DEF','STOP_TIME'}},
+     {?eh,cth,{empty_cth,terminate,[[]]}},
+     {?eh,stop_logging,[]}
+    ];
+
+test_events(bad_return_groups) ->
+    [
+     {?eh,start_logging,{'DEF','RUNDIR'}},
+     {?eh,test_start,{'DEF',{'START_TIME','LOGDIR'}}},
+     {?eh,cth,{empty_cth,id,[[]]}},
+     {?eh,cth,{empty_cth,init,[{'_','_','_'},[]]}},
+     {?eh,cth,{empty_cth,post_groups,[all_and_groups_SUITE,not_a_list]}},
+     {?eh,start_info,{1,0,0}},
+     {?eh,cth,{empty_cth,post_groups,[all_and_groups_SUITE,not_a_list]}},
+     {?eh,tc_start,{ct_framework,error_in_suite}},
+     {?eh,tc_done,
+      {ct_framework,error_in_suite,
+       {failed,
+        {error,
+         {'Bad return value from post_groups/2 hook function',not_a_list}}}}},
+     {?eh,test_done,{'DEF','STOP_TIME'}},
+     {?eh,cth,{empty_cth,terminate,[[]]}},
+     {?eh,stop_logging,[]}
+    ];
+
+test_events(bad_return_all) ->
+    [
+     {?eh,start_logging,{'DEF','RUNDIR'}},
+     {?eh,test_start,{'DEF',{'START_TIME','LOGDIR'}}},
+     {?eh,cth,{empty_cth,id,[[]]}},
+     {?eh,cth,{empty_cth,init,[{'_','_','_'},[]]}},
+     {?eh,cth,{empty_cth,post_groups,[all_and_groups_SUITE,'_']}},
+     {?eh,cth,{empty_cth,post_all,[all_and_groups_SUITE,not_a_list,'_']}},
+     {?eh,start_info,{1,0,0}},
+     {?eh,cth,{empty_cth,post_groups,[all_and_groups_SUITE,'_']}},
+     {?eh,cth,{empty_cth,post_all,[all_and_groups_SUITE,not_a_list,'_']}},
+     {?eh,tc_start,{ct_framework,error_in_suite}},
+     {?eh,tc_done,
+      {ct_framework,error_in_suite,
+       {failed,
+        {error,{'Bad return value from post_all/3 hook function',not_a_list}}}}},
+     {?eh,test_done,{'DEF','STOP_TIME'}},
+     {?eh,cth,{empty_cth,terminate,[[]]}},
+     {?eh,stop_logging,[]}
+    ];
+
+test_events(crash_groups) ->
+    [
+     {?eh,start_logging,{'DEF','RUNDIR'}},
+     {?eh,test_start,{'DEF',{'START_TIME','LOGDIR'}}},
+     {?eh,cth,{empty_cth,id,[[]]}},
+     {?eh,cth,{empty_cth,init,[{'_','_','_'},[]]}},
+     {?eh,cth,{empty_cth,post_groups,[all_and_groups_SUITE,crash]}},
+     {?eh,start_info,{1,0,0}},
+     {?eh,cth,{empty_cth,post_groups,[all_and_groups_SUITE,crash]}},
+     {?eh,tc_start,{ct_framework,error_in_suite}},
+     {?eh,tc_done,{ct_framework,error_in_suite,
+                   {failed,
+                    {error,"all_and_groups_cth:post_groups/2 CTH call failed"}}}},
+     {?eh,test_done,{'DEF','STOP_TIME'}},
+     {?eh,cth,{empty_cth,terminate,[[]]}},
+     {?eh,stop_logging,[]}
+    ];
+
+test_events(crash_all) ->
+    [
+     {?eh,start_logging,{'DEF','RUNDIR'}},
+     {?eh,test_start,{'DEF',{'START_TIME','LOGDIR'}}},
+     {?eh,cth,{empty_cth,id,[[]]}},
+     {?eh,cth,{empty_cth,init,[{'_','_','_'},[]]}},
+     {?eh,cth,{empty_cth,post_groups,[all_and_groups_SUITE,'_']}},
+     {?eh,cth,{empty_cth,post_all,[all_and_groups_SUITE,crash,'_']}},
+     {?eh,start_info,{1,0,0}},
+     {?eh,cth,{empty_cth,post_groups,[all_and_groups_SUITE,'_']}},
+     {?eh,cth,{empty_cth,post_all,[all_and_groups_SUITE,crash,'_']}},
+     {?eh,tc_start,{ct_framework,error_in_suite}},
+     {?eh,tc_done,{ct_framework,error_in_suite,
+                   {failed,
+                    {error,"all_and_groups_cth:post_all/3 CTH call failed"}}}},
+     {?eh,test_done,{'DEF','STOP_TIME'}},
+     {?eh,cth,{empty_cth,terminate,[[]]}},
+     {?eh,stop_logging,[]}
+    ];
 
 test_events(ok) ->
     ok.

commit fe8521bf93994a1d0d2021099f3a99473e1d3731

[ct] Fix bug where Hook:terminate/1 was called before Hook:init/2
This problem was introduced with commit
acd87425acf7705328b01aa8f0a804207ffe4790, when the new hook function
post_groups/2 and post_all/3 were added.

lib/common_test/src/ct_hooks.erl
@@ -373,7 +373,16 @@ terminate_if_scope_ends(HookId, Function0, Hooks) ->
     Function = strip_config(Function0),
     case lists:keyfind(HookId, #ct_hook_config.id, Hooks) of
         #ct_hook_config{ id = HookId, scope = Function} = Hook ->
-            terminate([Hook]),
+            case Function of
+                [AllOrGroup,_] when AllOrGroup=:=post_all;
+                                    AllOrGroup=:=post_groups ->
+                    %% The scope only contains one function (post_all
+                    %% or post_groups), and init has not been called,
+                    %% so skip terminate as well.
+                    ok;
+                _ ->
+                    terminate([Hook])
+            end,
             lists:keydelete(HookId, #ct_hook_config.id, Hooks);
         _ ->
             Hooks

lib/common_test/test/ct_hooks_SUITE.erl
@@ -666,9 +666,15 @@ test_events(scope_suite_cth) ->
      {?eh,test_start,{'DEF',{'START_TIME','LOGDIR'}}},
      %% check that post_groups and post_all comes before init when hook
      %% is installed in suite/0
+     %% And there should be no terminate after these, since init is
+     %% not yet called.
      {?eh,cth,{'_',post_groups,['_',[]]}},
-     {?eh,cth,{'_',post_all,['_','_',[]]}},
-     {?eh,tc_start,{ct_scope_suite_cth_SUITE,init_per_suite}},
+     {negative,
+      {?eh,cth,{'_',terminate,['_']}},
+      {?eh,cth,{'_',post_all,['_','_',[]]}}},
+     {negative,
+      {?eh,cth,{'_',terminate,['_']}},
+      {?eh,tc_start,{ct_scope_suite_cth_SUITE,init_per_suite}}},
      {?eh,cth,{'_',id,[[]]}},
      {?eh,cth,{'_',init,['_',[]]}},
      {?eh,cth,{'_',pre_init_per_suite,[ct_scope_suite_cth_SUITE,'$proplist',[]]}},