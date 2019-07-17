17fb07f0fb1c17d0712cc6176e14466efc19b93d
Micael Karlberg <bmk@erlang.org>
2019-07-17 12:15:52 +0200

    [megaco|test] Removed the "compile all" compiler directive (app)
    Removed the "compile all" compiler directive for the app
    megaco test suite.

--- lib/megaco/test/megaco_app_test.erl
+++ lib/megaco/test/megaco_app_test.erl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 2002-2016. All Rights Reserved.
+%% Copyright Ericsson AB 2002-2019. All Rights Reserved.
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -20,29 +20,42 @@
 %%----------------------------------------------------------------------
 %% Purpose: Verify the application specifics of the Megaco application
 %%----------------------------------------------------------------------
+
 -module(megaco_app_test).
 
--compile(export_all).
+-export([
+         all/0,
+
+         app/0,   app/1,
+         appup/0, appup/1
+        ]).
 
 -include_lib("common_test/include/ct.hrl").
+
 
 %%--------------------------------------------------------------------
 %% Common Test interface functions -----------------------------------
 %%--------------------------------------------------------------------
+
 all() -> 
     [
      app, 
      appup
     ].
 
+
 %%--------------------------------------------------------------------
 %% Test Cases --------------------------------------------------------
 %%--------------------------------------------------------------------
+
 app() ->
     [{doc, "Test that the megaco app file is ok"}].
 app(Config) when is_list(Config) ->
     ok = test_server:app_test(megaco).
+
+
 %%--------------------------------------------------------------------
+
 appup() ->
     [{doc, "Test that the megaco appup file is ok"}].
 appup(Config) when is_list(Config) ->
