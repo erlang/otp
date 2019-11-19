78241111e38a29934936ae11d37cebb308413b1a
Hans Nilsson <hans@erlang.org>
2019-11-19 13:51:58 +0100

    crypto: Add prop__crypto_init_update to property tests

--- lib/crypto/test/property_test/crypto_ng_api.erl
+++ lib/crypto/test/property_test/crypto_ng_api.erl
@@ -55,6 +55,7 @@
 
 -include("crypto_prop_generators.hrl").
 
+%%%================================================================
 %%% Properties:
 
 prop__crypto_one_time() ->
@@ -67,6 +68,17 @@ prop__crypto_one_time() ->
                     )
             ).
 
+prop__crypto_init_update() ->
+    numtests(10000,
+             ?FORALL({TextPlain, Cipher, Key, IV}, ?LET(Ciph,cipher(),
+                                                                 {text_plain(), Ciph, key(Ciph), iv(Ciph)}),
+                     equal(TextPlain,
+                           full_blocks(TextPlain, Cipher),
+                           decrypt_encrypt_init_update(Cipher, Key, IV, TextPlain))
+                    )
+            ).
+
+%%%================================================================
 %%% Lib
 
 equal(_, T, T) -> true;
@@ -79,14 +91,44 @@ equal(F, Tp, Td) ->
 
 
 decrypt_encrypt_one_time(Cipher, Key, IV, TextPlain) ->
+    io:format("~p:~p Cipher: ~p, BlockSize: ~p, Key: ~p, IV: ~p, TextPlain: ~p (~p chunks)",
+              [?MODULE,?LINE, Cipher, block_size(Cipher), size(Key), size(IV), size(iolist_to_binary(TextPlain)),
+               num_chunks(TextPlain)]),
     TextCrypto = crypto:crypto_one_time(Cipher, Key, IV, TextPlain, true),
-    crypto:crypto_one_time(Cipher, Key, IV, TextCrypto, false).
+    io:format("~p:~p TextCrypto: ~p", [?MODULE,?LINE, size(TextCrypto)]),
+    TextDecrypt = crypto:crypto_one_time(Cipher, Key, IV, TextCrypto, false),
+    io:format("~p:~p TextDecrypt: ~p", [?MODULE,?LINE, size(TextDecrypt)]),
+    TextDecrypt.
 
+
+decrypt_encrypt_init_update(Cipher, Key, IV, TextPlain) when is_binary(TextPlain) ->
+    decrypt_encrypt_init_update(Cipher, Key, IV, [TextPlain]);
+
+decrypt_encrypt_init_update(Cipher, Key, IV, TextPlain) ->
+    io:format("~p:~p Cipher: ~p, BlockSize: ~p, Key: ~p, IV: ~p, TextPlain: ~p (~p chunks)",
+              [?MODULE,?LINE, Cipher, block_size(Cipher), size(Key), size(IV), size(iolist_to_binary(TextPlain)),
+               num_chunks(TextPlain)]),
+    Cenc = crypto:crypto_init(Cipher, Key, IV, true),
+    TextOut = lists:foldl(fun(TextIn, TextOutAcc) ->
+                                  [crypto:crypto_update(Cenc,TextIn) | TextOutAcc]
+                          end, [], TextPlain),
+    TextCrypto = lists:reverse(TextOut),
+    io:format("~p:~p TextCrypto: ~p",
+              [?MODULE,?LINE, size(iolist_to_binary(TextCrypto))]),
     
+    Cdec = crypto:crypto_init(Cipher, Key, IV, false),
+    TextDec = lists:foldl(fun(TextC, TextDecAcc) ->
+                                  [crypto:crypto_update(Cdec,TextC) | TextDecAcc]
+                          end, [], TextCrypto),
+    iolist_to_binary(lists:reverse(TextDec)).
+
 full_blocks(TextPlain, Cipher) ->
     TextPlainBin = iolist_to_binary(TextPlain),
     {Head,_Tail} = split_binary(TextPlainBin, (size(TextPlainBin) - num_rest_bytes(TextPlainBin,Cipher))),
     Head.
+
+num_chunks(B) when is_binary(B) -> 1;
+num_chunks(L) when is_list(L) -> length(L).
 
 num_rest_bytes(Bin, Cipher) -> size(Bin) rem block_size(Cipher).
 
