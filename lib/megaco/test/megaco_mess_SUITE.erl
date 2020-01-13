50005d8a3d334ec1eacd688221f75951186b708b
Micael Karlberg <bmk@erlang.org>
2020-01-13 12:26:09 +0100

    [megaco|test] Convert another (mess) test case to try-tc
    'mess' test case request_and_reply_pending_ack_one_pending converted.

--- lib/megaco/test/megaco_mess_SUITE.erl
+++ lib/megaco/test/megaco_mess_SUITE.erl
@@ -1476,20 +1476,25 @@ request_and_reply_pending_ack_one_pending(doc) ->
      "value handle_pending_ack from handle_trans_request when ONE "
      "pending message has been sent"];
 request_and_reply_pending_ack_one_pending(Config) when is_list(Config) ->
-    put(verbosity, ?TEST_VERBOSITY),
-    put(sname,     "TEST"),
-    put(tc,        rar_paop),
-    i("starting"),
+    Pre = fun() ->
+                  MgcNode = make_node_name(mgc),
+                  MgNode  = make_node_name(mg),
+                  d("start nodes: "
+                    "~n      MgcNode: ~p"
+                    "~n      MgNode:  ~p",
+                    [MgcNode, MgNode]),
+                  Nodes = [MgcNode, MgNode],
+                  ok = ?START_NODES(Nodes, true),
+                  Nodes
+          end,
+    Case = fun do_request_and_reply_pending_ack_one_pending/1,
+    Post = fun(Nodes) ->
+                   d("stop nodes"),
+                   ?STOP_NODES(lists:reverse(Nodes))
+           end,
+    try_tc(rar_paop, Pre, Case, Post).
 
-    MgcNode = make_node_name(mgc),
-    MgNode  = make_node_name(mg),
-    d("start nodes: "
-      "~n   MgcNode: ~p"
-      "~n   MgNode:  ~p",
-      [MgcNode, MgNode]),
-    Nodes = [MgcNode, MgNode],
-    ok = ?START_NODES(Nodes, true),
-
+do_request_and_reply_pending_ack_one_pending([MgcNode, MgNode]) ->
     d("[MGC] start the simulator"),
     {ok, Mgc} = megaco_test_megaco_generator:start_link("MGC", MgcNode),
 
@@ -1536,10 +1541,6 @@ request_and_reply_pending_ack_one_pending(Config) when is_list(Config) ->
     %% Tell Mg to stop
     i("[MG] stop generator"),
     megaco_test_megaco_generator:stop(Mg),
-
-    %% Cleanup
-    d("stop nodes"),
-    ?STOP_NODES(lists:reverse(Nodes)),
 
     i("done", []),
     ok.
