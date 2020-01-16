d2e5233a9d0869ab6c4f670bae037639d64424e9
Micael Karlberg <bmk@erlang.org>
2020-01-16 14:20:42 +0100

    [megaco|test] Convert another (mess) test case to try-tc
    'mess' test case request_and_reply_and_late_ack converted.

--- lib/megaco/test/megaco_mess_SUITE.erl
+++ lib/megaco/test/megaco_mess_SUITE.erl
@@ -4497,20 +4497,25 @@ request_and_reply_and_late_ack(doc) ->
     ["This test case tests that megaco handles three-way-handshake "
      "when the ack is late (and requeire a retransmission)"];
 request_and_reply_and_late_ack(Config) when is_list(Config) ->
-    put(verbosity, ?TEST_VERBOSITY),
-    put(sname,     "TEST"),
-    put(tc,        rarala),
-    i("starting"),
+    Pre = fun() ->
+                  MgcNode = make_node_name(mgc),
+                  MgNode  = make_node_name(mg),
+                  d("start nodes: "
+                    "~n   MgcNode: ~p"
+                    "~n   MgNode:  ~p",
+                    [MgcNode, MgNode]),
+                  Nodes = [MgcNode, MgNode], 
+                  ok = ?START_NODES(Nodes, true),
+                  Nodes
+          end,
+    Case = fun do_request_and_reply_and_late_ack/1,
+    Post = fun(Nodes) ->
+                   d("stop nodes"),
+                   ?STOP_NODES(lists:reverse(Nodes))
+           end,
+    try_tc(rarala, Pre, Case, Post).
 
-    MgcNode = make_node_name(mgc),
-    MgNode  = make_node_name(mg),
-    d("start nodes: "
-      "~n   MgcNode: ~p"
-      "~n   MgNode:  ~p",
-      [MgcNode, MgNode]),
-    Nodes = [MgcNode, MgNode], 
-    ok = ?START_NODES(Nodes, true),
-
+do_request_and_reply_and_late_ack([MgcNode, MgNode]) ->
     d("[MGC] start the simulator "),
     {ok, Mgc} = megaco_test_megaco_generator:start_link("MGC", MgcNode),
 
@@ -4555,10 +4560,6 @@ request_and_reply_and_late_ack(Config) when is_list(Config) ->
     %% Tell Mg to stop
     i("[MG] stop generator"),
     megaco_test_tcp_generator:stop(Mg),
-
-    %% Cleanup
-    d("stop nodes"),
-    ?STOP_NODES(lists:reverse(Nodes)),
 
     i("done", []),
     ok.
