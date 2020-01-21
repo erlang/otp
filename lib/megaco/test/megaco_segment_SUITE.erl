1259167b9fd3fa574d8cdd309f12a4f62ecccc21
Micael Karlberg <bmk@erlang.org>
2020-01-21 12:56:10 +0100

    [megaco|test] Convert another (segment) test case to try-tc
    'segment' test case recv_segmented_msg_ooo_seg converted.

--- lib/megaco/test/megaco_segment_SUITE.erl
+++ lib/megaco/test/megaco_segment_SUITE.erl
@@ -5668,18 +5668,25 @@ recv_segmented_msg_ooo_seg(suite) ->
 recv_segmented_msg_ooo_seg(doc) ->
     "Received segmented megaco message [out-of-order segments]";
 recv_segmented_msg_ooo_seg(Config) when is_list(Config) ->
-    put(verbosity, ?TEST_VERBOSITY),
-    put(sname,     "TEST"),
-    put(tc,        rsmos),
-    i("starting"),
+    Pre = fun() ->
+                  MgcNode = make_node_name(mgc),
+                  MgNode  = make_node_name(mg),
+                  d("start nodes: "
+                    "~n   MgcNode: ~p"
+                    "~n   MgNode:  ~p",
+                    [MgcNode, MgNode]),
+                  Nodes = [MgcNode, MgNode],
+                  ok = ?START_NODES(Nodes),
+                  Nodes
+          end,
+    Case = fun do_recv_segmented_msg_ooo_seg/1,
+    Post = fun(Nodes) ->
+                   d("stop nodes"),
+                   ?STOP_NODES(lists:reverse(Nodes))
+           end,
+    try_tc(rsmos, Pre, Case, Post).
 
-    MgcNode = make_node_name(mgc),
-    MgNode  = make_node_name(mg),
-    d("start nodes: "
-      "~n   MgcNode: ~p"
-      "~n   MgNode:  ~p",
-      [MgcNode, MgNode]),
-    ok = megaco_test_lib:start_nodes([MgcNode, MgNode], ?FILE, ?LINE),
+do_recv_segmented_msg_ooo_seg([MgcNode, MgNode]) ->
 
     d("[MGC] start the simulator "),
     {ok, Mgc} = megaco_test_tcp_generator:start_link("MGC", MgcNode),
