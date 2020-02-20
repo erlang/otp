e949205f58846d0e9dac44529978ed74c6b8d15f
Ingela Anderton Andin <ingela@erlang.org>
2020-02-20 10:19:25 +0100

    ssl: Make Erlang distribution run TLS-1.2 for now

--- lib/ssl/src/inet_tls_dist.erl
+++ lib/ssl/src/inet_tls_dist.erl
@@ -759,7 +759,7 @@ nodelay() ->
 get_ssl_options(Type) ->
     try ets:lookup(ssl_dist_opts, Type) of
         [{Type, Opts}] ->
-            [{erl_dist, true} | Opts];
+            [{erl_dist, true}, {versions, ['tlsv1.2']} | Opts];
         _ ->
             get_ssl_dist_arguments(Type)
     catch
@@ -770,9 +770,9 @@ get_ssl_options(Type) ->
 get_ssl_dist_arguments(Type) ->
     case init:get_argument(ssl_dist_opt) of
 	{ok, Args} ->
-	    [{erl_dist, true} | ssl_options(Type, lists:append(Args))];
+	    [{erl_dist, true}, {versions, ['tlsv1.2']} | ssl_options(Type, lists:append(Args))];
 	_ ->
-	    [{erl_dist, true}]
+	    [{erl_dist, true}, {versions, ['tlsv1.2']}]
     end.
 
 
