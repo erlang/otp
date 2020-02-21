48ecc7a66a23be4d3b8bb7976aaae0ba8e72f0e8
Lukas Larsson <lukas@erlang.org>
2020-02-21 10:19:05 +0100

    Add DOC_TARGETS makefile variable
    The variable can be set to pdf, html, man or chunks
    and also any combination of the above.

--- make/doc.mk
+++ make/doc.mk
@@ -75,6 +75,8 @@ EMPTY_CHUNK_FILES = $(filter-out $(NO_CHUNKS:%.xml=$(CHUNKSDIR)/%.chunk) $(CHUNK
 
 SPECS_FLAGS = -I$(ERL_TOP)/lib -I$(ERL_TOP)/lib/*/include -I$(ERL_TOP)/lib/*/src
 
+
+
 # ----------------------------------------------------
 # Targets
 # ----------------------------------------------------
@@ -85,13 +87,15 @@ $(HTMLDIR)/%.png: %.png
 $(HTMLDIR)/%.jpg: %.jpg
 	$(INSTALL_DATA) $< $@
 
-docs: man pdf html chunks $(INFO_FILE)
+DOC_TARGETS?=man pdf html chunks
+
+docs: $(INFO_FILE) $(DOC_TARGETS)
 
 $(TOP_PDF_FILE): $(XML_FILES)
 
 pdf: $(TOP_PDF_FILE)
 
-html: images $(HTML_REF_MAN_FILE)
+html: images $(HTML_REF_MAN_FILE) $(HTMLDIR)/$(APPLICATION).eix
 
 man: $(MAN1_FILES) $(MAN2_FILES) $(MAN3_FILES) $(MAN4_FILES) $(MAN5_FILES) $(MAN6_FILES) $(MAN7_FILES)
 
@@ -146,19 +150,27 @@ clean_chunks:
 # ----------------------------------------------------
 include $(ERL_TOP)/make/otp_release_targets.mk
 
-release_docs_spec: docs
+$(RELSYSDIR):
+	$(INSTALL_DIR) "$(RELSYSDIR)"
+
+release_pdf_spec: pdf
 	$(INSTALL_DIR) "$(RELSYSDIR)/doc/pdf"
 	$(INSTALL_DATA) $(TOP_PDF_FILE) "$(RELSYSDIR)/doc/pdf"
+
+release_html_spec: html
 	$(INSTALL_DIR) "$(RELSYSDIR)/doc/html"
 	$(INSTALL_DIR_DATA) $(HTMLDIR) "$(RELSYSDIR)/doc/html"
 ifneq ($(HTML_EXTRA_FILES),)
 	$(INSTALL_DATA) $(HTML_EXTRA_FILES) "$(RELSYSDIR)/doc/html"
 endif
+
+release_chunks_spec: chunks
 ifneq ($(CHUNK_FILES),)
 	$(INSTALL_DIR) "$(RELCHUNKSDIR)/doc/chunks"
 	$(INSTALL_DATA) $(CHUNKSDIR)/* "$(RELCHUNKSDIR)/doc/chunks"
 endif
-	$(INSTALL_DATA) $(INFO_FILE) "$(RELSYSDIR)"
+
+release_man_spec: man
 ifneq ($(MAN1_FILES),)
 	$(INSTALL_DIR) "$(RELEASE_PATH)/man/man1"
 	$(INSTALL_DATA) $(MAN1DIR)/* "$(RELEASE_PATH)/man/man1"
@@ -187,6 +199,9 @@ ifneq ($(MAN7_FILES),)
 	$(INSTALL_DIR) "$(RELEASE_PATH)/man/man7"
 	$(INSTALL_DATA) $(MAN7_FILES) "$(RELEASE_PATH)/man/man7"
 endif
+
+release_docs_spec: $(RELSYSDIR) $(INFO_FILE) $(DOC_TARGETS:%=release_%_spec)
+	$(INSTALL_DATA) $(INFO_FILE) $(RELSYSDIR)
 ifneq ($(STANDARDS),)
 	$(INSTALL_DIR) "$(RELEASE_PATH)/doc/standard"
 	$(INSTALL_DATA) $(STANDARDS) "$(RELEASE_PATH)/doc/standard"
--- make/otp_release_targets.mk
+++ make/otp_release_targets.mk
@@ -142,8 +142,6 @@ $(HTMLDIR)/$(APPLICATION).eix: $(XML_GEN_FILES) $(SPECS_FILES)
 	        -path $(DOCGEN)/priv/dtd_html_entities \
 	        $(DOCGEN)/priv/xsl/db_eix.xsl $(XMLDIR)/book.xml >  $@
 
-docs: $(HTMLDIR)/$(APPLICATION).eix
-
 ## Here awk is used to find all xi:include files in $(BOOK_FILES)
 ## Then we look into all those files check for xi:includes
 BOOK_XI_INC_FILES:=$(foreach file,$(BOOK_FILES),$(shell awk -F\" '/xi:include/ {print $$2}' $(file))) $(BOOK_FILES)
@@ -207,15 +205,21 @@ endif
 # Standard release target
 # ----------------------------------------------------
 
+pdf man chunks html: $(XML_GEN_FILES) $(SPECS_FILES) $(TOP_SPECS_FILE)
+release_man_spec: man
+release_pdf_spec: pdf
+release_chunks_spec: chunks
+release_html_spec: html
+
 ifeq ($(TESTROOT),)
 
-release release_docs release_tests release_html:
+release release_docs release_tests:
 	$(MAKE) $(MFLAGS) RELEASE_PATH=$(OTP_DEFAULT_RELEASE_PATH) \
 		$(TARGET_MAKEFILE)  $@_spec
 
 else
 
-release release_docs release_tests release_html:
+release release_docs release_tests:
 	$(MAKE) $(MFLAGS) RELEASE_PATH="$(TESTROOT)" $(TARGET_MAKEFILE)  $@_spec 
 
 endif
