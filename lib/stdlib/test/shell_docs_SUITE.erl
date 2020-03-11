c078dd312ba9487e50a696d53f12c74b631f5c30
Lukas Larsson <lukas@erlang.org>
2020-03-11 08:53:57 +0100

    stdlib: Add shell_docs test to check links

--- lib/stdlib/test/shell_docs_SUITE.erl
+++ lib/stdlib/test/shell_docs_SUITE.erl
@@ -22,7 +22,7 @@
 	 init_per_group/2,end_per_group/2]).
 -export([init_per_testcase/2, end_per_testcase/2]).
 
--export([render/1]).
+-export([render/1, links/1]).
 
 -include_lib("kernel/include/eep48.hrl").
 
@@ -36,7 +36,7 @@ suite() ->
     [{timetrap,{minutes,10}}].
 
 all() ->
-    [render].
+    [render, links].
 
 groups() ->
     [].
@@ -54,35 +54,90 @@ end_per_group(_GroupName, Config) ->
     Config.
 
 render(_Config) ->
-    Avail = code:all_available(),
-    [render_test(Mod) || {Mod,_,_} <- Avail],
+    docsmap(
+      fun(Mod, #docs_v1{ docs = Docs } = D) ->
+              try
+                  shell_docs:render(Mod, D),
+                  shell_docs:render_type(Mod, D),
+                  [try
+                       shell_docs:render(Mod, F, A, D)
+                   catch _E:R:ST ->
+                           io:format("Failed to render ~p:~p/~p~n~p:~p~n~p~n",
+                                     [Mod,F,A,R,ST,shell_docs:get_doc(Mod,F,A)]),
+                           erlang:raise(error,R,ST)
+                   end || {F,A} <- Mod:module_info(exports)],
+                  [try
+                       shell_docs:render_type(Mod, T, A, D)
+                   catch _E:R:ST ->
+                           io:format("Failed to render type ~p:~p/~p~n~p:~p~n~p~n",
+                                     [Mod,T,A,R,ST,shell_docs:get_type_doc(Mod,T,A)]),
+                           erlang:raise(error,R,ST)
+                   end || {{type,T,A},_,_,_,_} <- Docs]
+              catch throw:R:ST ->
+                      io:format("Failed to render ~p~n~p:~p~n",[Mod,R,ST]),
+                      exit(R)
+              end
+      end).
+
+links(_Config) ->
+    docsmap(
+      fun(Mod, #docs_v1{ module_doc = MDoc, docs = Docs }) ->
+              try
+                  [check_links(Mod, maps:get(<<"en">>,MDoc)) || MDoc =/= none, MDoc =/= hidden]
+              catch _E1:R1:ST1 ->
+                      io:format("Failed to render ~p~n~p:~p~n~p~n",
+                                [Mod,R1,ST1,MDoc]),
+                      erlang:raise(error,R1,ST1)
+              end,
+              [try
+                  check_links(Mod, D)
+               catch _E:R:ST ->
+                       io:format("Failed to render ~p:~p~n~p:~p~n~p~n",
+                                 [Mod,Kind,R,ST,D]),
+                       erlang:raise(error,R,ST)
+               end || {Kind,_Anno,_Sig,#{ <<"en">> := D },_MD} <- Docs]
+      end).
+
+check_links(Mod, [{a,Attr,C}|T]) ->
+    TargetMod = binary_to_atom(proplists:get_value(module, Attr, atom_to_binary(Mod))),
+    case code:get_doc(TargetMod) of
+        {ok, _ModDoc} ->
+            case {proplists:get_value(function, Attr), proplists:get_value(arity, Attr) } of
+                {undefined, undefined} ->
+                    ok;
+                {Func, Arity} ->
+                    case shell_docs:get_doc(TargetMod,
+                                            binary_to_atom(Func),
+                                            binary_to_integer(Arity)) of
+                        [_]  -> ok;
+                        _Else -> throw({could_not_find,TargetMod,
+                                        binary_to_atom(Func),
+                                        binary_to_integer(Arity)})
+                    end
+            end;
+        _Else ->
+            throw({could_not_find, TargetMod})
+    end,
+    check_links(Mod, C),
+    check_links(Mod, T);
+check_links(Mod, [{_,_,C}|T]) ->
+    check_links(Mod, C),
+    check_links(Mod, T);
+check_links(Mod, [C|T]) when is_binary(C) ->
+    check_links(Mod, T);
+check_links(_, []) ->
     ok.
-render_test(Mod) when is_list(Mod) ->
-    render_test(list_to_atom(Mod));
-render_test(Mod) ->
-    try
-        case code:get_doc(Mod) of
-            {error,missing} ->
-                ok;
-            {ok, #docs_v1{ docs = Docs } = D} ->
-                shell_docs:render(Mod, D),
-                shell_docs:render_type(Mod, D),
-                [try
-                     shell_docs:render(Mod, F, A, D)
-                 catch _E:R:ST ->
-                         io:format("Failed to render ~p:~p/~p~n~p:~p~n~p~n",
-                                   [Mod,F,A,R,ST,shell_docs:get_doc(Mod,F,A)]),
-                         erlang:raise(error,R,ST)
-                 end || {F,A} <- Mod:module_info(exports)],
-                [try
-                     shell_docs:render_type(Mod, T, A, D)
-                 catch _E:R:ST ->
-                         io:format("Failed to render type ~p:~p/~p~n~p:~p~n~p~n",
-                                   [Mod,T,A,R,ST,shell_docs:get_type_doc(Mod,T,A)]),
-                         erlang:raise(error,R,ST)
-                 end || {{type,T,A},_,_,_,_} <- Docs]
-        end
-    catch throw:R:ST ->
-            io:format("Failed to render ~p~n~p:~p~n",[Mod,R,ST]),
-            exit(R)
-    end.
+
+docsmap(Fun) ->
+    lists:map(fun F({Mod,_,_}) ->
+                      F(Mod);
+                  F(Mod) when is_list(Mod) ->
+                      F(list_to_atom(Mod));
+                  F(Mod) ->
+                      case code:get_doc(Mod) of
+                          {error, missing} ->
+                              ok;
+                          {ok, Docs} ->
+                              Fun(Mod, Docs)
+                      end
+              end, code:all_available()).
