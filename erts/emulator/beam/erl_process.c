1a5ac74bff9ee198d5552823167f1a2c5080bd18
Rickard Green <rickard@erlang.org>
2020-05-07 15:13:25 +0200

    Fix literal area gc bug

--- erts/emulator/beam/erl_process.c
+++ erts/emulator/beam/erl_process.c
@@ -6921,13 +6921,25 @@ schedule_process_sys_task(Process *p, erts_aint32_t prio, ErtsProcSysTask *st,
 {
     erts_aint32_t fail_state, state;
 
+    fail_state = *fail_state_p;
+
     /* Elevate priority if needed. */
-    state = erts_atomic32_read_nob(&p->state);
-    if (ERTS_PSFLGS_GET_ACT_PRIO(state) > prio) {
+    state = erts_atomic32_read_acqb(&p->state);
+    if (ERTS_PSFLGS_GET_ACT_PRIO(state) <= prio) {
+        if (state & fail_state) {
+            *fail_state_p = state & fail_state;
+            return 0;
+        }
+    }
+    else {
         erts_aint32_t n, a, e;
 
         a = state;
         do {
+            if (a & fail_state) {
+                *fail_state_p = a & fail_state;
+                return 0;
+            }
             if (ERTS_PSFLGS_GET_ACT_PRIO(a) <= prio) {
                 n = a;
                 break;
@@ -6940,8 +6952,6 @@ schedule_process_sys_task(Process *p, erts_aint32_t prio, ErtsProcSysTask *st,
 
         state = n;
     }
-
-    fail_state = *fail_state_p;
 
     return !(active_sys_enqueue(p, st, prio, ERTS_PSFLG_SYS_TASKS,
                                 state, fail_state_p) & fail_state);
@@ -10642,7 +10652,7 @@ request_system_task(Process *c_p, Eterm requester, Eterm target,
 		    Eterm priority, Eterm operation)
 {
     BIF_RETTYPE ret;
-    Process *rp = erts_proc_lookup(target);
+    Process *rp = erts_proc_lookup_raw(target);
     ErtsProcSysTask *st = NULL;
     erts_aint32_t prio, fail_state = ERTS_PSFLG_EXITING;
     Eterm noproc_res, req_type;
