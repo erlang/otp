16225f77e4c8c793461006823ab56b4a1ad50978
Lukas Larsson <lukas@erlang.org>
2020-06-02 15:55:33 +0200

    kernel: Fix erl_dist SUITE nodenames tc
    When giving multiple -sname it is the first one that counts.

--- lib/kernel/test/erl_distribution_SUITE.erl
+++ lib/kernel/test/erl_distribution_SUITE.erl
@@ -216,7 +216,7 @@ nodenames(Config) when is_list(Config) ->
     legal("a_1@b"),
 
     %% Test that giving two -sname works as it should
-    test_node("a_1@b", false, long_or_short() ++ "a_0@b"),
+    started = test_node("a_1@b", false, long_or_short() ++ "a_0@b"),
 
     illegal("cdé@a"),
     illegal("te欢st@a").
@@ -274,8 +274,8 @@ test_node(Name, Illigal) ->
     test_node(Name, Illigal, "").
 test_node(Name, Illigal, ExtraArgs) ->
     ProgName = ct:get_progname(),
-    Command = ProgName ++ " -noinput " ++ ExtraArgs ++
-        long_or_short() ++ Name ++
+    Command = ProgName ++ " -noinput " ++
+        long_or_short() ++ Name ++ ExtraArgs ++
         " -eval \"net_adm:ping('" ++ atom_to_list(node()) ++ "')\"" ++
         case Illigal of
             true ->
