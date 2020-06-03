88a8a54e291615f4f98a70c83fa27f70fe8e9e3e
Péter Dimitrov <peterdmv@erlang.org>
2020-06-03 10:47:54 +0200

    ssl: Catch exceptions when closing ports

--- lib/ssl/test/ssl_test_lib.erl
+++ lib/ssl/test/ssl_test_lib.erl
@@ -572,7 +572,7 @@ openssl_server_loop(Pid, SslPort, Args, StartBuffer) ->
             openssl_server_loop(Pid, SslPort, Args, StartBuffer);
         close ->
             ct:log("~p:~p~n[openssl server] Server closing~n", [?MODULE,?LINE]),
-            port_close(SslPort);
+            catch port_close(SslPort);
         {ssl_closed, _Socket} ->
             %% TODO
             ok
@@ -655,7 +655,7 @@ openssl_client_loop_core(Pid, SslPort, Args) ->
             openssl_client_loop_core(Pid, SslPort, Args);
         close ->
             ct:log("~p:~p~nClient closing~n", [?MODULE,?LINE]),
-            port_close(SslPort);
+            catch port_close(SslPort);
         {ssl_closed, _Socket} ->
             %% TODO
             ok
