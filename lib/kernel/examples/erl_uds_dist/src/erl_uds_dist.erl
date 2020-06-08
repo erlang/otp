2ee0ea70c0cb44d3757d9952864982891da57420
Rickard Green <rickard@erlang.org>
2020-06-08 09:22:34 +0200

    Mark erl_uds_dist with %ExternalCopyright%
    and remove %CopyrightBegin%/%CopyrightEnd% to prevent OTP scripts
    from modifying.

--- lib/kernel/examples/erl_uds_dist/src/erl_uds_dist.erl
+++ lib/kernel/examples/erl_uds_dist/src/erl_uds_dist.erl
@@ -1,6 +1,4 @@
 %%
-%% %CopyrightBegin%
-%%
 %% Copyright 2020 Jérôme de Bretagne
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,7 +13,7 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 %%
-%% %CopyrightEnd%
+%% %ExternalCopyright%
 %%
 -module(erl_uds_dist).
 
