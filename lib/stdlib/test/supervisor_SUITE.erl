erlang/otp

commit 59d4182054866e1bfb68bdc8529df236ee94c7b3

supervisor: Eliminate the use of sets for storing dynamic children

lib/stdlib/src/supervisor.erl
@@ -122,7 +122,7 @@
 		strategy               :: strategy() | 'undefined',
 		children = {[],#{}}    :: children(), % Ids in start order
                 dynamics               :: {'maps', #{pid() => list()}}
-                                        | {'sets', sets:set(pid())}
+                                        | {'mapsets', #{pid() => []}}
                                         | 'undefined',
 		intensity              :: non_neg_integer() | 'undefined',
 		period                 :: pos_integer() | 'undefined',
@@ -1439,36 +1439,41 @@ format_status(_, [_PDict, State]) ->
      {supervisor, [{"Callback", State#state.module}]}].
 
 %%%-----------------------------------------------------------------
-%%% Dynamics database access
-dyn_size(#state{dynamics = {Mod,Db}}) ->
-    Mod:size(Db).
+%%% Dynamics database access.
+%%%
+%%% Store all dynamic children in a map with the pid as the key. If
+%%% the children are permanent, store the start arguments as the value,
+%%% otherwise store [] as the value.
+%%%
 
-dyn_erase(Pid,#state{dynamics={sets,Db}}=State) ->
-    State#state{dynamics={sets,sets:del_element(Pid,Db)}};
-dyn_erase(Pid,#state{dynamics={maps,Db}}=State) ->
+dyn_size(#state{dynamics = {_Kind,Db}}) ->
+    map_size(Db).
+
+dyn_erase(Pid,#state{dynamics={_Kind,Db}}=State) ->
     State#state{dynamics={maps,maps:remove(Pid,Db)}}.
 
-dyn_store(Pid,_,#state{dynamics={sets,Db}}=State) ->
-    State#state{dynamics={sets,sets:add_element(Pid,Db)}};
-dyn_store(Pid,Args,#state{dynamics={maps,Db}}=State) ->
-    State#state{dynamics={maps,Db#{Pid => Args}}}.
+dyn_store(Pid,Args,#state{dynamics={Kind,Db}}=State) ->
+    case Kind of
+        mapsets ->
+            %% Children are temporary. The start arguments
+            %% will not be needed again. Store [].
+            State#state{dynamics={mapsets,Db#{Pid => []}}};
+        maps ->
+            %% Children are permanent and may be restarted.
+            %% Store the start arguments.
+            State#state{dynamics={maps,Db#{Pid => Args}}}
+    end.
 
-dyn_fold(Fun,Init,#state{dynamics={sets,Db}}) ->
-    sets:fold(Fun,Init,Db);
-dyn_fold(Fun,Init,#state{dynamics={maps,Db}}) ->
+dyn_fold(Fun,Init,#state{dynamics={_Kind,Db}}) ->
     maps:fold(fun(Pid,_,Acc) -> Fun(Pid,Acc) end, Init, Db).
 
-dyn_map(Fun, #state{dynamics={sets,Db}}) ->
-    lists:map(Fun, sets:to_list(Db));
-dyn_map(Fun, #state{dynamics={maps,Db}}) ->
+dyn_map(Fun, #state{dynamics={_Kind,Db}}) ->
     lists:map(Fun, maps:keys(Db)).
 
-dyn_exists(Pid, #state{dynamics={sets, Db}}) ->
-    sets:is_element(Pid, Db);
-dyn_exists(Pid, #state{dynamics={maps, Db}}) ->
-    maps:is_key(Pid, Db).
+dyn_exists(Pid, #state{dynamics={_Kind, Db}}) ->
+    is_map_key(Pid, Db).
 
-dyn_args(_Pid, #state{dynamics={sets, _Db}}) ->
+dyn_args(_Pid, #state{dynamics={mapsets, _Db}}) ->
     {ok,undefined};
 dyn_args(Pid, #state{dynamics={maps, Db}}) ->
     maps:find(Pid, Db).
@@ -1477,6 +1482,6 @@ dyn_init(State) ->
     dyn_init(get_dynamic_child(State),State).
 
 dyn_init(Child,State) when ?is_temporary(Child) ->
-    State#state{dynamics={sets,sets:new()}};
+    State#state{dynamics={mapsets,maps:new()}};
 dyn_init(_Child,State) ->
     State#state{dynamics={maps,maps:new()}}.

commit e78ec220622b0c0bff329a54d79bfbfeb4ced018

supervisor: Eliminate a subtle internal state corruption
When a temporary child of a `simple_one_for_one` supervisor died, the
internal state of the supervisor would be corrupted in a way that
would cause the supervisor to retain the start arguments for
subsequent children started by the supervisor, causing
unnecessary growth of the supervisor's heap.

This bug was introduced in 59d418205486.

Thanks to Zeyu Zhang (@zzydxm) for noticing this bug and to
Maxim Fedorov for making it known to me.

lib/stdlib/src/supervisor.erl
@@ -1605,8 +1605,8 @@ format_status(_, [_PDict, State]) ->
 dyn_size(#state{dynamics = {_Kind,Db}}) ->
     map_size(Db).
 
-dyn_erase(Pid,#state{dynamics={_Kind,Db}}=State) ->
-    State#state{dynamics={maps,maps:remove(Pid,Db)}}.
+dyn_erase(Pid,#state{dynamics={Kind,Db}}=State) ->
+    State#state{dynamics={Kind,maps:remove(Pid,Db)}}.
 
 dyn_store(Pid,Args,#state{dynamics={Kind,Db}}=State) ->
     case Kind of

lib/stdlib/test/supervisor_SUITE.erl
@@ -63,6 +63,7 @@
 	  one_for_one_escalation/1, one_for_all/1,
 	  one_for_all_escalation/1, one_for_all_other_child_fails_restart/1,
 	  simple_one_for_one/1, simple_one_for_one_escalation/1,
+          simple_one_for_one_corruption/1,
 	  rest_for_one/1, rest_for_one_escalation/1,
 	  rest_for_one_other_child_fails_restart/1,
 	  simple_one_for_one_extra/1, simple_one_for_one_shutdown/1]).
@@ -139,7 +140,8 @@ groups() ->
        one_for_all_other_child_fails_restart]},
      {restart_simple_one_for_one, [],
       [simple_one_for_one, simple_one_for_one_shutdown,
-       simple_one_for_one_extra, simple_one_for_one_escalation]},
+       simple_one_for_one_extra, simple_one_for_one_escalation,
+       simple_one_for_one_corruption]},
      {restart_rest_for_one, [],
       [rest_for_one, rest_for_one_escalation,
        rest_for_one_other_child_fails_restart]}].
@@ -1276,6 +1278,62 @@ simple_one_for_one_extra(Config) when is_list(Config) ->
     [{Id4, Pid4, _, _}|_] = supervisor:which_children(sup_test),
     terminate(SupPid, Pid4, Id4, abnormal),
     check_exit([SupPid]).
+
+%%-------------------------------------------------------------------------
+%% Test for subtle corruption of internal state for a
+%% simple_one_for_one supervisor. Thanks to Zeyu Zhang (@zzydxm) and
+%% Maxim Fedorov for noticing this bug. (OTP-16804)
+simple_one_for_one_corruption(Config) when is_list(Config) ->
+    process_flag(trap_exit, true),
+
+    logger:add_handler(?MODULE, ?MODULE, #{test_case_pid => self()}),
+
+    try
+        Child = #{id => child, start => {supervisor_1, start_child, []},
+                  restart => temporary, shutdown => 1000,
+                  type => worker, modules => []},
+        {ok, SupPid} = start_link({ok, {{simple_one_for_one, 2, 3600}, [Child]}}),
+        {ok, CPid1} = supervisor:start_child(sup_test, []),
+
+        terminate(SupPid, CPid1, child1, abnormal),
+
+        %% The first time a child of simple_one_for_one supervisor
+        %% with restart strategy `temporary` dies, the internal state
+        %% for the supervisor will become inconsistent (the `dynamics`
+        %% field would change from `{mapsets,Map}` to
+        %% `{maps,Map}`). That inconsistency will make the supervisor
+        %% retain the start arguments even for temporary processes.
+        %%
+        %% To test that the bug is fixed, start a new child process
+        %% with a large term in its argument list.
+
+        N = 50000,
+        BigData = binary_to_list(<<0:N/unit:8>>),
+        {ok, CPid2, BigData} = supervisor:start_child(sup_test, [BigData]),
+
+        %% Since the child is temporary, the supervisor should not keep
+        %% the argument list for the child and the supervisor's heap
+        %% size should shrink after a GC.
+
+        true = erlang:garbage_collect(SupPid),
+        {total_heap_size, HeapSize} = process_info(SupPid, total_heap_size),
+        if
+            HeapSize > 2*N ->
+                %% The start arguments for the child have been kept.
+                ct:fail({excessive_heap_size,HeapSize});
+            true ->
+                ok
+        end,
+
+        terminate(SupPid, CPid2, child2, abnormal),
+
+        exit(SupPid, kill)
+    after
+        logger:remove_handler(?MODULE)
+    end,
+
+    ok.
+
 
 %%-------------------------------------------------------------------------
 %% Test restart escalation on a simple_one_for_one supervisor.