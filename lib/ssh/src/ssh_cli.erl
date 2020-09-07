77d8fdedb26cf5b9696c8494ab34da114ddcd2f8
Hans Nilsson <hans@erlang.org>
2020-09-07 14:41:57 +0200

    ssh: Add ssh_dbg cli trace to ssh_cli.erl

--- lib/ssh/src/ssh_cli.erl
+++ lib/ssh/src/ssh_cli.erl
@@ -675,13 +675,85 @@ not_zero(A, _) ->
 %%%# Tracing
 %%%#
 
-ssh_dbg_trace_points() -> [terminate].
+ssh_dbg_trace_points() -> [terminate, cli, cli_details].
 
+ssh_dbg_flags(cli) -> [c];
 ssh_dbg_flags(terminate) -> [c].
 
+ssh_dbg_on(cli) -> dbg:tp(?MODULE,handle_ssh_msg,2,x),
+                   dbg:tp(?MODULE,write_chars,4,x);
+ssh_dbg_on(cli_details) -> dbg:tp(?MODULE,handle_msg,2,x);
 ssh_dbg_on(terminate) -> dbg:tp(?MODULE,  terminate, 2, x).
 
+
+ssh_dbg_off(cli) -> dbg:ctpg(?MODULE,handle_ssh_msg,2),
+                    dbg:ctpg(?MODULE,write_chars,4);
+ssh_dbg_off(cli_details) -> dbg:ctpg(?MODULE,handle_msg,2);
 ssh_dbg_off(terminate) -> dbg:ctpg(?MODULE, terminate, 2).
+
+
+ssh_dbg_format(cli, {call,{?MODULE,handle_ssh_msg,
+                           [{ssh_cm, _ConnectionHandler, Request},
+                            #state{channel=Ch}]}}) when is_tuple(Request) ->
+    [io_lib:format("CLI conn ~p chan ~p, req ~p", 
+                   [self(),Ch,element(1,Request)]),
+     case Request of
+         {window_change, ChannelId, Width, Height, PixWidth, PixHeight} ->
+             fmt_kv([{channel_id,ChannelId}, 
+                     {width,Width}, {height,Height},
+                     {pix_width,PixWidth}, {pixel_hight,PixHeight}]);
+
+         {env, ChannelId, WantReply, Var, Value} ->
+             fmt_kv([{channel_id,ChannelId}, {want_reply,WantReply}, {Var,Value}]);
+
+         {exec, ChannelId, WantReply, Cmd} ->
+             fmt_kv([{channel_id,ChannelId}, {want_reply,WantReply}, {command,Cmd}]);
+
+         {pty, ChannelId, WantReply,
+          {TermName, Width, Height, PixWidth, PixHeight, Modes}} ->
+             fmt_kv([{channel_id,ChannelId}, {want_reply,WantReply}, 
+                     {term,TermName},
+                     {width,Width}, {height,Height}, {pix_width,PixWidth}, {pixel_hight,PixHeight},
+                     {pty_opts, Modes}]);
+
+         {data, ChannelId, Type, Data} -> 
+             fmt_kv([{channel_id,ChannelId},
+                     {type, type(Type)},
+                     {data, us, ssh_dbg:shrink_bin(Data)},
+                     {hex, h, Data}
+                    ]);
+         _ ->
+             io_lib:format("~nunder construction:~nRequest = ~p",[Request])
+     end];
+ssh_dbg_format(cli, {call,{?MODULE,handle_ssh_msg,_}}) -> skip;
+ssh_dbg_format(cli, {return_from,{?MODULE,handle_ssh_msg,2},_Result}) -> skip;
+
+ssh_dbg_format(cli, {call,{?MODULE,write_chars, [C, Ch, Type, Chars]}}) ->
+    [io_lib:format("CLI conn ~p chan ~p reply", [C,Ch]),
+     fmt_kv([{channel_id,Ch},
+             {type, type(Type)},
+             {data, us, ssh_dbg:shrink_bin(Chars)},
+             {hex, h, Chars}
+            ])];
+ssh_dbg_format(cli, {return_from,{?MODULE,write_chars,4},_Result}) -> skip;
+
+
+ssh_dbg_format(cli_details, {call,{?MODULE,handle_msg,
+                                   [{Group,Arg}, #state{channel=Ch}]}}) ->
+    [io_lib:format("CLI detail conn ~p chan ~p group ~p",
+                   ['?', Ch, Group]),
+     case Arg of
+         {put_chars_sync,Class,Cs,Reply} ->
+             fmt_kv([{op, put_chars_sync},
+                     {class, Class},
+                     {data, us, ssh_dbg:shrink_bin(Cs)},
+                     {hex, h, Cs},
+                     {reply, Reply}]);
+         _ ->
+             io_lib:format("~nunder construction:~nRequest = ~p",[Arg])
+     end];
+ssh_dbg_format(cli_details, {call,{?MODULE,handle_msg,_}}) -> skip;
+ssh_dbg_format(cli_details, {return_from,{?MODULE,handle_msg,2},_Result}) -> skip;
 
 ssh_dbg_format(terminate, {call, {?MODULE,terminate, [Reason, State]}}) ->
     ["Cli Terminating:\n",
@@ -692,3 +764,14 @@ ssh_dbg_format(terminate, {return_from, {?MODULE,terminate,2}, _Ret}) ->
 
 
 ?wr_record(state).
+
+fmt_kv(KVs) -> lists:map(fun fmt_kv1/1, KVs).
+
+fmt_kv1({K,V})   -> io_lib:format("~n~p: ~p",[K,V]);
+fmt_kv1({K,s,V}) -> io_lib:format("~n~p: ~s",[K,V]);
+fmt_kv1({K,us,V}) -> io_lib:format("~n~p: ~ts",[K,V]);
+fmt_kv1({K,h,V}) -> io_lib:format("~n~p: ~s",[K, [$\n|ssh_dbg:hex_dump(V)]]).
+
+type(0) -> "0 (normal data)";
+type(1) -> "1 (extended data, i.e. errors)";
+type(T) -> T.
