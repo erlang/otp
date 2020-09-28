erlang/otp

commit 3641ec51b8025a13290e359ea4c63f5e0941cb9f

beam_validator: Fix regression introduced in 5474277e858138d1dc853

lib/compiler/src/beam_validator.erl
@@ -856,15 +856,16 @@ vi({test,bs_get_binary2=Op,{f,Fail},Live,[Ctx,{atom,all},Unit,_],Dst}, Vst) ->
     Type = #t_bitstring{size_unit=Unit},
     validate_bs_get_all(Op, Fail, Ctx, Live, Unit, Type, Dst, Vst);
 vi({test,bs_get_binary2=Op,{f,Fail},Live,[Ctx,{integer,Sz},Unit,_],Dst}, Vst) ->
-    NumBits = Sz * Unit,
-    Type = #t_bitstring{size_unit=NumBits},
-    validate_bs_get(Op, Fail, Ctx, Live, NumBits, Type, Dst, Vst);
+    Stride = Unit * max(1, Sz),
+    Type = #t_bitstring{size_unit=Stride},
+    validate_bs_get(Op, Fail, Ctx, Live, Stride, Type, Dst, Vst);
 vi({test,bs_get_binary2=Op,{f,Fail},Live,[Ctx,_,Unit,_],Dst}, Vst) ->
     Type = #t_bitstring{size_unit=Unit},
     validate_bs_get(Op, Fail, Ctx, Live, Unit, Type, Dst, Vst);
 vi({test,bs_get_integer2=Op,{f,Fail},Live,
     [Ctx,{integer,Sz},Unit,{field_flags,Flags}],Dst},Vst) ->
-    NumBits = Sz * Unit,
+    NumBits = Unit * Sz,
+    Stride = max(1, NumBits),
     Type = case member(unsigned, Flags) of
                true when 0 =< NumBits, NumBits =< 64 ->
                    beam_types:make_integer(0, (1 bsl NumBits)-1);
@@ -872,11 +873,12 @@ vi({test,bs_get_integer2=Op,{f,Fail},Live,
                    %% Signed integer, way too large, or negative size.
                    #t_integer{}
            end,
-    validate_bs_get(Op, Fail, Ctx, Live, NumBits, Type, Dst, Vst);
+    validate_bs_get(Op, Fail, Ctx, Live, Stride, Type, Dst, Vst);
 vi({test,bs_get_integer2=Op,{f,Fail},Live,[Ctx,_Sz,Unit,_Flags],Dst},Vst) ->
     validate_bs_get(Op, Fail, Ctx, Live, Unit, #t_integer{}, Dst, Vst);
 vi({test,bs_get_float2=Op,{f,Fail},Live,[Ctx,{integer,Sz},Unit,_],Dst},Vst) ->
-    validate_bs_get(Op, Fail, Ctx, Live, Sz * Unit, #t_float{}, Dst, Vst);
+    Stride = Unit * max(1, Sz),
+    validate_bs_get(Op, Fail, Ctx, Live, Stride, #t_float{}, Dst, Vst);
 vi({test,bs_get_float2=Op,{f,Fail},Live,[Ctx,_,_,_],Dst}, Vst) ->
     validate_bs_get(Op, Fail, Ctx, Live, 32, #t_float{}, Dst, Vst);
 vi({test,bs_get_utf8=Op,{f,Fail},Live,[Ctx,_],Dst}, Vst) ->

lib/compiler/test/bs_match_SUITE.erl
@@ -48,7 +48,7 @@
          exceptions_after_match_failure/1,
          bad_phi_paths/1,many_clauses/1,
          combine_empty_segments/1,hangs_forever/1,
-         bs_saved_position_units/1]).
+         bs_saved_position_units/1,empty_get_binary/1]).
 
 -export([coverage_id/1,coverage_external_ignore/2]).
 
@@ -87,7 +87,7 @@ groups() ->
        matching_meets_apply,bs_start_match2_defs,
        exceptions_after_match_failure,bad_phi_paths,
        many_clauses,combine_empty_segments,hangs_forever,
-       bs_saved_position_units]}].
+       bs_saved_position_units,empty_get_binary]}].
 
 init_per_suite(Config) ->
     test_lib:recompile(?MODULE),
@@ -2390,6 +2390,21 @@ bs_saved_position_units(Config) when is_list(Config) ->
 bspu_1(<<Bin/binary>> = Bin) ->
     [Chunk || <<Chunk:5/binary>> <= Bin].
 
+
+empty_get_binary(Config) when is_list(Config) ->
+    {<<>>, <<1,2,3,4:4>>} = egb_1(<<1,2,3,4:4>>),
+    {<<>>, <<1,2,3>>} = egb_1(<<1,2,3>>),
+    {<<>>, <<>>} = egb_1(<<>>),
+
+    ok.
+
+egb_1(Bytes) ->
+    {Term, Bytes} = begin
+                        <<V2@V0:0/binary-unit:8,V2@Buf1/bitstring>> = Bytes,
+                        V2@Conv2 = binary:copy(V2@V0),
+                        {V2@Conv2, V2@Buf1}
+                    end,
+    {Term, Bytes}.
 
 id(I) -> I.
 

commit 8875a8bf97ff1658655e389ecc08d5ad17834320

beam_validator: Fix regression introduced in 3641ec5

lib/compiler/src/beam_validator.erl
@@ -1502,9 +1502,13 @@ validate_bs_skip_1(Fail, Ctx, Stride, Live, Vst) ->
            end).
 
 advance_bs_context(Ctx, Stride, Vst0) ->
-    %% slots/valid must remain untouched to support +r21.
+    %% slots/valid must remain untouched to support +r21, and the prior unit
+    %% must be retained if we _KNOW_ we won't advance.
     CtxType0 = get_raw_type(Ctx, Vst0),
-    CtxType = CtxType0#t_bs_context{ tail_unit=max(1, Stride) },
+    CtxType = case Stride of
+                  0 -> CtxType0;
+                  N -> CtxType0#t_bs_context{ tail_unit=N }
+              end,
 
     Vst = update_type(fun join/2, CtxType, Ctx, Vst0),
 

lib/compiler/test/bs_match_SUITE.erl
@@ -2396,6 +2396,9 @@ empty_get_binary(Config) when is_list(Config) ->
     {<<>>, <<1,2,3>>} = egb_1(<<1,2,3>>),
     {<<>>, <<>>} = egb_1(<<>>),
 
+    <<0,1,0,2,0,3>> = egb_2(id(<<1,2,3>>)),
+    <<>> = egb_2(id(<<>>)),
+
     ok.
 
 egb_1(Bytes) ->
@@ -2405,6 +2408,11 @@ egb_1(Bytes) ->
                         {V2@Conv2, V2@Buf1}
                     end,
     {Term, Bytes}.
+
+egb_2(Bin) ->
+    <<
+      <<K,N>> || <<K:0,N>> <= Bin
+    >>.
 
 id(I) -> I.
 