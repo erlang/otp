erlang/otp

commit 8b8c7a63030360436f2ee6d6390d120199260823

erts: Make NIL positive
This makes generated x86 code more compact as we don't need to
sign-extend it everywhere.

Before:
    ; 48 c7 c0 fb ff ff ff
    mov rax, 0xfffffffffffffffb

After:
    ; b8 3b 00 00 00
    mov eax, 0x3b

erts/emulator/beam/erl_term.h
@@ -283,7 +283,7 @@ _ET_DECLARE_CHECKED(Sint,signed_val,Eterm)
 #endif
 
 /* NIL access methods */
-#define NIL		((~((Uint) 0) << _TAG_IMMED2_SIZE) | _TAG_IMMED2_NIL)
+#define NIL		_TAG_IMMED2_NIL
 #define is_nil(x)	((x) == NIL)
 #define is_not_nil(x)	((x) != NIL)
 

erts/etc/unix/etp-commands.in
@@ -4712,12 +4712,11 @@ end
 define etp-init
   set $etp_arch64 = (sizeof(void *) == 8)
   if $etp_arch64
-    set $etp_nil = 0xfffffffffffffffb
     set $etp_MBC_ABLK_OFFSET_BITS = 23
   else
-    set $etp_nil = 0xfffffffb
     set $etp_MBC_ABLK_OFFSET_BITS = 8
   end
+  set $etp_nil = 0x3b
   set $etp_MBC_ABLK_OFFSET_SHIFT = (sizeof(UWord)*8 - 1 - $etp_MBC_ABLK_OFFSET_BITS)
   set $etp_MBC_ABLK_OFFSET_MASK = ((((UWord)1 << $etp_MBC_ABLK_OFFSET_BITS) - 1) << $etp_MBC_ABLK_OFFSET_SHIFT)
   set $etp_MBC_ABLK_SZ_MASK = ((UWord)1 << $etp_MBC_ABLK_OFFSET_SHIFT) - 1 - 7

commit 42455fb53d4becc2e9a6b8c73fe5f3b54b6ee307

erts: Fix NIL to be correct size
When used as an argument to a variadic function NIL has to
have the correct size otherwise it is assumed to be an int
which is not what the va_list function expects.

This was broken in: 8b8c7a63030360436f2ee6d6390d120199260823

erts/emulator/beam/erl_term.h
@@ -283,7 +283,7 @@ _ET_DECLARE_CHECKED(Sint,signed_val,Eterm)
 #endif
 
 /* NIL access methods */
-#define NIL		_TAG_IMMED2_NIL
+#define NIL		((Eterm)(_TAG_IMMED2_NIL))
 #define is_nil(x)	((x) == NIL)
 #define is_not_nil(x)	((x) != NIL)
 