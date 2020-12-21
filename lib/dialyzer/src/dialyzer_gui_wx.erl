erlang/otp

commit 3ffed207dffdaad6924e3333f8ee30ac5920aa24

dialyzer: Do not use two records for PLTs
Instead of two records, #plt{} with dict:s and #mini_plt{} with ETS
tables, one record is used for representing PLT:s in RAM.

The #mini_plt{} is the one now used throughout analyses, but it is
called #plt{}.

When writing the PLT to file, another record is used, #file_plt{} (as
before). When creating #file_plt{}, the #plt{} is deleted (it cannot
be used for further analyses).

lib/dialyzer/src/dialyzer_gui_wx.erl
@@ -498,9 +498,9 @@ gui_loop(#gui_state{backend_pid = BackendPid, doc_plt = DocPlt,
 	end,
       ExplanationPid = spawn_link(Fun),
       gui_loop(State#gui_state{expl_pid = ExplanationPid});
-    {BackendPid, done, NewMiniPlt, NewDocPlt} ->
+    {BackendPid, done, NewPlt, NewDocPlt} ->
       message(State, "Analysis done"),
-      dialyzer_plt:delete(NewMiniPlt),
+      dialyzer_plt:delete(NewPlt),
       config_gui_stop(State),
       gui_loop(State#gui_state{doc_plt = NewDocPlt});
     {'EXIT', BackendPid, {error, Reason}} ->

lib/dialyzer/src/dialyzer_analysis_callgraph.erl
@@ -92,11 +92,12 @@ loop(#server_state{parent = Parent} = State,
       send_warnings(Parent, Warnings),
       loop(State, Analysis, ExtCalls);
     {AnalPid, cserver, CServer, Plt} ->
+      skip_ets_transfer(AnalPid),
       send_codeserver_plt(Parent, CServer, Plt),
       loop(State, Analysis, ExtCalls);
-    {AnalPid, done, MiniPlt, DocPlt} ->
+    {AnalPid, done, Plt, DocPlt} ->
       send_ext_calls(Parent, ExtCalls),
-      send_analysis_done(Parent, MiniPlt, DocPlt);
+      send_analysis_done(Parent, Plt, DocPlt);
     {AnalPid, ext_calls, NewExtCalls} ->
       loop(State, Analysis, NewExtCalls);
     {AnalPid, ext_types, ExtTypes} ->
@@ -168,16 +169,17 @@ analysis_start(Parent, Analysis, LegalWarnings) ->
       false -> Callgraph
     end,
   State2 = analyze_callgraph(NewCallgraph, State1),
-  #analysis_state{plt = MiniPlt2,
+  #analysis_state{plt = Plt2,
                   doc_plt = DocPlt,
                   codeserver = Codeserver0} = State2,
-  {Codeserver, MiniPlt3} = move_data(Codeserver0, MiniPlt2),
+  {Codeserver, Plt3} = move_data(Codeserver0, Plt2),
   dialyzer_callgraph:dispose_race_server(NewCallgraph),
   %% Since the PLT is never used, a dummy is sent:
   DummyPlt = dialyzer_plt:new(),
   send_codeserver_plt(Parent, Codeserver, DummyPlt),
-  MiniPlt4 = dialyzer_plt:delete_list(MiniPlt3, NonExportsList),
-  send_analysis_done(Parent, MiniPlt4, DocPlt).
+  dialyzer_plt:delete(DummyPlt),
+  Plt4 = dialyzer_plt:delete_list(Plt3, NonExportsList),
+  send_analysis_done(Parent, Plt4, DocPlt).
 
 contracts_and_records(CodeServer, Parent) ->
   Fun = contrs_and_recs(CodeServer, Parent),
@@ -185,7 +187,16 @@ contracts_and_records(CodeServer, Parent) ->
   dialyzer_codeserver:give_away(CodeServer, Pid),
   Pid ! {self(), go},
   receive {'DOWN', Ref, process, Pid, Return} ->
+      skip_ets_transfer(Pid),
       Return
+  end.
+
+skip_ets_transfer(Pid) ->
+  receive
+    {'ETS-TRANSFER', _Tid, Pid, _HeriData} ->
+      skip_ets_transfer(Pid)
+  after 0 ->
+      ok
   end.
 
 -spec contrs_and_recs(dialyzer_codeserver:codeserver(), pid()) ->
@@ -202,12 +213,12 @@ contrs_and_recs(TmpCServer2, Parent) ->
       exit(TmpServer4)
   end.
 
-move_data(CServer, MiniPlt) ->
+move_data(CServer, Plt) ->
   {CServer1, Records} = dialyzer_codeserver:extract_records(CServer),
-  MiniPlt1 = dialyzer_plt:insert_types(MiniPlt, Records),
+  Plt1 = dialyzer_plt:insert_types(Plt, Records),
   {NewCServer, ExpTypes} = dialyzer_codeserver:extract_exported_types(CServer1),
-  NewMiniPlt = dialyzer_plt:insert_exported_types(MiniPlt1, ExpTypes),
-  {NewCServer, NewMiniPlt}.
+  NewPlt = dialyzer_plt:insert_exported_types(Plt1, ExpTypes),
+  {NewCServer, NewPlt}.
 
 analyze_callgraph(Callgraph, #analysis_state{codeserver = Codeserver,
 					     doc_plt = DocPlt,
@@ -217,19 +228,19 @@ analyze_callgraph(Callgraph, #analysis_state{codeserver = Codeserver,
                                              solvers = Solvers} = State) ->
   case State#analysis_state.analysis_type of
     plt_build ->
-      NewMiniPlt =
+      NewPlt =
         dialyzer_succ_typings:analyze_callgraph(Callgraph, Plt, Codeserver,
                                                 TimingServer, Solvers, Parent),
       dialyzer_callgraph:delete(Callgraph),
-      State#analysis_state{plt = NewMiniPlt, doc_plt = DocPlt};
+      State#analysis_state{plt = NewPlt, doc_plt = DocPlt};
     succ_typings ->
-      {Warnings, NewMiniPlt, NewDocPlt} =
+      {Warnings, NewPlt, NewDocPlt} =
         dialyzer_succ_typings:get_warnings(Callgraph, Plt, DocPlt, Codeserver,
                                            TimingServer, Solvers, Parent),
       dialyzer_callgraph:delete(Callgraph),
       Warnings1 = filter_warnings(Warnings, Codeserver),
       send_warnings(State#analysis_state.parent, Warnings1),
-      State#analysis_state{plt = NewMiniPlt, doc_plt = NewDocPlt}
+      State#analysis_state{plt = NewPlt, doc_plt = NewDocPlt}
     end.
 
 %%--------------------------------------------------------------------
@@ -565,9 +576,8 @@ is_ok_fun({_Filename, _Line, {_M, _F, _A} = MFA}, Codeserver) ->
 is_ok_tag(Tag, {_F, _L, MorMFA}, Codeserver) ->
   not dialyzer_utils:is_suppressed_tag(MorMFA, Tag, Codeserver).
   
-send_analysis_done(Parent, MiniPlt, DocPlt) ->
-  ok = dialyzer_plt:give_away(MiniPlt, Parent),
-  Parent ! {self(), done, MiniPlt, DocPlt},
+send_analysis_done(Parent, Plt, DocPlt) ->
+  Parent ! {self(), done, Plt, DocPlt},
   ok.
 
 send_ext_calls(_Parent, none) ->

lib/dialyzer/src/dialyzer_callgraph.erl
@@ -778,7 +778,6 @@ to_ps(#callgraph{} = CG, File, Args) ->
   ok.
 
 condensation(G) ->
-  erlang:garbage_collect(), % reduce heap size
   {Pid, Ref} = erlang:spawn_monitor(do_condensation(G, self())),
   receive {'DOWN', Ref, process, Pid, Result} ->
       {SCCInts, OutETS, InETS, MapsETS} = Result,

lib/dialyzer/src/dialyzer_cl.erl
@@ -637,8 +637,8 @@ cl_loop(State, LogCache) ->
     {BackendPid, cserver, CodeServer, _Plt} -> % Plt is ignored
       NewState = State#cl_state{code_server = CodeServer},
       cl_loop(NewState, LogCache);
-    {BackendPid, done, NewMiniPlt, _NewDocPlt} ->
-      return_value(State, NewMiniPlt);
+    {BackendPid, done, NewPlt, _NewDocPlt} ->
+      return_value(State, NewPlt);
     {BackendPid, ext_calls, ExtCalls} ->
       cl_loop(State#cl_state{external_calls = ExtCalls}, LogCache);
     {BackendPid, ext_types, ExtTypes} ->
@@ -700,7 +700,7 @@ return_value(State = #cl_state{code_server = CodeServer,
 			       output_plt = OutputPlt,
 			       plt_info = PltInfo,
 			       stored_warnings = StoredWarnings},
-	     MiniPlt) ->
+	     Plt) ->
   %% Just for now:
   case CodeServer =:= none of
     true ->
@@ -710,18 +710,9 @@ return_value(State = #cl_state{code_server = CodeServer,
   end,
   case OutputPlt =:= none of
     true ->
-      dialyzer_plt:delete(MiniPlt);
+      dialyzer_plt:delete(Plt);
     false ->
-      Fun = to_file_fun(OutputPlt, MiniPlt, ModDeps, PltInfo),
-      {Pid, Ref} = erlang:spawn_monitor(Fun),
-      dialyzer_plt:give_away(MiniPlt, Pid),
-      Pid ! go,
-      receive {'DOWN', Ref, process, Pid, Result} ->
-          case Result of
-            ok -> ok;
-            Thrown -> throw(Thrown)
-          end
-      end
+      dialyzer_plt:to_file(OutputPlt, Plt, ModDeps, PltInfo)
   end,
   UnknownWarnings = unknown_warnings(State),
   RetValue =
@@ -740,16 +731,6 @@ return_value(State = #cl_state{code_server = CodeServer,
       AllWarnings =
         UnknownWarnings ++ process_warnings(StoredWarnings),
       {RetValue, set_warning_id(AllWarnings)}
-  end.
-
--spec to_file_fun(_, _, _, _) -> fun(() -> no_return()).
-
-to_file_fun(Filename, MiniPlt, ModDeps, PltInfo) ->
-  fun() ->
-      receive go -> ok end,
-      Plt = dialyzer_plt:restore_full_plt(MiniPlt),
-      dialyzer_plt:to_file(Filename, Plt, ModDeps, PltInfo),
-      exit(ok)
   end.
 
 unknown_warnings(State = #cl_state{legal_warnings = LegalWarnings}) ->

lib/dialyzer/src/dialyzer_codeserver.erl
@@ -375,7 +375,7 @@ store_temp_contracts(Mod, SpecMap, CallbackMap,
 		     #codeserver{temp_contracts = Cn,
 				 temp_callbacks = Cb} = CS)
   when is_atom(Mod) ->
-  %% Make sure Mod is stored even if there are not callbacks or
+  %% Make sure Mod is stored even if there are no callbacks or
   %% contracts.
   CS1 = CS#codeserver{temp_contracts = ets_map_store(Mod, SpecMap, Cn)},
   CS1#codeserver{temp_callbacks = ets_map_store(Mod, CallbackMap, Cb)}.

lib/dialyzer/src/dialyzer_succ_typings.erl
@@ -97,14 +97,13 @@ init_state_and_get_success_typings(Callgraph, Plt, Codeserver,
 				   TimingServer, Solvers, Parent) ->
   {SCCs, Callgraph1} =
     ?timing(TimingServer, "order", dialyzer_callgraph:finalize(Callgraph)),
-  State = #st{callgraph = Callgraph1, plt = dialyzer_plt:get_mini_plt(Plt),
+  State = #st{callgraph = Callgraph1, plt = Plt,
 	      codeserver = Codeserver, parent = Parent,
 	      timing_server = TimingServer, solvers = Solvers},
   get_refined_success_typings(SCCs, State).
 
 get_refined_success_typings(SCCs, #st{callgraph = Callgraph,
 				      timing_server = TimingServer} = State) ->
-  erlang:garbage_collect(),
   case find_succ_typings(SCCs, State) of
     {fixpoint, State1} -> State1;
     {not_fixpoint, NotFixpoint1, State1} ->
@@ -139,17 +138,15 @@ get_warnings(Callgraph, Plt, DocPlt, Codeserver,
     init_state_and_get_success_typings(Callgraph, Plt, Codeserver,
 				       TimingServer, Solvers, Parent),
   Mods = dialyzer_callgraph:modules(InitState#st.callgraph),
-  MiniPlt = InitState#st.plt,
+  Plt = InitState#st.plt,
   CWarns =
-    dialyzer_contracts:get_invalid_contract_warnings(Mods, Codeserver,
-                                                     MiniPlt),
-  MiniDocPlt = dialyzer_plt:get_mini_plt(DocPlt),
+    dialyzer_contracts:get_invalid_contract_warnings(Mods, Codeserver, Plt),
   ModWarns =
     ?timing(TimingServer, "warning",
-	    get_warnings_from_modules(Mods, InitState, MiniDocPlt)),
+	    get_warnings_from_modules(Mods, InitState, DocPlt)),
   {postprocess_warnings(CWarns ++ ModWarns, Codeserver),
-   MiniPlt,
-   dialyzer_plt:restore_full_plt(MiniDocPlt)}.
+   Plt,
+   DocPlt}.
 
 get_warnings_from_modules(Mods, State, DocPlt) ->
   #st{callgraph = Callgraph, codeserver = Codeserver,

lib/dialyzer/src/dialyzer_utils.erl
@@ -40,6 +40,7 @@
 	 src_compiler_opts/0,
 	 refold_pattern/1,
          ets_tab2list/1,
+         ets_move/2,
 	 parallelism/0,
          family/1
 	]).
@@ -998,18 +999,26 @@ label(Tree) ->
 %%  ets:tab2list(T), ets:delete(T)
 %% to save some memory at the expense of somewhat longer execution time.
 ets_tab2list(T) ->
-  tab2list(ets:first(T), T, []).
+  F = fun(Vs, A) -> Vs ++ A end,
+  ets_take(ets:first(T), T, F, []).
 
-tab2list('$end_of_table', T, A) ->
+-spec ets_move(From :: ets:tid(), To :: ets:tid()) -> 'ok'.
+
+ets_move(T1, T2) ->
+  F = fun(Es, A) -> true = ets:insert(T2, Es), A end,
+  [] = ets_take(ets:first(T1), T1, F, []),
+  ok.
+
+ets_take('$end_of_table', T, F, A) ->
   case ets:first(T) of % no safe_fixtable()...
     '$end_of_table' -> A;
-    Key -> tab2list(Key, T, A)
+    Key -> ets_take(Key, T, F, A)
   end;
-tab2list(Key, T, A) ->
+ets_take(Key, T, F, A) ->
   Vs = ets:lookup(T, Key),
   Key1 = ets:next(T, Key),
-  ets:delete(T, Key),
-  tab2list(Key1, T, Vs ++ A).
+  true = ets:delete(T, Key),
+  ets_take(Key1, T, F, F(Vs, A)).
 
 -spec parallelism() -> integer().
 

commit 84bc3ec7f7d467d52b331347ea8a5f266c6657b1

dialyzer: Fix handling of PLTs in GUI
The analysis PLT was erroneously deleted.
The bug was introduced in 3ffed20, or earlier.

Doc PLTs were never deleted, which caused ETS table leakage.

lib/dialyzer/src/dialyzer_gui_wx.erl
@@ -498,10 +498,10 @@ gui_loop(#gui_state{backend_pid = BackendPid, doc_plt = DocPlt,
 	end,
       ExplanationPid = spawn_link(Fun),
       gui_loop(State#gui_state{expl_pid = ExplanationPid});
-    {BackendPid, done, NewPlt, NewDocPlt} ->
+    {BackendPid, done, _NewPlt, NewDocPlt} ->
       message(State, "Analysis done"),
-      dialyzer_plt:delete(NewPlt),
       config_gui_stop(State),
+      dialyzer_plt:delete(State#gui_state.doc_plt),
       gui_loop(State#gui_state{doc_plt = NewDocPlt});
     {'EXIT', BackendPid, {error, Reason}} ->
       free_editor(State, ?DIALYZER_ERROR_TITLE, Reason),