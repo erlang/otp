9c493f513a2aeda36cc5c55aafd8b6fa7c5ee042
Wojtek Mach <wojtek@wojtekmach.pl>
2021-01-07 12:29:56 +0100

    Explicitly set path to the openssl dir

--- .github/scripts/build_macos.sh
+++ .github/scripts/build_macos.sh
@@ -2,9 +2,11 @@
 set -euox pipefail
 
 tmp_dir=$1
+# https://github.com/actions/virtual-environments/blob/main/images/macos/macos-10.15-Readme.md#utilities
+ssl_dir=/usr/local/opt/openssl
 
 ./otp_build autoconf
-./configure --with-ssl --disable-dynamic-ssl-lib
+./configure --with-ssl=$ssl_dir --disable-dynamic-ssl-lib
 make release -j$(getconf _NPROCESSORS_ONLN) RELEASE_ROOT=$PWD/release
 make release_docs RELEASE_ROOT=$PWD/release DOC_TARGETS=chunks
 tar czf ${tmp_dir}/release.tar.gz release/
