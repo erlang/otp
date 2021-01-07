717cb6f6696e5ce1e19b1d46b2ea6596d0055414
Micael Karlberg <bmk@erlang.org>
2021-01-07 17:03:06 +0100

    [kernel|gen-tcp|test] Add some printouts to local fdopen case

--- lib/kernel/test/gen_tcp_api_SUITE.erl
+++ lib/kernel/test/gen_tcp_api_SUITE.erl
@@ -777,25 +777,51 @@ do_local_unbound(Config) ->
 
 
 t_local_fdopen(Config) ->
+    ?TC_TRY(t_local_fdopen, fun() -> do_local_fdopen(Config) end).
+
+do_local_fdopen(Config) ->
+    ?P("create local (server) filename"),
     SFile = local_filename(server),
     SAddr = {local,bin_filename(SFile)},
     _ = file:delete(SFile),
     %%
     InetBackendOpts = ?INET_BACKEND_OPTS(Config),
+    ?P("create listen socket with ifaddr ~p", [SAddr]),
     L = ok(gen_tcp:listen(0, InetBackendOpts ++ [{ifaddr,SAddr},{active,false}])),
+    ?P("listen socket created: ~p"
+       "~n   => try connect", [L]),
     C0 = ok(gen_tcp:connect(SAddr, 0, InetBackendOpts ++ [{active,false}])),
+    ?P("connected: ~p"
+       "~n   => get fd", [C0]),
     Fd = ok(prim_inet:getfd(C0)),
+    ?P("FD: ~p"
+       "~n   => ignore fd", [Fd]),
     ok = prim_inet:ignorefd(C0, true),
+    ?P("ignored fd:"
+       "~n   => try fdopen (local)"),
     C = ok(gen_tcp:fdopen(Fd, [local])),
+    ?P("fd open: ~p"
+       "~n   => try accept", [C]),
     S = ok(gen_tcp:accept(L)),
+    ?P("accepted: ~p"
+       "~n   => sockname", [S]),
     SAddr = ok(inet:sockname(L)),
+    ?P("sockname: ~p"
+       "~n   => peername (expect enotconn)", [SAddr]),
     {error,enotconn} = inet:peername(L),
+    ?P("try local handshake"),
     local_handshake(S, SAddr, C, {local,<<>>}),
+    ?P("close listen socket"),
     ok = gen_tcp:close(L),
+    ?P("close accepted socket"),
     ok = gen_tcp:close(S),
+    ?P("close connected socket (final)"),
     ok = gen_tcp:close(C),
+    ?P("close connected socket (pre)"),
     ok = gen_tcp:close(C0),
+    ?P("delete (local) file"),
     ok = file:delete(SFile),
+    ?P("done"),
     ok.
 
 t_local_fdopen_listen(Config) ->
