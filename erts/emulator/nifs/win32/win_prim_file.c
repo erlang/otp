erlang/otp

commit 7e88dc8e447d46e030971b2f9310c9ee1aa0223a

Add erl option for reading config from file descriptor
Make it possible for users to specify the option "-configfd FD" when
executing the erl command. When this option is given the system will
try to read and parse configuration parameters from the file
descriptor. At the moment the only configuration format that is
supported is the format that is supported when using the -config
option.

Motivation
----------

Allow users to configure an Erlang system without writing files to
disk (which can be an issue in some container environments with
read-only file systems).

erts/emulator/nifs/win32/win_prim_file.c
@@ -84,9 +84,20 @@
 typedef struct {
     efile_data_t common;
     HANDLE handle;
+    /* The following field is only used when the handle has been
+       obtained from an already exisiting file descriptor (i.e.,
+       prim_file:file_desc_to_ref/2). common.modes is set to
+       EFILE_MODE_FROM_ALREADY_OPEN_FD when that is the case. It is
+       needed because we can't close using handle in that case. */
+    int fd;
 } efile_win_t;
 
 static int windows_to_posix_errno(DWORD last_error);
+static void tmp_nop_invalid_parameter_handler(const wchar_t* expression,
+                                              const wchar_t* function,
+                                              const wchar_t* file,
+                                              unsigned int line,
+                                              uintptr_t pReserved);
 
 static int has_invalid_null_termination(const ErlNifBinary *path) {
     const WCHAR *null_pos, *end_pos;
@@ -491,10 +502,59 @@ posix_errno_t efile_open(const efile_path_t *path, enum efile_modes_t modes,
     }
 }
 
+static void tmp_nop_invalid_parameter_handler(const wchar_t* expression,
+                                              const wchar_t* function,
+                                              const wchar_t* file,
+                                              unsigned int line,
+                                              uintptr_t pReserved) {
+    (void)expression;
+    (void)function;
+    (void)file;
+    (void)line;
+    (void)pReserved;
+}
+
+posix_errno_t efile_from_fd(int fd,
+                            ErlNifResourceType *nif_type,
+                            efile_data_t **d) {
+    HANDLE handle;
+
+    _invalid_parameter_handler old_handler;
+
+    /* Temporarily disable the parameter handler so we don't crash */
+    old_handler =
+        _set_thread_local_invalid_parameter_handler(tmp_nop_invalid_parameter_handler);
+
+    handle = (HANDLE)_get_osfhandle(fd);
+
+    /* Enable old parameter handler again */
+    _set_thread_local_invalid_parameter_handler(old_handler);
+
+    if(handle != INVALID_HANDLE_VALUE && handle != ((HANDLE)-2)) {
+        efile_win_t *w;
+
+        w = (efile_win_t*)enif_alloc_resource(nif_type, sizeof(efile_win_t));
+        w->handle = handle;
+        w->fd = fd;
+
+        EFILE_INIT_RESOURCE(&w->common, EFILE_MODE_FROM_ALREADY_OPEN_FD);
+        (*d) = &w->common;
+
+        return 0;
+    } else {
+        return EBADF;
+    }
+}
+
 int efile_close(efile_data_t *d, posix_errno_t *error) {
     efile_win_t *w = (efile_win_t*)d;
     HANDLE handle;
-
+    int from_already_open_fd =
+        w->common.modes & EFILE_MODE_FROM_ALREADY_OPEN_FD;
+    int fd;
+    if (from_already_open_fd) {
+        fd = w->fd;
+    }
     ASSERT(enif_thread_type() == ERL_NIF_THR_DIRTY_IO_SCHEDULER);
     ASSERT(erts_atomic32_read_nob(&d->state) == EFILE_STATE_CLOSED);
     ASSERT(w->handle != INVALID_HANDLE_VALUE);
@@ -504,7 +564,12 @@ int efile_close(efile_data_t *d, posix_errno_t *error) {
 
     enif_release_resource(d);
 
-    if(!CloseHandle(handle)) {
+    if(from_already_open_fd) {
+        if(_close(fd) == -1) {
+            *error = EBADF;
+            return 0;
+        }
+    } else if(!CloseHandle(handle)) {
         *error = windows_to_posix_errno(GetLastError());
         return 0;
     }
@@ -558,7 +623,8 @@ static void shift_iov(SysIOVec **iov, int *iovlen, DWORD shift) {
 typedef BOOL (WINAPI *io_op_t)(HANDLE, LPVOID, DWORD, LPDWORD, LPOVERLAPPED);
 
 static Sint64 internal_sync_io(efile_win_t *w, io_op_t operation,
-        SysIOVec *iov, int iovlen, OVERLAPPED *overlapped) {
+                               SysIOVec *iov, int iovlen, OVERLAPPED *overlapped,
+                               int is_read) {
 
     Sint64 bytes_processed = 0;
 
@@ -574,10 +640,17 @@ static Sint64 internal_sync_io(efile_win_t *w, io_op_t operation,
             &block_bytes_processed, overlapped);
         last_error = GetLastError();
 
-        if(!succeeded && (last_error != ERROR_HANDLE_EOF)) {
-            w->common.posix_errno = windows_to_posix_errno(last_error);
-            return -1;
-        } else if(block_bytes_processed == 0) {
+        if(is_read && !succeeded) {
+            if(last_error == ERROR_BROKEN_PIPE) {
+                /* Pipes gives ERROR_BROKEN_PIPE instead of EOF when the
+                   write end has been closed */
+                return bytes_processed;
+            } else if(last_error != ERROR_HANDLE_EOF) {
+                w->common.posix_errno = windows_to_posix_errno(last_error);
+                return -1;
+            }
+        }
+        if(block_bytes_processed == 0) {
             /* EOF */
             return bytes_processed;
         }
@@ -595,7 +668,7 @@ static Sint64 internal_sync_io(efile_win_t *w, io_op_t operation,
 Sint64 efile_readv(efile_data_t *d, SysIOVec *iov, int iovlen) {
     efile_win_t *w = (efile_win_t*)d;
 
-    return internal_sync_io(w, ReadFile, iov, iovlen, NULL);
+    return internal_sync_io(w, ReadFile, iov, iovlen, NULL, 1);
 }
 
 Sint64 efile_writev(efile_data_t *d, SysIOVec *iov, int iovlen) {
@@ -614,7 +687,7 @@ Sint64 efile_writev(efile_data_t *d, SysIOVec *iov, int iovlen) {
         overlapped = NULL;
     }
 
-    return internal_sync_io(w, WriteFile, iov, iovlen, overlapped);
+    return internal_sync_io(w, WriteFile, iov, iovlen, overlapped, 0);
 }
 
 Sint64 efile_preadv(efile_data_t *d, Sint64 offset, SysIOVec *iov, int iovlen) {
@@ -626,7 +699,7 @@ Sint64 efile_preadv(efile_data_t *d, Sint64 offset, SysIOVec *iov, int iovlen) {
     overlapped.OffsetHigh = (offset >> 32) & 0xFFFFFFFF;
     overlapped.Offset = offset & 0xFFFFFFFF;
 
-    return internal_sync_io(w, ReadFile, iov, iovlen, &overlapped);
+    return internal_sync_io(w, ReadFile, iov, iovlen, &overlapped, 1);
 }
 
 Sint64 efile_pwritev(efile_data_t *d, Sint64 offset, SysIOVec *iov, int iovlen) {
@@ -638,7 +711,7 @@ Sint64 efile_pwritev(efile_data_t *d, Sint64 offset, SysIOVec *iov, int iovlen) 
     overlapped.OffsetHigh = (offset >> 32) & 0xFFFFFFFF;
     overlapped.Offset = offset & 0xFFFFFFFF;
 
-    return internal_sync_io(w, WriteFile, iov, iovlen, &overlapped);
+    return internal_sync_io(w, WriteFile, iov, iovlen, &overlapped, 0);
 }
 
 int efile_seek(efile_data_t *d, enum efile_seek_t seek, Sint64 offset, Sint64 *new_position) {

erts/doc/src/erl_cmd.xml
@@ -175,7 +175,7 @@
           and lets the shell start in parallel with the rest of
           the system.</p>
       </item>
-      <tag><c><![CDATA[-boot File]]></c></tag>
+      <tag><marker id="boot" /><c><![CDATA[-boot File]]></c></tag>
       <item>
         <p>Specifies the name of the boot file, <c><![CDATA[File.boot]]></c>,
           which is used to start the system; see
@@ -207,14 +207,79 @@
         <p>Not recommended; use <seecom marker="erlc"><c>erlc</c></seecom>
           instead.</p>
       </item>
-      <tag><c><![CDATA[-config Config [Config]]]></c></tag>
+      <tag><marker id="config" /><c><![CDATA[-config Config [Config ...]]]></c></tag>
       <item>
         <p>Specifies the name of one or more configuration files,
           <c><![CDATA[Config.config]]></c>, which is used to configure
           applications; see
           <seefile marker="kernel:app"><c>app(4)</c></seefile> and
           <seeerl marker="kernel:application">
-          <c>application(3)</c></seeerl>.</p>
+          <c>application(3)</c></seeerl>. See the documentation for
+          the <seefile marker="kernel:config">configuration file
+          format</seefile> for a description of the configuration
+          format and the order in which configuration parameters are
+          read.</p>
+      </item>
+      <tag><marker id="configfd" /><c><![CDATA[-configfd FD [FD ...]]]></c></tag>
+      <item>
+        <p>Specifies the name of one or more file descriptors (called
+        configuration file descriptors from here on) with
+        configuration data for applications; see <seefile
+        marker="kernel:app"><c>app(4)</c></seefile> and
+        <seeerl marker="kernel:application">
+          <c>application(3)</c></seeerl>. See the documentation for
+          the <seefile marker="kernel:config">configuration file
+          format</seefile> for a description of the configuration
+          format and the order in which configuration parameters are
+          read.
+        </p>
+        <p>
+          A configuration file descriptor will be read until its end
+          and will then be closed.
+        </p>
+        <p>
+          The content of a configuration file
+          descriptor is stored so that it can be reused when <seemfa
+          marker="erts:init#restart/0"><c>init:restart/0</c></seemfa>
+          or <seemfa
+          marker="erts:init#restart/1"><c>init:restart/1</c></seemfa>
+          is called.
+        </p>
+        <p>
+          The parameter <c><![CDATA[-configfd 0]]></c> implies <c><![CDATA[-noinput]]></c>.
+        </p>
+        <note>
+          <p>
+            It is not recommended to use file descriptors 1 (standard
+            output), and 2 (standard error) together with
+            <c><![CDATA[-configfd]]></c> as these file descriptors are
+            typically used to print information to the console the program
+            is running in.
+          </p>
+        </note>
+        <p>Examples (Unix shell):</p>
+<pre>
+$ <input>erl \
+-noshell \
+-configfd 3 \
+-eval \
+'io:format("~p~n",[application:get_env(kernel, logger_level)]),erlang:halt()' 3&lt; \
+&lt;(echo '[{kernel, [{logger_level, warning}]}].')</input>
+{ok,warning}
+</pre>
+<pre>
+$ <input>echo '[{kernel, [{logger_level, warning}]}].' > test1.config</input>
+$ <input>echo '[{kernel, [{logger_level, error}]}].' > test2.config</input>
+$ <input>erl \
+-noshell \
+-configfd 3 \
+-configfd 4 \
+-eval \
+'io:format("~p~n",[application:get_env(kernel, logger_level)]),erlang:halt()' \
+3&lt; test1.config 4&lt; test2.config</input>
+{ok,error}
+</pre>
+
       </item>
       <tag><marker id="connect_all"/><c><![CDATA[-connect_all false]]></c></tag>
       <item>

erts/emulator/nifs/common/prim_file_nif.c
@@ -101,10 +101,16 @@ static ERL_NIF_TERM read_file_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM a
 static ERL_NIF_TERM open_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
 static ERL_NIF_TERM close_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
 
+static ERL_NIF_TERM file_desc_to_ref_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
+
 /* Internal ops */
 static ERL_NIF_TERM delayed_close_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
 static ERL_NIF_TERM get_handle_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
 static ERL_NIF_TERM altname_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]);
+
+/* Helper functions */
+
+static ERL_NIF_TERM create_ref_or_error_tuple(ErlNifEnv *env, efile_data_t *d);
 
 /* All file handle operations are passed through a wrapper that handles state
  * transitions, marking it as busy during the course of the operation, and
@@ -205,6 +211,7 @@ static ErlNifFunc nif_funcs[] = {
     {"get_handle_nif", 1, get_handle_nif},
     {"delayed_close_nif", 1, delayed_close_nif, ERL_NIF_DIRTY_JOB_IO_BOUND},
     {"altname_nif", 1, altname_nif, ERL_NIF_DIRTY_JOB_IO_BOUND},
+    {"file_desc_to_ref_nif", 1, file_desc_to_ref_nif, ERL_NIF_DIRTY_JOB_IO_BOUND},
 };
 
 ERL_NIF_INIT(prim_file, nif_funcs, load, NULL, upgrade, unload)
@@ -472,28 +479,10 @@ static enum efile_modes_t efile_translate_modelist(ErlNifEnv *env, ERL_NIF_TERM 
     return modes;
 }
 
-static ERL_NIF_TERM open_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
-    posix_errno_t posix_errno;
-    efile_data_t *d;
-
+static ERL_NIF_TERM create_ref_or_error_tuple(ErlNifEnv *env, efile_data_t *d) {
     ErlNifPid controlling_process;
-    enum efile_modes_t modes;
     ERL_NIF_TERM result;
-    efile_path_t path;
-
-    ASSERT(argc == 2);
-    if(!enif_is_list(env, argv[1])) {
-        return enif_make_badarg(env);
-    }
-
-    modes = efile_translate_modelist(env, argv[1]);
-
-    if((posix_errno = efile_marshal_path(env, argv[0], &path))) {
-        return posix_error_to_tuple(env, posix_errno);
-    } else if((posix_errno = efile_open(&path, modes, efile_resource_type, &d))) {
-        return posix_error_to_tuple(env, posix_errno);
-    }
-
+    
     enif_self(env, &controlling_process);
 
     if(enif_monitor_process(env, d, &controlling_process, &d->monitor)) {
@@ -519,6 +508,52 @@ static ERL_NIF_TERM open_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]
     result = enif_make_resource(env, d);
 
     return enif_make_tuple2(env, am_ok, result);
+}
+
+static ERL_NIF_TERM open_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
+    posix_errno_t posix_errno;
+    efile_data_t *d;
+
+    enum efile_modes_t modes;
+    efile_path_t path;
+
+    ASSERT(argc == 2);
+    if(!enif_is_list(env, argv[1])) {
+        return enif_make_badarg(env);
+    }
+
+    modes = efile_translate_modelist(env, argv[1]);
+
+    if((posix_errno = efile_marshal_path(env, argv[0], &path))) {
+        return posix_error_to_tuple(env, posix_errno);
+    } else if((posix_errno = efile_open(&path, modes, efile_resource_type, &d))) {
+        return posix_error_to_tuple(env, posix_errno);
+    }
+
+    return create_ref_or_error_tuple(env, d);
+}
+
+static ERL_NIF_TERM file_desc_to_ref_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
+    posix_errno_t posix_errno;
+    efile_data_t *d;
+
+    int fd;
+
+    ASSERT(argc == 1);
+
+    if(!enif_is_number(env, argv[0])) {
+        return enif_make_badarg(env);
+    }
+
+    if(!enif_get_int(env, argv[0], &fd)) {
+        return enif_make_badarg(env);
+    }
+
+    if((posix_errno = efile_from_fd(fd, efile_resource_type, &d))) {
+        return posix_error_to_tuple(env, posix_errno);
+    }
+
+    return create_ref_or_error_tuple(env, d);
 }
 
 static ERL_NIF_TERM close_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {

erts/emulator/nifs/common/prim_file_nif.h
@@ -32,6 +32,8 @@ enum efile_modes_t {
 
     EFILE_MODE_DIRECTORY = (1 << 7),
 
+    EFILE_MODE_FROM_ALREADY_OPEN_FD = (1 << 8),
+
     EFILE_MODE_READ_WRITE = EFILE_MODE_READ | EFILE_MODE_WRITE
 };
 
@@ -159,6 +161,10 @@ int efile_truncate(efile_data_t *d);
 
 posix_errno_t efile_open(const efile_path_t *path, enum efile_modes_t modes,
         ErlNifResourceType *nif_type, efile_data_t **d);
+
+posix_errno_t efile_from_fd(int fd,
+                            ErlNifResourceType *nif_type,
+                            efile_data_t **d);
 
 /** @brief Closes a file. The file must have entered the CLOSED state prior to
  * calling this to prevent double close.

erts/emulator/nifs/unix/unix_prim_file.c
@@ -123,13 +123,8 @@ static int open_file_is_dir(const efile_path_t *path, int fd) {
     return error == 0 && S_ISDIR(file_info.st_mode);
 }
 
-posix_errno_t efile_open(const efile_path_t *path, enum efile_modes_t modes,
-        ErlNifResourceType *nif_type, efile_data_t **d) {
-
-    int mode, flags, fd;
-
-    flags = 0;
-
+static int get_flags(enum efile_modes_t modes) {
+    int flags = 0;
     if(modes & EFILE_MODE_READ && !(modes & EFILE_MODE_WRITE)) {
         flags |= O_RDONLY;
     } else if(modes & EFILE_MODE_WRITE && !(modes & EFILE_MODE_READ)) {
@@ -160,6 +155,15 @@ posix_errno_t efile_open(const efile_path_t *path, enum efile_modes_t modes,
         flags |= O_SYNC;
 #endif
     }
+    return flags;
+}
+
+posix_errno_t efile_open(const efile_path_t *path, enum efile_modes_t modes,
+        ErlNifResourceType *nif_type, efile_data_t **d) {
+
+    int mode, flags, fd;
+
+    flags = get_flags(modes);
 
     if(modes & EFILE_MODE_DIRECTORY) {
         mode = DIR_MODE;
@@ -205,6 +209,24 @@ posix_errno_t efile_open(const efile_path_t *path, enum efile_modes_t modes,
         return 0;
     }
 
+    (*d) = NULL;
+    return errno;
+}
+
+posix_errno_t efile_from_fd(int fd,
+                            ErlNifResourceType *nif_type,
+                            efile_data_t **d) {
+    if (fcntl(fd, F_GETFL) != -1 || errno != EBADF) {
+        efile_unix_t *u;
+
+        u = (efile_unix_t*)enif_alloc_resource(nif_type, sizeof(efile_unix_t));
+        u->fd = fd;
+
+        EFILE_INIT_RESOURCE(&u->common, EFILE_MODE_FROM_ALREADY_OPEN_FD);
+        (*d) = &u->common;
+
+        return 0;
+    }
     (*d) = NULL;
     return errno;
 }

erts/etc/common/erlexec.c
@@ -18,11 +18,6 @@
  * %CopyrightEnd%
  */
 
-/*
- * This is a C version of the erl.exec Bourne shell script, including
- * additions required for Windows NT.
- */
-
 #include "etc_common.h"
 
 #include "erl_driver.h"
@@ -651,6 +646,16 @@ int main(int argc, char **argv)
                         } while ((i+1) < argc && argv[i+1][0] != '-' && argv[i+1][0] != '+');
 		    }
 #endif
+                    else if (strcmp(argv[i], "-configfd") == 0) {
+                        if (i+1 >= argc)
+ 			    usage("-configfd");
+                        if ( strcmp(argv[i+1], "0") != 0 ) {
+			    add_arg(argv[i]);
+                        } else {
+                            add_args("-noshell", "-noinput", NULL);
+                            add_arg(argv[i]);
+                        }
+		    }
 		    else {
 			add_arg(argv[i]);
 		    }

commit 73c44412aa93e57f8af63cc2ef9a831a979c6686

erts: Fix file write error check on windows
This bug was introduced in 7e88dc8e447d46e030971b2f9310c9ee1aa0223a.

erts/emulator/nifs/win32/win_prim_file.c
@@ -640,12 +640,12 @@ static Sint64 internal_sync_io(efile_win_t *w, io_op_t operation,
             &block_bytes_processed, overlapped);
         last_error = GetLastError();
 
-        if(is_read && !succeeded) {
-            if(last_error == ERROR_BROKEN_PIPE) {
+        if(!succeeded) {
+            if(is_read && last_error == ERROR_BROKEN_PIPE) {
                 /* Pipes gives ERROR_BROKEN_PIPE instead of EOF when the
                    write end has been closed */
                 return bytes_processed;
-            } else if(last_error != ERROR_HANDLE_EOF) {
+            } else if(!is_read || last_error != ERROR_HANDLE_EOF) {
                 w->common.posix_errno = windows_to_posix_errno(last_error);
                 return -1;
             }