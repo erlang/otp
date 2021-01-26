erlang/otp

commit ff9e0228b4b56c979da905b95adc6b3fa0490ac0

Support of 64-bit ports

erts/emulator/beam/utils.c
@@ -919,13 +919,20 @@ tail_recur:
 	    break;
 	}
     case PID_DEF:
+	/* only 15 bits... */
 	UINT32_HASH_RET(internal_pid_number(term),FUNNY_NUMBER5,FUNNY_NUMBER6);
     case EXTERNAL_PID_DEF:
+	/* only 15 bits... */
 	UINT32_HASH_RET(external_pid_number(term),FUNNY_NUMBER5,FUNNY_NUMBER6);
     case PORT_DEF:
-	UINT32_HASH_RET(internal_port_number(term),FUNNY_NUMBER9,FUNNY_NUMBER10);
-    case EXTERNAL_PORT_DEF:
-	UINT32_HASH_RET(external_port_number(term),FUNNY_NUMBER9,FUNNY_NUMBER10);
+    case EXTERNAL_PORT_DEF: {
+	Uint64 number = port_number(term);
+	Uint32 low = (Uint32) (number & 0xffffffff);
+	Uint32 high = (Uint32) ((number >> 32) & 0xffffffff);
+	if (high)
+	    UINT32_HASH_STEP(high, FUNNY_NUMBER11);
+	UINT32_HASH_RET(low,FUNNY_NUMBER9,FUNNY_NUMBER10);
+    }
     case REF_DEF:
 	UINT32_HASH_RET(internal_ref_numbers(term)[0],FUNNY_NUMBER9,FUNNY_NUMBER10);
     case EXTERNAL_REF_DEF:
@@ -1858,10 +1865,13 @@ make_hash2_helper(Eterm term_param, const int can_trap, Eterm* state_mref_write_
 		/* Only 15 bits are hashed. */
 		UINT32_HASH(external_pid_number(term), HCONST_5);
 		goto hash2_common;
-	    case EXTERNAL_PORT_SUBTAG:
-		/* Only 15 bits are hashed. */
-		UINT32_HASH(external_port_number(term), HCONST_6);
+	    case EXTERNAL_PORT_SUBTAG: {
+		Uint64 number = external_port_number(term);
+		Uint32 low = (Uint32) (number & 0xffffffff);
+		Uint32 high = (Uint32) ((number >> 32) & 0xffffffff);
+		UINT32_HASH_2(low, high, HCONST_6);
 		goto hash2_common;
+	    }
 	    case FLOAT_SUBTAG:
 	    {
 		FloatDef ff;
@@ -1890,10 +1900,13 @@ make_hash2_helper(Eterm term_param, const int can_trap, Eterm* state_mref_write_
 		/* Only 15 bits are hashed. */
 		UINT32_HASH(internal_pid_number(term), HCONST_5);
 		goto hash2_common;
-	    case _TAG_IMMED1_PORT:
-		/* Only 15 bits are hashed. */
-		UINT32_HASH(internal_port_number(term), HCONST_6);
+	    case _TAG_IMMED1_PORT: {
+		Uint64 number = internal_port_number(term);
+		Uint32 low = (Uint32) (number & 0xffffffff);
+		Uint32 high = (Uint32) ((number >> 32) & 0xffffffff);
+		UINT32_HASH_2(low, high, HCONST_6);
 		goto hash2_common;
+	    }
 	    case _TAG_IMMED1_IMMED2:
 		switch (term & _TAG_IMMED2_MASK) {
 		case _TAG_IMMED2_ATOM:
@@ -3109,8 +3122,6 @@ Sint erts_cmp_compound(Eterm a, Eterm b, int exact, int eq_only)
     int b_tag;
     ErlNode *anode;
     ErlNode *bnode;
-    Uint adata;
-    Uint bdata;
     Uint alen;
     Uint blen;
     Uint32 *anum;
@@ -3178,26 +3189,23 @@ tailrecur_ne:
 	switch ((a & _TAG_IMMED1_MASK) >> _TAG_PRIMARY_SIZE) {
 	case (_TAG_IMMED1_PORT >> _TAG_PRIMARY_SIZE):
 	    if (is_internal_port(b)) {
-		bnode = erts_this_node;
-		bdata = internal_port_data(b);
+		Uint adata = internal_port_data(a);
+		Uint bdata = internal_port_data(b);
+		ON_CMP_GOTO((Sint)(adata - bdata));
 	    } else if (is_external_port(b)) {
+		anode = erts_this_node;
 		bnode = external_port_node(b);
-		bdata = external_port_data(b);
+		CMP_NODES(anode, bnode);
+		ERTS_INTERNAL_ERROR("different nodes compared equal");
 	    } else {
 		a_tag = PORT_DEF;
 		goto mixed_types;
 	    }
-	    anode = erts_this_node;
-	    adata = internal_port_data(a);
-
-	port_common:
-	    CMP_NODES(anode, bnode);
-	    ON_CMP_GOTO((Sint)(adata - bdata));
 
         case (_TAG_IMMED1_PID >> _TAG_PRIMARY_SIZE):
             if (is_internal_pid(b)) {
-                adata = internal_pid_data(a);
-                bdata = internal_pid_data(b);
+                Uint adata = internal_pid_data(a);
+                Uint bdata = internal_pid_data(b);
                 ON_CMP_GOTO((Sint)(adata - bdata));
             }
             else if (is_not_external_pid(b)) {
@@ -3473,18 +3481,24 @@ tailrecur_ne:
 
 	    case (_TAG_HEADER_EXTERNAL_PORT >> _TAG_PRIMARY_SIZE):
 		if (is_internal_port(b)) {
+		    anode = external_port_node(a);
 		    bnode = erts_this_node;
-		    bdata = internal_port_data(b);
+		    CMP_NODES(anode, bnode);
+		    ERTS_INTERNAL_ERROR("different nodes compared equal");
 		} else if (is_external_port(b)) {
+		    Uint64 anum, bnum;
+		    anode = external_port_node(a);
 		    bnode = external_port_node(b);
-		    bdata = external_port_data(b);
+		    CMP_NODES(anode, bnode);
+		    anum = external_port_number(a);
+		    bnum = external_port_number(b);
+		    if (anum == bnum)
+			goto pop_next;
+		    RETURN_NEQ(anum < bnum ? -1 : 1);
 		} else {
 		    a_tag = EXTERNAL_PORT_DEF;
 		    goto mixed_types;
 		}
-		anode = external_port_node(a);
-		adata = external_port_data(a);
-		goto port_common;
 	    case (_TAG_HEADER_REF >> _TAG_PRIMARY_SIZE):
 		/*
 		 * Note! When comparing refs we need to compare ref numbers

erts/doc/src/erl_dist_protocol.xml
@@ -1110,12 +1110,15 @@ DiB == gen_digest(ChA, ICA)?
 	  request from the connecting node to receive its node name from the
 	  accepting node as part of the handshake.</p>
 	</item>
-	<tag><marker id="DFLAG_V4_PIDS_REFS"/><c>-define(DFLAG_V4_PIDS_REFS, (1 bsl 34)).</c></tag>
+	<tag><marker id="DFLAG_V4_NC"/><c>-define(DFLAG_V4_NC, (1 bsl 34)).</c></tag>
         <item>
-	  <p>The node accepts a larger amount of data in pids and references.
-          In the pid case full 32-bit <c>ID</c> and <c>Serial</c> fields in
-	  <seeguide marker="erl_ext_dist#NEW_PID_EXT"><c>NEW_PID_EXT</c></seeguide>.
-	  In the reference case up to 5 32-bit ID words are now accepted in
+	  <p>The node accepts a larger amount of data in pids, ports and
+	  references (node container types). In the pid case full 32-bit
+	  <c>ID</c> and <c>Serial</c> fields in
+	  <seeguide marker="erl_ext_dist#NEW_PID_EXT"><c>NEW_PID_EXT</c></seeguide>,
+	  in the port case a 64-bit integer in
+	  <seeguide marker="erl_ext_dist#V4_PORT_EXT"><c>V4_PORT_EXT</c></seeguide>,
+	  and in the reference case up to 5 32-bit ID words are now accepted in
 	  <seeguide marker="erl_ext_dist#NEWER_REFERENCE_EXT"><c>NEWER_REFERENCE_EXT</c></seeguide>.
 	  Introduced in OTP 24.</p>
         </item>

erts/doc/src/erl_ext_dist.xml
@@ -631,16 +631,9 @@
 	  </row>
 	<tcaption>NEW_PORT_EXT</tcaption></table>
 	<p>
-	  Encodes a port identifier (obtained from
-          <seemfa marker="erlang#open_port/2"><c>erlang:open_port/2</c></seemfa>).
-	  <c>Node</c> is an encoded atom, that is,
-	  <seeguide marker="#ATOM_UTF8_EXT"><c>ATOM_UTF8_EXT</c></seeguide>,
-	  <seeguide marker="#SMALL_ATOM_UTF8_EXT"><c>SMALL_ATOM_UTF8_EXT</c></seeguide>
-	  or <seeguide marker="#ATOM_CACHE_REF"><c>ATOM_CACHE_REF</c></seeguide>.
-	  <c>ID</c> is a 32-bit big endian unsigned integer. Only 28 bits are
-	  significant; the rest are to be 0. The <c>Creation</c> works just like in
-	  <seeguide marker="#NEW_PID_EXT"><c>NEW_PID_EXT</c></seeguide>.
-	  Port operations are not allowed across node boundaries.
+	  Same as <seeguide marker="#V4_PORT_EXT"><c>V4_PORT_EXT</c></seeguide>
+	  except the <c>ID</c> field is only four bytes. Only 28 bits are
+	  significant; the rest are to be 0. 
 	</p>
 	<p><c>NEW_PORT_EXT</c> was introduced in OTP 19, but only to be decoded
 	  and echoed back. Not encoded for local ports.
@@ -650,6 +643,39 @@
 	  became mandatory. All ports are now
 	  encoded using <c>NEW_PORT_EXT</c>, even external ports received as <seeguide
 	  marker="#PORT_EXT"><c>PORT_EXT</c></seeguide> from older nodes.
+	</p>
+    </section>
+
+    <section>
+      <marker id="V4_PORT_EXT"/>
+      <title>V4_PORT_EXT</title>
+	<table align="left">
+	  <row>
+	    <cell align="center">1</cell>
+	    <cell align="center">N</cell>
+	    <cell align="center">8</cell>
+	    <cell align="center">4</cell>
+	  </row>
+	  <row>
+	    <cell align="center"><c>89</c></cell>
+	    <cell align="center"><c>Node</c></cell>
+	    <cell align="center"><c>ID</c></cell>
+	    <cell align="center"><c>Creation</c></cell>
+	  </row>
+	<tcaption>NEW_PORT_EXT</tcaption></table>
+	<p>
+	  Encodes a port identifier (obtained from
+          <seemfa marker="erlang#open_port/2"><c>erlang:open_port/2</c></seemfa>).
+	  <c>Node</c> is an encoded atom, that is,
+	  <seeguide marker="#ATOM_UTF8_EXT"><c>ATOM_UTF8_EXT</c></seeguide>,
+	  <seeguide marker="#SMALL_ATOM_UTF8_EXT"><c>SMALL_ATOM_UTF8_EXT</c></seeguide>
+	  or <seeguide marker="#ATOM_CACHE_REF"><c>ATOM_CACHE_REF</c></seeguide>.
+	  <c>ID</c> is a 64-bit big endian unsigned integer. The <c>Creation</c>
+	  works just like in <seeguide marker="#NEW_PID_EXT"><c>NEW_PID_EXT</c></seeguide>.
+	  Port operations are not allowed across node boundaries.
+	</p>
+	<p><c>V4_PORT_EXT</c> was introduced in OTP 24, but only to be decoded
+	  and echoed back. Not encoded for local ports.
 	</p>
     </section>
 
@@ -711,12 +737,12 @@
 	  </item>
 	  <tag><c>ID</c></tag>
 	  <item><p>A 32-bit big endian unsigned integer. If distribution flag
-	  <seeguide marker="erl_dist_protocol#DFLAG_V4_PIDS_REFS"><c>DFLAG_V4_PIDS_REFS</c></seeguide>
+	  <seeguide marker="erl_dist_protocol#DFLAG_V4_NC"><c>DFLAG_V4_NC</c></seeguide>
 	  is not set, only 15 bits may be used and the rest must be 0.</p>
 	  </item>
 	  <tag><c>Serial</c></tag>
 	  <item><p>A 32-bit big endian unsigned integer. If distribution flag
-	  <seeguide marker="erl_dist_protocol#DFLAG_V4_PIDS_REFS"><c>DFLAG_V4_PIDS_REFS</c></seeguide>
+	  <seeguide marker="erl_dist_protocol#DFLAG_V4_NC"><c>DFLAG_V4_NC</c></seeguide>
 	  is not set, only 13 bits may be used and the rest must be 0.</p>
 	  </item>
 	  <tag><c>Creation</c></tag>
@@ -739,7 +765,7 @@
 	  <seeguide marker="#PID_EXT"><c>PID_EXT</c></seeguide> from older nodes.
 	</p>
 	<p>In OTP 24 distribution flag
-	  <seeguide marker="erl_dist_protocol#DFLAG_V4_PIDS_REFS"><c>DFLAG_V4_PIDS_REFS</c></seeguide>
+	  <seeguide marker="erl_dist_protocol#DFLAG_V4_NC"><c>DFLAG_V4_NC</c></seeguide>
 	  was introduced, accepting full 64-bit pids to be decoded and echoed back.
 	</p>
     </section>
@@ -1053,7 +1079,7 @@
 	  </item>
 	  <tag><c>Len</c></tag>
 	  <item><p>A 16-bit big endian unsigned integer not larger than 5 when the
-	  <seeguide marker="erl_dist_protocol#DFLAG_V4_PIDS_REFS"><c>DFLAG_V4_PIDS_REFS</c></seeguide>
+	  <seeguide marker="erl_dist_protocol#DFLAG_V4_NC"><c>DFLAG_V4_NC</c></seeguide>
 	  has been set; otherwise not larger than 3.</p>
 	  </item>
 	  <tag><c>ID</c></tag>

erts/emulator/beam/bif.c
@@ -4393,15 +4393,17 @@ BIF_RETTYPE list_to_port_1(BIF_ALIST_1)
 {
     /*
      * A valid port identifier is on the format
-     * "#Port<N.P>" where N is node and P is
-     * the port id. Both N and P are of type Uint32.
+     * "#Port<Node.Num>" where Node is node and Num is
+     * the port id.
      */
-    Uint32 n, p;
-    char* cp;
+    Uint32 node;
+    Uint64 num;
+    char c, *cp;
     int i;
     DistEntry *dep = NULL;
     char buf[6 /* #Port< */
-             + (2)*(10 + 1) /* N.P> */
+	     + 11 /* Node. */
+	     + 21 /* Num> */
              + 1 /* \0 */];
 
     /* walk down the list and create a C string */
@@ -4416,19 +4418,43 @@ BIF_RETTYPE list_to_port_1(BIF_ALIST_1)
 
     cp += 6; /* sys_strlen("#Port<") */
 
-    if (sscanf(cp, "%u.%u>", (unsigned int*)&n, (unsigned int*)&p) < 2)
-        goto bad;
+    node = 0;
+    c = *cp;
+    if (!('0' <= c && c <= '9'))
+	goto bad;
+    do {
+	node *= 10;
+	node += (Uint32) (c - '0');
+	c = *(++cp);
+    } while ('0' <= c && c <= '9');
 
-    if (p > ERTS_MAX_PORT_NUMBER)
+    if (*(cp++) != '.')
 	goto bad;
 
-    dep = erts_channel_no_to_dist_entry(n);
+    c = *cp;
+    num = 0;
+    if (!('0' <= c && c <= '9'))
+	goto bad;
+    do {
+	num *= 10;
+	num += (Uint64) (c - '0');
+	c = *(++cp);
+    } while ('0' <= c && c <= '9');
+
+    if (*(cp++) != '>')
+	goto bad;
+    if (*cp != '\0')
+	goto bad;
+    
+    dep = erts_channel_no_to_dist_entry(node);
 
     if (!dep)
 	goto bad;
 
     if(dep == erts_this_dist_entry) {
-	BIF_RET(make_internal_port(p));
+	if (num > ERTS_MAX_INTERNAL_PORT_NUMBER)
+	    goto bad;
+	BIF_RET(make_internal_port(num));
     }
     else {
       ExternalThing *etp;
@@ -4437,15 +4463,20 @@ BIF_RETTYPE list_to_port_1(BIF_ALIST_1)
       if (is_nil(dep->cid))
 	  goto bad;
 
-      etp = (ExternalThing *) HAlloc(BIF_P, EXTERNAL_THING_HEAD_SIZE + 1);
+      etp = (ExternalThing *) HAlloc(BIF_P, EXTERNAL_PORT_HEAP_SIZE);
       enp = erts_find_or_insert_node(dep->sysname, dep->creation,
                                      make_boxed(&etp->header));
       ASSERT(enp != erts_this_node);
 
-      etp->header = make_external_port_header(1);
+      etp->header = make_external_port_header();
       etp->next = MSO(BIF_P).first;
       etp->node = enp;
-      etp->data.ui[0] = p;
+#ifdef ARCH_64
+      etp->data.ui[0] = (Uint) num;
+#else
+      etp->data.ui[0] = (Uint) (num & 0xffffffff);
+      etp->data.ui[1] = (Uint) ((num >> 32) & 0xffffffff);
+#endif
 
       MSO(BIF_P).first = (struct erl_off_heap_header*) etp;
       BIF_RET(make_external_port(etp));

erts/emulator/beam/dist.h
@@ -58,7 +58,7 @@
  */
 #define DFLAG_SPAWN            (((Uint64)0x1) << 32)
 #define DFLAG_NAME_ME          (((Uint64)0x2) << 32)
-#define DFLAG_V4_PIDS_REFS     (((Uint64)0x4) << 32)
+#define DFLAG_V4_NC            (((Uint64)0x4) << 32)
 #define DFLAG_ALIAS            (((Uint64)0x8) << 32)
 
 /* Mandatory flags for distribution */
@@ -95,7 +95,7 @@
                             | DFLAG_FRAGMENTS                 \
                             | DFLAG_HANDSHAKE_23              \
                             | DFLAG_SPAWN                     \
-                            | DFLAG_V4_PIDS_REFS	      \
+                            | DFLAG_V4_NC		      \
                             | DFLAG_ALIAS)
 
 /* Flags addable by local distr implementations */

erts/emulator/beam/erl_debug.c
@@ -144,7 +144,7 @@ pdisplay1(fmtfn_t to, void *to_arg, Process* p, Eterm obj)
 	break;
     case PORT_DEF:
     case EXTERNAL_PORT_DEF:
-	erts_print(to, to_arg, "#Port<%lu.%lu>",
+	erts_print(to, to_arg, "#Port<%lu.%b64u>",
 		   port_channel_no(obj),
 		   port_number(obj));
 	break;

erts/emulator/beam/erl_node_container_utils.h
@@ -200,7 +200,7 @@ extern ErtsPTab erts_port;
 #define internal_port_data(PRT)		(ASSERT(is_internal_port((PRT))), \
 					 erts_ptab_id2data(&erts_port, (PRT)))
 
-#define internal_port_number(x) _GET_PORT_NUM(internal_port_data((x)))
+#define internal_port_number(x) ((Uint64) _GET_PORT_NUM(internal_port_data((x))))
 
 #define internal_port_node_name(x)	(internal_port_node((x))->sysname)
 #define external_port_node_name(x)	(external_port_node((x))->sysname)
@@ -245,7 +245,8 @@ extern ErtsPTab erts_port;
 */
 #define ERTS_MAX_PORTS			(ERTS_PTAB_MAX_SIZE-1)
 #define ERTS_MAX_PORT_DATA		((1 << _PORT_DATA_SIZE) - 1)
-#define ERTS_MAX_PORT_NUMBER		((1 << _PORT_NUM_SIZE) - 1)
+#define ERTS_MAX_INTERNAL_PORT_NUMBER	((1 << _PORT_NUM_SIZE) - 1)
+#define ERTS_MAX_V3_PORT_NUMBER		((1 << _PORT_NUM_SIZE) - 1)
 
 #define ERTS_PORTS_BITS			(_PORT_NUM_SIZE)
 

erts/emulator/beam/erl_printf_term.c
@@ -76,6 +76,22 @@ do {									\
     (CNT) += res__;							\
 } while (0)
 
+#define PRINT_UWORD64(CNT, FN, ARG, C, P, W, I)				\
+do {									\
+    int res__ = erts_printf_uword64((FN), (ARG), (C), (P), (W), (I));	\
+    if (res__ < 0)							\
+	return res__;							\
+    (CNT) += res__;							\
+} while (0)
+
+#define PRINT_SWORD64(CNT, FN, ARG, C, P, W, I)				\
+do {									\
+    int res__ = erts_printf_sword64((FN), (ARG), (C), (P), (W), (I));	\
+    if (res__ < 0)							\
+	return res__;							\
+    (CNT) += res__;							\
+} while (0)
+
 #define PRINT_DOUBLE(CNT, FN, ARG, C, P, W, I)				\
 do {									\
     int res__ = erts_printf_double((FN), (ARG), (C), (P), (W), (I));	\
@@ -438,8 +454,8 @@ print_term(fmtfn_t fn, void* arg, Eterm obj, long *dcount) {
 	    PRINT_UWORD(res, fn, arg, 'u', 0, 1,
 			(ErlPfUWord) port_channel_no(wobj));
 	    PRINT_CHAR(res, fn, arg, '.');
-	    PRINT_UWORD(res, fn, arg, 'u', 0, 1,
-			(ErlPfUWord) port_number(wobj));
+	    PRINT_UWORD64(res, fn, arg, 'u', 0, 1,
+			  (ErlPfUWord64) port_number(wobj));
 	    PRINT_CHAR(res, fn, arg, '>');
 	    break;
 	case LIST_DEF:

commit f463e57f0d3d28adb8f1da664cb9ce74338d8262

erts: Fix equality of external ports on 32-bit
bug introduced on master by
ff9e0228b4b56c979da905b95adc6b3fa0490ac0

erts/emulator/beam/utils.c
@@ -2860,6 +2860,7 @@ tailrecur_ne:
 	    case EXTERNAL_PORT_SUBTAG: {
 		ExternalThing *ap;
 		ExternalThing *bp;
+		int i;
 
 		if(!is_external(b))
 		    goto not_equal;
@@ -2867,13 +2868,17 @@ tailrecur_ne:
 		ap = external_thing_ptr(a);
 		bp = external_thing_ptr(b);
 
-		if(ap->header == bp->header && ap->node == bp->node) {
-		    ASSERT(1 == external_data_words(a));
-		    ASSERT(1 == external_data_words(b));
+		if(ap->header != bp->header || ap->node != bp->node)
+		    goto not_equal;
 
-                    if (ap->data.ui[0] == bp->data.ui[0]) goto pop_next;
+		ASSERT(EXTERNAL_PORT_DATA_WORDS == external_data_words(a));
+		ASSERT(EXTERNAL_PORT_DATA_WORDS == external_data_words(b));
+
+		for (i = 0; i < EXTERNAL_PORT_DATA_WORDS; i++) {
+		    if (ap->data.ui[i] != bp->data.ui[i])
+			goto not_equal;
 		}
-		break; /* not equal */
+		goto pop_next;
 	    }
 	    case EXTERNAL_REF_SUBTAG: {
 		/*