erlang/otp

commit 1c0e962c7a739e646dc6df6d8074392888126918

erl_docgen: Do not strip tags in datatype/name links

lib/erl_docgen/priv/xsl/db_html.xsl
@@ -2470,7 +2470,7 @@
 	    <xsl:apply-templates/>   <!-- like <ret> and <nametext> -->
 	  </xsl:when>
 	  <xsl:otherwise>
-	    <xsl:value-of select="$title"/>
+            <xsl:copy-of select="$title"/>
 	  </xsl:otherwise>
         </xsl:choose>
       </a>

commit 0f0ddab2826d1b4f4b2004e8a425c1a9e94faef9

erl_docgen: Fix rendering of sub section titles
This was broken in 1c0e962c7a739e646dc6df6d8074392888126918

lib/erl_docgen/priv/xsl/db_html.xsl
@@ -1069,7 +1069,9 @@
       </xsl:for-each>
       <!-- xsl:value-of select="$partnum"/>.<xsl:value-of select="$chapnum"/>.<xsl:value-of select="$sectnum"/>.<xsl:number/ -->
       <xsl:call-template name="title_link">
-        <xsl:with-param name="title" select="title"/>
+        <xsl:with-param name="title">
+          <xsl:value-of select="title"/>
+        </xsl:with-param>
       </xsl:call-template>
     </h4>
     <xsl:apply-templates>
@@ -1102,7 +1104,9 @@
 	<xsl:call-template name="marker-before-title"/>
       </xsl:for-each>
       <xsl:call-template name="title_link">
-        <xsl:with-param name="title" select="title"/>
+        <xsl:with-param name="title">
+          <xsl:value-of select="title"/>
+        </xsl:with-param>
       </xsl:call-template>
     </h3>
     <div class="REFBODY rb-3">