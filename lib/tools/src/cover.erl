e28e11b5af0fa552ea46adf1210adf7933834077
Mikael Pettersson <mikpelinux@gmail.com>
2021-05-04 20:02:06 +0200

    cover:filter_options/1: preserve tuple_calls

--- lib/tools/src/cover.erl
+++ lib/tools/src/cover.erl
@@ -335,6 +335,7 @@ filter_options(Options) ->
                              {d, _Macro} -> true;
                              {d, _Macro, _Value} -> true;
                              export_all -> true;
+                             tuple_calls -> true;
                              _ -> false
                          end
                  end,
