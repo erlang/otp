68ff8d95570867166e4023e725bcbbd19c9ce843
Frej Drejhammar <frej.drejhammar@gmail.com>
2021-06-18 09:19:07 +0200

    x86-jit: Use erlang-style <name>/<arity> for labels in asm dump
    When running with `+JDdump true` the most verbose x86-assembly dump is
    produced by the debug emulator. In it, labels present in the
    beam-assembly are printed as `label_<index>`. This patch improves the
    readability of the x86-assembly dumps by pretty-printing the label
    marking the start of a function using its <name>/<arity> Erlang name.
    
    Previously pretty-printing in the `label_<index>` format was only
    available in the debug emulator. Readability and the ease with which a
    user can cross-correlate x86-assembly and beam-assembly dumps is
    significantly improved with pretty-printed labels, thus this patch
    enables the functionality as soon as `+JDdump true` is enabled.

--- erts/emulator/beam/jit/x86/beam_asm_module.cpp
+++ erts/emulator/beam/jit/x86/beam_asm_module.cpp
@@ -86,18 +86,44 @@ BeamModuleAssembler::BeamModuleAssembler(BeamGlobalAssembler *ga,
     this->mod = mod;
 
     labels.reserve(num_labels + 1);
-    for (unsigned i = 1; i < num_labels; i++) {
-        Label lbl;
 
-#ifdef DEBUG
-        std::string lblName = "label_" + std::to_string(i);
-        lbl = a.newNamedLabel(lblName.data());
-#else
-        lbl = a.newLabel();
-#endif
-
-        labels[i] = lbl;
+    if (erts_jit_asm_dump && named_labels) {
+        BeamFile_ExportEntry *e = &named_labels->entries[0];
+        for (unsigned i = 1; i < num_labels; i++) {
+            Label lbl;
+            char tmp[512]; // Large enough to hold most realistic
+                           // function names. We will truncate too
+                           // long names, but as the label name is not
+                           // important for the functioning of asmjit
+                           // and this functionality is probably only
+                           // used by developers, we don't bother with
+                           // dynamic allocation.
+            // The named_labels are sorted, so no need for a search.
+            if ((unsigned)e->label == i) {
+                erts_snprintf(tmp, sizeof(tmp), "%T/%d", e->function, e->arity);
+                lbl = a.newNamedLabel(tmp);
+                e++;
+            } else {
+                std::string lblName = "label_" + std::to_string(i);
+                lbl = a.newNamedLabel(lblName.data());
+            }
+            labels[i] = lbl;
+        }
+        return;
     }
+    if (erts_jit_asm_dump) {
+        // There is no naming info, but dumping of the assembly code
+        // has been requested, so do the best we can and number the
+        // labels.
+        for (unsigned i = 1; i < num_labels; i++) {
+            std::string lblName = "label_" + std::to_string(i);
+            labels[i] = a.newNamedLabel(lblName.data());
+        }
+        return;
+    }
+    // No output is requested, go with unnamed labels
+    for (unsigned i = 1; i < num_labels; i++)
+        labels[i] = a.newLabel();
 }
 
 BeamModuleAssembler::BeamModuleAssembler(BeamGlobalAssembler *ga,
