4ed5f9377ce072b264cac4107eb7b6f8a16a276d
Thomas Depierre <depierre.thomas@gmail.com>
2021-07-08 16:20:16 +0200

    gnu99 std CFLAGS for ryu

--- erts/emulator/ryu/ryu.mk
+++ erts/emulator/ryu/ryu.mk
@@ -45,5 +45,11 @@ $(RYU_LIBRARY): $(RYU_OBJS)
 	-@ ($(RANLIB) $@ || true) 2>/dev/null
 endif
 
+ifeq ($(TARGET), win32)
+	RYU_CFLAGS = $(CFLAGS)
+else
+	RYU_CFLAGS = -std=gnu99 $(CFLAGS)
+endif
+
 $(RYU_OBJDIR)/%.o: ryu/%.c
-	$(V_CC) -c $(CFLAGS) -o $@ $<
+	$(V_CC) -c $(RYU_CFLAGS) -o $@ $<
