055a6c52ab9fce944064cd3e3ffcb81784bcae03
Hans Bolinder <hasse@erlang.org>
2021-08-02 16:13:12 +0200

    dialyzer: Parallelize remote types processing in records
    This has effect if many processes can run i parallel.
    
    The cashe used to be re-used by modules, but now a new cache is
    created for each module.

--- lib/dialyzer/src/dialyzer_worker.erl
+++ lib/dialyzer/src/dialyzer_worker.erl
@@ -67,7 +67,7 @@ init(#state{job = SCC, mode = Mode, init_data = InitData,
   loop(updating, State#state{depends_on = DependsOn});
 init(#state{mode = Mode} = State) when
     Mode =:= 'compile'; Mode =:= 'warnings';
-    Mode =:= 'contract_remote_types' ->
+    Mode =:= 'contract_remote_types'; Mode =:= 'record_remote_types' ->
   loop(running, State).
 
 loop(updating, #state{mode = Mode} = State) when
@@ -84,7 +84,8 @@ loop(waiting, #state{mode = Mode} = State) when
   ?debug("~w: Wait: ~p\n", [self(), State#state.job]),
   NewState = wait_for_success_typings(State),
   loop(updating, NewState);
-loop(running, #state{mode = 'contract_remote_types'} = State) ->
+loop(running, #state{mode = Mode} = State) when
+    Mode =:= 'contract_remote_types'; Mode =:= 'record_remote_types' ->
   request_activation(State),
   ?debug("~w: Remote types: ~p\n", [self(), State#state.job]),
   Result = do_work(State),
@@ -135,7 +136,9 @@ do_work(#state{mode = Mode, job = Job, init_data = InitData}) ->
     typesig -> dialyzer_succ_typings:find_succ_types_for_scc(Job, InitData);
     dataflow -> dialyzer_succ_typings:refine_one_module(Job, InitData);
     contract_remote_types ->
-      dialyzer_contracts:process_contract_remote_types_module(Job, InitData)
+      dialyzer_contracts:process_contract_remote_types_module(Job, InitData);
+    record_remote_types ->
+      dialyzer_utils:process_record_remote_types_module(Job, InitData)
   end.
 
 report_to_coordinator(Result, #state{job = Job, coordinator = Coordinator}) ->
