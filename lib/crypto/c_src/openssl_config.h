710ef353225b188d94dd4bbce565bc23347b03b7
Marcel Lanz <marcellanz@n-1.ch>
2021-10-24 00:42:37 +0200

    [crypto/4871] Add support for SHAKE128 and SHAKE256.

--- lib/crypto/c_src/openssl_config.h
+++ lib/crypto/c_src/openssl_config.h
@@ -188,6 +188,12 @@
 # ifdef NID_sha3_512
 #  define HAVE_SHA3_512
 # endif
+# ifdef NID_shake128
+#  define HAVE_SHAKE128
+# endif
+# ifdef NID_shake256
+#  define HAVE_SHAKE256
+# endif
 
 // BLAKE2:
 #if OPENSSL_VERSION_NUMBER >= PACKED_OPENSSL_VERSION_PLAIN(1,1,1) \
