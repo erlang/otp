797f3508b54099da03ae6dc3dd62f8d69255579e
Lukas Larsson <lukas@erlang.org>
2021-10-26 10:51:51 +0200

    Add .editorconfig

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,11 +5,9 @@
  (java-mode (indent-tabs-mode . nil))
  (perl-mode (indent-tabs-mode . nil))
  (xml-mode (indent-tabs-mode . nil))
- ;; In C code indentation is 4 spaces and in C++ 2 spaces
- ;;   erts/ overrides C++ to 4 spaces
  (c++-mode
   (indent-tabs-mode . nil)
-  (c-basic-offset . 2))
+  (c-basic-offset . 4))
  (c-mode
   (indent-tabs-mode . nil)
   (c-basic-offset . 4)))
