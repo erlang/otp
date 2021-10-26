bb52da16fb9dea632d9bb88e43def34c681ecaee
Justin Davis <jrcd83@gmail.com>
2021-10-26 13:28:16 -0400

    Fix emacs' erlang-mode: port EIEIO to CL-lib.
    Xref is a package which is bundled with emacs. Xref switched its own
    internally used classes from EIEIO to CL-lib at
    emacs-mirror/emacs@86da812afb2572c7fead2bb07570b976bffd7c55.
    
    Convert all usage of EIEIO's defclass to CL-lib's def-struct.
    erlang-mode had subclassed Xref's xref-file-location class as
    erlang-xref-file-location. Change xref-file-location to be a
    which "includes" xref-file-location.
    
    Avoids calling make-instance to create an object and simply call the
    struct constructor.
    
    Updates comments that make references to classes.
    
    Fixes issue #5314.
    
    Testing before and after applying patch:
      1. Install the latest version of the Xref package in Emacs:
         (in emacs) M-x package-install RET xref RET
      2. Verify that opening any erlang source file will cause an error.
      3. Update erlang-mode with this patch and reload erlang-mode (i.e.
         restart emacs).
      4. Verify that opening any erlang file will not cause an error.
      5. Generate a TAGS file for the project with Exuberant Tags:
         (in shell) ctags -e *.erl
      6. Verify xref works. Move the cursor to a function call and find
         the definition:
         (in emacs) M-x xref-find-definitions

--- lib/tools/emacs/erlang.el
+++ lib/tools/emacs/erlang.el
@@ -5120,8 +5120,8 @@ about Erlang modules."
 ;;
 ;; As mentioned this xref implementation is based on the etags xref
 ;; implementation.  But in the cases where arity is considered the
-;; etags information structures (class xref-etags-location) will be
-;; translated to our own structures which include arity (class
+;; etags information structures (struct xref-etags-location) will be
+;; translated to our own structures which include arity (struct
 ;; erlang-xref-location).  This translation is started in the function
 ;; `erlang-refine-xrefs'.
 
@@ -5137,7 +5137,7 @@ about Erlang modules."
 
 (when (and (erlang-soft-require 'xref)
            (erlang-soft-require 'cl-generic)
-           (erlang-soft-require 'eieio)
+           (erlang-soft-require 'cl-lib)
            (erlang-soft-require 'etags))
   ;; The purpose of using eval here is to avoid compilation
   ;; warnings in emacsen without cl-defmethod etc.
@@ -5165,10 +5165,9 @@ about Erlang modules."
         (let ((erlang-replace-etags-tags-completion-table t))
           (tags-completion-table)))
 
-      (defclass erlang-xref-location (xref-file-location)
-        ((arity :type fixnum :initarg :arity
-                :reader erlang-xref-location-arity))
-        :documentation "An erlang location is a file location plus arity.")
+      (cl-defstruct (erlang-xref-location (:include xref-file-location))
+        "An erlang location is a file location plus arity."
+        (arity 0 :type fixnum))
 
       ;; This method definition only calls the superclass which is
       ;; the default behaviour if it was not defined.  It is only
@@ -5369,17 +5368,15 @@ is non-nil then TAG is a regexp."
                    for name = (match-string-no-properties 1)
                    for arity = (save-excursion
                                  (erlang-get-arity))
-                   for loc = (make-instance 'erlang-xref-location
-                                            :file file
-                                            :line (line-number-at-pos)
-                                            :column 0
-                                            :arity arity)
+                   for loc = (make-erlang-xref-location
+                              :file file
+                              :line (line-number-at-pos)
+                              :column 0
+                              :arity arity)
                    for sum = (erlang-xref-summary kind name arity)
                    when (and arity
                              (not (eq arity last-arity)))
-                   collect (make-instance 'xref-item
-                                          :summary sum
-                                          :location loc)
+                   collect (xref-make :summary sum :location loc)
                    do (setq last-arity arity)))))))
 
 (defun erlang-xref-summary (kind tag arity)
