8d3052ba8ac45208aa6a2512ddc8162804591bc3
Hans Nilsson <hans@erlang.org>
2021-11-11 08:46:14 +0100

    crypto: FIPS - rewrite fips.c in a more clear way

--- lib/crypto/c_src/fips.c
+++ lib/crypto/c_src/fips.c
@@ -26,8 +26,9 @@ ERL_NIF_TERM info_fips(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
 #ifdef FIPS_SUPPORT
     return FIPS_MODE() ? atom_enabled : atom_not_enabled;
-#endif
+#else
     return atom_not_supported;
+#endif
 }
 
 ERL_NIF_TERM enable_fips_mode_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
@@ -36,25 +37,27 @@ ERL_NIF_TERM enable_fips_mode_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM a
 }
 
 
-ERL_NIF_TERM enable_fips_mode(ErlNifEnv* env, ERL_NIF_TERM fips_mode)
+ERL_NIF_TERM enable_fips_mode(ErlNifEnv* env, ERL_NIF_TERM fips_mode_to_set)
+#ifdef FIPS_SUPPORT
 {
-    if (fips_mode == atom_true) {
-#ifdef FIPS_SUPPORT
-        if (FIPS_mode_set(1)) {
-            init_digest_types(env);
-            return atom_true;
-        }
-#endif
-        return atom_false;
-    } else if (fips_mode == atom_false) {
-#ifdef FIPS_SUPPORT
-        if (!FIPS_mode_set(0)) {
-            return atom_false;
-        }
-        init_digest_types(env);
-#endif
-        return atom_true;
-    } else {
+    if (fips_mode_to_set == atom_true) {
+        if (FIPS_mode_set(1)) return atom_true;
+                         else return atom_false;
+
+    } else if (fips_mode_to_set == atom_false) {
+        if (!FIPS_mode_set(0)) return atom_false;
+                          else return atom_true;
+
+    } else
         return enif_make_badarg(env);
-    }
 }
+#else
+
+{
+    if (fips_mode_to_set == atom_true) return atom_false;
+    else if (fips_mode_to_set == atom_false) return atom_true;
+    else return enif_make_badarg(env);
+}
+#endif    
+
+
