337edbfa9f2fbf447ef59c7b6ac7b3ba44c9e96e
Micael Karlberg <bmk@erlang.org>
2021-11-11 17:04:45 +0100

    [esock|test] Add (simple) test case for ioctl request 'gifbrdaddr'
    OTP-17528

--- lib/kernel/test/socket_SUITE.erl
+++ lib/kernel/test/socket_SUITE.erl
@@ -304,6 +304,7 @@
          ioctl_get_gifindex/1,
          ioctl_get_gifaddr/1,
          ioctl_get_gifdstaddr/1,
+         ioctl_get_gifbrdaddr/1,
          %% ioctl_set_requests/1,
 
          %% *** Traffic ***
@@ -1240,7 +1241,8 @@ ioctl_get_cases() ->
      ioctl_get_gifname,
      ioctl_get_gifindex,
      ioctl_get_gifaddr,
-     ioctl_get_gifdstaddr
+     ioctl_get_gifdstaddr,
+     ioctl_get_gifbrdaddr
     ].
 
 
@@ -35185,6 +35187,63 @@ do_ioctl_get_gifdstaddr(_State) ->
 
 
 
+%% --- gifbrdaddr ---
+
+ioctl_get_gifbrdaddr(suite) ->
+    [];
+ioctl_get_gifbrdaddr(doc) ->
+    [];
+ioctl_get_gifbrdaddr(_Config) when is_list(_Config) ->
+    ?TT(?SECS(5)),
+    tc_try(?FUNCTION_NAME,
+           fun() ->
+                   has_support_net_if_names(),
+                   has_support_ioctl_gifbrdaddr()
+           end,
+           fun() ->
+                   InitState = #{},
+                   ok = do_ioctl_get_gifbrdaddr(InitState)
+           end).
+
+
+do_ioctl_get_gifbrdaddr(_State) ->
+    i("create dummy stream:TCP socket"),
+    {ok, Sock} = socket:open(inet, stream, tcp),
+
+    i("get if names"),
+    {ok, IfNames} = net:if_names(),
+
+    i("try ioctl all if indexes: "
+      "~n      ~p", [IfNames]),
+    %% This a *very* simple test...
+    %% ...just to check that we actually get an socket address
+    _ = [case socket:ioctl(Sock, gifbrdaddr, IfName) of
+             {ok, #{family := Fam,
+                    addr   := Addr}} ->
+                 i("got (expected) (dest) socket address for interface ~p (~w): "
+                   "~n      (~w) ~p", [IfName, IfIdx, Fam, Addr]),
+                 ok;
+             {ok, Crap} ->
+                 %% Oups?!
+                 i("<ERROR> got unexpected result for interface ~p (~w)"
+                   "~n      ~p", [IfName, IfIdx, Crap]),
+                 socket:close(Sock),
+                 ?FAIL({unexpected_addr, IfName, IfIdx, Crap});
+             {error, Reason} ->
+                 i("<ERROR> got unexpected error for interface ~p (~w)"
+                   "~n      Reason: ~p", [IfName, IfIdx, Reason]),
+                 socket:close(Sock),
+                 ?FAIL({unexpected_failure, IfName, IfIdx, Reason})
+         end || {IfIdx, IfName} <- IfNames],
+
+    i("close dummy stream:TCP socket"),
+    ok = socket:close(Sock),
+
+    i("done"),
+    ok.
+
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% This test case is intended to (simply) test that the counters
 %% for both read and write.
@@ -49143,6 +49202,9 @@ has_support_ioctl_gifaddr() ->
 
 has_support_ioctl_gifdstaddr() ->
     has_support_ioctl_request(gifdstaddr).
+
+has_support_ioctl_gifbrdaddr() ->
+    has_support_ioctl_request(gifbrdaddr).
 
 has_support_ioctl_request(Req) when is_atom(Req) ->
     try socket:is_supported(ioctl_requests, Req) of
