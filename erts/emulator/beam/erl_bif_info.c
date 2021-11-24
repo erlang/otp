7241a6a3e6940b2efed5fde39f78ef8b69212b97
Rickard Green <rickard@erlang.org>
2021-11-24 22:50:13 +0100

    Introduce outstanding requests limit for system processes
    This limit effects system processes that orchestrates operations that
    involves all processes in the system. Currently there are two such
    processes, the code purger process and the literal area collector
    process. They previously sent out requests to all processes on the
    system at once, and then waited for responses from these processes in
    order to determine when the operation had completed.
    
    When a process had handled such a request it could (and still can)
    continue executing Erlang code once it had served this request. If
    executing on priority normal or low, it would however not get another
    opportunity to execute until all other requests had been served. This
    negatively impacted responsiveness of processes during operations like
    these on systems with a huge amount of processes.
    
    This change limits the amount of outstanding request which will cause
    the system to interleave handling of requests like these with other
    execution and by this improve responsiveness on systems with a huge
    amount of processes. By default the limit is set to two times the
    amount of schedulers. This will make sure that there always will be
    enough outstanding requests to utilize all schedulers fully during
    operations like these while also letting other work execute without
    having to wait very long.

--- erts/emulator/beam/erl_bif_info.c
+++ erts/emulator/beam/erl_bif_info.c
@@ -2514,6 +2514,9 @@ BIF_RETTYPE system_info_1(BIF_ALIST_1)
 	default:
 	    ERTS_INTERNAL_ERROR("Invalid time warp mode");
 	}
+    } else if (BIF_ARG_1 == am_outstanding_system_requests_limit) {
+        Uint val = erts_get_outstanding_system_requests_limit();
+        BIF_RET(make_small(val));
     } else if (BIF_ARG_1 == am_allocated_areas) {
 	res = erts_allocated_areas(NULL, NULL, BIF_P);
 	BIF_RET(res);
