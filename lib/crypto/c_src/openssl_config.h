8a9d4127fc4bc46ac720f7adf1d0411cf0ead4c5
Hans Nilsson <hans@erlang.org>
2022-01-13 13:43:33 +0100

    crypto: Temporarilly disable ENGINE for 3.0

--- lib/crypto/c_src/openssl_config.h
+++ lib/crypto/c_src/openssl_config.h
@@ -322,7 +322,9 @@
 /* If OPENSSL_NO_EC is set, there will be an error in ec.h included from engine.h
    So if EC is disabled, you can't use Engine either....
 */
-#if !defined(OPENSSL_NO_ENGINE)
+#if !defined(OPENSSL_NO_ENGINE) && \
+    !defined(HAS_3_0_API)
+/* Disable FIPS for 3.0 temporaryly until the support is added (might core dump) */
 # define HAS_ENGINE_SUPPORT
 #endif
 #endif
