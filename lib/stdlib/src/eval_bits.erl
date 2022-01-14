1499ec5ad07906ab892424dfd93180834e2b4bd1
José Valim <jose.valim@dashbit.co>
2022-01-14 18:08:42 +0100

    Pass Anno to ExternalFunctionHandler in erl_eval
    This allows code using `erl_eval` to provide better
    stacktraces if desired. For this purpose, we also
    make sure all errors raised from `erl_eval` pass
    through the ExternalFunctionHandler.

--- lib/stdlib/src/eval_bits.erl
+++ lib/stdlib/src/eval_bits.erl
@@ -22,8 +22,8 @@
 
 %% Avoid warning for local function error/1 clashing with autoimported BIF.
 -compile({no_auto_import,[error/1]}).
--export([expr_grp/3,expr_grp/5,match_bits/6, 
-	 match_bits/7,bin_gen/6]).
+-export([expr_grp/3,expr_grp/4,match_bits/6,
+	 match_bits/7,bin_gen/6,bin_gen/7]).
 
 -define(STACKTRACE,
         element(2, erlang:process_info(self(), current_stacktrace))).
@@ -40,11 +40,13 @@
 %% lookup a variable in the bindings, or add a new binding
 %%
 %% @type field(). Represents a field in a "bin".
+%%
+%% @type errorfun(). A closure invoked in case of errors.
 
 %%% Part 1: expression evaluation (binary construction)
 
 %% @spec expr_grp(Fields::[field()], Bindings::bindings(), 
-%%                EvalFun::evalfun(), term(), term()) ->
+%%                EvalFun::evalfun(), ErrorFun::errorfun()) ->
 %%                  {value, binary(), bindings()}
 %%
 %% @doc Returns a tuple with {value,Bin,Bs} where Bin is the binary
@@ -52,29 +54,23 @@
 %% contains the present bindings. This function can also throw an
 %% exception if the construction fails.
 
-expr_grp(Fields, Bindings, EvalFun, [], _) ->
-    expr_grp(Fields, Bindings, EvalFun, <<>>);
-expr_grp(Fields, Bindings, EvalFun, ListOfBits, _) ->
-    Bin = convert_list(ListOfBits),
-    expr_grp(Fields, Bindings, EvalFun, Bin).
-
-convert_list(List) ->
-    << <<X:1>> || X <- List >>.
+expr_grp(Fields, Bindings, EvalFun, ErrorFun) ->
+    expr_grp(Fields, Bindings, EvalFun, ErrorFun, <<>>).
 
 expr_grp(Fields, Bindings, EvalFun) ->
-    expr_grp(Fields, Bindings, EvalFun, <<>>).
+    expr_grp(Fields, Bindings, EvalFun, fun default_error/3, <<>>).
 
-expr_grp(FS, Bs0, Ef, Acc) ->
+expr_grp(FS, Bs0, EvalFun, ErrorFun, Acc) ->
     %% Separate the evaluation of values, sizes, and TLS:s from the
     %% creation of the binary in order to mimic compiled code when it
     %% comes to loops and failures.
-    {ListOfEvalField,Bs1} = expr_grp1(FS, Bs0, Ef, []),
+    {ListOfEvalField,Bs1} = expr_grp1(FS, Bs0, EvalFun, ErrorFun, []),
     {value,create_binary(ListOfEvalField, Acc),Bs1}.
 
-expr_grp1([Field | FS], Bs0, Ef, ListOfEvalField) ->
-    {EvalField,Bs} = eval_field(Field, Bs0, Ef),
-    expr_grp1(FS, Bs, Ef, [EvalField|ListOfEvalField]);
-expr_grp1([], Bs, _Ef, ListOfFieldData) ->
+expr_grp1([Field | FS], Bs0, EvalFun, ErrorFun, ListOfEvalField) ->
+    {EvalField,Bs} = eval_field(Field, Bs0, EvalFun, ErrorFun),
+    expr_grp1(FS, Bs, EvalFun, ErrorFun, [EvalField|ListOfEvalField]);
+expr_grp1([], Bs, _EvalFun, _ErrorFun, ListOfFieldData) ->
     {lists:reverse(ListOfFieldData),Bs}.
 
 create_binary([EvalField|ListOfEvalField], Acc) ->
@@ -83,44 +79,44 @@ create_binary([EvalField|ListOfEvalField], Acc) ->
 create_binary([], Acc) ->
     Acc.
 
-eval_field({bin_element, _, {string, _, S}, {integer,_,8}, [integer,{unit,1},unsigned,big]}, Bs0, _Fun) ->
+eval_field({bin_element, _, {string, _, S}, {integer,_,8}, [integer,{unit,1},unsigned,big]}, Bs0, _Fun, _ErrorFun) ->
     Latin1 = [C band 16#FF || C <- S],
     {fun() -> list_to_binary(Latin1) end,Bs0};
-eval_field({bin_element, _, {string, _, S}, default, default}, Bs0, _Fun) ->
+eval_field({bin_element, _, {string, _, S}, default, default}, Bs0, _Fun, _ErrorFun) ->
     Latin1 = [C band 16#FF || C <- S],
     {fun() ->list_to_binary(Latin1) end,Bs0};
-eval_field({bin_element, Anno, {string, _, S}, Size0, Options0}, Bs0, Fun) ->
+eval_field({bin_element, Anno, {string, _, S}, Size0, Options0}, Bs0, Fun, ErrorFun) ->
     {Size1,[Type,{unit,Unit},Sign,Endian]} =
-        make_bit_type(Anno, Size0, Options0),
+        make_bit_type(Anno, Size0, Options0, ErrorFun),
     {value,Size,Bs1} = Fun(Size1, Bs0),
     {fun() ->
-             Res = << <<(eval_exp_field1(C, Size, Unit,
-                                         Type, Endian, Sign))/bitstring>> ||
+             Res = << <<(eval_exp_field1(Anno, C, Size, Unit,
+                                         Type, Endian, Sign, ErrorFun))/bitstring>> ||
                        C <- S >>,
              case S of
                  "" -> % find errors also when the string is empty
-                     _ = eval_exp_field1(0, Size, Unit, Type, Endian, Sign),
+                     _ = eval_exp_field1(Anno, 0, Size, Unit, Type, Endian, Sign, ErrorFun),
                      ok;
                  _ ->
                      ok
              end,
              Res
      end,Bs1};
-eval_field({bin_element,Anno,E,Size0,Options0}, Bs0, Fun) ->
+eval_field({bin_element,Anno,E,Size0,Options0}, Bs0, Fun, ErrorFun) ->
     {value,V,Bs1} = Fun(E, Bs0),
     {Size1,[Type,{unit,Unit},Sign,Endian]} = 
-        make_bit_type(Anno, Size0, Options0),
+        make_bit_type(Anno, Size0, Options0, ErrorFun),
     {value,Size,Bs} = Fun(Size1, Bs1),
-    {fun() -> eval_exp_field1(V, Size, Unit, Type, Endian, Sign) end,Bs}.
+    {fun() -> eval_exp_field1(Anno, V, Size, Unit, Type, Endian, Sign, ErrorFun) end,Bs}.
 
-eval_exp_field1(V, Size, Unit, Type, Endian, Sign) ->
+eval_exp_field1(Anno, V, Size, Unit, Type, Endian, Sign, ErrorFun) ->
     try
 	eval_exp_field(V, Size, Unit, Type, Endian, Sign)
     catch
 	error:system_limit ->
-	    erlang:raise(error, system_limit, ?STACKTRACE);
+        ErrorFun(Anno, system_limit, ?STACKTRACE);
 	error:_ ->
-	    erlang:raise(error, badarg, ?STACKTRACE)
+        ErrorFun(Anno, badarg, ?STACKTRACE)
     end.
 
 eval_exp_field(Val, Size, Unit, integer, little, signed) ->
@@ -169,7 +165,8 @@ eval_exp_field(Val, Size, Unit, binary, _, _) ->
 %%% Part 2: matching in binary comprehensions
 %% @spec bin_gen(BinPattern::{bin,integer(),[field()]}, Bin::binary(),
 %%               GlobalEnv::bindings(), LocalEnv::bindings(),  
-%%               MatchFun::matchfun(), EvalFun::evalfun()) -> 
+%%               MatchFun::matchfun(), EvalFun::evalfun(),
+%%               ErrorFun::errorfun()) ->
 %%                 {match, binary(), bindings()} | {nomatch, binary()} | done
 %%
 %% @doc Used to perform matching in a comprehension. If the match
@@ -178,25 +175,28 @@ eval_exp_field(Val, Size, Unit, binary, _, _) ->
 %% If nothing remains of the binary the atom 'done' is returned.
 
 bin_gen({bin,_,Fs}, Bin, Bs0, BBs0, Mfun, Efun) ->
-    bin_gen(Fs, Bin, Bs0, BBs0, Mfun, Efun, true).
+    bin_gen(Fs, Bin, Bs0, BBs0, Mfun, Efun, fun default_error/3, true).
 
-bin_gen([F|Fs], Bin, Bs0, BBs0, Mfun, Efun, Flag)
+bin_gen({bin,_,Fs}, Bin, Bs0, BBs0, Mfun, Efun, ErrorFun) ->
+    bin_gen(Fs, Bin, Bs0, BBs0, Mfun, Efun, ErrorFun, true).
+
+bin_gen([F|Fs], Bin, Bs0, BBs0, Mfun, Efun, ErrorFun, Flag)
   when is_function(Mfun, 2), is_function(Efun, 2) ->
-    case bin_gen_field(F, Bin, Bs0, BBs0, Mfun, Efun) of
+    case bin_gen_field(F, Bin, Bs0, BBs0, Mfun, Efun, ErrorFun) of
         {match,Bs,BBs,Rest} ->
-            bin_gen(Fs, Rest, Bs, BBs, Mfun, Efun, Flag);
+            bin_gen(Fs, Rest, Bs, BBs, Mfun, Efun, ErrorFun, Flag);
         {nomatch,Rest} ->
-            bin_gen(Fs, Rest, Bs0, BBs0, Mfun, Efun, false);
+            bin_gen(Fs, Rest, Bs0, BBs0, Mfun, Efun, ErrorFun, false);
         done ->
             done
     end;
-bin_gen([], Bin, Bs0, _BBs0, _Mfun, _Efun, true) ->
+bin_gen([], Bin, Bs0, _BBs0, _Mfun, _Efun, _ErrorFun, true) ->
     {match, Bin, Bs0};
-bin_gen([], Bin, _Bs0, _BBs0, _Mfun, _Efun, false) ->
+bin_gen([], Bin, _Bs0, _BBs0, _Mfun, _Efun, _ErrorFun, false) ->
     {nomatch, Bin}.
   
 bin_gen_field({bin_element,_,{string,_,S},default,default},
-              Bin, Bs, BBs, _Mfun, _Efun) ->
+              Bin, Bs, BBs, _Mfun, _Efun, _ErrorFun) ->
     Bits = try list_to_binary(S)
            catch _:_ -> <<>>
            end,
@@ -210,9 +210,9 @@ bin_gen_field({bin_element,_,{string,_,S},default,default},
             done
     end;
 bin_gen_field({bin_element,Anno,{string,SAnno,S},Size0,Options0},
-              Bin0, Bs0, BBs0, Mfun, Efun) ->
+              Bin0, Bs0, BBs0, Mfun, Efun, ErrorFun) ->
     {Size1, [Type,{unit,Unit},Sign,Endian]} =
-        make_bit_type(Anno, Size0, Options0),
+        make_bit_type(Anno, Size0, Options0, ErrorFun),
     case catch Efun(Size1, BBs0) of
         {value, Size, _BBs} -> % 
             F = fun(C, Bin, Bs, BBs) ->
@@ -222,9 +222,9 @@ bin_gen_field({bin_element,Anno,{string,SAnno,S},Size0,Options0},
             bin_gen_field_string(S, Bin0, Bs0, BBs0, F)
     end;
 bin_gen_field({bin_element,Anno,VE,Size0,Options0},
-              Bin, Bs0, BBs0, Mfun, Efun) ->
+              Bin, Bs0, BBs0, Mfun, Efun, ErrorFun) ->
     {Size1, [Type,{unit,Unit},Sign,Endian]} = 
-        make_bit_type(Anno, Size0, Options0),
+        make_bit_type(Anno, Size0, Options0, ErrorFun),
     V = erl_eval:partial_eval(VE),
     NewV = coerce_to_float(V, Type),
     case catch Efun(Size1, BBs0) of
@@ -264,41 +264,42 @@ bin_gen_field1(Bin, Type, Size, Unit, Sign, Endian, NewV, Bs0, BBs0, Mfun) ->
 %%% Part 3: binary pattern matching 
 %% @spec match_bits(Fields::[field()], Bin::binary(),
 %%                  GlobalEnv::bindings(), LocalEnv::bindings(),  
-%%                  MatchFun::matchfun(),EvalFun::evalfun(), term()) ->
-%%                  {match, bindings()} 
+%%                  MatchFun::matchfun(),EvalFun::evalfun(),
+%%                  ErrorFun::errorfun()) ->
+%%                  {match, bindings()}
 %% @doc Used to perform matching. If the match succeeds a new
 %% environment is returned. If the match have some syntactic or
 %% semantic problem which would have been caught at compile time this
 %% function throws 'invalid', if the matching fails for other reasons
 %% the function throws 'nomatch'
 
-match_bits(Fs, Bin, Bs0, BBs, Mfun, Efun, _) ->
-    match_bits(Fs, Bin, Bs0, BBs, Mfun, Efun).
+match_bits(Fs, Bin, Bs0, BBs, Mfun, Efun) ->
+    match_bits(Fs, Bin, Bs0, BBs, Mfun, Efun, fun default_error/3).
 
-match_bits(Fs, Bin, Bs0, BBs, Mfun, Efun)
-  when is_function(Mfun, 2), is_function(Efun, 2) ->
-    case catch match_bits_1(Fs, Bin, Bs0, BBs, Mfun, Efun) of
+match_bits(Fs, Bin, Bs0, BBs, Mfun, Efun, ErrorFun)
+  when is_function(Mfun, 2), is_function(Efun, 2), is_function(ErrorFun, 3) ->
+    case catch match_bits_1(Fs, Bin, Bs0, BBs, Mfun, Efun, ErrorFun) of
         {match,Bs} -> {match,Bs};
         invalid -> throw(invalid);
         _Error -> throw(nomatch)
     end.
 
-match_bits_1([], <<>>,  Bs, _BBs, _Mfun, _Efun) -> 
+match_bits_1([], <<>>,  Bs, _BBs, _Mfun, _Efun, _ErrorFun) ->
     {match,Bs};
-match_bits_1([F|Fs], Bits0, Bs0, BBs0, Mfun, Efun) ->
-    {Bs,BBs,Bits} = match_field_1(F, Bits0, Bs0, BBs0, Mfun, Efun),
-    match_bits_1(Fs, Bits, Bs, BBs, Mfun, Efun).
+match_bits_1([F|Fs], Bits0, Bs0, BBs0, Mfun, Efun, ErrorFun) ->
+    {Bs,BBs,Bits} = match_field_1(F, Bits0, Bs0, BBs0, Mfun, Efun, ErrorFun),
+    match_bits_1(Fs, Bits, Bs, BBs, Mfun, Efun, ErrorFun).
 
 match_field_1({bin_element,_,{string,_,S},default,default},
-              Bin, Bs, BBs, _Mfun, _Efun) ->
+              Bin, Bs, BBs, _Mfun, _Efun, _ErrorFun) ->
     Bits = list_to_binary(S), % fails if there are characters > 255
     Size = byte_size(Bits),
     <<Bits:Size/binary,Rest/binary-unit:1>> = Bin,
     {Bs,BBs,Rest};
 match_field_1({bin_element,Anno,{string,SAnno,S},Size0,Options0},
-              Bin0, Bs0, BBs0, Mfun, Efun) ->
+              Bin0, Bs0, BBs0, Mfun, Efun, ErrorFun) ->
     {Size1, [Type,{unit,Unit},Sign,Endian]} =
-        make_bit_type(Anno, Size0, Options0),
+        make_bit_type(Anno, Size0, Options0, ErrorFun),
     Size2 = erl_eval:partial_eval(Size1),
     {value, Size, _BBs} = Efun(Size2, BBs0),
     F = fun(C, Bin, Bs, BBs) ->
@@ -307,9 +308,9 @@ match_field_1({bin_element,Anno,{string,SAnno,S},Size0,Options0},
         end,
     match_field_string(S, Bin0, Bs0, BBs0, F);
 match_field_1({bin_element,Anno,VE,Size0,Options0},
-              Bin, Bs0, BBs0, Mfun, Efun) ->
+              Bin, Bs0, BBs0, Mfun, Efun, ErrorFun) ->
     {Size1, [Type,{unit,Unit},Sign,Endian]} = 
-        make_bit_type(Anno, Size0, Options0),
+        make_bit_type(Anno, Size0, Options0, ErrorFun),
     V = erl_eval:partial_eval(VE),
     NewV = coerce_to_float(V, Type),
     Size2 = erl_eval:partial_eval(Size1),
@@ -410,15 +411,18 @@ get_float(Bin, Size, big) ->
     {Val,Rest}.
 
 %% Identical to the one in sys_pre_expand.
-make_bit_type(Anno, default, Type0) ->
+make_bit_type(Anno, default, Type0, ErrorFun) ->
     case erl_bits:set_bit_type(default, Type0) of
         {ok,all,Bt} -> {{atom,Anno,all},erl_bits:as_list(Bt)};
 	{ok,undefined,Bt} -> {{atom,Anno,undefined},erl_bits:as_list(Bt)};
         {ok,Size,Bt} -> {{integer,Anno,Size},erl_bits:as_list(Bt)};
-        {error,Reason} -> erlang:raise(error, Reason, ?STACKTRACE)
+        {error,Reason} -> ErrorFun(Anno, Reason, ?STACKTRACE)
     end;
-make_bit_type(_Anno, Size, Type0) -> %Size evaluates to an integer or 'all'
+make_bit_type(Anno, Size, Type0, ErrorFun) -> %Size evaluates to an integer or 'all'
     case erl_bits:set_bit_type(Size, Type0) of
         {ok,Size,Bt} -> {Size,erl_bits:as_list(Bt)};
-        {error,Reason} -> erlang:raise(error, Reason, ?STACKTRACE)
+        {error,Reason} -> ErrorFun(Anno, Reason, ?STACKTRACE)
     end.
+
+default_error(_Anno, Reason, Stacktrace) ->
+    erlang:raise(error, Reason, Stacktrace).
