9d7f67db904ebb1a7eee023062b38eb3c4f2b049
Ingela Anderton Andin <ingela@erlang.org>
2022-02-14 08:47:12 +0100

    ssl: Use non timing dependent reference to assert ditribution mode

--- lib/ssl/src/tls_gen_connection.erl
+++ lib/ssl/src/tls_gen_connection.erl
@@ -121,6 +121,7 @@ initialize_tls_sender(#state{static_env = #static_env{
                              socket_options = SockOpts, 
                              ssl_options = #{renegotiate_at := RenegotiateAt,
                                              key_update_at := KeyUpdateAt,
+                                             erl_dist := ErlDist,
                                              log_level := LogLevel},
                              connection_states = #{current_write := ConnectionWriteState},
                              protocol_specific = #{sender := Sender}}) ->
@@ -128,6 +129,7 @@ initialize_tls_sender(#state{static_env = #static_env{
              role => Role,
              socket => Socket,
              socket_options => SockOpts,
+             erl_dist => ErlDist, 
              trackers => Trackers,
              transport_cb => Transport,
              negotiated_version => Version,
