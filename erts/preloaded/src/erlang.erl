erlang/otp

commit c4d92b48e27aa8ed550d02e3511e2bc288847579

erts: Let erlang:is_alive/0 return true for dynamic node names

erts/preloaded/src/erlang.erl
@@ -1276,7 +1276,7 @@ iolist_to_iovec(_IoListOrBinary) ->
 %% is_alive/0
 -spec is_alive() -> boolean().
 is_alive() ->
-    erlang:nif_error(undefined).
+    net_kernel:is_alive().
 
 %% is_builtin/3
 -spec erlang:is_builtin(Module, Function, Arity) -> boolean() when

lib/kernel/src/net_kernel.erl
@@ -70,6 +70,7 @@
          nodename/0,
 	 protocol_childspecs/0,
 	 epmd_module/0,
+         is_alive/0,
          dist_listen/0]).
 
 -export([disconnect/1, passive_cnct/1]).
@@ -189,6 +190,10 @@ allowed() ->                   request(allowed).
 longnames() ->                 request(longnames).
 
 nodename() ->                  request(nodename).
+
+is_alive() ->
+    whereis(net_kernel) =/= undefined.
+
 
 -spec stop() -> ok | {error, Reason} when
       Reason :: not_allowed | not_found.

erts/emulator/beam/bif.tab
@@ -83,7 +83,6 @@ bif erlang:phash2/1
 bif erlang:phash2/2
 ubif erlang:hd/1
 bif erlang:integer_to_list/1
-bif erlang:is_alive/0
 ubif erlang:length/1
 bif erlang:link/1
 bif erlang:list_to_atom/1

erts/emulator/beam/dist.c
@@ -5921,15 +5921,6 @@ error:
 }
 
 /**********************************************************************/
-/* is_alive() -> Bool */
-
-BIF_RETTYPE is_alive_0(BIF_ALIST_0)
-{
-    Eterm res = erts_is_alive ? am_true : am_false;
-    BIF_RET(res);
-}
-
-/**********************************************************************/
 /* erlang:monitor_node(Node, Bool, Options) -> Bool */
 
 static BIF_RETTYPE

commit 7fd016388ba8facc563e74dc7b9792fafa244fe6

erts: Fix erlang:is_alive/0 to not call net_kernel
Module erlang is preloaded and not allowed to call non-preloaded
modules like net_kernel.

Have been seen to cause spinning beam.smp when piped STDIN
and STDOUT.

Bug introduced by c4d92b48e27aa8ed550d02e3511e2bc288847579
and included in OTP-25.0-rc1.

erts/preloaded/src/erlang.erl
@@ -1276,7 +1276,7 @@ iolist_to_iovec(_IoListOrBinary) ->
 %% is_alive/0
 -spec is_alive() -> boolean().
 is_alive() ->
-    net_kernel:is_alive().
+    erlang:whereis(net_kernel) =/= undefined.
 
 %% is_builtin/3
 -spec erlang:is_builtin(Module, Function, Arity) -> boolean() when

lib/kernel/src/net_kernel.erl
@@ -70,7 +70,6 @@
          nodename/0,
 	 protocol_childspecs/0,
 	 epmd_module/0,
-         is_alive/0,
          get_state/0,
          dist_listen/0]).
 
@@ -191,9 +190,6 @@ allowed() ->                   request(allowed).
 longnames() ->                 request(longnames).
 
 nodename() ->                  request(nodename).
-
-is_alive() ->
-    whereis(net_kernel) =/= undefined.
 
 get_state() ->
     case whereis(net_kernel) of