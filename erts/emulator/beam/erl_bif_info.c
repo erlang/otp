379a1a583b35a5510dcd675c1c5af3304436e7b0
henrythebuilder <henrythebuilder@yahoo.it>
2022-02-25 11:38:30 +0100

    Add 'Erlang OTP Version' to the Erlang shell title row

--- erts/emulator/beam/erl_bif_info.c
+++ erts/emulator/beam/erl_bif_info.c
@@ -150,6 +150,9 @@ static char erts_system_version[] = ("Erlang/OTP " ERLANG_OTP_RELEASE
 #ifdef SHCOPY
 				     " [sharing-preserving]"
 #endif
+#ifdef ERLANG_OTP_VERSION
+             " [otp-version:" ERLANG_OTP_VERSION "]"
+#endif
 				     "\n");
 
 #define ASIZE(a) (sizeof(a)/sizeof(a[0]))
