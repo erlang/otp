be3f0920eb680121ba1a1af9e20e222d7ef93f7b
Micael Karlberg <bmk@erlang.org>
2022-03-07 14:14:35 +0100

    [erts|net] Update preloaded with adapters addresses type(s)
    Add and export adapters addresses types and stuff.
    
    OTP-16464

--- erts/preloaded/src/prim_net.erl
+++ erts/preloaded/src/prim_net.erl
@@ -58,7 +58,26 @@
               ip_interface_info/0,
 
               mib_ip_address_row/0,
-              mib_ip_address_table/0
+              mib_ip_address_table/0,
+
+              adapters_addresses_flags/0,
+              adapters_addresses_args/0,
+              adapter_unicast_dad_state/0,
+              adapter_unicast_prefix_origin/0,
+              adapter_unicast_suffix_origin/0,
+              adapter_unicast_flags/0,
+              adapter_unicast_addr/0,
+              adapter_anycast_flags/0,
+              adapter_anycast_addr/0,
+              adapter_multicast_flags/0,
+              adapter_multicast_addr/0,
+              adapter_dns_server_addr/0,
+              adapter_flags/0,
+              adapter_type/0,
+              adapter_oper_status/0,
+              adapter_prefix/0,
+              adapter_addresses/0,
+              adapters_addresses/0
              ]).
 
 
@@ -139,6 +158,116 @@
                                   reasm_size     := non_neg_integer()}.
 -type mib_ip_address_table() :: [mib_ip_address_row()].
 
+-type adapters_addresses_flags() ::
+        #{skip_unicast                := boolean(),  % default false
+          skip_anycast                := boolean(),  % default true
+          skip_multicast              := boolean(),  % default true
+          skip_dns_server             := boolean(),  % default true
+          skip_friendly_name          := boolean(),  % default true
+          include_prefix              := boolean(),  % default true
+          include_wins_info           := boolean(),  % default false
+          include_gateways            := boolean(),  % default false
+          include_all_interfaces      := boolean(),  % default false
+          include_all_compartments    := boolean(),  % default false
+          include_tunnel_bindingorder := boolean()}. % default false)
+
+%% defaults to:
+%% #{family => unspec,
+%%   flags  => #{skip_unicast                => false
+%%               skip_anycast                => true
+%%               skip_multicast              => true
+%%               skip_dns_server             => true
+%%               skip_friendly_name          => true
+%%               include_prefix              => true
+%%               include_wins_info           => false
+%%               include_gateways            => false
+%%               include_all_interfaces      => false
+%%               include_all_compartments    => false
+%%               include_tunnel_bindingorder => false},
+%%   debug  => false}.
+-type adapters_addresses_args() :: #{family := unspec | inet | inet6,
+                                     flags  := adapters_addresses_flags(),
+                                     debug  := boolean()}.
+
+-type adapter_unicast_dad_state() :: invalid | tentative | duplicate |
+                                     deprecated | preferred |
+                                     integer().
+-type adapter_unicast_prefix_origin() :: other | manual | well_known | dhcp |
+                                         router_advertisement | unchanged |
+                                         integer().
+-type adapter_unicast_suffix_origin() :: other | manual | well_known | dhcp |
+                                         link_layer_address | random |
+                                         unchanged |
+                                         integer().
+-type adapter_unicast_flags() :: #{dns_eligible := boolean(),
+                                   transient    := boolean()}.
+
+-type adapter_unicast_addr() ::
+        #{addr               := socket:address(),
+          dad_state          := adapter_unicast_dad_state(),
+          flags              := adapter_unicast_flags(),
+          prefix_origin      := adapter_unicast_prefix_origin(),
+          suffix_origin      := adapter_unicast_suffix_origin(),
+          lease_lifetime     := non_neg_integer(),
+          preferred_lifetime := non_neg_integer(),
+          valid_lifetime     := non_neg_integer()}.
+
+-type adapter_anycast_flags() :: #{dns_eligible := boolean(),
+                                   transient    := boolean()}.
+
+-type adapter_anycast_addr() :: #{addr  := socket:address(),
+                                  flags := adapter_anycast_flags()}.
+
+-type adapter_multicast_flags() :: #{dns_eligible := boolean(),
+                                     transient    := boolean()}.
+
+-type adapter_multicast_addr() :: #{addr  := socket:address(),
+                                    flags := adapter_multicast_flags()}.
+
+-type adapter_dns_server_addr() :: #{addr := socket:address()}.
+
+-type adapter_flags() :: #{ddns_enabled                          := boolean(),
+                           register_adapter_suffix               := boolean(),
+                           dhcp_v4_enabled                       := boolean(),
+                           receive_only                          := boolean(),
+                           no_multicast                          := boolean(),
+                           ipv6_other_stateful_config            := boolean(),
+                           netbios_over_tcpip_enabled            := boolean(),
+                           ipv4_enabled                          := boolean(),
+                           ipv6_enabled                          := boolean(),
+                           ipv6_managed_address_config_supported := boolean()}.
+
+-type adapter_type() :: other | ethernet_csmacd | iso88025_tokenring |
+                        fddi | ppp | software_loopback | atm | ieee80211 |
+                        tunnel | ieee1394 | ieee80216_wman | wwanpp | wwanpp2 |
+                        integer().
+
+-type adapter_oper_status() :: up | down | testing | dormant | not_present |
+                               lower_layer_down |
+                               integer().
+
+-type adapter_prefix() :: #{addr   := socket:address(),
+                            length := non_neg_integer()}.
+-type adapter_addresses()  ::
+        #{index            := non_neg_integer(),
+          name             := string(),
+          unicast_addrs    := [adapter_unicast_addr()],
+          anycast_addrs    := [adapter_anycast_addr()],
+          multicast_addrs  := [adapter_multicast_addr()],
+          dns_server_addrs := [adapter_dns_server_addr()],
+          dns_suffix       := string(),
+          description      := string(),
+          friendly_name    := string(),
+          phys_addr        := binary(),
+          flags            := adapter_flags(),
+          mtu              := non_neg_integer(),
+          type             := adapter_type(),
+          oper_status      := adapter_oper_status(),
+          zone_indices     := [non_neg_integer()], % Length 16
+          ipv6_index       := non_neg_integer(),
+          prefixes         := [adapter_prefix()]}.
+-type adapters_addresses() :: [adapter_addresses()].
+
 -type name_info_flag()          :: namereqd |
                                    dgram |
                                    nofqdn |
@@ -157,6 +286,56 @@
                                      address  := socket:sockaddr()}.
 -type network_interface_name()  :: string().
 -type network_interface_index() :: non_neg_integer().
+
+
+-define(NO_SKIPS_ALL_INCLUDES,
+        #{skip_unicast                => false,
+          skip_anycast                => false,
+          skip_multicast              => false,
+          skip_dns_server             => false,
+          skip_friendly_name          => false,
+          include_prefix              => true,
+          include_wins_info           => true,
+          include_gateways            => true,
+          include_all_interfaces      => true,
+          include_all_compartments    => true,
+          include_tunnel_bindingorder => true}).
+-define(ALL_SKIPS_NO_INCLUDES,
+        #{skip_unicast                => true,
+          skip_anycast                => true,
+          skip_multicast              => true,
+          skip_dns_server             => true,
+          skip_friendly_name          => true,
+          include_prefix              => false,
+          include_wins_info           => false,
+          include_gateways            => false,
+          include_all_interfaces      => false,
+          include_all_compartments    => false,
+          include_tunnel_bindingorder => false}).
+-define(NO_SKIPS_NO_INCLUDES,
+        #{skip_unicast                => false,
+          skip_anycast                => false,
+          skip_multicast              => false,
+          skip_dns_server             => false,
+          skip_friendly_name          => false,
+          include_prefix              => false,
+          include_wins_info           => false,
+          include_gateways            => false,
+          include_all_interfaces      => false,
+          include_all_compartments    => false,
+          include_tunnel_bindingorder => false}).
+-define(ALL_SKIPS_ALL_INCLUDES,
+        #{skip_unicast                => true,
+          skip_anycast                => true,
+          skip_multicast              => true,
+          skip_dns_server             => true,
+          skip_friendly_name          => true,
+          include_prefix              => true,
+          include_wins_info           => true,
+          include_gateways            => true,
+          include_all_interfaces      => true,
+          include_all_compartments    => true,
+          include_tunnel_bindingorder => true}).
 
 
 %% ===========================================================================
@@ -302,11 +481,25 @@ getifaddrs(Extra) when is_map(Extra) ->
 %%
 
 -spec get_adapters_addresses(Args) -> {ok, Addrs} | {error, Reason} when
-      %% #{family := unspec (default) | inet | inet6, debug := boolean()}
-      Args   :: map(),
-      Addrs  :: term(), % adapters_addresses(),
+      Args   :: adapters_addresses_args(),
+      Addrs  :: adapters_addresses(),
       Reason :: term().
 
+get_adapters_addresses(default) ->
+    Args = #{},
+    get_adapters_addresses(Args);
+get_adapters_addresses(no_skips_all_includes) ->
+    Args = #{flags => ?NO_SKIPS_ALL_INCLUDES},
+    get_adapters_addresses(Args);
+get_adapters_addresses(no_skips_no_includes) ->
+    Args = #{flags => ?NO_SKIPS_NO_INCLUDES},
+    get_adapters_addresses(Args);
+get_adapters_addresses(all_skips_no_includes) ->
+    Args = #{flags => ?ALL_SKIPS_NO_INCLUDES},
+    get_adapters_addresses(Args);
+get_adapters_addresses(all_skips_all_includes) ->
+    Args = #{flags => ?ALL_SKIPS_ALL_INCLUDES},
+    get_adapters_addresses(Args);
 get_adapters_addresses(Args) when is_map(Args) ->
     nif_get_adapters_addresses(Args).
 
