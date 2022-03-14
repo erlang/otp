9de5e230ca6ab13181560990123d6fd726967d1e
Raimo Niskanen <raimo@erlang.org>
2022-03-14 15:25:27 +0100

    Implement dummy generator to measure plug-in overhead

--- lib/stdlib/test/rand_SUITE.erl
+++ lib/stdlib/test/rand_SUITE.erl
@@ -1,7 +1,7 @@
 %%
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 2000-2020. All Rights Reserved.
+%% Copyright Ericsson AB 2000-2022. All Rights Reserved.
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -1090,7 +1090,7 @@ do_measure(_Config) ->
                     end,
                     State)
           end,
-          Algs),
+          Algs ++ [dummy]),
     _ =
         measure_1(
           fun (_) -> 0 end,
