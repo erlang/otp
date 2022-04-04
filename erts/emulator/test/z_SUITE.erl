976a8d7c1529b6dc083898304d306d75bba054f6
Lukas Larsson <lukas@erlang.org>
2022-04-04 11:45:15 +0200

    erts: Fix z_SUITE inet_gethost detection

--- erts/emulator/test/z_SUITE.erl
+++ erts/emulator/test/z_SUITE.erl
@@ -330,8 +330,9 @@ check_io_debug_test() ->
 has_gethost() ->
     has_gethost(erlang:ports()).
 has_gethost([P|T]) ->
-    case erlang:port_info(P, name) of
-        {name,"inet_gethost"++_} ->
+    {name, Name} = erlang:port_info(P, name),
+    case filename:basename(Name) of
+        "inet_gethost"++_ ->
             true;
         _ ->
             has_gethost(T)
