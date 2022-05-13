6e2453d004b247e39a2f32205aaee8b887c4f397
Lars Thorsen <lars@erlang.org>
2022-05-13 09:48:43 +0200

    Add new nifs to nif definition list

--- lib/crypto/src/crypto.erl
+++ lib/crypto/src/crypto.erl
@@ -152,7 +152,8 @@
        engine_ctrl_cmd_strings_nif/3, engine_register_nif/2,
        engine_unregister_nif/2, engine_add_nif/1, engine_remove_nif/1,
        engine_get_first_nif/0, engine_get_next_nif/1, engine_get_id_nif/1,
-       engine_get_name_nif/1, engine_get_all_methods_nif/0
+       engine_get_name_nif/1, engine_get_all_methods_nif/0,
+       ensure_engine_loaded_nif/3, ensure_engine_unloaded_nif/2
       ]).
 
 -export_type([ %% A minimum exported: only what public_key needs.
