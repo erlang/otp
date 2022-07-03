a27297b7b9319ca319abed8900b7ab3afed54076
Lukas Larsson <lukas@erlang.org>
2022-07-03 18:47:06 +0200

    gh: Add installation of old releases for compat test

--- .github/dockerfiles/init.sh
+++ .github/dockerfiles/init.sh
@@ -8,4 +8,6 @@ sudo service postgresql start
 
 sudo -E bash -c "apt-get update && apt-get install -y linux-tools-common linux-tools-generic linux-tools-`uname -r`"
 
+PATH=$PATH:$(ls -1d /usr/local/lib/erlang-*/bin | tr '\n' ':')
+
 exec /bin/bash -c "$1"
