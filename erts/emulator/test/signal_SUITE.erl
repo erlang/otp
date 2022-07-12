04c913d6ce9eb8e9aed0e4b9f8c3d026861530e9
Rickard Green <rickard@erlang.org>
2022-07-12 16:38:17 +0200

    [erts] Don't send exit signal due to a link with ongoing unlink op

--- erts/emulator/test/signal_SUITE.erl
+++ erts/emulator/test/signal_SUITE.erl
@@ -35,7 +35,8 @@
 
 % Test cases
 -export([xm_sig_order/1,
-         kill2killed/1]).
+         kill2killed/1,
+         unlink_exit/1]).
 
 init_per_testcase(Func, Config) when is_atom(Func), is_list(Config) ->
     [{testcase, Func}|Config].
@@ -55,7 +56,8 @@ suite() ->
 
 all() -> 
     [xm_sig_order,
-     kill2killed].
+     kill2killed,
+     unlink_exit].
 
 
 %% Test that exit signals and messages are received in correct order
@@ -66,7 +68,6 @@ xm_sig_order(Config) when is_list(Config) ->
     repeat(fun () -> xm_sig_order_test(RNode) end, 1000),
     stop_node(RNode),
     ok.
-    
 
 xm_sig_order_test(Node) ->
     P = spawn(Node, fun () -> xm_sig_order_proc() end),
@@ -152,6 +153,102 @@ spawn_link_line(NodeA, NodeB, Type, N, Tester) ->
                        receive after infinity -> ok end
                end).
 
+    
+unlink_exit(Config) when is_list(Config) ->
+    %% OTP-18177
+    %%
+    %% This bug is theoretically possible, at least in the
+    %% node local scenario, but more or less undetectable and
+    %% quite harmless when it hits. A process A (the child in
+    %% the testcase) could get actual exit reason of another
+    %% process B (the parent in the testcase) when it should
+    %% have gotten 'noproc' as exit reason. This can happen if
+    %% 1. B unlinks A
+    %% 2. B begin terminating before it has received an unlink
+    %%    ack from A
+    %% 3. A links to B after it has received the unlink signal
+    %%
+    %% This testcase hammers on the above scenario, but I have
+    %% not seen it fail yet though when the bug is present...
+    repeat(fun unlink_exit_test/0, 1000).
+
+unlink_exit_test() ->
+    Tester = self(),
+    ChildFun =
+        fun () ->
+                process_flag(trap_exit, true),
+                Tester ! {child, self()},
+                Parent = receive {tester_parent, Tester, Pid} -> Pid end,
+                Parent ! {go, self()},
+                busy_wait_until(fun () ->
+                                        receive {go, Parent} -> true
+                                        after 0 -> false
+                                        end
+                                end),
+                IsAlive = erlang:is_process_alive(Parent),
+                try
+                    link(Parent),
+                    case IsAlive of
+                        false ->
+                            receive
+                                {'EXIT', Parent, noproc} ->
+                                    exit(ok);
+                                {'EXIT', Parent, R1} ->
+                                    exit({not_alive_unexpected_exit_reason, R1})
+                            after 1000 ->
+                                    exit(not_alive_missing_exit)
+                            end;
+                        true ->
+                            receive
+                                {'EXIT', Parent, R2} when R2 == noproc;
+                                                          R2 == bye ->
+                                    exit(ok);
+                                {'EXIT', Parent, R2} ->
+                                    exit({alive_unexpected_exit_reason, R2})
+                            after 1000 ->
+                                    exit(alive_missing_exit)
+                            end
+                    end
+                catch error:noproc ->
+                        receive
+                            {'EXIT', Parent, _} = X0 ->
+                                exit({unexpected_exit, X0})
+                        after 1000 ->
+                                exit(ok)
+                        end
+                end
+        end,
+    {Parent, PMon} = spawn_opt(fun () ->
+                                       %% Work to do when terminating in order
+                                       %% to increase the likelyhood of the
+                                       %% bug triggering (if present)...
+                                       T = ets:new(x,[]),
+                                       ets:insert(T, lists:map(fun (I) ->
+                                                                       {I,I}
+                                                               end,
+                                                               lists:seq(1,10000))),
+
+                                       Child = spawn_opt(ChildFun,
+                                                         [{priority, high},
+                                                          link]),
+                                       receive {go, Child} -> ok end,
+                                       unlink(Child),
+                                       Child ! {go, self()},
+                                       exit(bye)
+                               end, [{priority, high}, monitor]),
+    Child = receive {child, Chld} -> Chld end,
+    CMon = erlang:monitor(process, Child),
+    Child ! {tester_parent, Tester, Parent},
+    receive
+        {'DOWN', PMon, process, Parent, bye} ->
+            ok
+    end,
+    receive
+        {'DOWN', CMon, process, Child, ok} ->
+            ok;
+        {'DOWN', CMon, process, Child, ChildReason} ->
+            ct:fail(ChildReason)
+    end.
 
 %%
 %% -- Internal utils --------------------------------------------------------
@@ -162,6 +259,12 @@ repeat(_Fun, N) when is_integer(N), N =< 0 ->
 repeat(Fun, N) when is_integer(N)  ->
     Fun(),
     repeat(Fun, N-1).
+
+busy_wait_until(Fun) ->
+    case catch Fun() of
+        true -> ok;
+        _ -> busy_wait_until(Fun)
+    end.
 
 start_node(Config) ->
     Name = list_to_atom(atom_to_list(?MODULE)
