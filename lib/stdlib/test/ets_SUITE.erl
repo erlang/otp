c6572d02a8ee433128cd4347c5a09f5aebb34cd6
Robin Morisset <rmorisset@fb.com>
2022-09-20 10:39:07 +0200

    erts: Optimize ETS named table lookup
    The ETS meta name table was previously guarded
    by (striped) read-write locks.
    
    With this patch, there are still (regular) locks for protecting
    writing threads from each other, but threads that only need to lookup
    table from name (the overwhelming majority) no longer need to use any
    lock. Instead we now rely on lightweight atomic synchronization, and on
    the thread_progress functionality for memory reclamation.
    
    This patch was motivated by noticing that heavyweight
    services can spend several % of CPU time in db_get_table_aux.
    A trial in production confirmed that this patch roughly
    halves the time spent in that function.
    
    New test case ets_SUITE:racy_rename.

--- lib/stdlib/test/ets_SUITE.erl
+++ lib/stdlib/test/ets_SUITE.erl
@@ -114,6 +114,7 @@
 -export([ms_excessive_nesting/1]).
 -export([error_info/1]).
 -export([bound_maps/1]).
+-export([racy_rename/1, racy_rename_writer/3]).
 
 -export([init_per_testcase/2, end_per_testcase/2]).
 %% Convenience for manual testing
@@ -199,7 +200,8 @@ all() ->
      test_decentralized_counters_setting,
      ms_excessive_nesting,
      error_info,
-     bound_maps
+     bound_maps,
+     racy_rename
     ].
 
 
@@ -7970,6 +7972,38 @@ types_do(Opts) ->
     ets:delete(T),
     verify_etsmem(EtsMem).
 
+
+racy_rename(_Config) ->
+    EtsMem = etsmem(),
+    ets:new(name_a, [named_table, public, {keypos, 1}]),
+    % A writer process does rename(A->B), insert(B, {key, value}), delete(B, key), rename(B->A) in a loop
+    % A reader process does lookup(A, key) in a loop. We want to make sure that it never sees value.
+    Parent = self(),
+    WriterIterations=10000,
+    Reader = spawn_link(fun () -> racy_rename_reader() end),
+    spawn_link(?MODULE, racy_rename_writer, [Parent, Reader, WriterIterations]),
+    receive done -> ok end,
+    true=ets:delete(name_a),
+    verify_etsmem(EtsMem).
+
+racy_rename_writer(Parent, Reader, 0) ->
+    exit(Reader, normal),
+    Parent ! done;
+racy_rename_writer(Parent, Reader, Iterations) ->
+    ets:rename(name_a, name_b),
+    ets:insert(name_b, {key, value}),
+    ets:delete(name_b, key),
+    ets:rename(name_b, name_a),
+    racy_rename_writer(Parent, Reader, Iterations - 1).
+
+racy_rename_reader() ->
+    try ets:lookup(name_a, key) of
+        [] -> ok;
+        Result -> exit({racy_lookup_result, Result})
+    catch
+        _:_ -> ok
+    end,
+    racy_rename_reader().
 
 %% OTP-9932: Memory overwrite when inserting large integers in compressed bag.
 %% Will crash with segv on 64-bit opt if not fixed.
