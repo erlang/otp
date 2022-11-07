abad0491f19adbc08c437780484ff4b2babcc22d
Björn Gustavsson <bjorn@erlang.org>
2022-11-07 08:51:53 +0100

    beam_ssa_codegen: Remove incorrect spec for is_gc_bif/2
    The spec is incorrect, because the arguments in the list is of
    type `cg_value()`.
    
    Because this function is not exported, we will remove spec instead
    of correcting it.

--- lib/compiler/src/beam_ssa_codegen.erl
+++ lib/compiler/src/beam_ssa_codegen.erl
@@ -2210,8 +2210,6 @@ local_func_label(Key, #cg{functable=Map}=St0) ->
 %% is_gc_bif(Name, Args) -> true|false.
 %%  Determines whether the BIF Name/Arity might do a GC.
 
--spec is_gc_bif(atom(), [beam_ssa:value()]) -> boolean().
-
 is_gc_bif(hd, [_]) -> false;
 is_gc_bif(tl, [_]) -> false;
 is_gc_bif(self, []) -> false;
