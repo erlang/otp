aec4d555f1c37ec2f90507d098322171940c5f56
Michael Davis <mcarsondavis@gmail.com>
2022-11-08 13:20:08 -0600

    beam_lib: Fix spec for all_chunks/1
    `beam_lib:all_chunks/1` returns the module contained in the given beam
    file/binary rather than the `beam_lib` atom if successful. Also see
    `scan_beam/2` which originally returns the module.

--- lib/stdlib/src/beam_lib.erl
+++ lib/stdlib/src/beam_lib.erl
@@ -156,7 +156,7 @@ chunks(File, Chunks, Options) ->
     catch Error -> Error end.
 
 -spec all_chunks(beam()) ->
-           {'ok', 'beam_lib', [{chunkid(), dataB()}]} | {'error', 'beam_lib', info_rsn()}.
+           {'ok', module(), [{chunkid(), dataB()}]} | {'error', 'beam_lib', info_rsn()}.
 
 all_chunks(File) ->
     read_all_chunks(File).
