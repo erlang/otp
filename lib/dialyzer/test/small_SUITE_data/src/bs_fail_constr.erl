ac41c1f941d83a959cf67c9b056d3f74d7ef9b06
Björn Gustavsson <bjorn@erlang.org>
2022-11-17 09:12:34 +0100

    Eliminate crash in dialyzer_typesig
    Closes #6473

--- lib/dialyzer/test/small_SUITE_data/src/bs_fail_constr.erl
+++ lib/dialyzer/test/small_SUITE_data/src/bs_fail_constr.erl
@@ -1,6 +1,6 @@
 -module(bs_fail_constr).
 
--export([w1/1, w2/1, w3/1, w4/1, bad_size_1/1, bad_size_2/1]).
+-export([w1/1, w2/1, w3/1, w4/1, bad_size_1/1, bad_size_2/1, bad_size_3/1]).
 
 w1(V) when is_float(V) ->
   <<V/integer>>.
@@ -24,3 +24,8 @@ bad_size_2(Bin) ->
         <<X:Size>> ->
             ok
     end.
+
+%% GH-6473
+bad_size_3(X) when X; <<X:X>> ->
+    bad_size_3(X),
+    X.
