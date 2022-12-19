c2d10bd23f864b34cbb0d0bfe0f2f4d4f9b08afb
Micael Karlberg <bmk@erlang.org>
2022-12-19 17:46:41 +0100

    [erts|esock] Removed duplicated defs from the source
    The stuff that has been added to the prim_socket_int.h
    was removed from the source file.
    
    OTP-18029

--- erts/emulator/nifs/common/prim_socket_nif.c
+++ erts/emulator/nifs/common/prim_socket_nif.c
@@ -384,6 +384,7 @@ static void (*esock_sctp_freepaddrs)(struct sockaddr *addrs) = NULL;
 #include "socket_tarray.h"
 #include "socket_int.h"
 #include "socket_util.h"
+#include "prim_socket_int.h"
 #include "prim_file_nif_dyncall.h"
 
 #if defined(ERTS_INLINE)
@@ -410,41 +411,6 @@ static void (*esock_sctp_freepaddrs)(struct sockaddr *addrs) = NULL;
 /* Counters and stuff (Don't know where to send this stuff anyway) */
 #define ESOCK_NIF_IOW_DEFAULT FALSE
 
-
-#ifdef __WIN32__
-
-//#define INVALID_HANDLE        from Windows header file
-//typedef void   *HANDLE        from Windows header file
-//#define INVALID_SOCKET        from Windows header file
-//typedef void   *SOCKET        from Windows header file
-#define INVALID_EVENT NULL
-
-#else
-
-#define INVALID_HANDLE (-1)
-typedef int HANDLE;
-#define INVALID_SOCKET (-1)
-typedef int SOCKET; /* A subset of HANDLE */
-#define INVALID_EVENT INVALID_HANDLE
-
-#endif
-
-
-/* ==============================================================================
- * The ESOCK_IS_ERROR macro below is used for portability reasons.
- * While POSIX specifies that errors from socket-related system calls
- * should be indicated with a -1 return value, some users have experienced
- * non-Windows OS kernels that return negative values other than -1.
- * While one can argue that such kernels are technically broken, comparing
- * against values less than 0 covers their out-of-spec return values without
- * imposing incorrect semantics on systems that manage to correctly return -1
- * for errors, thus increasing Erlang's portability.
- */
-#ifdef __WIN32__
-#define ESOCK_IS_ERROR(val) ((val) == INVALID_SOCKET)
-#else
-#define ESOCK_IS_ERROR(val) ((val) < 0)
-#endif
 
 
 /* *** Misc macros and defines *** */
@@ -522,24 +488,6 @@ typedef int SOCKET; /* A subset of HANDLE */
 #define ESOCK_DESC_PATTERN_CREATED 0x03030303
 #define ESOCK_DESC_PATTERN_DTOR    0xC0C0C0C0
 
-/*
-typedef union {
-    struct {
-        // 0 = not open, 1 = open
-        unsigned int open:1;
-        // 0 = not conn, 1 = connecting, 2 = connected
-        unsigned int connect:2;
-        // unsigned int connecting:1;
-        // unsigned int connected:1;
-        // 0 = not listen, 1 = listening, 2 = accepting
-        unsigned int listen:2;
-        // unsigned int listening:1;
-        // unsigned int accepting:1;
-        / * Room for more... * /
-    } flags;
-    unsigned int field; // Make it easy to reset all flags...
-} SocketState;
-*/
 
 /*----------------------------------------------------------------------------
  * Interface constants.
@@ -1068,239 +1016,20 @@ static unsigned long one_value  = 1;
 #endif /* #ifdef __WIN32__  #else */
 
 
-/* We can use the IPv4 def for this since the beginning
- * is the same for INET and INET6 */
-#define which_address_port(sap)		     \
-  ((((sap)->in4.sin_family == AF_INET) ||  \
-    ((sap)->in4.sin_family == AF_INET6)) ? \
-   ((sap)->in4.sin_port) : -1)
-
-
-typedef struct {
-    ErlNifMonitor mon;
-    BOOLEAN_T     isActive;
-} ESockMonitor;
-
-typedef struct {
-    ErlNifPid    pid; // PID of the requesting process
-    ESockMonitor mon; // Monitor to the requesting process
-
-    /* We need an environment for the copy of the ref we store here.
-     * We will also use this environment for any messages we send
-     * (with the ref in it). Such as the select message (used in the 
-     * select call) or the abort message.
-     */
-    ErlNifEnv*   env;
-    ERL_NIF_TERM ref; // The (unique) reference (ID) of the request
-} ESockRequestor;
-
-typedef struct{
-    // Holding the socket level 'otp' option 'meta' term
-    ErlNifEnv* env;
-    ERL_NIF_TERM ref;
-} ESockMeta;
-
-typedef struct esock_request_queue_element {
-    struct esock_request_queue_element* nextP;
-    ESockRequestor                      data;
-} ESockRequestQueueElement;
-
-typedef struct {
-    ESockRequestQueueElement* first;
-    ESockRequestQueueElement* last;
-} ESockRequestQueue;
-
-
-/*** The point of this is primarily testing ***/
-/*
-#if defined(ESOCK_COUNTER_SIZE)
-#undef ESOCK_COUNTER_SIZE
-// #define ESOCK_COUNTER_SIZE 16
-// #define ESOCK_COUNTER_SIZE 24
-// #define ESOCK_COUNTER_SIZE 32
-// #define ESOCK_COUNTER_SIZE 48
-// #define ESOCK_COUNTER_SIZE 64
-
-#endif
-*/
-
-#if ESOCK_COUNTER_SIZE == 16
-
-typedef Uint16                   ESockCounter;
-#define ESOCK_COUNTER_MAX        ((ESockCounter) 0xFFFF)
-#define MKCNT(ENV, CNT)          MKUI((ENV), (CNT))
-#define MKCT(ENV, TAG, CNT)      MKT2((ENV), (TAG), MKCNT((CNT)))
-#define ESOCK_COUNTER_FORMAT_STR "%u"
-
-#elif ESOCK_COUNTER_SIZE == 24
-
-typedef Uint32                   ESockCounter;
-#define ESOCK_COUNTER_MAX        ((ESockCounter) 0xFFFFFF)
-#define MKCNT(ENV, CNT)          MKUI((ENV), (CNT))
-#define MKCT(ENV, TAG, CNT)      MKT2((ENV), (TAG), MKCNT((ENV), (CNT)))
-#define ESOCK_COUNTER_FORMAT_STR "%lu"
-
-#elif ESOCK_COUNTER_SIZE == 32
-
-typedef Uint32 ESockCounter;
-#define ESOCK_COUNTER_MAX        (~((ESockCounter) 0))
-#define MKCNT(ENV, CNT)          MKUI((ENV), (CNT))
-#define MKCT(ENV, TAG, CNT)      MKT2((ENV), (TAG), MKCNT((ENV), (CNT)))
-#define ESOCK_COUNTER_FORMAT_STR "%lu"
-
-#elif ESOCK_COUNTER_SIZE == 48
-
-typedef Uint64                   ESockCounter;
-#define ESOCK_COUNTER_MAX        ((ESockCounter) 0xFFFFFFFFFFFF)
-#define MKCNT(ENV, CNT)          MKUI64((ENV), (CNT))
-#define MKCT(ENV, TAG, CNT)      MKT2((ENV), (TAG), MKCNT((ENV), (CNT)))
-#define ESOCK_COUNTER_FORMAT_STR "%llu"
-
-#elif ESOCK_COUNTER_SIZE == 64
-
-typedef Uint64                   ESockCounter;
-#define ESOCK_COUNTER_MAX        (~((ESockCounter) 0))
-#define MKCNT(ENV, CNT)          MKUI64((ENV), (CNT))
-#define MKCT(ENV, TAG, CNT)      MKT2((ENV), (TAG), MKCNT((ENV), (CNT)))
-#define ESOCK_COUNTER_FORMAT_STR "%llu"
-
-#else
-
-#error "Invalid counter size"
-
-#endif
-
-// static const ESockCounter esock_counter_max = ESOCK_COUNTER_MAX;
-
 #ifdef HAVE_SENDFILE
 
-typedef struct {
-    ESockCounter       cnt;     // Calls to OS sendfile()
-    ESockCounter       byteCnt; // Bytes sent with sendfile
-    ESockCounter       fails;   // Failed sendfile operations
-    ESockCounter       max;     // Largest sendfile operation
-    ESockCounter       maxCnt;  // Counter for ="=
-    ESockCounter       pkg;     // Sendfile chunks
-    ESockCounter       pkgMax;  // Largest sendfile chunk
-    ESockCounter       tries;   // Started sendfile operations
-    ESockCounter       waits;   // Select's during sendfile
-} ESockSendfileCounters;
 static ESockSendfileCounters initESockSendfileCounters =
     {0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 #endif
 
 
-typedef struct {
-    /* 
-     * +++ This is a way to, possibly, detect memory overrides "and stuff" +++
-     *
-     * We have two patterns. One is set when the descriptor is created
-     * (allocated) and one is set when the descriptor is dtor'ed.
-     */
-    Uint32             pattern;
-
-    /* +++ Stuff "about" the socket +++ */
-
-    /* "Constant" - set when socket is created and never changed */
-    int                domain;
-    int                type;
-    int                protocol;
-
-    /* The state is partly for debugging, decisions are made often
-     * based on other variables.  The state is divided in
-     * a readState half and a writeState half that can be
-     * OR:ed together to create the complete state.
-     * The halves are locked by their corresponding lock.
-     */
-
-    /* +++ Write stuff +++ */
-    ErlNifMutex*       writeMtx;
-    /**/
-    unsigned int       writeState; // For debugging
-    ESockRequestor     currentWriter;
-    ESockRequestor*    currentWriterP; // NULL or &currentWriter
-    ESockRequestQueue  writersQ;
-    ESockCounter       writePkgCnt;
-    ESockCounter       writePkgMax;
-    ESockCounter       writePkgMaxCnt;
-    ESockCounter       writeByteCnt;
-    ESockCounter       writeTries;
-    ESockCounter       writeWaits;
-    ESockCounter       writeFails;
-#ifdef HAVE_SENDFILE
-    HANDLE                 sendfileHandle;
-    ESockSendfileCounters* sendfileCountersP;
-#endif
-    /* +++ Connector +++ */
-    ESockRequestor     connector;
-    ESockRequestor*    connectorP; // NULL or &connector
-    /* +++ Config stuff +++ */
-    size_t             wCtrlSz; // Write control buffer size
-    ESockMeta          meta;    // Level 'otp' option 'meta' term
-
-    /* +++ Read stuff +++ */
-    ErlNifMutex*       readMtx;
-    /**/
-    unsigned int       readState; // For debugging
-    ESockRequestor     currentReader;
-    ESockRequestor*    currentReaderP; // NULL or &currentReader
-    ESockRequestQueue  readersQ;
-    ErlNifBinary       rbuffer;      // DO WE NEED THIS
-    Uint32             readCapacity; // DO WE NEED THIS
-    ESockCounter       readPkgCnt;
-    ESockCounter       readPkgMax;
-    ESockCounter       readPkgMaxCnt;
-    ESockCounter       readByteCnt;
-    ESockCounter       readTries;
-    ESockCounter       readWaits;
-    ESockCounter       readFails;
-    /* +++ Accept stuff +++ */
-    ESockRequestor     currentAcceptor;
-    ESockRequestor*    currentAcceptorP; // NULL or &currentAcceptor
-    ESockRequestQueue  acceptorsQ;
-    ESockCounter       accSuccess;
-    ESockCounter       accTries;
-    ESockCounter       accWaits;
-    ESockCounter       accFails;
-    /* +++ Config stuff +++ */
-    size_t             rBufSz;  // Read buffer size (when data length = 0)
-    /* rNum and rNumCnt are used (together with rBufSz) when calling the recv 
-     * function with the Length argument set to 0 (zero).
-     * If rNum is 0 (zero), then rNumCnt is not used and only *one* read will
-     * be done. Also, when get'ing the value of the option (rcvbuf) with 
-     * getopt, the value will be reported as an integer. If the rNum has a 
-     * value greater then 0 (zero), then it will instead be reported as {N, BufSz}.
-     */
-    unsigned int       rNum;    // recv: Number of reads using rBufSz
-    unsigned int       rNumCnt; // recv: Current number of reads (so far)
-    size_t             rCtrlSz; // Read control buffer size
-
-    /* Locked by readMtx and writeMtx combined for writing,
-     * which means only one of them is required for reading
-     */
-    /* +++ Close stuff +++ */
-    ErlNifPid          closerPid;
-    ESockMonitor       closerMon;
-    ErlNifEnv*         closeEnv;
-    ERL_NIF_TERM       closeRef;
-    /* +++ Inform On (counter) Wrap +++ */
-    BOOLEAN_T          iow;
-    /* +++ Controller (owner) process +++ */
-    ErlNifPid          ctrlPid;
-    ESockMonitor       ctrlMon;
-    /* +++ The actual socket +++ */
-    SOCKET             sock;
-    ErlNifEvent        event;
-    SOCKET             origFD; // A 'socket' created from this FD
-    BOOLEAN_T          closeOnClose; // Have we dup'ed or not
-    /* +++ The dbg flag for SSDBG +++ */
-    BOOLEAN_T          dbg;
-    BOOLEAN_T          useReg;
-
-    /* Lock order: readMtx, writeMtx, cntMtx
-     */
-} ESockDescriptor;
+/* We can use the IPv4 def for this since the beginning
+ * is the same for INET and INET6 */
+#define which_address_port(sap)		     \
+  ((((sap)->in4.sin_family == AF_INET) ||  \
+    ((sap)->in4.sin_family == AF_INET6)) ? \
+   ((sap)->in4.sin_port) : -1)
 
 
 /* Global stuff.
@@ -1346,6 +1075,7 @@ typedef struct {
     //
     BOOLEAN_T    sockDbg;
 } ESockData;
+
 
 
 /* ----------------------------------------------------------------------
