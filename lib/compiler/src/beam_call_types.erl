6013936d8399a8b0e9fd639089a9a93aa192e28f
John Högberg <john@erlang.org>
2023-01-02 15:43:26 +0100

    beam_call_types: Handle more boolean ops in will_succeed/3

--- lib/compiler/src/beam_call_types.erl
+++ lib/compiler/src/beam_call_types.erl
@@ -139,8 +139,14 @@ will_succeed(erlang, length, [Arg]) ->
     succeeds_if_type(Arg, proper_list());
 will_succeed(erlang, map_size, [Arg]) ->
     succeeds_if_type(Arg, #t_map{});
+will_succeed(erlang, 'and', [_, _]=Args) ->
+    succeeds_if_types(Args, beam_types:make_boolean());
 will_succeed(erlang, 'not', [Arg]) ->
     succeeds_if_type(Arg, beam_types:make_boolean());
+will_succeed(erlang, 'or', [_, _]=Args) ->
+    succeeds_if_types(Args, beam_types:make_boolean());
+will_succeed(erlang, 'xor', [_, _]=Args) ->
+    succeeds_if_types(Args, beam_types:make_boolean());
 will_succeed(erlang, setelement, [Pos, Tuple0, _Value]) ->
     PosRange = #t_integer{elements={1,?MAX_TUPLE_SIZE}},
     case {meet(Pos, PosRange), meet(Tuple0, #t_tuple{size=1})} of
