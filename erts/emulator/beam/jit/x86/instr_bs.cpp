stritzinger/otp-jhogberg

JIT: Optimize matching of utf8 segments
Currently, matching a Unicode code point as UTF-8 in the binary syntax
is done by calling a helper function written in C.

Optimize this by inlining all code for handling the ASCII subset of
Unicode (code points less than 128). Handle other code points by
calling a helper fragment implemented in native code. Calling a
fragment has slightly less overhead than calling a function
implemented in C.

Co-authored-by: John Högberg <john@erlang.org>

erts/emulator/beam/jit/x86/instr_bs.cpp
@@ -1018,19 +1018,359 @@ void BeamModuleAssembler::emit_i_bs_put_utf8(const ArgLabel &Fail,
     }
 }
 
+/*
+ * ARG1 = pointer to match state
+ * ARG2 = position in binary in bits
+ * ARG3 = base pointer to binary data
+ * RET = number of bits left in binary (< 32)
+ *
+ * See the comment for emit_bs_get_utf8_shared() for details about the
+ * return value.
+ */
+void BeamGlobalAssembler::emit_bs_get_utf8_short_shared() {
+    const int position_offset = offsetof(ErlBinMatchBuffer, offset);
+
+    const x86::Gp ctx = ARG1;
+    const x86::Gp bin_position = ARG2;
+    const x86::Gp bin_base = ARG3;
+
+    Label at_least_one = a.newLabel();
+    Label two = a.newLabel();
+    Label three_or_more = a.newLabel();
+    Label four = a.newLabel();
+    Label five = a.newLabel();
+    Label read_done = a.newLabel();
+    Label ascii = a.newLabel();
+
+    /* Calculate the number of bytes remaining in the binary and error
+     * out if less than one. */
+    a.shr(RETd, imm(3));
+    a.test(RETd, RETd);
+    a.short_().jne(at_least_one);
+
+    /* ZF is is already set. */
+    a.ret();
+
+    a.bind(at_least_one);
+
+    /* Save number of bytes remaining in binary. */
+    a.mov(ARG5d, RETd);
+
+    /* If the position in the binary is not byte-aligned, we'll need
+     * to read one more byte. */
+    a.test(bin_position, imm(7));
+    a.setne(ARG4.r8());
+    a.add(RETb, ARG4.r8());
+
+    /* Save original position in bits and set up byte offset for
+     * reading. */
+    a.push(bin_position);
+    a.shr(bin_position, imm(3));
+
+    a.cmp(RETb, imm(2));
+    a.short_().je(two);
+    a.short_().ja(three_or_more);
+
+    /* Read one byte (always byte-aligned). */
+    a.mov(RETb, x86::byte_ptr(bin_base, bin_position));
+    a.movzx(RETd, RETb);
+    a.short_().jmp(read_done);
+
+    /* Read two bytes. */
+    a.bind(two);
+    a.mov(RET.r16(), x86::word_ptr(bin_base, bin_position));
+    a.movzx(RETd, RET.r16());
+    a.short_().jmp(read_done);
+
+    a.bind(three_or_more);
+    a.cmp(RETb, imm(4));
+    a.short_().je(four);
+    a.short_().ja(five);
+
+    /* Read three bytes. */
+    a.mov(RET.r8(), x86::byte_ptr(bin_base, bin_position, 0, 2));
+    a.movzx(RETd, RETb);
+    a.shl(RETd, imm(16));
+    a.mov(RET.r16(), x86::word_ptr(bin_base, bin_position));
+    a.short_().jmp(read_done);
+
+    /* Read four bytes (always unaligned). */
+    a.bind(four);
+    a.mov(RETd, x86::dword_ptr(bin_base, bin_position));
+    a.short_().jmp(read_done);
+
+    /* Read five bytes (always unaligned). */
+    a.bind(five);
+    a.mov(RETd, x86::dword_ptr(bin_base, bin_position));
+    a.mov(ARG4.r8(), x86::byte_ptr(bin_base, bin_position, 0, 4));
+    a.movzx(ARG4d, ARG4.r8());
+    a.shl(ARG4, imm(32));
+    a.or_(RET, ARG4);
+
+    /* Handle the bytes read. */
+    a.bind(read_done);
+    a.pop(bin_position);
+    a.bswap(RET);
+
+    if (x86::rcx == ctx) {
+        a.push(x86::rcx);
+    }
+    a.mov(x86::ecx, bin_position.r32());
+    a.and_(x86::cl, imm(7));
+    a.shl(RET, x86::cl);
+
+    /* Calculate a byte mask and zero out trailing garbage. */
+    a.mov(x86::cl, 64);
+    a.shl(ARG5, imm(3));
+    a.sub(x86::cl, ARG5.r8());
+    mov_imm(ARG5, -1);
+    a.shl(ARG5, x86::cl);
+    a.and_(RET, ARG5);
+
+    if (x86::rcx == ctx) {
+        a.pop(x86::rcx);
+    }
+
+    a.test(RET, RET);
+    a.short_().jns(ascii);
+
+    /* The bs_get_utf8_shared expects the contents in RETd. */
+    a.shr(RET, imm(32));
+    a.jmp(labels[bs_get_utf8_shared]);
+
+    /* Handle plain old ASCII (code point < 128). */
+    a.bind(ascii);
+    a.add(x86::qword_ptr(ctx, position_offset), imm(8));
+    a.shr(RET, imm(56 - _TAG_IMMED1_SIZE));
+    a.or_(RET, imm(_TAG_IMMED1_SMALL)); /* Always clears ZF. */
+    a.ret();
+}
+
+/*
+ * ARG1 = pointer to match state
+ * ARG2 = position in binary in bits
+ * RETd = 4 bytes read from the binary in big-endian order
+ *
+ * On successful return, the extracted code point is in RET, the
+ * position in the match state has been updated, and the ZF is clear.
+ * On failure, the ZF is set.
+ */
+void BeamGlobalAssembler::emit_bs_get_utf8_shared() {
+    Label error = a.newLabel();
+
+    x86::Gp shift_q = ARG4, shift_d = ARG4d, shift_b = ARG4.r8();
+    x86::Gp original_value_d = RETd;
+
+    x86::Gp byte_count_q = ARG2, byte_count_d = ARG2d;
+    x86::Gp extracted_value_d = ARG3d, extracted_value_b = ARG3.r8();
+    x86::Gp control_mask_d = ARG5d;
+    x86::Gp error_mask_d = ARG6d;
+
+    ASSERT(extracted_value_d != shift_d);
+    ASSERT(control_mask_d != shift_d);
+    ASSERT(error_mask_d != shift_d);
+    ASSERT(byte_count_d != shift_d);
+
+    /* UTF-8 has the following layout, where 'x' are data bits:
+     *
+     * 1 byte:  0xxxxxxx (not handled by this path)
+     * 2 bytes: 110xxxxx, 10xxxxxx
+     * 3 bytes: 1110xxxx, 10xxxxxx 10xxxxxx
+     * 4 bytes: 11110xxx, 10xxxxxx 10xxxxxx 10xxxxxx
+     *
+     * Note that the number of leading bits is equal to the number of bytes,
+     * which makes it very easy to create masks for extraction and error
+     * checking. */
+
+    /* The PEXT instruction has poor latency on some processors, so we try to
+     * hide that by extracting early on. Should this be a problem, it's not
+     * much slower to hand-roll it with shifts or BEXTR.
+     *
+     * The mask covers data bits from all variants. This includes the 23rd bit
+     * to support the 2-byte case, which is set on all well-formed 4-byte
+     * codepoints, so it must be cleared before range testing .*/
+    a.mov(extracted_value_d, imm(0x1F3F3F3F));
+    a.pext(extracted_value_d, original_value_d, extracted_value_d);
+
+    /* Preserve current match buffer and bit offset. */
+    a.push(ARG1);
+    a.push(ARG2);
+
+    /* Byte count = leading bit count. */
+    a.mov(byte_count_d, original_value_d);
+    a.not_(byte_count_d);
+    a.lzcnt(byte_count_d, byte_count_d);
+
+    /* Mask shift = (4 - byte count) * 8 */
+    a.mov(shift_d, imm(4));
+    a.sub(shift_d, byte_count_d);
+    a.lea(shift_d, x86::qword_ptr(0, shift_q, 3));
+
+    /* Shift the original value and masks into place. */
+    a.shrx(original_value_d, original_value_d, shift_d);
+
+    /* Matches the '10xxxxxx' components, leaving the header byte alone. */
+    a.mov(control_mask_d, imm(0x00C0C0C0));
+    a.shrx(control_mask_d, control_mask_d, shift_d);
+    a.mov(error_mask_d, imm(0x00808080));
+    a.shrx(error_mask_d, error_mask_d, shift_d);
+
+    /* Extracted value shift = (4 - byte count) * 6, as the leading '10' on
+     * every byte has been removed through PEXT.
+     *
+     * We calculate the shift here to avoid depending on byte_count_d later on
+     * when it may have changed. */
+    a.mov(shift_d, imm(4));
+    a.sub(shift_d, byte_count_d);
+    a.add(shift_d, shift_d);
+    a.lea(shift_d, x86::qword_ptr(shift_q, shift_q, 1));
+
+    /* Assert that the header bits of each '10xxxxxx' component is correct,
+     * signalling errors by trashing the byte count with a guaranteed-illegal
+     * value. */
+    a.and_(original_value_d, control_mask_d);
+    a.cmp(original_value_d, error_mask_d);
+    a.cmovne(byte_count_d, error_mask_d);
+
+    /* Shift the extracted value into place. */
+    a.shrx(RETd, extracted_value_d, shift_d);
+
+    /* The extraction mask is a bit too wide, see above for details. */
+    a.and_(RETd, imm(~(1 << 22)));
+
+    /* Check for too large code point. */
+    a.cmp(RETd, imm(0x10FFFF));
+    a.cmova(byte_count_d, error_mask_d);
+
+    /* Check for the illegal range 16#D800 - 16#DFFF. */
+    a.mov(shift_d, RETd);
+    a.and_(shift_d, imm(-0x800));
+    a.cmp(shift_d, imm(0xD800));
+    a.cmove(byte_count_d, error_mask_d);
+
+    /* Test for overlong UTF-8 sequence. That can be done by testing
+     * that the bits marked y below are all zero.
+     *
+     * 1 byte:  0xxxxxxx (not handled by this path)
+     * 2 bytes: 110yyyyx, 10xxxxxx
+     * 3 bytes: 1110yyyy, 10yxxxxx 10xxxxxx
+     * 4 bytes: 11110yyy, 10yyxxxx 10xxxxxx 10xxxxxx
+     *
+     * 1 byte:                   xx'xxxxx
+     * 2 bytes:             y'yyyxx'xxxxx
+     * 3 bytes:       y'yyyyx'xxxxx'xxxxx
+     * 4 bytes: y'yyyyx'xxxxx'xxxxx'xxxxx
+     *
+     * The y bits can be isolated by shifting down by the number of bits
+     * shown in this table:
+     *
+     * 2:  7    (byte_count * 4 - 1)
+     * 3: 11    (byte_count * 4 - 1)
+     * 4: 16    (byte_count * 4)
+     */
+
+    /* Calculate number of bits to shift. */
+    a.lea(shift_d, x86::qword_ptr(0, byte_count_q, 2));
+    a.cmp(byte_count_d, imm(4));
+    a.setne(extracted_value_b);
+    a.sub(shift_b, extracted_value_b);
+    a.movzx(shift_q, shift_b);
+
+    /* Now isolate the y bits and compare to zero. */
+    a.shrx(extracted_value_d, RETd, shift_d);
+    a.test(extracted_value_d, extracted_value_d);
+    a.cmove(byte_count_d, error_mask_d);
+
+    /* Restore current bit offset and match buffer. */
+    ASSERT(ARG1 != byte_count_q && ARG3 != byte_count_q);
+    a.pop(ARG3);
+    a.pop(ARG1);
+
+    /* Advance our current position. */
+    a.lea(ARG3, x86::qword_ptr(ARG3, byte_count_q, 3));
+
+    /* Byte count must be 2, 3, or 4. */
+    a.sub(byte_count_d, imm(2));
+    a.cmp(byte_count_d, imm(2));
+    a.ja(error);
+
+    a.mov(x86::qword_ptr(ARG1, offsetof(ErlBinMatchBuffer, offset)), ARG3);
+
+    a.shl(RETd, imm(_TAG_IMMED1_SIZE));
+    a.or_(RETd, imm(_TAG_IMMED1_SMALL)); /* Always clears ZF. */
+
+    a.ret();
+
+    a.bind(error);
+    {
+        /* Signal error by setting ZF. */
+        a.xor_(RET, RET);
+        a.ret();
+    }
+}
+
 void BeamModuleAssembler::emit_bs_get_utf8(const ArgRegister &Ctx,
                                            const ArgLabel &Fail) {
-    mov_arg(ARG1, Ctx);
+    const int base_offset = offsetof(ErlBinMatchBuffer, base);
+    const int position_offset = offsetof(ErlBinMatchBuffer, offset);
+    const int size_offset = offsetof(ErlBinMatchBuffer, size);
 
-    emit_enter_runtime();
+    const x86::Gp ctx = ARG1;
+    const x86::Gp bin_position = ARG2;
+    const x86::Gp bin_base = ARG3;
 
-    a.lea(ARG1, emit_boxed_val(ARG1, offsetof(ErlBinMatchState, mb)));
-    runtime_call<1>(erts_bs_get_utf8);
+    Label multi_byte = a.newLabel(), fallback = a.newLabel(),
+          check = a.newLabel(), done = a.newLabel();
 
-    emit_leave_runtime();
+    mov_arg(ctx, Ctx);
+    a.lea(ctx, emit_boxed_val(ARG1, offsetof(ErlBinMatchState, mb)));
 
-    emit_test_the_non_value(RET);
+    a.mov(bin_position, x86::qword_ptr(ctx, position_offset));
+    a.mov(RET, x86::qword_ptr(ctx, size_offset));
+    a.mov(bin_base, x86::qword_ptr(ctx, base_offset));
+    a.sub(RET, bin_position);
+    a.cmp(RET, imm(32));
+    a.short_().jb(fallback);
+
+    a.test(bin_position, imm(7));
+    a.short_().jnz(fallback);
+
+    /* We're byte-aligned and can read at least 32 bits. */
+    a.mov(RET, bin_position);
+    a.shr(RET, 3);
+
+    /* The most significant bits come first, so we'll read the the next four
+     * bytes as big-endian so we won't have to reorder them later. */
+    if (hasCpuFeature(CpuFeatures::X86::kMOVBE)) {
+        a.movbe(RETd, x86::dword_ptr(bin_base, RET));
+    } else {
+        a.mov(RETd, x86::dword_ptr(bin_base, RET));
+        a.bswap(RETd);
+    }
+    a.test(RETd, RETd);
+    a.short_().js(multi_byte);
+
+    /* Handle plain old ASCII (code point < 128). */
+    a.add(x86::qword_ptr(ctx, position_offset), imm(8));
+    a.shr(RETd, imm(24 - _TAG_IMMED1_SIZE));
+    a.or_(RETd, imm(_TAG_IMMED1_SMALL));
+    a.short_().jmp(done);
+
+    a.bind(multi_byte);
+
+    if (BeamAssembler::hasCpuFeature(CpuFeatures::X86::kBMI2)) {
+        /* This CPU supports the PEXT and SHRX instructions. */
+        safe_fragment_call(ga->get_bs_get_utf8_shared());
+        a.short_().jmp(check);
+    }
+
+    a.bind(fallback);
+    safe_fragment_call(ga->get_bs_get_utf8_short_shared());
+
+    a.bind(check);
     a.je(resolve_beam_label(Fail));
+
+    a.bind(done);
 }
 
 void BeamModuleAssembler::emit_i_bs_get_utf8(const ArgRegister &Ctx,

erlang/otp

erts/emulator/beam/jit/x86/instr_bs.cpp
@@ -1018,19 +1018,383 @@ void BeamModuleAssembler::emit_i_bs_put_utf8(const ArgLabel &Fail,
     }
 }
 
+/*
+ * ARG1 = pointer to match state
+ * ARG2 = position in binary in bits
+ * ARG3 = base pointer to binary data
+ * RET = number of bits left in binary
+ *
+ * This fragment is called if the binary is unaligned and/or the number
+ * of remaining bits is less than 32.
+ *
+ * See the comment for emit_bs_get_utf8_shared() for details about the
+ * return value.
+ */
+void BeamGlobalAssembler::emit_bs_get_utf8_short_shared() {
+    const int position_offset = offsetof(ErlBinMatchBuffer, offset);
+
+    const x86::Gp ctx = ARG1;
+    const x86::Gp bin_position = ARG2;
+    const x86::Gp bin_base = ARG3;
+
+    Label at_least_one = a.newLabel();
+    Label two = a.newLabel();
+    Label three_or_more = a.newLabel();
+    Label four = a.newLabel();
+    Label five = a.newLabel();
+    Label read_done = a.newLabel();
+    Label no_masking = a.newLabel();
+    Label ascii = a.newLabel();
+
+    /* Calculate the number of bytes remaining in the binary and error
+     * out if less than one. */
+    a.shr(RET, imm(3));
+    a.test(RET, RET);
+    a.short_().jne(at_least_one);
+
+    /* ZF is is already set. */
+    a.ret();
+
+    a.bind(at_least_one);
+
+    /* Save number of bytes remaining in binary. */
+    a.mov(ARG5, RET);
+
+    /* If the position in the binary is not byte-aligned, we'll need
+     * to read one more byte. */
+    a.test(bin_position, imm(7));
+    a.setne(ARG4.r8());
+    a.movzx(ARG4d, ARG4.r8());
+    a.add(RET, ARG4);
+
+    /* Save original position in bits and set up byte offset for
+     * reading. */
+    a.push(bin_position);
+    a.shr(bin_position, imm(3));
+
+    a.cmp(RET, imm(2));
+    a.short_().je(two);
+    a.short_().ja(three_or_more);
+
+    /* Read one byte (always byte-aligned). */
+    a.mov(RETb, x86::byte_ptr(bin_base, bin_position));
+    a.movzx(RETd, RETb);
+    a.short_().jmp(read_done);
+
+    /* Read two bytes. */
+    a.bind(two);
+    a.mov(RET.r16(), x86::word_ptr(bin_base, bin_position));
+    a.movzx(RETd, RET.r16());
+    a.short_().jmp(read_done);
+
+    a.bind(three_or_more);
+    a.cmp(RET, imm(4));
+    a.short_().je(four);
+    a.short_().ja(five);
+
+    /* Read three bytes. */
+    a.mov(RET.r8(), x86::byte_ptr(bin_base, bin_position, 0, 2));
+    a.movzx(RETd, RETb);
+    a.shl(RETd, imm(16));
+    a.mov(RET.r16(), x86::word_ptr(bin_base, bin_position));
+    a.short_().jmp(read_done);
+
+    /* Read four bytes (always unaligned). */
+    a.bind(four);
+    a.mov(RETd, x86::dword_ptr(bin_base, bin_position));
+    a.short_().jmp(read_done);
+
+    /* Read five bytes (always unaligned). */
+    a.bind(five);
+    a.mov(RETd, x86::dword_ptr(bin_base, bin_position));
+    a.mov(ARG4.r8(), x86::byte_ptr(bin_base, bin_position, 0, 4));
+    a.movzx(ARG4d, ARG4.r8());
+    a.shl(ARG4, imm(32));
+    a.or_(RET, ARG4);
+
+    /* Handle the bytes read. */
+    a.bind(read_done);
+    a.pop(bin_position);
+    a.bswap(RET);
+
+    if (x86::rcx == ctx) {
+        a.push(x86::rcx);
+    }
+    a.mov(x86::ecx, bin_position.r32());
+    a.and_(x86::cl, imm(7));
+    a.shl(RET, x86::cl);
+
+    /* Check whether we will need to clear out trailing
+     * garbage not part of the binary. */
+    a.mov(x86::cl, 64);
+    a.cmp(ARG5, imm(3));
+    a.short_().ja(no_masking);
+
+    /* Calculate a byte mask and zero out trailing garbage. */
+    a.shl(ARG5d, imm(3));
+    a.sub(x86::cl, ARG5.r8());
+    mov_imm(ARG5, -1);
+    a.shl(ARG5, x86::cl);
+    a.and_(RET, ARG5);
+
+    a.bind(no_masking);
+    if (x86::rcx == ctx) {
+        a.pop(x86::rcx);
+    }
+
+    a.test(RET, RET);
+    a.short_().jns(ascii);
+
+    /* The bs_get_utf8_shared fragment expects the contents in RETd. */
+    a.shr(RET, imm(32));
+    a.jmp(labels[bs_get_utf8_shared]);
+
+    /* Handle plain old ASCII (code point < 128). */
+    a.bind(ascii);
+    a.add(x86::qword_ptr(ctx, position_offset), imm(8));
+    a.shr(RET, imm(56 - _TAG_IMMED1_SIZE));
+    a.or_(RET, imm(_TAG_IMMED1_SMALL)); /* Always clears ZF. */
+    a.ret();
+}
+
+/*
+ * ARG1 = pointer to match state
+ * ARG2 = position in binary in bits
+ * RETd = 4 bytes read from the binary in big-endian order
+ *
+ * On successful return, the extracted code point is in RET, the
+ * position in the match state has been updated, and the ZF is clear.
+ * On failure, the ZF is set.
+ */
+void BeamGlobalAssembler::emit_bs_get_utf8_shared() {
+    Label error = a.newLabel();
+
+    x86::Gp shift_q = ARG4, shift_d = ARG4d, shift_b = ARG4.r8();
+    x86::Gp original_value_d = RETd;
+
+    x86::Gp byte_count_q = ARG2, byte_count_d = ARG2d;
+    x86::Gp extracted_value_d = ARG3d, extracted_value_b = ARG3.r8();
+    x86::Gp control_mask_d = ARG5d;
+    x86::Gp error_mask_d = ARG6d;
+
+    ASSERT(extracted_value_d != shift_d);
+    ASSERT(control_mask_d != shift_d);
+    ASSERT(error_mask_d != shift_d);
+    ASSERT(byte_count_d != shift_d);
+
+    /* UTF-8 has the following layout, where 'x' are data bits:
+     *
+     * 1 byte:  0xxxxxxx (not handled by this path)
+     * 2 bytes: 110xxxxx, 10xxxxxx
+     * 3 bytes: 1110xxxx, 10xxxxxx 10xxxxxx
+     * 4 bytes: 11110xxx, 10xxxxxx 10xxxxxx 10xxxxxx
+     *
+     * Note that the number of leading bits is equal to the number of bytes,
+     * which makes it very easy to create masks for extraction and error
+     * checking. */
+
+    /* The PEXT instruction has poor latency on some processors, so we try to
+     * hide that by extracting early on. Should this be a problem, it's not
+     * much slower to hand-roll it with shifts or BEXTR.
+     *
+     * The mask covers data bits from all variants. This includes the 23rd bit
+     * to support the 2-byte case, which is set on all well-formed 4-byte
+     * codepoints, so it must be cleared before range testing .*/
+    a.mov(extracted_value_d, imm(0x1F3F3F3F));
+    a.pext(extracted_value_d, original_value_d, extracted_value_d);
+
+    /* Preserve current match buffer and bit offset. */
+    a.push(ARG1);
+    a.push(ARG2);
+
+    /* Byte count = leading bit count. */
+    a.mov(byte_count_d, original_value_d);
+    a.not_(byte_count_d);
+    a.lzcnt(byte_count_d, byte_count_d);
+
+    /* Mask shift = (4 - byte count) * 8 */
+    a.mov(shift_d, imm(4));
+    a.sub(shift_d, byte_count_d);
+    a.lea(shift_d, x86::qword_ptr(0, shift_q, 3));
+
+    /* Shift the original value and masks into place. */
+    a.shrx(original_value_d, original_value_d, shift_d);
+
+    /* Matches the '10xxxxxx' components, leaving the header byte alone. */
+    a.mov(control_mask_d, imm(0x00C0C0C0));
+    a.shrx(control_mask_d, control_mask_d, shift_d);
+    a.mov(error_mask_d, imm(0x00808080));
+    a.shrx(error_mask_d, error_mask_d, shift_d);
+
+    /* Extracted value shift = (4 - byte count) * 6, as the leading '10' on
+     * every byte has been removed through PEXT.
+     *
+     * We calculate the shift here to avoid depending on byte_count_d later on
+     * when it may have changed. */
+    a.mov(shift_d, imm(4));
+    a.sub(shift_d, byte_count_d);
+    a.add(shift_d, shift_d);
+    a.lea(shift_d, x86::qword_ptr(shift_q, shift_q, 1));
+
+    /* Assert that the header bits of each '10xxxxxx' component is correct,
+     * signalling errors by trashing the byte count with a guaranteed-illegal
+     * value. */
+    a.and_(original_value_d, control_mask_d);
+    a.cmp(original_value_d, error_mask_d);
+    a.cmovne(byte_count_d, error_mask_d);
+
+    /* Shift the extracted value into place. */
+    a.shrx(RETd, extracted_value_d, shift_d);
+
+    /* The extraction mask is a bit too wide, see above for details. */
+    a.and_(RETd, imm(~(1 << 22)));
+
+    /* Check for too large code point. */
+    a.cmp(RETd, imm(0x10FFFF));
+    a.cmova(byte_count_d, error_mask_d);
+
+    /* Check for the illegal range 16#D800 - 16#DFFF. */
+    a.mov(shift_d, RETd);
+    a.and_(shift_d, imm(-0x800));
+    a.cmp(shift_d, imm(0xD800));
+    a.cmove(byte_count_d, error_mask_d);
+
+    /* Test for overlong UTF-8 sequence. That can be done by testing
+     * that the bits marked y below are all zero.
+     *
+     * 1 byte:  0xxxxxxx (not handled by this path)
+     * 2 bytes: 110yyyyx, 10xxxxxx
+     * 3 bytes: 1110yyyy, 10yxxxxx 10xxxxxx
+     * 4 bytes: 11110yyy, 10yyxxxx 10xxxxxx 10xxxxxx
+     *
+     * 1 byte:                   xx'xxxxx
+     * 2 bytes:             y'yyyxx'xxxxx
+     * 3 bytes:       y'yyyyx'xxxxx'xxxxx
+     * 4 bytes: y'yyyyx'xxxxx'xxxxx'xxxxx
+     *
+     * The y bits can be isolated by shifting down by the number of bits
+     * shown in this table:
+     *
+     * 2:  7    (byte_count * 4 - 1)
+     * 3: 11    (byte_count * 4 - 1)
+     * 4: 16    (byte_count * 4)
+     */
+
+    /* Calculate number of bits to shift. */
+    a.lea(shift_d, x86::qword_ptr(0, byte_count_q, 2));
+    a.cmp(byte_count_d, imm(4));
+    a.setne(extracted_value_b);
+    a.sub(shift_b, extracted_value_b);
+    a.movzx(shift_q, shift_b);
+
+    /* Now isolate the y bits and compare to zero. */
+    a.shrx(extracted_value_d, RETd, shift_d);
+    a.test(extracted_value_d, extracted_value_d);
+    a.cmove(byte_count_d, error_mask_d);
+
+    /* Restore current bit offset and match buffer. */
+    ASSERT(ARG1 != byte_count_q && ARG3 != byte_count_q);
+    a.pop(ARG3);
+    a.pop(ARG1);
+
+    /* Advance our current position. */
+    a.lea(ARG3, x86::qword_ptr(ARG3, byte_count_q, 3));
+
+    /* Byte count must be 2, 3, or 4. */
+    a.sub(byte_count_d, imm(2));
+    a.cmp(byte_count_d, imm(2));
+    a.ja(error);
+
+    a.mov(x86::qword_ptr(ARG1, offsetof(ErlBinMatchBuffer, offset)), ARG3);
+
+    a.shl(RETd, imm(_TAG_IMMED1_SIZE));
+    a.or_(RETd, imm(_TAG_IMMED1_SMALL)); /* Always clears ZF. */
+
+    a.ret();
+
+    a.bind(error);
+    {
+        /* Signal error by setting ZF. */
+        a.xor_(RET, RET);
+        a.ret();
+    }
+}
+
 void BeamModuleAssembler::emit_bs_get_utf8(const ArgRegister &Ctx,
                                            const ArgLabel &Fail) {
-    mov_arg(ARG1, Ctx);
+    const int base_offset = offsetof(ErlBinMatchBuffer, base);
+    const int position_offset = offsetof(ErlBinMatchBuffer, offset);
+    const int size_offset = offsetof(ErlBinMatchBuffer, size);
 
-    emit_enter_runtime();
+    const x86::Gp ctx = ARG1;
+    const x86::Gp bin_position = ARG2;
+    const x86::Gp bin_base = ARG3;
 
-    a.lea(ARG1, emit_boxed_val(ARG1, offsetof(ErlBinMatchState, mb)));
-    runtime_call<1>(erts_bs_get_utf8);
+    Label multi_byte = a.newLabel(), fallback = a.newLabel(),
+          check = a.newLabel(), done = a.newLabel();
 
-    emit_leave_runtime();
+    mov_arg(ctx, Ctx);
+    a.lea(ctx, emit_boxed_val(ARG1, offsetof(ErlBinMatchState, mb)));
 
-    emit_test_the_non_value(RET);
+    a.mov(bin_position, x86::qword_ptr(ctx, position_offset));
+    a.mov(RET, x86::qword_ptr(ctx, size_offset));
+    a.mov(bin_base, x86::qword_ptr(ctx, base_offset));
+    a.sub(RET, bin_position);
+    a.cmp(RET, imm(32));
+    a.short_().jb(fallback);
+
+    a.test(bin_position, imm(7));
+    a.short_().jnz(fallback);
+
+    /* We're byte-aligned and can read at least 32 bits. */
+    a.mov(RET, bin_position);
+    a.shr(RET, 3);
+
+    /* The most significant bits come first, so we'll read the the next four
+     * bytes as big-endian so we won't have to reorder them later. */
+    if (hasCpuFeature(CpuFeatures::X86::kMOVBE)) {
+        a.movbe(RETd, x86::dword_ptr(bin_base, RET));
+    } else {
+        a.mov(RETd, x86::dword_ptr(bin_base, RET));
+        a.bswap(RETd);
+    }
+    a.test(RETd, RETd);
+    a.short_().js(multi_byte);
+
+    /* Handle plain old ASCII (code point < 128). */
+    a.add(x86::qword_ptr(ctx, position_offset), imm(8));
+    a.shr(RETd, imm(24 - _TAG_IMMED1_SIZE));
+    a.or_(RETd, imm(_TAG_IMMED1_SMALL));
+    a.short_().jmp(done);
+
+    a.bind(multi_byte);
+
+    if (BeamAssembler::hasCpuFeature(CpuFeatures::X86::kBMI2)) {
+        /* This CPU supports the PEXT and SHRX instructions. */
+        safe_fragment_call(ga->get_bs_get_utf8_shared());
+        a.short_().jmp(check);
+    }
+
+    /* Take care of unaligned binaries and binaries with less than 32
+     * bits left. */
+    a.bind(fallback);
+    if (BeamAssembler::hasCpuFeature(CpuFeatures::X86::kBMI2)) {
+        /* This CPU supports the PEXT and SHRX instructions. */
+        safe_fragment_call(ga->get_bs_get_utf8_short_shared());
+    } else {
+        emit_enter_runtime();
+
+        runtime_call<1>(erts_bs_get_utf8);
+
+        emit_leave_runtime();
+
+        emit_test_the_non_value(RET);
+    }
+
+    a.bind(check);
     a.je(resolve_beam_label(Fail));
+
+    a.bind(done);
 }
 
 void BeamModuleAssembler::emit_i_bs_get_utf8(const ArgRegister &Ctx,