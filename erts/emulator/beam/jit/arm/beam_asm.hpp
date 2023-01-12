erlang/otp

commit 976b2a61c86f4d7588347d2b9a400f9059dd3ccc

JIT: Optimize using the new type information
* Eliminate rendundant test when appending to binaries by using the
new information about "size unit" for binaries.

* Eliminate redundant overflow checks for the `+` and `-` operators.

* While we're at it, optimize multiplication and division by a power
of two.

* Optimize `<`, `=<`, `>`, and `>=` tests using the better information
about ranges.

erts/emulator/beam/jit/arm/beam_asm.hpp
@@ -1053,7 +1053,7 @@ protected:
         return beam->types.entries[typeIndex].type_union;
     }
 
-    auto getIntRange(const ArgSource &arg) const {
+    auto getClampedRange(const ArgSource &arg) const {
         if (arg.isSmall()) {
             Sint value = arg.as<ArgSmall>().getSigned();
             return std::make_pair(value, value);
@@ -1063,9 +1063,40 @@ protected:
 
             ASSERT(typeIndex < beam->types.count);
             const auto &entry = beam->types.entries[typeIndex];
-            ASSERT(entry.type_union & BEAM_TYPE_INTEGER);
-            return std::make_pair(entry.min, entry.max);
+            if (entry.min <= entry.max) {
+                return std::make_pair(entry.min, entry.max);
+            } else if (IS_SSMALL(entry.min) && !IS_SSMALL(entry.max)) {
+                return std::make_pair(entry.min, MAX_SMALL);
+            } else if (!IS_SSMALL(entry.min) && IS_SSMALL(entry.max)) {
+                return std::make_pair(MIN_SMALL, entry.max);
+            } else {
+                return std::make_pair(MIN_SMALL, MAX_SMALL);
+            }
         }
+    }
+
+    int getSizeUnit(const ArgSource &arg) const {
+        auto typeIndex =
+                arg.isRegister() ? arg.as<ArgRegister>().typeIndex() : 0;
+
+        ASSERT(typeIndex < beam->types.count);
+        return beam->types.entries[typeIndex].size_unit;
+    }
+
+    bool hasLowerBound(const ArgSource &arg) const {
+        auto typeIndex =
+                arg.isRegister() ? arg.as<ArgRegister>().typeIndex() : 0;
+        ASSERT(typeIndex < beam->types.count);
+        const auto &entry = beam->types.entries[typeIndex];
+        return IS_SSMALL(entry.min) && !IS_SSMALL(entry.max);
+    }
+
+    bool hasUpperBound(const ArgSource &arg) const {
+        auto typeIndex =
+                arg.isRegister() ? arg.as<ArgRegister>().typeIndex() : 0;
+        ASSERT(typeIndex < beam->types.count);
+        const auto &entry = beam->types.entries[typeIndex];
+        return !IS_SSMALL(entry.min) && IS_SSMALL(entry.max);
     }
 
     bool always_small(const ArgSource &arg) const {
@@ -1073,13 +1104,11 @@ protected:
             return true;
         }
 
-        int type_union = getTypeUnion(arg);
-        if (type_union == BEAM_TYPE_INTEGER) {
-            auto [min, max] = getIntRange(arg);
-            return min <= max;
-        } else {
-            return false;
-        }
+        auto typeIndex =
+                arg.isRegister() ? arg.as<ArgRegister>().typeIndex() : 0;
+        ASSERT(typeIndex < beam->types.count);
+        const auto &entry = beam->types.entries[typeIndex];
+        return entry.type_union == BEAM_TYPE_INTEGER && entry.min <= entry.max;
     }
 
     bool always_immediate(const ArgSource &arg) const {
@@ -1142,67 +1171,53 @@ protected:
         return always_one_of(arg, type_id);
     }
 
-    bool is_sum_small(const ArgSource &LHS, const ArgSource &RHS) {
-        if (!(always_small(LHS) && always_small(RHS))) {
-            return false;
-        } else {
-            Sint min, max;
-            auto [min1, max1] = getIntRange(LHS);
-            auto [min2, max2] = getIntRange(RHS);
-            min = min1 + min2;
-            max = max1 + max2;
-            return IS_SSMALL(min) && IS_SSMALL(max);
-        }
+    bool is_sum_small_if_args_are_small(const ArgSource &LHS,
+                                        const ArgSource &RHS) {
+        Sint min, max;
+        auto [min1, max1] = getClampedRange(LHS);
+        auto [min2, max2] = getClampedRange(RHS);
+        min = min1 + min2;
+        max = max1 + max2;
+        return IS_SSMALL(min) && IS_SSMALL(max);
     }
 
-    bool is_difference_small(const ArgSource &LHS, const ArgSource &RHS) {
-        if (!(always_small(LHS) && always_small(RHS))) {
-            return false;
-        } else {
-            Sint min, max;
-            auto [min1, max1] = getIntRange(LHS);
-            auto [min2, max2] = getIntRange(RHS);
-            min = min1 - max2;
-            max = max1 - min2;
-            return IS_SSMALL(min) && IS_SSMALL(max);
-        }
+    bool is_diff_small_if_args_are_small(const ArgSource &LHS,
+                                         const ArgSource &RHS) {
+        Sint min, max;
+        auto [min1, max1] = getClampedRange(LHS);
+        auto [min2, max2] = getClampedRange(RHS);
+        min = min1 - max2;
+        max = max1 - min2;
+        return IS_SSMALL(min) && IS_SSMALL(max);
     }
 
-    bool is_product_small(const ArgSource &LHS, const ArgSource &RHS) {
-        if (!(always_small(LHS) && always_small(RHS))) {
-            return false;
-        } else {
-            auto [min1, max1] = getIntRange(LHS);
-            auto [min2, max2] = getIntRange(RHS);
-            auto mag1 = std::max(std::abs(min1), std::abs(max1));
-            auto mag2 = std::max(std::abs(min2), std::abs(max2));
+    bool is_product_small_if_args_are_small(const ArgSource &LHS,
+                                            const ArgSource &RHS) {
+        auto [min1, max1] = getClampedRange(LHS);
+        auto [min2, max2] = getClampedRange(RHS);
+        auto mag1 = std::max(std::abs(min1), std::abs(max1));
+        auto mag2 = std::max(std::abs(min2), std::abs(max2));
 
-            /*
-             * mag1 * mag2 <= MAX_SMALL
-             * mag1 <= MAX_SMALL / mag2   (when mag2 != 0)
-             */
-            ERTS_CT_ASSERT(MAX_SMALL < -MIN_SMALL);
-            return mag2 == 0 || mag1 <= MAX_SMALL / mag2;
-        }
+        /*
+         * mag1 * mag2 <= MAX_SMALL
+         * mag1 <= MAX_SMALL / mag2   (when mag2 != 0)
+         */
+        ERTS_CT_ASSERT(MAX_SMALL < -MIN_SMALL);
+        return mag2 == 0 || mag1 <= MAX_SMALL / mag2;
     }
 
     bool is_bsl_small(const ArgSource &LHS, const ArgSource &RHS) {
-        /*
-         * In the code compiled by scripts/diffable, there never
-         * seems to be any range information for the RHS. Therefore,
-         * don't bother unless RHS is an immediate small.
-         */
-        if (!(always_small(LHS) && RHS.isSmall())) {
+        if (!(always_small(LHS) && always_small(RHS))) {
             return false;
         } else {
-            auto [min1, max1] = getIntRange(LHS);
-            auto rhs_val = RHS.as<ArgSmall>().getSigned();
+            auto [min1, max1] = getClampedRange(LHS);
+            auto [min2, max2] = getClampedRange(RHS);
 
-            if (min1 < 0 || max1 == 0 || rhs_val < 0) {
+            if (min1 < 0 || max1 == 0 || min2 < 0) {
                 return false;
             }
 
-            return rhs_val < Support::clz(max1) - _TAG_IMMED1_SIZE;
+            return max2 < Support::clz(max1) - _TAG_IMMED1_SIZE;
         }
     }
 
@@ -1248,6 +1263,19 @@ protected:
     void emit_get_list(const arm::Gp boxed_ptr,
                        const ArgRegister &Hd,
                        const ArgRegister &Tl);
+
+    void emit_add_sub_types(bool is_small_result,
+                            const ArgSource &LHS,
+                            const a64::Gp lhs_reg,
+                            const ArgSource &RHS,
+                            const a64::Gp rhs_reg,
+                            const Label next);
+
+    void emit_are_both_small(const ArgSource &LHS,
+                             const a64::Gp lhs_reg,
+                             const ArgSource &RHS,
+                             const a64::Gp rhs_reg,
+                             const Label next);
 
     void emit_div_rem(const ArgLabel &Fail,
                       const ArgSource &LHS,

erts/emulator/beam/jit/arm/instr_bs.cpp
@@ -19,6 +19,7 @@
  */
 
 #include "beam_asm.hpp"
+#include <numeric>
 
 extern "C"
 {
@@ -1735,6 +1736,17 @@ void BeamModuleAssembler::emit_i_bs_create_bin(const ArgLabel &Fail,
          * Test whether we can omit the code for the error handler.
          */
         switch (seg.type) {
+        case am_append:
+            if (std::gcd(seg.unit, getSizeUnit(seg.src)) != seg.unit) {
+                need_error_handler = true;
+            }
+            break;
+        case am_binary:
+            if (!(seg.size.isAtom() && seg.size.as<ArgAtom>().get() == am_all &&
+                  std::gcd(seg.unit, getSizeUnit(seg.src)) == seg.unit)) {
+                need_error_handler = true;
+            }
+            break;
         case am_integer:
             if (!always_one_of(seg.src, BEAM_TYPE_INTEGER)) {
                 need_error_handler = true;
@@ -1848,7 +1860,7 @@ void BeamModuleAssembler::emit_i_bs_create_bin(const ArgLabel &Fail,
             comment("size binary/integer/float/string");
 
             if (always_small(seg.size)) {
-                auto [min, _] = getIntRange(seg.size);
+                auto [min, _] = getClampedRange(seg.size);
                 if (min >= 0) {
                     can_fail = false;
                 }
@@ -1999,15 +2011,21 @@ void BeamModuleAssembler::emit_i_bs_create_bin(const ArgLabel &Fail,
         emit_leave_runtime<Update::eStack | Update::eHeap | Update::eXRegs |
                            Update::eReductions>(Live.get() + 1);
 
-        if (Fail.get() == 0) {
-            mov_arg(ARG3, ArgXRegister(Live.get()));
-            mov_imm(ARG4,
-                    beam_jit_update_bsc_reason_info(seg.error_info,
-                                                    BSC_REASON_BADARG,
-                                                    BSC_INFO_FVALUE,
-                                                    BSC_VALUE_ARG3));
+        if (std::gcd(seg.unit, getSizeUnit(seg.src)) == seg.unit) {
+            /* There is no way the call can fail with a system_limit
+             * exception on a 64-bit architecture. */
+            comment("skipped test for success because units are compatible");
+        } else {
+            if (Fail.get() == 0) {
+                mov_arg(ARG3, ArgXRegister(Live.get()));
+                mov_imm(ARG4,
+                        beam_jit_update_bsc_reason_info(seg.error_info,
+                                                        BSC_REASON_BADARG,
+                                                        BSC_INFO_FVALUE,
+                                                        BSC_VALUE_ARG3));
+            }
+            emit_branch_if_not_value(ARG1, resolve_label(error, dispUnknown));
         }
-        emit_branch_if_not_value(ARG1, resolve_label(error, dispUnknown));
     } else if (segments[0].type == am_private_append) {
         BscSegment seg = segments[0];
         comment("private append to binary");
@@ -2084,7 +2102,15 @@ void BeamModuleAssembler::emit_i_bs_create_bin(const ArgLabel &Fail,
 
     segments = bs_combine_segments(segments);
 
+    /* Keep track of the bit offset from the being of the binary.
+     * Set to -1 if offset is not known (when a segment of unknown
+     * size has been seen). */
     Sint bit_offset = 0;
+
+    /* Keep track of whether the current segment is byte-aligned.  (A
+     * segment can be known to be byte-aligned even if the bit offset
+     * is unknown.) */
+    bool is_byte_aligned = true;
 
     /* Build each segment of the binary. */
     for (auto seg : segments) {
@@ -2126,8 +2152,9 @@ void BeamModuleAssembler::emit_i_bs_create_bin(const ArgLabel &Fail,
                                                              BSC_REASON_BADARG,
                                                              BSC_INFO_UNIT,
                                                              BSC_VALUE_FVALUE);
-                if (seg.unit == 1) {
-                    comment("skipped test for success because unit =:= 1");
+                if (std::gcd(seg.unit, getSizeUnit(seg.src)) == seg.unit) {
+                    comment("skipped test for success because units are "
+                            "compatible");
                     can_fail = false;
                 }
             } else {
@@ -2318,13 +2345,14 @@ void BeamModuleAssembler::emit_i_bs_create_bin(const ArgLabel &Fail,
                                  seg.effectiveSize,
                                  arm::Gp());
 
-                if (bit_offset < 0) {
-                    /* Bit offset is unknown. Must test alignment. */
+                if (bit_offset < 0 && !is_byte_aligned) {
+                    /* Bit offset is unknown and is not known to be
+                     * byte aligned. Must test alignment. */
                     a.tst(bin_offset, imm(7));
                     a.b_eq(store);
                 }
 
-                if (bit_offset % 8 != 0) {
+                if (!is_byte_aligned) {
                     /* Bit offset is tested or known to be unaligned. */
                     a.str(bin_data, TMP_MEM2q); /* MEM1q is already in use. */
                     lea(ARG1, TMP_MEM2q);
@@ -2335,14 +2363,16 @@ void BeamModuleAssembler::emit_i_bs_create_bin(const ArgLabel &Fail,
                     emit_leave_runtime(Live.get());
 
                     if (bit_offset < 0) {
-                        /* Need to jump around the store code. */
+                        /* The bit offset is unknown, which implies that
+                         * there exists store code that we will need to
+                         * branch past. */
                         a.b(done);
                     }
                 }
 
                 a.bind(store);
 
-                if (bit_offset < 0 || bit_offset % 8 == 0) {
+                if (bit_offset < 0 || is_byte_aligned) {
                     /* Bit offset is tested or known to be
                      * byte-aligned. Emit inline code to store the
                      * value of the accumulator into the binary. */
@@ -2427,7 +2457,7 @@ void BeamModuleAssembler::emit_i_bs_create_bin(const ArgLabel &Fail,
                     }
                 }
 
-                if (bit_offset % 8 == 0 && seg.src.isSmall() &&
+                if (is_byte_aligned && seg.src.isSmall() &&
                     seg.src.as<ArgSmall>().getSigned() == 0) {
                     /* Optimize the special case of setting a known
                      * byte-aligned segment to zero. */
@@ -2541,6 +2571,7 @@ void BeamModuleAssembler::emit_i_bs_create_bin(const ArgLabel &Fail,
             break;
         }
 
+        /* Try to keep track of the bit offset. */
         if (bit_offset >= 0 && (seg.action == BscSegment::action::DIRECT ||
                                 seg.action == BscSegment::action::STORE)) {
             if (seg.effectiveSize >= 0) {
@@ -2548,6 +2579,22 @@ void BeamModuleAssembler::emit_i_bs_create_bin(const ArgLabel &Fail,
             } else {
                 bit_offset = -1;
             }
+        }
+
+        /* Try to keep track whether the next segment is byte
+         * aligned. */
+        if (seg.type == am_append || seg.type == am_private_append) {
+            if (std::gcd(getSizeUnit(seg.src), 8) != 8) {
+                is_byte_aligned = false;
+            }
+        } else if (bit_offset % 8 == 0) {
+            is_byte_aligned = true;
+        } else if (seg.effectiveSize >= 0) {
+            if (seg.effectiveSize % 8 != 0) {
+                is_byte_aligned = false;
+            }
+        } else if (std::gcd(seg.unit, 8) != 8) {
+            is_byte_aligned = false;
         }
     }
 

commit 7c7095688217da88c90988a1e96a57b94a5279c4

Fix crash in binary construction
The JIT did not properly check the type of `Something` in code similar
to:

    b(Something) ->
        <<Something/bits>>.

Calling `b/1` with a non-binary argument would most of the time crash
the runtime system immediately instead of raising an exception.

This bug was introduced in 976b2a61c86f4d (#6086).

Closes #6655

erts/emulator/beam/jit/arm/beam_asm.hpp
@@ -1229,6 +1229,8 @@ protected:
                 arg.isRegister() ? arg.as<ArgRegister>().typeIndex() : 0;
 
         ASSERT(typeIndex < beam->types.count);
+        ASSERT(beam->types.entries[typeIndex].type_union ==
+               BEAM_TYPE_BITSTRING);
         return beam->types.entries[typeIndex].size_unit;
     }
 

erts/emulator/beam/jit/arm/instr_bs.cpp
@@ -2289,12 +2289,14 @@ void BeamModuleAssembler::emit_i_bs_create_bin(const ArgLabel &Fail,
          */
         switch (seg.type) {
         case am_append:
-            if (std::gcd(seg.unit, getSizeUnit(seg.src)) != seg.unit) {
+            if (!(exact_type(seg.src, BEAM_TYPE_BITSTRING) &&
+                  std::gcd(seg.unit, getSizeUnit(seg.src)) == seg.unit)) {
                 need_error_handler = true;
             }
             break;
         case am_binary:
             if (!(seg.size.isAtom() && seg.size.as<ArgAtom>().get() == am_all &&
+                  exact_type(seg.src, BEAM_TYPE_BITSTRING) &&
                   std::gcd(seg.unit, getSizeUnit(seg.src)) == seg.unit)) {
                 need_error_handler = true;
             }
@@ -2656,7 +2658,8 @@ void BeamModuleAssembler::emit_i_bs_create_bin(const ArgLabel &Fail,
         emit_leave_runtime<Update::eStack | Update::eHeap | Update::eXRegs |
                            Update::eReductions>(Live.get() + 1);
 
-        if (std::gcd(seg.unit, getSizeUnit(seg.src)) == seg.unit) {
+        if (exact_type(seg.src, BEAM_TYPE_BITSTRING) &&
+            std::gcd(seg.unit, getSizeUnit(seg.src)) == seg.unit) {
             /* There is no way the call can fail with a system_limit
              * exception on a 64-bit architecture. */
             comment("skipped test for success because units are compatible");
@@ -2842,7 +2845,8 @@ void BeamModuleAssembler::emit_i_bs_create_bin(const ArgLabel &Fail,
                                                              BSC_REASON_BADARG,
                                                              BSC_INFO_UNIT,
                                                              BSC_VALUE_FVALUE);
-                if (std::gcd(seg.unit, getSizeUnit(seg.src)) == seg.unit) {
+                if (exact_type(seg.src, BEAM_TYPE_BITSTRING) &&
+                    std::gcd(seg.unit, getSizeUnit(seg.src)) == seg.unit) {
                     comment("skipped test for success because units are "
                             "compatible");
                     can_fail = false;
@@ -3264,7 +3268,8 @@ void BeamModuleAssembler::emit_i_bs_create_bin(const ArgLabel &Fail,
         /* Try to keep track whether the next segment is byte
          * aligned. */
         if (seg.type == am_append || seg.type == am_private_append) {
-            if (std::gcd(getSizeUnit(seg.src), 8) != 8) {
+            if (!exact_type(seg.src, BEAM_TYPE_BITSTRING) ||
+                std::gcd(getSizeUnit(seg.src), 8) != 8) {
                 is_byte_aligned = false;
             }
         } else if (bit_offset % 8 == 0) {

erts/emulator/beam/jit/x86/beam_asm.hpp
@@ -1290,6 +1290,8 @@ protected:
                 arg.isRegister() ? arg.as<ArgRegister>().typeIndex() : 0;
 
         ASSERT(typeIndex < beam->types.count);
+        ASSERT(beam->types.entries[typeIndex].type_union ==
+               BEAM_TYPE_BITSTRING);
         return beam->types.entries[typeIndex].size_unit;
     }
 

erts/emulator/beam/jit/x86/instr_bs.cpp
@@ -2396,12 +2396,14 @@ void BeamModuleAssembler::emit_i_bs_create_bin(const ArgLabel &Fail,
          */
         switch (seg.type) {
         case am_append:
-            if (std::gcd(seg.unit, getSizeUnit(seg.src)) != seg.unit) {
+            if (!(exact_type(seg.src, BEAM_TYPE_BITSTRING) &&
+                  std::gcd(seg.unit, getSizeUnit(seg.src)) == seg.unit)) {
                 need_error_handler = true;
             }
             break;
         case am_binary:
             if (!(seg.size.isAtom() && seg.size.as<ArgAtom>().get() == am_all &&
+                  exact_type(seg.src, BEAM_TYPE_BITSTRING) &&
                   std::gcd(seg.unit, getSizeUnit(seg.src)) == seg.unit)) {
                 need_error_handler = true;
             }
@@ -2798,7 +2800,8 @@ void BeamModuleAssembler::emit_i_bs_create_bin(const ArgLabel &Fail,
         load_x_reg_array(ARG2);
         runtime_call<6>(erts_bs_append_checked);
 
-        if (std::gcd(seg.unit, getSizeUnit(seg.src)) == seg.unit) {
+        if (exact_type(seg.src, BEAM_TYPE_BITSTRING) &&
+            std::gcd(seg.unit, getSizeUnit(seg.src)) == seg.unit) {
             /* There is no way the call can fail with a system_limit
              * exception on a 64-bit architecture. */
             comment("skipped test for success because units are compatible");
@@ -2900,7 +2903,8 @@ void BeamModuleAssembler::emit_i_bs_create_bin(const ArgLabel &Fail,
                                                              BSC_REASON_BADARG,
                                                              BSC_INFO_UNIT,
                                                              BSC_VALUE_FVALUE);
-                if (std::gcd(seg.unit, getSizeUnit(seg.src)) == seg.unit) {
+                if (exact_type(seg.src, BEAM_TYPE_BITSTRING) &&
+                    std::gcd(seg.unit, getSizeUnit(seg.src)) == seg.unit) {
                     comment("skipped test for success because units are "
                             "compatible");
                     can_fail = false;
@@ -3317,7 +3321,8 @@ void BeamModuleAssembler::emit_i_bs_create_bin(const ArgLabel &Fail,
         /* Try to keep track whether the next segment is byte
          * aligned. */
         if (seg.type == am_append || seg.type == am_private_append) {
-            if (std::gcd(getSizeUnit(seg.src), 8) != 8) {
+            if (!exact_type(seg.src, BEAM_TYPE_BITSTRING) ||
+                std::gcd(getSizeUnit(seg.src), 8) != 8) {
                 is_byte_aligned = false;
             }
         } else if (bit_offset % 8 == 0) {

erts/emulator/test/bs_construct_SUITE.erl
@@ -472,6 +472,20 @@ testf(Config) when is_list(Config) ->
     ?FAIL(<<<<7,8,9,3:7>>/binary-unit:16>>),
     ?FAIL(<<<<7,8,9,3:7>>/binary-unit:17>>),
 
+    %% Failures not deteced by v3_core. Those must be detected at
+    %% runtime.
+    Atom = id(ok),
+    Float = id(2.71),
+    List = id([-1,0,1]),
+    NonBinaries = [{'Atom',Atom}, {'Float',Float}, {'List',List}],
+    ?FAIL_VARS(<<Atom/bits>>, NonBinaries),
+    ?FAIL_VARS(<<Atom/bits,0>>, NonBinaries),
+    ?FAIL_VARS(<<0,Atom/bits>>, NonBinaries),
+    ?FAIL_VARS(<<Float/bits>>, NonBinaries),
+    ?FAIL_VARS(<<Float/bits,0>>, NonBinaries),
+    ?FAIL_VARS(<<List/bits>>, NonBinaries),
+    ?FAIL_VARS(<<List/bits,0>>, NonBinaries),
+
     ok.
 
 testf_1(W, B) ->