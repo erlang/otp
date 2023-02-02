4a5e5e489221fd40f7b119af66cb7a7895b1262e
Kiko Fernandez-Reyes <kiko@erlang.org>
2023-02-02 14:22:23 +0100

    snmp: replace size/1 by xxx_size/1
    The <c>size/1</c> BIF is not optimized by the JIT, and its use can
    result in worse types for Dialyzer.
    
    When one knows that the value being tested must be a tuple,
    <c>tuple_size/1</c> should always be preferred.
    
    When one knows that the value being tested must be a binary,
    <c>byte_size/1</c> should be preferred. However, <c>byte_size/1</c> also
    accepts a bitstring (rounding up size to a whole number of bytes), so
    one must make sure that the call to <c>byte_size/</c> is preceded by a
    call to <c>is_binary/1</c> to ensure that bitstrings are rejected. Note
    that the compiler removes redundant calls to <c>is_binary/1</c>, so if
    one is not sure whether previous code had made sure that the argument is
    a binary, it does not harm to add an <c>is_binary/1</c> test immediately
    before the call to <c>byte_size/1</c>.

--- lib/snmp/src/agent/snmp_index.erl
+++ lib/snmp/src/agent/snmp_index.erl
@@ -155,7 +155,7 @@ is_snmp_type(_) -> false.
 key_to_oid_i(Key, _Type) when is_integer(Key) -> [Key];
 key_to_oid_i(Key, fix_string) -> Key;
 key_to_oid_i(Key, _Type) when is_list(Key) -> [length(Key) | Key];
-key_to_oid_i(Key, Types) -> keys_to_oid(size(Key), Key, [], Types).
+key_to_oid_i(Key, Types) when is_tuple(Key) -> keys_to_oid(tuple_size(Key), Key, [], Types).
 
 keys_to_oid(0, _Key, Oid, _Types) -> Oid;
 keys_to_oid(N, Key, Oid, Types) ->
--- lib/snmp/src/misc/snmp_pdus.erl
+++ lib/snmp/src/misc/snmp_pdus.erl
@@ -828,8 +828,8 @@ enint(N, Acc) ->
  
 enc_oct_str_tag(OStr) when is_list(OStr) ->
     lists:append([4|elength(length(OStr))],OStr);
-enc_oct_str_tag(OBin) ->
-    [4 | elength(size(OBin))] ++ binary_to_list(OBin).
+enc_oct_str_tag(OBin) when is_binary(OBin) ->
+    [4 | elength(byte_size(OBin))] ++ binary_to_list(OBin).
 
 
 enc_oct_str_notag(OStr) -> OStr.
