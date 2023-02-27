f77a95e9cd4f8f5d201219f2837fdf7e68e34550
anupamasingh10 <anupamasingh31@gmail.com>
2023-02-27 10:22:10 +0100

    Add new api conn_info for SSL connections to LDAP server

--- lib/ssl/src/ssl.erl
+++ lib/ssl/src/ssl.erl
@@ -138,7 +138,9 @@
               srp_param_type/0,
               named_curve/0,
               sign_scheme/0,
-              group/0]).
+              group/0,
+              connection_info/0,
+              connection_info_items/0]).
 
 %% -------------------------------------------------------------------------------------------------------
 
