erlang/otp

commit cadd19e84478edc6e62e6b125a4d37745e913ca8

Improve optimization of binary matching
The pattern matching compiler conservatively assumed that binary
patterns can never be re-ordered. Actually it is safe to re-order
clauses that match binaries if the first segment in each clause
match the same number of bits. Re-ordering clauses can help
generating better code and also help detect redundant clauses.

For the following code:

    word(<<"AND">>) -> <<"and">>;
    word(<<"AS">>) -> <<"as">>;
    word(<<"A">>) -> <<"a">>;
    word(<<"AS">>) -> <<"as">>.

the compiler will now generate the following warning:

    t.erl:6:1: Warning: this clause cannot match because a previous clause at line 5 always matches
    %    6| word(<<"AS">>) -> <<"as">>;
    %     | ^

The compiler will also generate slightly better code for the same
example without the redundant clause:

    word(<<"AND">>) -> <<"and">>;
    word(<<"AS">>) -> <<"as">>;
    word(<<"A">>) -> <<"a">>.

Closes #6535

lib/compiler/src/v3_kernel.erl
@@ -1375,8 +1375,9 @@ select_bin_int([#iclause{pats=[#k_bin_seg{anno=A,type=integer,
 	true -> throw(not_possible);
 	false -> ok
     end,
-    Cs = select_bin_int_1(Cs0, Bits, Fl, Val),
-    [{k_bin_int,[C#iclause{pats=[P|Ps]}|Cs]}];
+    Cs1 = [C#iclause{pats=[P|Ps]}|select_bin_int_1(Cs0, Bits, Fl, Val)],
+    Cs = reorder_bin_ints(Cs1),
+    [{k_bin_int,Cs}];
 select_bin_int(_) -> throw(not_possible).
 
 select_bin_int_1([#iclause{pats=[#k_bin_seg{anno=A,type=integer,
@@ -1417,6 +1418,24 @@ select_assert_match_possible(_, _, _) ->
 match_fun(Val) ->
     fun(match, {{integer,_,_},NewV,Bs}) when NewV =:= Val ->
 	    {match,Bs}
+    end.
+
+reorder_bin_ints([_]=Cs) ->
+    Cs;
+reorder_bin_ints(Cs0) ->
+    %% It is safe to reorder clauses that matches binaries if the
+    %% first segments for all of them match the same number of bits.
+    Cs = sort([{reorder_bin_int_sort_key(C),C} || C <- Cs0]),
+    [C || {_,C} <- Cs].
+
+reorder_bin_int_sort_key(#iclause{pats=[Pats|_]}) ->
+    case Pats of
+        #k_bin_int{val=Val,next=#k_bin_end{}} ->
+            %% Sort before clauses with additional segments. This usually results in
+            %% better code.
+            [Val];
+        #k_bin_int{val=Val} ->
+            [Val,more]
     end.
 
 %% match_value([Var], Con, [Clause], Default, State) -> {SelectExpr,State}.

lib/compiler/test/warnings_SUITE.erl
@@ -159,10 +159,15 @@ pattern3(Config) when is_list(Config) ->
             f({A,_}) -> {ok,A};
             f([_|_]=B) -> {ok,B};
             f({urk,nisse}) -> urka_glurka.
+            word(<<\"AND\">>) -> <<\"and\">>;
+            word(<<\"AS\">>) -> <<\"as\">>;
+            word(<<\"A\">>) -> <<\"a\">>;
+            word(<<\"AS\">>) -> <<\"as\">>.
            ">>,
 	   [nowarn_unused_vars],
 	   {warnings,
-            [{{4,13},v3_kernel,{nomatch,{shadow,2}}}]}}],
+            [{{4,13},v3_kernel,{nomatch,{shadow,2}}},
+             {{8,13},v3_kernel,{nomatch,{shadow,6}}}]}}],
     [] = run(Config, Ts),
 
     ok.

commit 39f97eefb6f9ec801f3ce87cbd30b42eedbcc1c0

Correct unsafe optimization of binary matching
When compiling generated code from forms where the line numbers in the
annotations did not correspond to the actual order of the clauses, the
compiler could rearrange clauses even when it was not safe. For
example:

    match_route([<<"prefix">>, <<"action">>]) -> first;  %Line 4
    match_route([<<"prefix">>, _Ignore]) -> second.      %Line 2

The compiler would rearrange that to:

    match_route([<<"prefix">>, _Ignore]) -> second;      %Line 2
    match_route([<<"prefix">>, <<"action">>]) -> first.  %Line 4

which would make the second clause (former first clause) unreachable.

This bug was introduced in cadd19e84478ed (#6538).

Closes #6923

lib/compiler/src/v3_kernel.erl
@@ -1438,11 +1438,29 @@ reorder_bin_ints([_]=Cs) ->
     Cs;
 reorder_bin_ints(Cs0) ->
     %% It is safe to reorder clauses that matches binaries if the
-    %% first segments for all of them match the same number of bits.
-    Cs = sort([{reorder_bin_int_sort_key(C),C} || C <- Cs0]),
-    [C || {_,C} <- Cs].
+    %% first segments for all of them match the same number of bits
+    %% and if the patterns that follow are also safe to re-order.
+    try
+        Cs = sort([{reorder_bin_int_sort_key(C),C} || C <- Cs0]),
+        [C || {_,C} <- Cs]
+    catch
+        throw:not_possible ->
+            Cs0
+    end.
 
-reorder_bin_int_sort_key(#iclause{pats=[Pats|_]}) ->
+reorder_bin_int_sort_key(#iclause{pats=[Pats|More],guard=#c_literal{val=true}}) ->
+    case all(fun(#k_var{}) -> true;
+                (_) -> false
+             end, More) of
+        true ->
+            %% Only variables. Safe to re-order.
+            ok;
+        false ->
+            %% Not safe to re-order. For example:
+            %%    f([<<"prefix">>, <<"action">>]) -> ...
+            %%    f([<<"prefix">>, Variable]) -> ...
+            throw(not_possible)
+    end,
     case Pats of
         #k_bin_int{val=Val,next=#k_bin_end{}} ->
             %% Sort before clauses with additional segments. This usually results in
@@ -1450,7 +1468,9 @@ reorder_bin_int_sort_key(#iclause{pats=[Pats|_]}) ->
             [Val];
         #k_bin_int{val=Val} ->
             [Val,more]
-    end.
+    end;
+reorder_bin_int_sort_key(#iclause{}) ->
+    throw(not_possible).
 
 %% match_value([Var], Con, [Clause], Default, State) -> {SelectExpr,State}.
 %%  At this point all the clauses have the same constructor, we must

lib/compiler/test/bs_match_SUITE.erl
@@ -51,7 +51,7 @@
          bs_saved_position_units/1,empty_matches/1,
          trim_bs_start_match_resume/1,
          gh_6410/1,bs_match/1,
-         binary_aliases/1]).
+         binary_aliases/1,gh_6923/1]).
 
 -export([coverage_id/1,coverage_external_ignore/2]).
 
@@ -92,7 +92,8 @@ groups() ->
        many_clauses,combine_empty_segments,hangs_forever,
        bs_saved_position_units,empty_matches,
        trim_bs_start_match_resume,
-       gh_6410,bs_match,binary_aliases]}].
+       gh_6410,bs_match,binary_aliases,
+       gh_6923]}].
 
 init_per_suite(Config) ->
     test_lib:recompile(?MODULE),
@@ -3127,6 +3128,54 @@ gh6415_match_f(<<_:(true andalso 0), Size, Var:Size>> =
     Var;
 gh6415_match_f(_) ->
     error.
+
+gh_6923(_Config) ->
+    Mod = list_to_atom(?MODULE_STRING ++ "_" ++ atom_to_list(?FUNCTION_NAME)),
+
+    %% The second clause of match_route/1 has lower line numbers than
+    %% the first clause.
+    %%
+    %% -module(bs_match_SUITE_gh_6923).                     %Line 29
+    %% -export([match_route/1]).                            %Line 29
+    %% match_route([<<"prefix">>, <<"action">>]) -> first;  %Line 4
+    %% match_route([<<"prefix">>, _Ignore]) -> second.      %Line 2
+    Forms =
+        [{attribute,29,module,Mod},
+         {attribute,29,export,[{match_route,1}]},
+         {function,4,match_route,1,
+          [{clause,4,
+            [{cons,4,
+              {bin,4,[{bin_element,4,{string,4,"prefix"},default,default}]},
+              {cons,4,
+               {bin,4,
+                [{bin_element,4,
+                  {string,4,"action"},
+                  default,default}]},
+               {nil,4}}}],
+            [],
+            [{atom,4,first}]},
+           {clause,2,
+            [{cons,2,
+              {bin,2,[{bin_element,2,{string,2,"prefix"},default,default}]},
+              {cons,2,{var,2,'_Ignore'},{nil,2}}}],
+            [],
+            [{atom,2,second}]}]}],
+    Opts = test_lib:opt_opts(?MODULE),
+    {ok, Mod, Beam} = compile:forms(Forms, Opts),
+    {module, Mod} = code:load_binary(Mod, "", Beam),
+    first = Mod:match_route([<<"prefix">>, <<"action">>]),
+    second = Mod:match_route([<<"prefix">>, whatever]),
+    _ = code:delete(Mod),
+    _ = code:purge(Mod),
+
+    %% For coverage.
+    first = do_gh_6923([id(<<"abc">>), id(42)]),
+    second = do_gh_6923([id(<<"abc">>), id({a,b,c})]),
+
+    ok.
+
+do_gh_6923([<<"abc">>, A]) when is_integer(A) -> first;
+do_gh_6923([<<"abc">>, A]) when is_tuple(A) -> second.
 
 %%% Utilities.
 