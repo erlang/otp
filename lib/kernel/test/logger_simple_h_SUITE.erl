767244b1e8d49de545dde593f92ce6baf8c0c584
Rickard Green <rickard@erlang.org>
2023-03-20 14:24:56 +0100

    [kernel] Fix logger_simple_h_SUITE:start_crash test

--- lib/kernel/test/logger_simple_h_SUITE.erl
+++ lib/kernel/test/logger_simple_h_SUITE.erl
@@ -108,7 +108,7 @@ start_crash(_Config) ->
     Output = os:cmd(ct:get_progname() ++ " -user baduser"),
     ErrorOutput = re:replace(unicode:characters_to_binary(Output),"\r\n","\n",[global]),
     ct:log("~ts",[ErrorOutput]),
-    {match,[_]} = re:run(ErrorOutput,"(^=SUPERVISOR REPORT====| supervisor_report *\n)",[global]),
+    {match,[_]} = re:run(ErrorOutput,"(^=SUPERVISOR REPORT====| supervisor_report *\n)",[global,multiline]),
     {match,[_, _]} = re:run(ErrorOutput," crash_report *\n",[global]),
     {match,[_]} = re:run(ErrorOutput," std_info *\n",[global]),
     {match,[[CD]]} = re:run(ErrorOutput,"\nCrash dump is being written to: (.*)\\.\\.\\.done",
