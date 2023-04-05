c6bc1cdff90d83bd7477eb01c08e6da66950d927
Frej Drejhammar <frej.drejhammar@gmail.com>
2023-04-05 15:57:22 +0200

    erts: Fix warning about bitwise operations on booleans
    Clang 16 warns when `&` and `|` are used on boolean operands.

--- erts/emulator/beam/dist.c
+++ erts/emulator/beam/dist.c
@@ -4884,7 +4884,7 @@ BIF_RETTYPE setnode_2(BIF_ALIST_2)
     erts_thr_progress_block();
 
     success = (!ERTS_PROC_IS_EXITING(net_kernel)
-               & !ERTS_PROC_GET_DIST_ENTRY(net_kernel));
+               && !ERTS_PROC_GET_DIST_ENTRY(net_kernel));
     if (success) {
         inc_no_nodes();
         erts_set_this_node(BIF_ARG_1, (Uint32) creation);
