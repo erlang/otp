c2c0247a47ebd8a69e7843d403468f8bdbd63acd
Sverker Eriksson <sverker@erlang.org>
2023-05-16 14:39:35 +0200

    erts: Fix optimization bug in maps:put
    maps:put with existing key and identical value was not optimized
    as a no-op correctly in collision node.
    
    Bug was harmeless though as CDR(ptr) is the first element of the
    collision tuple which is an internal key-value cons that can never
    be equal to 'value'. That is, the optimization cannot kick in
    and yield incorrect result.

--- erts/emulator/beam/erl_map.c
+++ erts/emulator/beam/erl_map.c
@@ -2554,7 +2554,7 @@ int erts_hashmap_insert_down(Uint32 hx, Eterm key, Eterm value, Eterm node, Uint
                             ckey = CAR(kv);
                             c = CMP_TERM(key, ckey);
                             if (c == 0) {
-                                if (CDR(ptr) == value) {
+                                if (CDR(kv) == value) {
                                     *sz = 0;
                                     return 1;
                                 }
