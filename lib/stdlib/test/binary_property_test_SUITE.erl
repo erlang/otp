4b675e57c3934b8fec05922686de0e08670ccf19
Maria Scott <maria-12648430@hnc-agency.org>
2023-08-30 16:54:52 +0200

    Property-based tests for the binary module
    Co-authored-by: Jan Uhlig <juhlig@hnc-agency.org>

--- lib/stdlib/test/binary_property_test_SUITE.erl
+++ lib/stdlib/test/binary_property_test_SUITE.erl
@@ -22,7 +22,29 @@
 -compile([export_all, nowarn_export_all]).
 
 all() ->
-    [encode_hex_case].
+    [at_case, at_invalid_case,
+     bin_to_list_1_case,
+     bin_to_list_2_3_case, bin_to_list_2_3_invalid_case,
+     compile_pattern_case, compile_pattern_invalid_case,
+     copy_case, copy_2_invalid_case,
+     decode_hex_case, decode_hex_invalid_case,
+     decode_unsigned_case,
+     encode_hex_case,
+     encode_unsigned_case, encode_unsigned_invalid_case,
+     first_case,
+     last_case,
+     list_to_bin_case, list_to_bin_invalid_case,
+     longest_common_prefix_case,
+     longest_common_suffix_case,
+     match_2_case,
+     match_3_case, match_3_invalid_case,
+     matches_2_case,
+     matches_3_case, matches_3_invalid_case,
+     part_case, part_invalid_case,
+     replace_3_case,
+     replace_4_case, replace_4_invalid1_case, replace_4_invalid2_case,
+     split_2_case,
+     split_3_case, split_3_invalid_case].
 
 init_per_suite(Config) ->
     ct_property_test:init_per_suite(Config).
@@ -30,6 +52,114 @@ init_per_suite(Config) ->
 end_per_suite(Config) ->
     Config.
 
+do_proptest(Prop, Config) ->
+    ct_property_test:quickcheck(binary_prop:Prop(), Config).
+
+at_case(Config) ->
+    do_proptest(prop_at, Config).
+
+at_invalid_case(Config) ->
+    do_proptest(prop_at_invalid, Config).
+
+bin_to_list_1_case(Config) ->
+    do_proptest(prop_bin_to_list_1, Config).
+
+bin_to_list_2_3_case(Config) ->
+    do_proptest(prop_bin_to_list_2_3, Config).
+
+bin_to_list_2_3_invalid_case(Config) ->
+    do_proptest(prop_bin_to_list_2_3_invalid, Config).
+
+compile_pattern_case(Config) ->
+    do_proptest(prop_compile_pattern, Config).
+
+compile_pattern_invalid_case(Config) ->
+    do_proptest(prop_compile_pattern_invalid, Config).
+
+copy_case(Config) ->
+    do_proptest(prop_copy, Config).
+
+copy_2_invalid_case(Config) ->
+    do_proptest(prop_copy_2_invalid, Config).
+
+decode_hex_case(Config) ->
+    do_proptest(prop_decode_hex, Config).
+
+decode_hex_invalid_case(Config) ->
+    do_proptest(prop_decode_hex_invalid, Config).
+
+decode_unsigned_case(Config) ->
+    do_proptest(prop_decode_unsigned, Config).
+
 encode_hex_case(Config) ->
-    ct_property_test:quickcheck(binary_prop:prop_hex_encode_2(), Config).
+    do_proptest(prop_encode_hex, Config).
+
+encode_unsigned_case(Config) ->
+    do_proptest(prop_encode_unsigned, Config).
+
+encode_unsigned_invalid_case(Config) ->
+    do_proptest(prop_encode_unsigned_invalid, Config).
+
+first_case(Config) ->
+    do_proptest(prop_first, Config).
+
+last_case(Config) ->
+    do_proptest(prop_last, Config).
+
+list_to_bin_case(Config) ->
+    do_proptest(prop_list_to_bin, Config).
+
+list_to_bin_invalid_case(Config) ->
+    do_proptest(prop_list_to_bin_invalid, Config).
+
+longest_common_prefix_case(Config) ->
+    do_proptest(prop_longest_common_prefix, Config).
+
+longest_common_suffix_case(Config) ->
+    do_proptest(prop_longest_common_suffix, Config).
+
+match_2_case(Config) ->
+    do_proptest(prop_match_2, Config).
+
+match_3_case(Config) ->
+    do_proptest(prop_match_3, Config).
+
+match_3_invalid_case(Config) ->
+    do_proptest(prop_match_3_invalid, Config).
+
+matches_2_case(Config) ->
+    do_proptest(prop_matches_2, Config).
+
+matches_3_case(Config) ->
+    do_proptest(prop_matches_3, Config).
+
+matches_3_invalid_case(Config) ->
+    do_proptest(prop_matches_3_invalid, Config).
+
+part_case(Config) ->
+    do_proptest(prop_part, Config).
+
+part_invalid_case(Config) ->
+    do_proptest(prop_part_invalid, Config).
+
+replace_3_case(Config) ->
+    do_proptest(prop_replace_3, Config).
+
+replace_4_case(Config) ->
+    do_proptest(prop_replace_4, Config).
+
+replace_4_invalid1_case(Config) ->
+    do_proptest(prop_replace_4_invalid1, Config).
+
+replace_4_invalid2_case(Config) ->
+    do_proptest(prop_replace_4_invalid2, Config).
+
+split_2_case(Config) ->
+    do_proptest(prop_split_2, Config).
+
+split_3_case(Config) ->
+    do_proptest(prop_split_3, Config).
+
+split_3_invalid_case(Config) ->
+    do_proptest(prop_split_3_invalid, Config).
 
