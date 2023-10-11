9da7af5ddb827143bd053b9f4647524145b9bf5b
Micael Karlberg <bmk@erlang.org>
2023-10-11 18:15:03 +0200

    [erts|esock] Fixed type warnings with casting

--- erts/emulator/nifs/common/prim_socket_nif.c
+++ erts/emulator/nifs/common/prim_socket_nif.c
@@ -8137,21 +8137,27 @@ ERL_NIF_TERM esock_setopt_str_opt(ErlNifEnv*       env,
     ESOCK_ASSERT( val != NULL );
 
     if ((optLen = GET_STR(env, eVal, val, max)) > 0) {
+
         optLen--;
 
         result =
             esock_setopt_level_opt(env, descP, level, opt,
                                    val, optLen);
+
     } else if (enif_inspect_binary(env, eVal, &bin)) {
-        optLen = esock_strnlen(bin.data, max - 1);
+
+        optLen = esock_strnlen((char*) bin.data, max - 1);
         sys_memcpy(val, bin.data, optLen);
         val[optLen] = '\0';
 
         result =
             esock_setopt_level_opt(env, descP, level, opt,
                                    val, optLen);
+
     } else {
+
         result = esock_make_invalid(env, esock_atom_value);
+
     }
 
     FREE(val);
