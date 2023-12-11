erlang/otp

commit 141a287d2cd293c0d0a4f94edfb3852f5e110ca3

erts, kernel: Add native coverage support
This implementation was inspired by and partially based on
a previous implementation by Robin Morisset <rmorisset@meta.com>.

To get an idea of the exuction overhead for using various cover modes,
I compiled the entire compiler with the line_coverage option, and then
run the test case compilation_SUITE:self_compile/1 with different
configurations. I ran each test case several times on an 2017 Intel
iMac for each configuration and noted the shortest time for each:

* +JPcover false: 11.5 sec
* +JPcover line: 11.5 sec
* +JPcover line_counters: 12.4 sec

On an M1 MacBook Pro (AAarc64) and the times were the following:

* +JPcover false: 7.2 sec
* +JPcover line: 7.2 sec
* +JPcover line_counters: 8.0 sec

At least according to this benchmark, the overhead for the `line` mode
is negligible, while the overhead for `line_counters` mode is less
than 10 percent.

erts/emulator/beam/jit/asm_load.c
@@ -69,6 +69,57 @@ int beam_load_prepare_emit(LoaderState *stp) {
     hdr->md5_ptr = NULL;
     hdr->are_nifs = NULL;
 
+    stp->coverage = hdr->coverage = NULL;
+    stp->line_coverage_valid = hdr->line_coverage_valid = NULL;
+
+    hdr->line_coverage_len = 0;
+
+    if (stp->beam.lines.flags & BEAMFILE_FORCE_LINE_COUNTERS) {
+        hdr->coverage_mode = ERTS_COV_LINE_COUNTERS;
+    } else if ((stp->beam.lines.flags & BEAMFILE_EXECUTABLE_LINE) == 0 &&
+               (erts_coverage_mode == ERTS_COV_LINE ||
+                erts_coverage_mode == ERTS_COV_LINE_COUNTERS)) {
+        /* A line coverage mode is enabled, but there are no
+         * executable_line instructions in this module; therefore,
+         * turn off coverage for this module. */
+        hdr->coverage_mode = ERTS_COV_NONE;
+    } else {
+        /* Use the system default coverage mode for this module. */
+        hdr->coverage_mode = erts_coverage_mode;
+    }
+
+    switch (hdr->coverage_mode) {
+    case ERTS_COV_FUNCTION:
+    case ERTS_COV_FUNCTION_COUNTERS: {
+        size_t alloc_size = hdr->num_functions;
+        if (hdr->coverage_mode == ERTS_COV_FUNCTION_COUNTERS) {
+            alloc_size *= sizeof(Uint);
+        }
+        stp->coverage = erts_alloc(ERTS_ALC_T_CODE_COVERAGE, alloc_size);
+        sys_memset(stp->coverage, 0, alloc_size);
+        break;
+    }
+    case ERTS_COV_LINE:
+    case ERTS_COV_LINE_COUNTERS: {
+        size_t alloc_size = stp->beam.lines.instruction_count;
+        Uint coverage_size;
+
+        if (hdr->coverage_mode == ERTS_COV_LINE) {
+            coverage_size = sizeof(byte);
+        } else {
+            coverage_size = sizeof(Uint);
+        }
+        stp->coverage = erts_alloc(ERTS_ALC_T_CODE_COVERAGE,
+                                   alloc_size * coverage_size);
+        sys_memset(stp->coverage, 0, alloc_size * coverage_size);
+        stp->line_coverage_valid =
+                erts_alloc(ERTS_ALC_T_CODE_COVERAGE, alloc_size);
+        sys_memset(stp->line_coverage_valid, 0, alloc_size);
+        hdr->line_coverage_len = alloc_size;
+        break;
+    }
+    }
+
     stp->load_hdr = hdr;
 
     stp->labels = erts_alloc(ERTS_ALC_T_PREPARED_CODE,
@@ -199,6 +250,16 @@ int beam_load_prepared_dtor(Binary *magic) {
     if (stp->func_line) {
         erts_free(ERTS_ALC_T_PREPARED_CODE, stp->func_line);
         stp->func_line = NULL;
+    }
+
+    if (stp->coverage) {
+        erts_free(ERTS_ALC_T_CODE_COVERAGE, stp->coverage);
+        stp->coverage = NULL;
+    }
+
+    if (stp->line_coverage_valid) {
+        erts_free(ERTS_ALC_T_CODE_COVERAGE, stp->line_coverage_valid);
+        stp->line_coverage_valid = NULL;
     }
 
     if (stp->ba) {
@@ -586,6 +647,30 @@ int beam_load_emit_op(LoaderState *stp, BeamOp *tmp_op) {
             goto load_error;
         }
         break;
+    case op_executable_line_I: {
+        byte coverage_size = 0;
+
+        /* We'll save some memory by not inserting a line entry that
+         * is equal to the previous one. */
+        if (add_line_entry(stp, tmp_op->a[0].val, 0)) {
+            goto load_error;
+        }
+        if (stp->load_hdr->coverage_mode == ERTS_COV_LINE) {
+            coverage_size = sizeof(byte);
+        } else if (stp->load_hdr->coverage_mode == ERTS_COV_LINE_COUNTERS) {
+            coverage_size = sizeof(Uint);
+        }
+        if (coverage_size) {
+            unsigned loc_index = stp->current_li - 1;
+            ASSERT(stp->beam.lines.item_count > 0);
+            stp->line_coverage_valid[loc_index] = 1;
+            beamasm_emit_coverage(stp->ba,
+                                  stp->coverage,
+                                  loc_index,
+                                  coverage_size);
+        }
+        break;
+    }
     case op_int_code_end:
         /* End of code found. */
         if (stp->function_number != stp->beam.code.function_count) {
@@ -598,6 +683,27 @@ int beam_load_emit_op(LoaderState *stp, BeamOp *tmp_op) {
         stp->function = THE_NON_VALUE;
         stp->genop = NULL;
         stp->specific_op = -1;
+
+        if (stp->load_hdr->coverage_mode == ERTS_COV_LINE ||
+            stp->load_hdr->coverage_mode == ERTS_COV_LINE_COUNTERS) {
+            stp->load_hdr->line_coverage_len = stp->current_li;
+        }
+        break;
+    case op_i_test_yield:
+        if (stp->load_hdr->coverage_mode == ERTS_COV_FUNCTION) {
+            ASSERT(stp->function_number != 0);
+            beamasm_emit_coverage(stp->ba,
+                                  stp->coverage,
+                                  stp->function_number - 1,
+                                  sizeof(byte));
+        } else if (stp->load_hdr->coverage_mode == ERTS_COV_FUNCTION_COUNTERS) {
+            ASSERT(stp->function_number != 0);
+            beamasm_emit_coverage(stp->ba,
+                                  stp->coverage,
+                                  stp->function_number - 1,
+                                  sizeof(Uint));
+        }
+        break;
     }
 
     return 1;
@@ -780,6 +886,13 @@ int beam_load_finish_emit(LoaderState *stp) {
     /* Save the updated code pointer and code size. */
     stp->code_hdr = code_hdr_ro;
     stp->loaded_size = module_size;
+
+    /* Transfer ownership of the coverage tables to the loaded code. */
+    code_hdr_rw->coverage = stp->coverage;
+    code_hdr_rw->line_coverage_valid = stp->line_coverage_valid;
+
+    stp->coverage = NULL;
+    stp->line_coverage_valid = NULL;
 
     /*
      * Place the literals in their own allocated heap (for fast range check)
@@ -1024,4 +1137,14 @@ void beam_load_finalize_code(LoaderState *stp,
     stp->executable_region = NULL;
     stp->writable_region = NULL;
     stp->code_hdr = NULL;
+}
+
+void beam_load_purge_aux(const BeamCodeHeader *hdr) {
+    if (hdr->coverage) {
+        erts_free(ERTS_ALC_T_CODE_COVERAGE, hdr->coverage);
+    }
+
+    if (hdr->line_coverage_valid) {
+        erts_free(ERTS_ALC_T_CODE_COVERAGE, hdr->line_coverage_valid);
+    }
 }

erts/doc/src/erl_cmd.xml
@@ -1035,6 +1035,62 @@ $ <input>erl \
           descriptor input events.
         </p>
       </item>
+      <tag><marker id="+JPcover"/><c>+JPcover true|false|function|function_counters|line|line_counters</c></tag>
+      <item>
+        <p>Since: OTP 27.0</p>
+        <p>Enables or disables support for coverage when running with
+        the JIT. Defaults to false.</p>
+        <taglist>
+          <tag><c>function</c></tag>
+          <item><p>All modules that are loaded will be instrumented to
+          keep track of which functions are executed. Information
+          about which functions that have been executed can be
+          retrieved by calling <seemfa
+          marker="kernel:code#get_coverage/2"><c>code:get_coverage(function, Module)</c></seemfa>.</p>
+	  </item>
+          <tag><c>function_counters</c></tag>
+          <item><p>All modules that are loaded will be instrumented to
+          count how many times each function is executed. Information
+          about how many times each function has been executed can be
+          retrieved by calling <seemfa
+          marker="kernel:code#get_coverage/2"><c>code:get_coverage(function,
+          Module)</c></seemfa>.</p>
+          </item>
+          <tag><c>line</c></tag>
+          <item><p>When modules that have been compiled with the
+          <seeerl
+          marker="compiler:compile#line_coverage"><c>line_coverage</c></seeerl>
+          option are loaded, they will be instrumented to keep track
+          of which lines have been executed. Information about which
+          lines have been executed can be retrieved by calling <seemfa
+          marker="kernel:code#get_coverage/2"><c>code:get_coverage(line,
+          Module)</c></seemfa>, and information about which functions
+          that have been executed can be retrieved by calling <seemfa
+          marker="kernel:code#get_coverage/2"><c>code:get_coverage(function,
+          Module)</c></seemfa>.</p>
+          </item>
+          <tag><c>line_counters</c></tag>
+          <item><p>When modules that have been compiled with the
+          <seeerl
+          marker="compiler:compile#line_coverage"><c>line_coverage</c></seeerl>
+          option are loaded, they will be instrumented to count the
+          number of times each line is executed. Information about how
+          many times each line has been executed can be retrieved by
+          calling <seemfa
+          marker="kernel:code#get_coverage/2"><c>code:get_coverage(line,
+          Module)</c></seemfa>, and information about which functions
+          that have been executed can be retrieved by calling <seemfa
+          marker="kernel:code#get_coverage/2"><c>code:get_coverage(function,
+          Module)</c></seemfa> (note that in this mode, counters for
+          the number of times each function has been executed
+          <strong>cannot</strong> be retrieved).</p>
+          </item>
+          <tag><c>true</c></tag>
+          <item><p>Same as <c>line_counters</c>.</p></item>
+          <tag><c>false</c></tag>
+          <item><p>Disables coverage.</p></item>
+        </taglist>
+      </item>
       <tag><marker id="+JPperf"/><c>+JPperf true|false|dump|map|fp|no_fp</c></tag>
       <item>
         <p>Enables or disables support for the <c>perf</c> profiler when

erts/emulator/Makefile.in
@@ -1119,7 +1119,8 @@ RUN_OBJS += \
 	$(OBJDIR)/erl_global_literals.o \
 	$(OBJDIR)/beam_file.o \
 	$(OBJDIR)/beam_types.o \
-	$(OBJDIR)/erl_term_hashing.o
+	$(OBJDIR)/erl_term_hashing.o \
+	$(OBJDIR)/erl_bif_coverage.o
 
 LTTNG_OBJS = $(OBJDIR)/erlang_lttng.o
 

erts/emulator/beam/atom.names
@@ -313,6 +313,7 @@ atom format_cpu_topology
 atom free
 atom fullsweep_after
 atom function
+atom function_counters
 atom functions
 atom function_clause
 atom garbage_collect
@@ -397,6 +398,7 @@ atom ldflags
 atom Le='=<'
 atom lf
 atom line
+atom line_counters
 atom line_delimiter
 atom line_length
 atom linked_in_driver

erts/emulator/beam/beam_bif_load.c
@@ -2079,6 +2079,7 @@ BIF_RETTYPE erts_internal_purge_module_2(BIF_ALIST_2)
                               modp->old.code_hdr->are_nifs);
                 }
 
+                beam_load_purge_aux(modp->old.code_hdr);
 #ifndef BEAMASM
                 erts_free(ERTS_ALC_T_CODE, (void *) modp->old.code_hdr);
 #else

erts/emulator/beam/beam_code.h
@@ -87,6 +87,18 @@ typedef struct beam_code_header {
      */
     const BeamCodeLineTab *line_table;
 
+#ifdef BEAMASM
+
+    /*
+     * Coverage support.
+     */
+    Uint coverage_mode;
+    void *coverage;
+    byte *line_coverage_valid;
+    Uint line_coverage_len;
+
+#endif
+
     /*
      * Pointer to the module MD5 sum (16 bytes)
      */

erts/emulator/beam/beam_common.c
@@ -948,6 +948,9 @@ save_stacktrace(Process* c_p, ErtsCodePtr pc, Eterm* reg,
             break;
 
             /* Kernel */
+        case am_code:
+            format_module = am_erl_kernel_errors;
+            break;
         case am_os:
             format_module = am_erl_kernel_errors;
             break;

erts/emulator/beam/beam_file.c
@@ -453,8 +453,8 @@ static int parse_line_chunk(BeamFile *beam, IFF_Chunk *chunk) {
      * have to special-case it anywhere else. */
     name_count++;
 
-    /* Flags are unused at the moment. */
-    (void)flags;
+    /* Save flags. */
+    lines->flags = flags;
 
     /* Reserve space for the "undefined location" entry. */
     item_count++;

commit aaf99674d75abf0a05523214355a5b46f192f777

Eliminate memory leak in the native coverage feature
If a module was prepared for loading, but the loading never finished,
the memory allocated for coverage information could leak. This bug
was introduced in 141a287d2cd293c0d0a4f94edfb3852f5e110ca3.

erts/emulator/beam/jit/asm_load.c
@@ -222,6 +222,14 @@ int beam_load_prepared_dtor(Binary *magic) {
             erts_free(ERTS_ALC_T_PREPARED_CODE, hdr->are_nifs);
             hdr->are_nifs = NULL;
         }
+        if (hdr->coverage) {
+            erts_free(ERTS_ALC_T_CODE_COVERAGE, hdr->coverage);
+            hdr->coverage = NULL;
+        }
+        if (hdr->line_coverage_valid) {
+            erts_free(ERTS_ALC_T_CODE_COVERAGE, hdr->line_coverage_valid);
+            hdr->line_coverage_valid = NULL;
+        }
 
         erts_free(ERTS_ALC_T_PREPARED_CODE, hdr);
         stp->load_hdr = NULL;
@@ -871,6 +879,12 @@ int beam_load_finish_emit(LoaderState *stp) {
                          (const char *)stp->beam.checksum,
                          sizeof(stp->beam.checksum));
 
+    /* Transfer ownership of the coverage tables to the prepared code. */
+    stp->load_hdr->coverage = stp->coverage;
+    stp->load_hdr->line_coverage_valid = stp->line_coverage_valid;
+    stp->coverage = NULL;
+    stp->line_coverage_valid = NULL;
+
     /* Move the code to its final location. */
     beamasm_codegen(stp->ba,
                     &stp->executable_region,
@@ -886,13 +900,6 @@ int beam_load_finish_emit(LoaderState *stp) {
     /* Save the updated code pointer and code size. */
     stp->code_hdr = code_hdr_ro;
     stp->loaded_size = module_size;
-
-    /* Transfer ownership of the coverage tables to the loaded code. */
-    code_hdr_rw->coverage = stp->coverage;
-    code_hdr_rw->line_coverage_valid = stp->line_coverage_valid;
-
-    stp->coverage = NULL;
-    stp->line_coverage_valid = NULL;
 
     /*
      * Place the literals in their own allocated heap (for fast range check)
@@ -1134,6 +1141,8 @@ void beam_load_finalize_code(LoaderState *stp,
     /* Prevent literals and code from being freed. */
     (stp->load_hdr)->literal_area = NULL;
     stp->load_hdr->are_nifs = NULL;
+    stp->load_hdr->coverage = NULL;
+    stp->load_hdr->line_coverage_valid = NULL;
     stp->executable_region = NULL;
     stp->writable_region = NULL;
     stp->code_hdr = NULL;