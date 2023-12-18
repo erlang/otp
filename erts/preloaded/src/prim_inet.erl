e34b8beb5d9f4625f4080bfa3d12330404a75bbc
Micael Karlberg <bmk@erlang.org>
2023-12-18 14:25:26 +0100

    [erts|preloaded] Fix option reuseaddr for FreeBSD 14
    On FreeBSD 14 (at the moment) the socket option 'reuseaddr' is
    not "inherited" (from the listen socket) by accepted socket.
    So, attempt to "patch" this by adding 'reuseaddr' to the list
    of options that are explicitly "copied" from the listen socket
    to the accepted socket.

--- erts/preloaded/src/prim_inet.erl
+++ erts/preloaded/src/prim_inet.erl
@@ -493,7 +493,7 @@ accept_opts(L, S, FamilyOpts) ->
     case
         getopts(
           L,
-          [active, nodelay, keepalive, delay_send, priority, linger]
+          [active, nodelay, keepalive, delay_send, priority, linger, reuseaddr]
           ++ FamilyOpts)
     of
 	{ok, Opts} ->
