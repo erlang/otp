5794aaaf52f2b313b702f047fd81722484cb03dc
Micael Karlberg <bmk@erlang.org>
2024-01-18 11:58:31 +0100

    [snmp] Types and specs for snmp_user_based_sm_mib:reconfigure/1
    OTP-18934

--- lib/snmp/src/agent/snmp_user_based_sm_mib.erl
+++ lib/snmp/src/agent/snmp_user_based_sm_mib.erl
@@ -162,9 +162,13 @@ configure(ConfDir) ->
 %% Fails: exit(configuration_error) |
 %%        exit({unsupported_crypto, Function})
 %%-----------------------------------------------------------------
-reconfigure(Dir) ->
+
+-spec reconfigure(ConfDir) -> snmp:void() when
+      ConfDir :: string().
+
+reconfigure(ConfDir) ->
     set_sname(),
-    case (catch do_reconfigure(Dir)) of
+    case (catch do_reconfigure(ConfDir)) of
 	ok ->
 	    ok;
 	{error, Reason} ->
