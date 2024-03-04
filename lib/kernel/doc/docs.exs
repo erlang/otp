9ddea3b3da82c54b01bd0a3c14872a0fe6dee559
Lukas Larsson <lukas@erlang.org>
2024-03-04 11:06:27 +0100

    kernel: Add groups to kernel module documentation

--- lib/kernel/doc/docs.exs
+++ lib/kernel/doc/docs.exs
@@ -1,4 +1,34 @@
 [
+  groups_for_modules:
+    [
+      Networking: [:gen_tcp, :gen_udp, :gen_sctp, :socket, :inet, :inet_res, :net],
+      Distribution: [
+        :net_adm,
+        :net_kernel,
+        :auth,
+        :erl_boot_server,
+        :erl_epmd,
+        :erpc,
+        :global,
+        :global_group,
+        :pg,
+        :rpc
+      ],
+      Logging: [
+        :logger,
+        :disk_log,
+        :error_logger,
+        :logger_disk_log_h,
+        :logger_filters,
+        :logger_formatter,
+        :logger_handler,
+        :logger_std_h,
+        :wrap_log_reader
+      ],
+      Code: [:code, :erl_ddll, :error_handler],
+      "File, OS and tracing": [:heart, :os, :file, :seq_trace]
+    ]
+    |> Enum.sort(),
   skip_code_autolink_to: ["t:file_descriptor/0"],
   ## The order of these items determine
   ## how they are listed in the docs
