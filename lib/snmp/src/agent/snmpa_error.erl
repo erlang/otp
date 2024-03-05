8f2a0dd4583d5438c920dfe93a89ad64593bb015
Micael Karlberg <bmk@erlang.org>
2024-03-05 18:51:38 +0100

    [snmp] Tweaked doc and some cosmetic stuff
    OTP-18934

--- lib/snmp/src/agent/snmpa_error.erl
+++ lib/snmp/src/agent/snmpa_error.erl
@@ -33,8 +33,6 @@ Two simple implementation(s) is provided with the toolkit; the modules
 
 The error report module is configured using the directive `error_report_mod`,
 see [configuration parameters](snmp_config.md#configuration_params).
-
-[](){: #config_err }
 """.
 
 -behaviour(snmpa_error_report).
@@ -56,6 +54,8 @@ The function is called if a user related error occurs at run-time, for example
 if a user defined instrumentation function returns erroneous.
 
 `Format` and `Args` are as in `io:format(Format, Args)`.
+
+[](){: #user_err }
 """.
 -spec user_err(Format, Args) -> snmp:void() when
       Format :: string(),
@@ -77,7 +77,7 @@ example if a syntax error is found in a configuration file.
 
 `Format` and `Args` are as in `io:format(Format, Args)`.
 
-[](){: #user_err }
+[](){: #config_err }
 """.
 -spec config_err(Format, Args) -> snmp:void() when
       Format :: string(),
