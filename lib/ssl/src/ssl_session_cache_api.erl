f99aa6623fd040ebe9298b29b365a0adfb87435e
Ingela Anderton Andin <ingela@erlang.org>
2024-03-22 17:30:36 +0100

    ssl: Polish behaviour APIs

--- lib/ssl/src/ssl_session_cache_api.erl
+++ lib/ssl/src/ssl_session_cache_api.erl
@@ -32,51 +32,88 @@ scheme can be replaced by defining a new callback module implementing this API.
 -include("ssl_internal.hrl").
 -include("ssl_api.hrl").
 
--export_type([session_cache_key/0, session/0, partial_key/0, session_cache_ref/0]).
+-export_type([session_cache_key/0, 
+              session/0, 
+              partial_key/0, 
+              session_cache_ref/0]).
 
+-doc "A term that can be used to reference the cache.".
 -type session_cache_ref() :: any().
+
 -doc "A key to an entry in the session cache.".
 -type session_cache_key() :: {partial_key(), ssl:session_id()}.
+
 -doc "The session data that is stored for each session.".
 -opaque session()         :: #session{}.
+
 -doc "The opaque part of the key. Does not need to be handled by the callback.".
 -opaque partial_key()     :: {ssl:host(), inet:port_number()} | inet:port_number().
 
 -doc """
-Includes property `{role, client | server}`. Currently this is the only
-predefined property, there can also be user-defined properties. See also
-application environment variable [session_cb_init_args](ssl_app.md).
-
 Performs possible initializations of the cache and returns a reference to it
-that is used as parameter to the other API functions. Is called by the cache
-handling processes `init` function, hence putting the same requirements on it as
-a normal process `init` function. This function is called twice when starting
-the SSL application, once with the role client and once with the role server, as
-the SSL application must be prepared to take on both roles.
+that is used as parameter to the other API functions. 
+
+Is called by the cache handling processes `init` function, hence
+putting the same requirements on it as a normal process `init`
+function. This function is called twice when starting the SSL
+application, once with the role client and once with the role server,
+as the SSL application must be prepared to take on both roles.
+
+Includes property `{role, client | server}` in init argument list. 
+Currently this is the only predefined property, there can also be
+user-defined properties. See also application environment variable
+[session_cb_init_args](ssl_app.md).
 """.
 -doc(#{since => <<"OTP 18.0">>}).
--callback init(list()) -> session_cache_ref().
+-callback init(InitArgs) -> CacheRef when 
+      InitArgs :: list(),
+      CacheRef :: session_cache_ref().
+
+-doc(#{since => <<"OTP R14B">>}).
 -doc """
 Takes care of possible cleanup that is needed when the cache handling process
 terminates.
 """.
+-callback terminate(CacheRef) -> DoNotCare when 
+      CacheRef :: session_cache_ref(),
+      DoNotCare :: any().
+
 -doc(#{since => <<"OTP R14B">>}).
--callback terminate(session_cache_ref()) -> any().
 -doc "Looks up a cache entry. Is to be callable from any process.".
+-callback lookup(CacheRef, Key) -> Session when
+      CacheRef:: session_cache_ref(),
+      Key :: session_cache_key(),
+      Session :: session() | undefined.
+
 -doc(#{since => <<"OTP R14B">>}).
--callback lookup(session_cache_ref(), session_cache_key()) -> #session{} | undefined.
 -doc """
-Caches a new session or updates an already cached one. Is only called from the
-cache handling process.
+Caches a new session or updates an already cached one.
+
+Is only called from the cache handling process.
 """.
+-callback update(CacheRef, Key, Session) -> DoNotCare when
+      CacheRef:: session_cache_ref(),
+      Key :: session_cache_key(),
+      Session :: session() | undefined,
+      DoNotCare :: any().
+
 -doc(#{since => <<"OTP R14B">>}).
--callback update(session_cache_ref(), session_cache_key(), #session{}) -> any().
--doc "Deletes a cache entry. Is only called from the cache handling process.".
+-doc """
+Deletes a cache entry.
+
+Is only called from the cache handling process.
+""".
+-callback delete(CacheRef, Key) -> DoNotCare when
+      CacheRef:: session_cache_ref(),
+      Key :: session_cache_key(),
+      DoNotCare :: any().
+
 -doc(#{since => <<"OTP R14B">>}).
--callback delete(session_cache_ref(), session_cache_key()) -> any().
 -doc """
 Calls `Fun(Elem, AccIn)` on successive elements of the cache, starting with
-`AccIn == Acc0`. `Fun/2` must return a new accumulator, which is passed to the
+ `AccIn == Acc0`.
+
+`Fun/2` must return a new accumulator, which is passed to the
 next call. The function returns the final value of the accumulator. `Acc0` is
 returned if the cache is empty.
 
@@ -85,8 +122,13 @@ returned if the cache is empty.
 > Since OTP-23.3 this functions is only used on the client side and does not
 > need to implemented for a server cache.
 """.
+-callback foldl(Fun, Acc0, CacheRef) ->  Acc when
+      Fun::fun(),
+      Acc0::term(),
+      CacheRef:: session_cache_ref(),
+      Acc::term().
+
 -doc(#{since => <<"OTP R14B">>}).
--callback foldl(fun(), term(), session_cache_ref()) -> term().
 -doc """
 Selects sessions that can be reused, that is sessions that include `PartialKey`
 in its key. Is to be callable from any process.
@@ -96,14 +138,21 @@ in its key. Is to be callable from any process.
 > Since OTP-23.3 This functions is only used on the client side and does not
 > need to implemented for a server cache.
 """.
--doc(#{since => <<"OTP R14B">>}).
--callback select_session(session_cache_ref(), {ssl:host(), inet:port_number()} | inet:port_number()) -> [#session{}].
--doc """
-Returns the number of sessions in the cache. If size exceeds the maximum number
-of sessions, the current cache entries will be invalidated regardless of their
-remaining lifetime. Is to be callable from any process.
-""".
+-callback select_session(CacheRef, Server) -> Sessions when
+      CacheRef :: session_cache_ref(),
+      Server ::{ssl:host(), inet:port_number()} | inet:port_number(),
+      Sessions :: [session()].
+
 -doc(#{since => <<"OTP 19.3">>}).
--callback size(session_cache_ref()) -> integer().
+-doc """
+Returns the number of sessions in the cache.
+
+If size exceeds the maximum number of sessions, the current cache
+entries will be invalidated regardless of their remaining lifetime. Is
+to be callable from any process.
+""".
+-callback size(CacheRef) -> Size when
+      CacheRef:: session_cache_ref(),
+      Size :: pos_integer().
 
 -optional_callbacks([select_session/2, foldl/3]).
