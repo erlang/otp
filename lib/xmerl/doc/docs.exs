8dc9eab0088d0765f2418fe93f0b3eaa770e92bd
Raimo Niskanen <raimo@erlang.org>
2024-05-03 19:04:42 +0200

    Rework documentation after Markdown conversion

--- lib/xmerl/doc/docs.exs
+++ lib/xmerl/doc/docs.exs
@@ -1,5 +1,5 @@
 [
-  skip_code_autolink_to: ["t:xmerl_scanner/0"],
+  skip_code_autolink_to: ["t:xmerl_scanner/0", "t:xsd_state/0"],
 
   ## The order of these items determine
   ## how they are listed in the docs
