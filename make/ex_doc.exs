e34ecfc5257e090983526a82313e8800f984d3cc
Lukas Larsson <lukas@erlang.org>
2024-05-31 10:41:50 +0200

    docs: Fix groups_for_docs in local config file

--- make/ex_doc.exs
+++ make/ex_doc.exs
@@ -174,7 +174,7 @@ config = [
   extras: extras,
   skip_undefined_reference_warnings_on: ["notes.md"],
   groups_for_docs:
-    (Access.get(local_config, :groups_for_extras, []) ++ groups_for_docs) |> Enum.uniq(),
+    (Access.get(local_config, :groups_for_docs, []) ++ groups_for_docs) |> Enum.uniq(),
   deps:
     deps ++
       [
@@ -227,4 +227,7 @@ config = [
   end
 ]
 
-Keyword.merge(config, local_config |> Keyword.drop([:extras, :groups_for_extras]))
+Keyword.merge(
+  config,
+  local_config |> Keyword.drop([:extras, :groups_for_extras, :group_for_docs])
+)
