a2b7c68b6433d25653a037a24ce6419a94aea121
Micael Karlberg <bmk@erlang.org>
2024-06-03 11:42:51 +0200

    [diameter|test] Add factor check for dict test case of examples suite

--- lib/diameter/test/diameter_examples_SUITE.erl
+++ lib/diameter/test/diameter_examples_SUITE.erl
@@ -35,6 +35,8 @@
          all/0,
          init_per_suite/1,
          end_per_suite/1,
+         init_per_testcase/2,
+         end_per_testcase/2,
          
          %% The test cases
          dict/1,
@@ -94,6 +96,31 @@ init_per_suite(Config) ->
 end_per_suite(Config) ->
     ?DUTIL:end_per_suite(Config).
 
+
+%% This test case can take a *long* time, so if the machine is too slow, skip
+init_per_testcase(dict = Case, Config) when is_list(Config) ->
+    ?EL("init_per_testcase(~w) -> check factor", [Case]),
+    Key = dia_factor,
+    case lists:keysearch(Key, 1, Config) of
+        {value, {Key, Factor}} when (Factor > 10) ->
+            ?EL("init_per_testcase(~w) -> Too slow (~w) => SKIP",
+                [Case, Factor]),
+            {skip, {machine_too_slow, Factor}};
+        _ ->
+            ?EL("init_per_testcase(~w) -> run test", [Case]),
+            Config
+    end;
+init_per_testcase(Case, Config) ->
+    ?EL("init_per_testcase(~w) -> entry", [Case]),
+    Config.
+
+
+end_per_testcase(Case, Config) when is_list(Config) ->
+    ?EL("end_per_testcase(~w) -> entry", [Case]),
+    Config.
+
+
+%% ===========================================================================
 
 dict(Config) ->
     ?EL("dict -> entry with"
