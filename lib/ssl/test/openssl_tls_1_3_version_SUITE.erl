a20816ad31b0c854393df62efcb6479d27b434f7
Jakub Witczak <kuba@erlang.org>
2024-10-03 10:58:20 +0200

    ssl: fix test code compilation warnings

--- lib/ssl/test/openssl_tls_1_3_version_SUITE.erl
+++ lib/ssl/test/openssl_tls_1_3_version_SUITE.erl
@@ -36,8 +36,8 @@
         ]).
 
 %% Test cases
--export([tls12_client_tls13_server/1
-        ]).
+-export([tls12_client_tls13_server/0,
+         tls12_client_tls13_server/1]).
 
 
 %%--------------------------------------------------------------------
@@ -124,15 +124,15 @@ end_per_group(GroupName, Config) ->
 %% In its ClientHello the supported_versions extension contains only one element
 %% [{3,4}] that the server does not accept if it is configured to not support
 %% TLS 1.3.
-tls13_client_tls12_server() ->
-    [{doc,"Test that a TLS 1.3 client can connect to a TLS 1.2 server."}].
+%% tls13_client_tls12_server() ->
+%%     [{doc,"Test that a TLS 1.3 client can connect to a TLS 1.2 server."}].
 
-tls13_client_tls12_server(Config) when is_list(Config) ->
-    ClientOpts = [{versions,
-                   ['tlsv1.3', 'tlsv1.2']} | ssl_test_lib:ssl_options(client_cert_opts, Config)],
-    ServerOpts =  [{versions,
-                   ['tlsv1.1', 'tlsv1.2']} | ssl_test_lib:ssl_options(server_cert_opts, Config)],
-    ssl_test_lib:basic_test(ClientOpts, ServerOpts, Config).
+%% tls13_client_tls12_server(Config) when is_list(Config) ->
+%%     ClientOpts = [{versions,
+%%                    ['tlsv1.3', 'tlsv1.2']} | ssl_test_lib:ssl_options(client_cert_opts, Config)],
+%%     ServerOpts =  [{versions,
+%%                    ['tlsv1.1', 'tlsv1.2']} | ssl_test_lib:ssl_options(server_cert_opts, Config)],
+%%     ssl_test_lib:basic_test(ClientOpts, ServerOpts, Config).
     
 %% tls13_client_with_ext_tls12_server() ->
 %%      [{doc,"Test basic connection between TLS 1.2 server and TLS 1.3 client when " 
