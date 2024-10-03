7f4340affb9b83797344cd222008fa06b6972a96
Marko Mindek <marko.mindek@gmail.com>
2024-10-03 10:46:01 +0200

    get_option error handling

--- lib/inets/src/http_client/httpc.erl
+++ lib/inets/src/http_client/httpc.erl
@@ -700,13 +700,11 @@ Sets options to be used for subsequent requests.
       DomainDesc :: string(),
       HostName :: uri_string:uri_string().
 set_options(Options, Profile) when is_atom(Profile) orelse is_pid(Profile) ->
-    {ok, IpFamily} = get_option(ipfamily, Profile),
-    {ok, UnixSock} = get_option(unix_socket, Profile),
-    case validate_options(Options, IpFamily, UnixSock) of
-	{ok, Opts} ->
-	    httpc_manager:set_options(Opts, profile_name(Profile));
-	{error, Reason} ->
-	    {error, Reason}
+    maybe
+        {ok, IpFamily} ?= get_option(ipfamily, Profile),
+        {ok, UnixSock} ?= get_option(unix_socket, Profile),
+        {ok, Opts} ?= validate_options(Options, IpFamily, UnixSock),
+	httpc_manager:set_options(Opts, profile_name(Profile))
     end.
 
 -doc false.
