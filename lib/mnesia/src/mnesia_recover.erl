e30ffd10f516e215a88f7360257621b89e1731e6
Richard Carlsson <carlsson.richard@gmail.com>
2024-12-03 13:50:29 +0100

    Fix race condition getting date and time in mnesia_recover
    This used an "export from subexpression" variable assignment style which
    warranted rewriting, but also had a race condition where the time and
    date could be sampled on separate sides of a date change.

--- lib/mnesia/src/mnesia_recover.erl
+++ lib/mnesia/src/mnesia_recover.erl
@@ -779,7 +779,8 @@ handle_call(Msg, _From, State) ->
     {noreply, State}.
 
 do_log_mnesia_up(Node) ->
-    Yoyo = {mnesia_up, Node, Date = date(), Time = time()},
+    {Date, Time} = erlang:localtime(),
+    Yoyo = {mnesia_up, Node, Date, Time},
     case mnesia_monitor:use_dir() of
 	true ->
 	    mnesia_log:append(latest_log, Yoyo),
@@ -790,7 +791,8 @@ do_log_mnesia_up(Node) ->
     note_up(Node, Date, Time).
 
 do_log_mnesia_down(Node) ->
-    Yoyo = {mnesia_down, Node, Date = date(), Time = time()},
+    {Date, Time} = erlang:localtime(),
+    Yoyo = {mnesia_down, Node, Date, Time},
     case mnesia_monitor:use_dir() of
 	true ->
 	    mnesia_log:append(latest_log, Yoyo),
