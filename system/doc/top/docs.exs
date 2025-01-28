2d30479403ac015fbed6aed3547dc4837483858c
Lukas Backström <lukas@erlang.org>
2025-01-28 16:04:32 +0100

    otp: Fix redirect js scripts

--- system/doc/top/docs.exs
+++ system/doc/top/docs.exs
@@ -3,9 +3,20 @@ system_guides =
   |> String.trim()
   |> String.split("\n")
   |> Enum.map(fn line ->
-    guide = line |> String.split(":") |> Enum.at(0)
-    "system/#{guide}.md"
+    line |> String.split(":") |> Enum.at(0)
   end)
+
+apps =
+  Path.wildcard("{core,database,oam,interfaces,tools,testing,documentation}/*.md")
+  |> Enum.map(&Path.basename/1)
+  |> Enum.map(&Path.rootname/1)
+
+redirects =
+  Enum.map(apps, fn
+    "erts" -> {"erts", "../erts/doc/html/index"}
+    app -> {app, "../lib/#{app}/doc/html/index"}
+  end) ++
+    Enum.map(system_guides, fn guide -> {guide, "../system/doc/html/#{guide}"} end)
 
 [
   application: :index,
@@ -18,10 +29,11 @@ system_guides =
       "../general_info/scheduled_for_removal.md",
       "../general_info/upcoming_incompatibilities.md"
     ] ++
-      system_guides ++
+      (system_guides |> Enum.map(fn guide -> "system/#{guide}.md" end)) ++
       Path.wildcard("{core,database,oam,interfaces,tools,testing,documentation}/*.md"),
   main: "readme",
   api_reference: false,
+  redirects: Map.new(redirects),
   groups_for_extras: [
     "System Documentation": ~r{system},
     Core: ~r/core/,
