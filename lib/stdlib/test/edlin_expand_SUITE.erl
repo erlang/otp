8c187a8d1d0bf3547621ca65b2942fe389736d8d
Fredrik Frantzen <frazze@erlang.org>
2025-02-10 13:22:30 +0100

    stdlib: update edlin_expand to group functions based on eep48 group

--- lib/stdlib/test/edlin_expand_SUITE.erl
+++ lib/stdlib/test/edlin_expand_SUITE.erl
@@ -85,16 +85,17 @@ normal(Config) when is_list(Config) ->
     {yes,"test:",[]} = do_expand("expand_"),
     {no, [], []} = do_expand("expandXX_"),
     {no,[],[#{
-               title:="functions",
+               title:="Functions",
                elems:=[{"a_fun_name",[{ending,"("}]},
                         {"a_less_fun_name",_},
                         {"b_comes_after_a",_},
                         {"expand0arity_entirely",_},
                         {"module_info",_}]
               }]} = do_expand("expand_test:"),
-    {yes,[],[#{title:="functions",
+    {yes,[],[#{title:="Functions",
                       elems:=[{"a_fun_name",_},{"a_less_fun_name",_}]}]} = do_expand("expand_test:a_"),
     {yes,"arity_entirely()",[]} = do_expand("expand_test:expand0"),
+    {no, [], [#{title:="Functions"}, #{title:="Obsolete API functions"}]} = do_expand("string:"),
     ok.
 
 to_atom(Str) ->
@@ -524,7 +525,7 @@ get_coverage(Config) ->
     lists:flatten(edlin_expand:format_matches(M10, 20)),
     %% Test that we are not filtering duplicates bit with different case or different string lengths
     {yes,"e", M11} = do_expand("complete_function_parameter:cas"),
-    "\e[;1;4mfunctions\e[0m\ncaseSensitiveFunction(        casesensitivefunction(        \ncaseSensitiveFunctionName(\n" = do_format(M11),
+    "\e[;1;4mFunctions\e[0m\ncaseSensitiveFunction(        casesensitivefunction(        \ncaseSensitiveFunctionName(\n" = do_format(M11),
     ok.
 
 %% Normal module name, some function names using quoted atoms.
@@ -534,7 +535,7 @@ quoted_fun(Config) when is_list(Config) ->
     %% should be no colon after test this time
     {yes, "test", [#{title:="modules", elems:=[{"expand_test",[{ending, ":"}]},{"expand_test1",_}]}]} = do_expand("expand_"),
     {no, [], []} = do_expand("expandXX_"),
-    {no,[],[#{title:="functions",
+    {no,[],[#{title:="Functions",
                      elems:=[{"'#weird-fun-name'",_},
                             {"'Quoted_fun_name'",_},
                             {"'Quoted_fun_too'",_},
@@ -567,7 +568,7 @@ quoted_module(Config) when is_list(Config) ->
                             {"a_less_fun_name",_},
                             {"b_comes_after_a",_},
                             {"module_info",_}]}]} = do_expand("'ExpandTestCaps':"),
-    {yes,[],[#{title:="functions", elems:=[{"a_fun_name",_},
+    {yes,[],[#{title:="Functions", elems:=[{"a_fun_name",_},
                                                 {"a_less_fun_name",_}]}]} = do_expand("'ExpandTestCaps':a_"),
     ok.
 
