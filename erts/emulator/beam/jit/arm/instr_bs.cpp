erlang/otp

commit 3290f0a8078beb352195b80037088424f59fe7a5

Refactor code generation for matching of integer segments
Refactor the code generation for matching of integer segments with
dynamic sizes to improve clarity. Also add a slight optimization by
calling `erts_bs_get_integer_2()` instead of the more expensive
`beam_jit_bs_get_integer()` for known small integers.

erts/emulator/beam/jit/arm/instr_bs.cpp
@@ -245,6 +245,60 @@ void BeamModuleAssembler::emit_i_bs_get_position(const ArgRegister &Ctx,
     flush_var(dst_reg);
 }
 
+void BeamModuleAssembler::emit_bs_get_small(const Label &fail,
+                                            const ArgRegister &Ctx,
+                                            const ArgWord &Live,
+                                            const ArgSource &Sz,
+                                            Uint unit,
+                                            Uint flags) {
+    if (emit_bs_get_field_size(Sz, unit, fail, ARG2) >= 0) {
+        auto ctx_reg = load_source(Ctx, TMP1);
+
+        comment("simplified helper call because the result is a known small");
+        emit_enter_runtime(Live.get());
+
+        /* We KNOW that the process argument is never actually used. */
+#ifdef DEBUG
+        mov_imm(ARG1, 0);
+#endif
+        mov_imm(ARG3, flags);
+        emit_untag_ptr(ARG4, ctx_reg.reg);
+        runtime_call<Eterm (*)(Process *, Uint, unsigned, ErlSubBits *),
+                     erts_bs_get_integer_2>();
+
+        emit_leave_runtime(Live.get());
+
+        emit_branch_if_not_value(ARG1, fail);
+    }
+}
+
+void BeamModuleAssembler::emit_bs_get_any_int(const Label &fail,
+                                              const ArgRegister &Ctx,
+                                              const ArgWord &Live,
+                                              const ArgSource &Sz,
+                                              Uint unit,
+                                              Uint flags) {
+    if (emit_bs_get_field_size(Sz, unit, fail, ARG5) >= 0) {
+        a.mov(ARG1, c_p);
+        load_x_reg_array(ARG2);
+        mov_arg(ARG3, Ctx);
+        mov_imm(ARG4, flags);
+        mov_arg(ARG6, Live);
+
+        emit_enter_runtime<Update::eHeapAlloc | Update::eXRegs |
+                           Update::eReductions>(Live.get());
+        runtime_call<Eterm (*)(Process *, Eterm *, Eterm, Uint, Uint, Uint),
+                     beam_jit_bs_get_integer>();
+        emit_leave_runtime<Update::eHeapAlloc | Update::eXRegs |
+                           Update::eReductions>(Live.get());
+
+        emit_branch_if_not_value(ARG1, fail);
+        /* Test for max heap size exceeded. */
+        emit_is_not_cons(resolve_fragment(ga->get_do_schedule(), dispUnknown),
+                         ARG1);
+    }
+}
+
 void BeamModuleAssembler::emit_bs_get_integer2(const ArgLabel &Fail,
                                                const ArgRegister &Ctx,
                                                const ArgWord &Live,
@@ -279,64 +333,17 @@ void BeamModuleAssembler::emit_bs_get_integer2(const ArgLabel &Fail,
     } else {
         Label fail = resolve_beam_label(Fail, dispUnknown);
         int unit = Unit.get();
+        auto max = std::get<1>(getClampedRange(Sz));
+        bool potential_gc =
+                max >= SMALL_BITS || (max * Unit.get()) >= SMALL_BITS;
 
-        if (emit_bs_get_field_size(Sz, unit, fail, ARG5) >= 0) {
-            /* If there cannot possibly be a GC in the code that
-             * follows, we can avoid loading registers that will never
-             * be used. */
-            auto max = std::get<1>(getClampedRange(Sz));
-            bool potential_gc =
-                    max >= SMALL_BITS || (max * Unit.get()) >= SMALL_BITS;
-
-            mov_arg(ARG3, Ctx);
-            mov_imm(ARG4, flags);
-            if (potential_gc) {
-                mov_arg(ARG6, Live);
-            } else {
-#ifdef DEBUG
-                /* Never actually used. */
-                mov_imm(ARG6, 1023);
-#endif
-            }
-
-            if (potential_gc) {
-                emit_enter_runtime<Update::eHeapAlloc | Update::eXRegs |
-                                   Update::eReductions>(Live.get());
-            } else {
-                comment("simplified entering runtime because result is always "
-                        "small");
-                emit_enter_runtime(Live.get());
-            }
-
-            a.mov(ARG1, c_p);
-            if (potential_gc) {
-                load_x_reg_array(ARG2);
-            } else {
-#ifdef DEBUG
-                /* Never actually used. */
-                mov_imm(ARG2, 0);
-#endif
-            }
-            runtime_call<Eterm (*)(Process *, Eterm *, Eterm, Uint, Uint, Uint),
-                         beam_jit_bs_get_integer>();
-
-            if (potential_gc) {
-                emit_leave_runtime<Update::eHeapAlloc | Update::eXRegs |
-                                   Update::eReductions>(Live.get());
-            } else {
-                emit_leave_runtime(Live.get());
-            }
-
-            emit_branch_if_not_value(ARG1, fail);
-            if (potential_gc) {
-                /* Test for max heap size exceeded. */
-                emit_is_not_cons(
-                        resolve_fragment(ga->get_do_schedule(), dispUnknown),
-                        ARG1);
-            }
-
-            mov_arg(Dst, ARG1);
+        if (potential_gc) {
+            emit_bs_get_any_int(fail, Ctx, Live, Sz, unit, flags);
+        } else {
+            emit_bs_get_small(fail, Ctx, Live, Sz, unit, flags);
         }
+
+        mov_arg(Dst, ARG1);
     }
 }
 

erts/emulator/beam/jit/x86/instr_bs.cpp
@@ -256,6 +256,67 @@ void BeamModuleAssembler::emit_i_bs_get_position(const ArgRegister &Ctx,
     mov_arg(Dst, ARG1);
 }
 
+void BeamModuleAssembler::emit_bs_get_small(const Label &fail,
+                                            const ArgRegister &Ctx,
+                                            const ArgWord &Live,
+                                            const ArgSource &Sz,
+                                            Uint unit,
+                                            Uint flags) {
+    /* Clobbers RET + ARG3, returns a negative result if we always
+     * fail and further work is redundant. */
+    if (emit_bs_get_field_size(Sz, unit, fail, ARG2) >= 0) {
+        comment("simplified helper call because the result is a known small");
+
+        mov_imm(ARG3, flags);
+        mov_arg(ARG4, Ctx);
+        a.sub(ARG4, imm(TAG_PRIMARY_BOXED));
+
+        emit_enter_runtime();
+
+        /* We KNOW that the process argument is never actually used. */
+#ifdef DEBUG
+        mov_imm(ARG1, 0);
+#endif
+        runtime_call<Eterm (*)(Process *, Uint, unsigned, ErlSubBits *),
+                     erts_bs_get_integer_2>();
+
+        emit_leave_runtime();
+
+        emit_test_the_non_value(RET);
+        a.je(fail);
+    }
+}
+
+void BeamModuleAssembler::emit_bs_get_any_int(const Label &fail,
+                                              const ArgRegister &Ctx,
+                                              const ArgWord &Live,
+                                              const ArgSource &Sz,
+                                              Uint unit,
+                                              Uint flags) {
+    /* Clobbers RET + ARG3, returns a negative result if we always
+     * fail and further work is redundant. */
+    if (emit_bs_get_field_size(Sz, unit, fail, ARG5) >= 0) {
+        mov_arg(ARG3, Ctx);
+        mov_imm(ARG4, flags);
+        mov_arg(ARG6, Live);
+
+        emit_enter_runtime<Update::eReductions | Update::eHeapAlloc>();
+
+        a.mov(ARG1, c_p);
+        load_x_reg_array(ARG2);
+        runtime_call<Eterm (*)(Process *, Eterm *, Eterm, Uint, Uint, Uint),
+                     beam_jit_bs_get_integer>();
+
+        emit_leave_runtime<Update::eReductions | Update::eHeapAlloc>();
+
+        emit_test_the_non_value(RET);
+        a.je(fail);
+
+        /* Test for max heap size exceeded. */
+        emit_is_not_cons(resolve_fragment(ga->get_do_schedule()), RET);
+    }
+}
+
 void BeamModuleAssembler::emit_bs_get_integer2(const ArgLabel &Fail,
                                                const ArgRegister &Ctx,
                                                const ArgWord &Live,
@@ -290,64 +351,17 @@ void BeamModuleAssembler::emit_bs_get_integer2(const ArgLabel &Fail,
     } else {
         Label fail = resolve_beam_label(Fail);
         int unit = Unit.get();
+        auto max = std::get<1>(getClampedRange(Sz));
+        bool potential_gc =
+                max >= SMALL_BITS || (max * Unit.get()) >= SMALL_BITS;
 
-        /* Clobbers RET + ARG3, returns a negative result if we always
-         * fail and further work is redundant. */
-        if (emit_bs_get_field_size(Sz, unit, fail, ARG5) >= 0) {
-            /* If there cannot possibly be a GC in the code that
-             * follows, we can avoid loading registers that will never
-             * be used. */
-            auto max = std::get<1>(getClampedRange(Sz));
-            bool potential_gc =
-                    max >= SMALL_BITS || (max * Unit.get()) >= SMALL_BITS;
-
-            mov_arg(ARG3, Ctx);
-            mov_imm(ARG4, flags);
-            if (potential_gc) {
-                mov_arg(ARG6, Live);
-            } else {
-#ifdef DEBUG
-                /* Never actually used. */
-                mov_imm(ARG6, 1023);
-#endif
-            }
-
-            if (potential_gc) {
-                emit_enter_runtime<Update::eReductions | Update::eHeapAlloc>();
-            } else {
-                comment("simplified entering runtime because result is always "
-                        "small");
-                emit_enter_runtime();
-            }
-
-            a.mov(ARG1, c_p);
-            if (potential_gc) {
-                load_x_reg_array(ARG2);
-            } else {
-#ifdef DEBUG
-                /* Never actually used. */
-                mov_imm(ARG2, 0);
-#endif
-            }
-
-            runtime_call<Eterm (*)(Process *, Eterm *, Eterm, Uint, Uint, Uint),
-                         beam_jit_bs_get_integer>();
-
-            if (potential_gc) {
-                emit_leave_runtime<Update::eReductions | Update::eHeapAlloc>();
-            } else {
-                emit_leave_runtime();
-            }
-
-            emit_test_the_non_value(RET);
-            a.je(fail);
-            if (potential_gc) {
-                /* Test for max heap size exceeded. */
-                emit_is_not_cons(resolve_fragment(ga->get_do_schedule()), RET);
-            }
-
-            mov_arg(Dst, RET);
+        if (potential_gc) {
+            emit_bs_get_any_int(fail, Ctx, Live, Sz, unit, flags);
+        } else {
+            emit_bs_get_small(fail, Ctx, Live, Sz, unit, flags);
         }
+
+        mov_arg(Dst, RET);
     }
 }
 

erts/emulator/beam/jit/arm/beam_asm.hpp
@@ -1255,6 +1255,20 @@ protected:
                                Label Fail,
                                const a64::Gp &out);
 
+    void emit_bs_get_small(const Label &fail,
+                           const ArgRegister &Ctx,
+                           const ArgWord &Live,
+                           const ArgSource &Sz,
+                           Uint unit,
+                           Uint flags);
+
+    void emit_bs_get_any_int(const Label &fail,
+                             const ArgRegister &Ctx,
+                             const ArgWord &Live,
+                             const ArgSource &Sz,
+                             Uint unit,
+                             Uint flags);
+
     void emit_bs_get_utf8(const ArgRegister &Ctx, const ArgLabel &Fail);
     void emit_bs_get_utf16(const ArgRegister &Ctx,
                            const ArgLabel &Fail,

erts/emulator/beam/jit/x86/beam_asm.hpp
@@ -1388,6 +1388,20 @@ protected:
                                const x86::Gp &out,
                                unsigned max_size = 0);
 
+    void emit_bs_get_small(const Label &fail,
+                           const ArgRegister &Ctx,
+                           const ArgWord &Live,
+                           const ArgSource &Sz,
+                           Uint unit,
+                           Uint flags);
+
+    void emit_bs_get_any_int(const Label &fail,
+                             const ArgRegister &Ctx,
+                             const ArgWord &Live,
+                             const ArgSource &Sz,
+                             Uint unit,
+                             Uint flags);
+
     void emit_bs_get_utf8(const ArgRegister &Ctx, const ArgLabel &Fail);
     void emit_bs_get_utf16(const ArgRegister &Ctx,
                            const ArgLabel &Fail,

commit 989eef5c91e76d713aa67b93f3c8fc5fe63cbb55

Eliminate crash in debug emulator on AArch64
In the debug emulator for the AArch64/ARM64 JIT, X registers
must not be accessed after entering runtime mode.

This bug was introduced in 3290f0a8078beb.

erts/emulator/beam/jit/arm/instr_bs.cpp
@@ -261,7 +261,6 @@ void BeamModuleAssembler::emit_bs_get_small(const Label &fail,
         auto ctx_reg = load_source(Ctx, TMP1);
 
         comment("simplified helper call because the result is a known small");
-        emit_enter_runtime(Live.get());
 
         /* We KNOW that the process argument is never actually used. */
 #ifdef DEBUG
@@ -269,6 +268,8 @@ void BeamModuleAssembler::emit_bs_get_small(const Label &fail,
 #endif
         mov_imm(ARG3, flags);
         emit_untag_ptr(ARG4, ctx_reg.reg);
+
+        emit_enter_runtime(Live.get());
         runtime_call<Eterm (*)(Process *, Uint, unsigned, ErlSubBits *),
                      erts_bs_get_integer_2>();
 