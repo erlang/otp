ae766abf7e851523622028bf4871d7ca28990869
Micael Karlberg <bmk@erlang.org>
2025-02-18 16:37:24 +0100

    [erts|esock] Partial requests
    When we only get part of the required length, send back a
    '{more, Bin}' completion status (instead of '{ok, Bin}').
    Also changed variable named (try to resolve which merge issues).
    
    OTP-19469

--- erts/emulator/nifs/win32/win_socket_asyncio.c
+++ erts/emulator/nifs/win32/win_socket_asyncio.c
@@ -3905,7 +3905,7 @@ ERL_NIF_TERM recv_check_ok(ErlNifEnv*       env,
                            ERL_NIF_TERM     sockRef,
                            ERL_NIF_TERM     recvRef)
 {
-    ERL_NIF_TERM data, result;
+    ERL_NIF_TERM data, eres;
     DWORD        read = 0, flags = 0;
 
     SSDBG( descP,
@@ -3944,7 +3944,7 @@ ERL_NIF_TERM recv_check_ok(ErlNifEnv*       env,
             ESOCK_CNT_INC(env, descP, sockRef,
                           esock_atom_read_fails, &descP->readFails, 1);
 
-            result = esock_make_error(env, esock_atom_closed);
+            eres = esock_make_error(env, esock_atom_closed);
             
         } else {
 
@@ -3974,7 +3974,7 @@ ERL_NIF_TERM recv_check_ok(ErlNifEnv*       env,
 
                 data = MKBIN(env, &opP->data.recv.buf);
 
-                result = esock_make_ok2(env, data);
+                eres = esock_make_ok2(env, data);
 
             } else if ((toRead == 0) ||
                        (descP->type != SOCK_STREAM)) {
@@ -3997,7 +3997,7 @@ ERL_NIF_TERM recv_check_ok(ErlNifEnv*       env,
                 data = MKBIN(env, &opP->data.recv.buf);
                 data = MKSBIN(env, data, 0, read);
 
-                result = esock_make_ok2(env, data);
+                eres = esock_make_ok2(env, data);
 
             } else {
                 
@@ -4018,8 +4018,7 @@ ERL_NIF_TERM recv_check_ok(ErlNifEnv*       env,
                 data = MKBIN(env, &opP->data.recv.buf);
                 data = MKSBIN(env, data, 0, read);
 
-                // result = esock_make_ok2(env, data);
-                result = MKT2(env, esock_atom_more, data);
+                eres = MKT2(env, esock_atom_more, data);
 
             }
 
@@ -4050,8 +4049,8 @@ ERL_NIF_TERM recv_check_ok(ErlNifEnv*       env,
 
             if (! IS_ZERO(recvRef)) {
                 
-                result = recv_check_pending(env, descP, opP, caller,
-                                            sockRef, recvRef);
+                eres = recv_check_pending(env, descP, opP, caller,
+                                          sockRef, recvRef);
             } else {
 
                 /* But we are not allowed to wait! => cancel */
@@ -4074,11 +4073,11 @@ ERL_NIF_TERM recv_check_ok(ErlNifEnv*       env,
                             "\r\n   %T"
                             "\r\n", sockRef, descP->sock, reason) );
 
-                    result = esock_make_error(env, MKT2(env, tag, reason));
+                    eres = esock_make_error(env, MKT2(env, tag, reason));
 
                 } else {
 
-                    result = esock_atom_ok; // Will trigger {error, timeout}
+                    eres = esock_atom_ok; // Will trigger {error, timeout}
 
                 }
             }
@@ -4100,7 +4099,7 @@ ERL_NIF_TERM recv_check_ok(ErlNifEnv*       env,
 
                 MUNLOCK(ctrl.cntMtx);
 
-                result = esock_make_error(env, reason);
+                eres = esock_make_error(env, reason);
             }
             break;
         }
@@ -4111,7 +4110,7 @@ ERL_NIF_TERM recv_check_ok(ErlNifEnv*       env,
             "\r\n",
             sockRef, descP->sock) );
 
-    return result;
+    return eres;
 }
 
 
@@ -9133,15 +9132,6 @@ ERL_NIF_TERM esaio_completion_recv_partial_done(ErlNifEnv*       env,
  *
  * A successful but only partial recv, which only partly fulfilled
  * the required read.
- * We do *not* want to risk ending up in a "never ending" read loop
- * here (by trying to read more data (and yet again getting partial)).
- * [worst case, we could up with all our worker threads busy trying
- * to read more data, and no one ready to respond to new requests].
- * So we simply return what we got to the user and let the user
- * decide what to do.
- *
- * We return the data as a complete result, and let "the user" figure
- * out what to do (done or continue).
  */
 
 static
@@ -9176,7 +9166,7 @@ ERL_NIF_TERM esaio_completion_recv_partial_part(ErlNifEnv*       env,
             "esaio_completion_recv_partial_part(%T) {%d} -> done\r\n",
             sockRef, descP->sock) );
 
-    return esock_make_ok2(opEnv, data);
+    return MKT2(env, esock_atom_more, data);
 
 }
 
