61be4997cf940461246e4c85ee358a721981dcba
Erlang/OTP <otp@erlang.org>
2025-02-20 15:17:44 +0100

    Prepare release

--- erts/vsn.mk
+++ erts/vsn.mk
@@ -18,7 +18,7 @@
 # %CopyrightEnd%
 # 
 
-VSN = 13.2.2.13
+VSN = 13.2.2.14
 
 # Port number 4365 in 4.2
 # Port number 4366 in 4.3
--- lib/ssh/vsn.mk
+++ lib/ssh/vsn.mk
@@ -1,4 +1,4 @@
 #-*-makefile-*-   ; force emacs to enter makefile-mode
 
-SSH_VSN = 4.15.3.9
+SSH_VSN = 4.15.3.10
 APP_VSN    = "ssh-$(SSH_VSN)"
