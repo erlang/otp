e74667cca64cf7f0dcb460b7224169d0afe0c176
Erlang/OTP <otp@erlang.org>
2025-05-05 18:19:46 +0200

    Update copyright year

--- lib/public_key/src/pubkey_moduli.hrl
+++ lib/public_key/src/pubkey_moduli.hrl
@@ -3,17 +3,18 @@
 %% SPDX-License-Identifier: BSD-2-Clause
 %%
 %% SPDX-FileCopyrightText: NOASSERTION
+%% Copyright Ericsson AB 2015-2025. All Rights Reserved.
 %%
 %% Redistribution and use in source and binary forms, with or without
 %% modification, are permitted provided that the following conditions
 %% are met:
-%% 
+%%
 %% 1. Redistributions of source code must retain the above copyright
 %% notice, this list of conditions and the following disclaimer.
 %% 2. Redistributions in binary form must reproduce the above copyright
 %% notice, this list of conditions and the following disclaimer in the
 %% documentation and/or other materials provided with the distribution.
-%% 
+%%
 %% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 %% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 %% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
