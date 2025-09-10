4a17b4e8b4cc96b81fa3c7b3756c564fea8dbdb8
Kiko Fernandez-Reyes <kiko@erlang.org>
2025-09-10 14:24:50 +0200

    add files with missing licenses

--- lib/wx/.dir-locals.el
+++ lib/wx/.dir-locals.el
@@ -1,3 +1,11 @@
+;; %CopyrightBegin%
+;;
+;; SPDX-License-Identifier: Apache-2.0
+;;
+;; Copyright Ericsson AB 2021. All Rights Reserved.
+;;
+;; %CopyrightEnd%
+
 ;; wx Emacs settings
 (
  ;; In C++ code indentation is 2 spaces
--- lib/wx/vsn.mk
+++ lib/wx/vsn.mk
@@ -1,1 +1,9 @@
 WX_VSN = 2.5.1
+
+# %CopyrightBegin%
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Copyright Ericsson AB 1996-2025. All Rights Reserved.
+#
+# %CopyrightEnd%
