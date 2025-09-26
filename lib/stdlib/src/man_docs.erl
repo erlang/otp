ae6b9e7cf858345a9b908122bf3e292d109796aa
Fredrik Frantzen <frazze@erlang.org>
2025-09-26 13:43:32 +0200

    doc: create man pages for apps with source files split up in sub dirs

--- lib/stdlib/src/man_docs.erl
+++ lib/stdlib/src/man_docs.erl
@@ -20,31 +20,37 @@
 %% %CopyrightEnd%
 %%
 -module(man_docs).
+-moduledoc false.
+
 -include_lib("kernel/include/eep48.hrl").
 
--export([module_to_manpage/2, module_to_manpage/3, markdown_to_manpage/2]).
+-export([module_to_manpage/3, module_to_manpage/4, markdown_to_manpage/3]).
 
 %% Formats a module documentation as a roff man page.
 %% Fetches the documentation for a module with `code:get_doc/1`
--spec module_to_manpage(Module, Path) -> unicode:chardata() when
+-spec module_to_manpage(Module, Path, Section) -> unicode:chardata() when
         Module :: module(),
-        Path :: string().
-module_to_manpage(Module, Path) when is_atom(Module) ->
+        Path :: string(),
+        Section :: string().
+module_to_manpage(Module, Path, Section) when is_atom(Module) ->
     case code:get_doc(Module) of
         {ok, Docs} ->
-                module_to_manpage(Module, Path, Docs);
+                module_to_manpage(Module, Path, Docs, Section);
         _Error ->
             ~""
     end.
--spec module_to_manpage(Module, Path, Docs) -> unicode:chardata() when
+-spec module_to_manpage(Module, Path, Docs, Section) -> unicode:chardata() when
         Module :: module(),
         Path :: string(),
-        Docs :: #docs_v1{}.
-module_to_manpage(_Module, _Path, #docs_v1{module_doc = None}) when None =:= none; None =:= hidden ->
+        Docs :: #docs_v1{},
+        Section :: string().
+module_to_manpage(_Module, _Path, #docs_v1{module_doc = None}, _Section) when None =:= none; None =:= hidden ->
     ~"";
-module_to_manpage(Module, Path, #docs_v1{module_doc = #{~"en" := ModuleDoc}, docs = AllDocs})
+module_to_manpage(Module, Path, #docs_v1{module_doc = #{~"en" := ModuleDoc}, docs = AllDocs}, Section)
     when is_atom(Module) ->
-    PreludeNDescription = markdown_to_manpage(ModuleDoc, Path),
+    PreludeNDescription = if is_binary(ModuleDoc) -> markdown_to_manpage(ModuleDoc, Path, Section);
+        true -> markdown_to_manpage1(ModuleDoc, Path, Section)
+    end,
 
     Types = [Doc || {{type,_,_},_,_,_,_}=Doc <- AllDocs],
     TypesSection = format_section("DATA TYPES", Types, Module, AllDocs),
@@ -56,10 +62,10 @@ module_to_manpage(Module, Path, #docs_v1{module_doc = #{~"en" := ModuleDoc}, doc
     iolist_to_binary([PreludeNDescription, TypesSection, FunctionsSection, CallbacksSection]).
 
 %% Formats markdown as a roff man page.
--spec markdown_to_manpage(binary() | shell_docs:chunk_elements(), file:filename()) -> binary().
-markdown_to_manpage(Markdown, Path) when is_binary(Markdown) ->
-        markdown_to_manpage(shell_docs_markdown:parse_md(Markdown), Path);
-markdown_to_manpage(MarkdownChunks, Path) ->
+-spec markdown_to_manpage(binary(), file:filename(), string()) -> binary().
+markdown_to_manpage(Markdown, Path, Section) ->
+        markdown_to_manpage1(shell_docs_markdown:parse_md(Markdown), Path, Section).
+markdown_to_manpage1(MarkdownChunks, Path, Section) ->
     Path1 = filename:absname(Path),
     App = case filename:split(string:prefix(Path1, os:getenv("ERL_TOP"))) of
         ["/", "lib", AppStr | _] ->
@@ -80,8 +86,8 @@ markdown_to_manpage(MarkdownChunks, Path) ->
     Extension = filename:extension(Path),
     FileName = list_to_binary(filename:rootname(filename:basename(Path), Extension)),
     Name = get_name(MarkdownChunks, FileName),
-    Prelude = io_lib:format(".TH ~s 3 \"~s ~s\" \"Ericsson AB\" \"Erlang Module Definition\"\n",
-                            [Name, atom_to_binary(App), Version]),
+    Prelude = io_lib:format(".TH ~s ~s \"~s ~s\" \"Ericsson AB\" \"Erlang Module Definition\"\n",
+                            [Name, Section, atom_to_binary(App), Version]),
     I = conv(MarkdownChunks, Name),
     iolist_to_binary([Prelude|I]).
 
@@ -113,10 +119,11 @@ conv([{h1,_,[Head]}|T],_) ->
     Name = ~".SH NAME\n",
     Desc = ~".SH DESCRIPTION\n",
     [Name,Head,$\n,Desc|format(T)];
-conv([H|T], Head) ->
+conv([{p,_,_}=ShortDesc0|T], Head) ->
     Name = ~".SH NAME\n",
     Desc = ~".SH DESCRIPTION\n",
-    [Name,Head,~" - ",format_one(H),$\n,Desc|format(T)].
+    [~".PP\n"|ShortDesc] = format_one(ShortDesc0),
+    [Name,Head,~B" \- ",ShortDesc,$\n,Desc|format(T)].
 
 escape(Text) when is_list(Text) ->
     escape(iolist_to_binary(Text));
@@ -183,16 +190,10 @@ format_p(Is0) ->
     Text = string:trim(Text0, leading),
     [~".PP\n",Text,$\n].
 
-format_p_item({code,_,Text}) ->
+format_p_item({Fi,_,Text}) when Fi =:= code; Fi =:= i; Fi =:= a ->
     [~B"\fI",format_p_item(Text),~B"\fR"];
-format_p_item({em,_,Text}) ->
+format_p_item({Fb,_,Text}) when Fb =:= em; Fb =:= strong ->
     [~B"\fB",format_p_item(Text),~B"\fR"];
-format_p_item({i,_,Text}) ->
-    [~B"\fI",format_p_item(Text),~B"\fR"];
-format_p_item({a,_,Text}) ->
-    [~B"\fI",format_p_item(Text),~B"\fR"];
-format_p_item({strong,_,Text}) ->
-    ["\\fB", format_p_item(Text), "\\fR"];
 format_p_item([H|T]) ->
     [format_p_item(H)|format_p_item(T)];
 format_p_item([]) ->
--- make/markdown_to_man.escript
+++ make/markdown_to_man.escript
@@ -27,44 +27,54 @@
 
 main(Args) ->
     try
-        case parse_args(Args, ".", []) of
+        case parse_args(Args, "3", ".", []) of
             {_,[]} ->
                 ok;
-            {OutDir,[_|_]=Files} ->
-                convert_files(Files, OutDir)
+            {OutDir,Section,[_|_]=Files} ->
+                convert_files(Files, OutDir, Section)
         end
     catch
         throw:{error,Error} ->
             io:put_chars(standard_error, Error)
     end.
 
-parse_args(["-o",OutDir|As], _OutDir, FilesAcc) ->
-    parse_args(As, OutDir, FilesAcc);
-parse_args([F|Fs], OutDir, FilesAcc) ->
-    parse_args(Fs, OutDir, [F|FilesAcc]);
-parse_args([], OutDir, FilesAcc) ->
-    {OutDir,lists:reverse(FilesAcc)}.
+parse_args(["-s", Num|As], _Section, OutDir, FilesAcc) ->
+    case lists:member(Num, ["1","3","4","6"]) of
+        true ->
+            parse_args(As, Num, OutDir, FilesAcc);
+        false ->
+            fail(io_lib:format("Invalid section number: ~s", [Num]))
+    end;
+parse_args(["-o",OutDir|As], Section, _OutDir, FilesAcc) ->
+    parse_args(As, Section, OutDir, FilesAcc);
+parse_args([F|Fs], Section, OutDir, FilesAcc) ->
+    parse_args(Fs, Section, OutDir, [F|FilesAcc]);
+parse_args([], Section, OutDir, FilesAcc) ->
+    {OutDir, Section, lists:reverse(FilesAcc)}.
 
-convert_files([F|Fs], OutDir) ->
-    convert_file(F, OutDir),
-    convert_files(Fs, OutDir);
-convert_files([], _) ->
+convert_files([F|Fs], OutDir, Section) ->
+    convert_file(F, OutDir, Section),
+    convert_files(Fs, OutDir, Section);
+convert_files([], _, _) ->
     ok.
 
-convert_file(Name, OutDir) ->
+convert_file(Name, OutDir, Section) ->
     case filename:extension(Name) of
         ".md" ->
             Base0 = filename:rootname(filename:basename(Name), ".md"),
             case lists:reverse(Base0) of
                     "dmc_" ++ Base1 ->
                             Base2 = lists:reverse(Base1),
-                            convert_markdown_to_man(Base2, OutDir, Name, ".1");
+                            convert_markdown_to_man(Base2, OutDir, Name, "1");
+                    "ppa_" ++ Base1 ->
+                            Base2 = lists:reverse(Base1),
+                            convert_markdown_to_man(Base2, OutDir, Name, "6");
                     _ ->
-                            convert_markdown_to_man(Base0, OutDir, Name, ".3")
+                            convert_markdown_to_man(Base0, OutDir, Name, Section)
                 end;
         ".erl" ->
             Base0 = filename:rootname(filename:basename(Name), ".erl"),
-            Output = man_docs:module_to_manpage(list_to_atom(Base0), Name),
+            Output = man_docs:module_to_manpage(list_to_atom(Base0), Name, "3"),
             Outfile = filename:join(OutDir, Base0 ++ ".3"),
             _ = filelib:ensure_dir(Outfile),
             case Output =/= <<>> andalso file:write_file(Outfile, Output) of
@@ -80,11 +90,11 @@ convert_file(Name, OutDir) ->
     end.
 
 convert_markdown_to_man(Base, OutDir, Name, Section) ->
-    OutFile = filename:join(OutDir, Base ++ Section),
+    OutFile = filename:join(OutDir, Base ++ "." ++ Section),
     _ = filelib:ensure_dir(OutFile),
     case file:read_file(Name) of
         {ok,Markdown} -> 
-            Man = man_docs:markdown_to_manpage(Markdown, Name),
+            Man = man_docs:markdown_to_manpage(Markdown, Name, Section),
             case file:write_file(OutFile, Man) of
                 ok ->
                     ok;
