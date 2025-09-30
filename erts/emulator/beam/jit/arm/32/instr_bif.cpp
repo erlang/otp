466ab91b36190e81a4aac4c19ac25d3724586ef7
Luca Succi <luca.succi@stritzinger.com>
2025-09-30 14:17:18 +0200

    Partial implementation of light BIF call support

--- erts/emulator/beam/jit/arm/32/instr_bif.cpp
+++ erts/emulator/beam/jit/arm/32/instr_bif.cpp
@@ -150,8 +150,23 @@ static Eterm debug_call_light_bif(Process *c_p,
                                   ErtsCodePtr I,
                                   ErtsBifFunc vbf) {
     Eterm result;
-    // TODO
-    ASSERT(false);
+
+    ERTS_ASSERT_TRACER_REFS(&c_p->common);
+    ERTS_UNREQ_PROC_MAIN_LOCK(c_p);
+    {
+        ERTS_CHK_MBUF_SZ(c_p);
+        ASSERT(!ERTS_PROC_IS_EXITING(c_p));
+        result = vbf(c_p, reg, I);
+        ASSERT(!ERTS_PROC_IS_EXITING(c_p) || is_non_value(result));
+        ERTS_CHK_MBUF_SZ(c_p);
+
+        ERTS_VERIFY_UNUSED_TEMP_ALLOC(c_p);
+        ERTS_HOLE_CHECK(c_p);
+    }
+    PROCESS_MAIN_CHK_LOCKS(c_p);
+    ERTS_REQ_PROC_MAIN_LOCK(c_p);
+    ERTS_ASSERT_TRACER_REFS(&c_p->common);
+
     return result;
 }
 #endif
@@ -160,19 +175,123 @@ static Eterm debug_call_light_bif(Process *c_p,
  * When doing any changes, make sure to look at the estone bif_dispatch
  * benchmark to make sure you don't introduce any regressions.
  *
+ * ARG2 = BIF pointer
  * ARG3 = entry
  * ARG4 = export entry
- * ARG8 = BIF pointer
  */
 void BeamGlobalAssembler::emit_call_light_bif_shared() {
-    // TODO
-    emit_nyi("emit_call_light_bif_shared");
+    arm::Mem entry_mem = TMP_MEM1q, export_mem = TMP_MEM2q,
+             mbuf_mem = TMP_MEM3q;
+
+    Label trace = a.newLabel(), yield = a.newLabel(), skip_trace = a.newLabel();
+
+    /* Spill everything we may need on the error and GC paths. */
+    a.ldr(TMP, arm::Mem(c_p, offsetof(Process, mbuf)));
+    a.str(TMP, mbuf_mem);
+    //make TMP point to TMP_MEM1q, stmia does not support offsets
+    arm::Mem tmp_mem1q = arm::Mem(TMP);
+    a.mov(TMP, scheduler_registers);
+    a.add(TMP, TMP, offsetof(ErtsSchedulerRegisters, aux_regs.d.TMP_MEM[0]));
+    a.stmia(tmp_mem1q, a32::GpList({ARG3, ARG4}));
+
+    /* Check if we should trace this bif call or handle save_calls. Both
+     * variants dispatch through the export entry. */
+    a.ldr(TMP, arm::Mem(ARG4, offsetof(Export, is_bif_traced)));
+    a.tst(TMP, TMP);
+    a.b_ne(skip_trace);
+    a.cmp(active_code_ix, imm(ERTS_SAVE_CALLS_CODE_IX));
+    a.b_eq(trace);
+    a.bind(skip_trace);
+
+    a.subs(FCALLS, FCALLS, imm(1));
+    a.b_le(yield);
+    {
+        Label check_bif_return = a.newLabel(), gc_after_bif_call = a.newLabel();
+
+        emit_enter_runtime_frame();
+        emit_enter_runtime<Update::eReductions | Update::eStack | Update::eHeap>();
+
+#ifdef ERTS_MSACC_EXTENDED_STATES
+        //TODO
+        ASSERT(false);
+#endif
+
+        {
+            // ARG2 is the BIF pointer
+            a.mov(TMP, ARG2);
+
+            /* Call the BIF proper. ARG3 and ARG8 have been set earlier. */
+            a.mov(ARG1, c_p);
+            load_x_reg_array(ARG2);
+
+#if defined(DEBUG) || defined(ERTS_ENABLE_LOCK_CHECK)
+            a.mov(ARG4, TMP);
+            runtime_call<4>(debug_call_light_bif);
+#else
+            runtime_call(TMP, 3);
+#endif
+        }
+
+#ifdef ERTS_MSACC_EXTENDED_STATES
+        //TOOD
+        ASSERT(false);
+#endif
+
+        /* We must update the active code index in case another process has
+         * loaded new code, as the result of this BIF may be observable on both
+         * ends.
+         *
+         * It doesn't matter whether the BIF modifies anything; if process A
+         * loads new code and calls erlang:monotonic_time/0 soon after, we'd
+         * break the illusion of atomic upgrades if process B still ran old code
+         * after seeing a later timestamp from its own call to
+         * erlang:monotonic_time/0. */
+        emit_leave_runtime<Update::eReductions | Update::eCodeIndex |
+                           Update::eHeap | Update::eStack>();
+        emit_leave_runtime_frame();
+
+        /* ERTS_IS_GC_DESIRED_INTERNAL */
+        {
+            emit_nyi("emit_call_light_bif_ERTS_IS_GC_DESIRED_INTERNAL");
+        }
+
+        a.bind(check_bif_return);
+        {
+          emit_nyi("emit_call_light_bif_check_bif_return");
+        }
+
+        a.bind(gc_after_bif_call);
+        {
+          emit_nyi("emit_call_light_bif_gc_after_bif_call");
+        }
+    }
+
+    a.bind(trace);
+    {
+        emit_nyi("emit_call_light_bif_trace");
+    }
+
+    a.bind(yield);
+    {
+        emit_nyi("emit_call_light_bif_yield");
+    }
 }
 
 void BeamModuleAssembler::emit_call_light_bif(const ArgWord &Bif,
                                               const ArgExport &Exp) {
-    // TODO
-    emit_nyi("emit_call_light_bif");
+    Label entry = a.newLabel();
+    BeamFile_ImportEntry *e = &beam->imports.entries[Exp.get()];
+
+    a.bind(entry);
+
+    mov_arg(ARG4, Exp);
+    mov_arg(ARG2, Bif);
+    a.adr(ARG3, entry);
+
+    if (logger.file()) {
+        comment("BIF: %T:%T/%d", e->module, e->function, e->arity);
+    }
+    fragment_call(ga->get_call_light_bif_shared());
 }
 
 void BeamModuleAssembler::emit_send() {
