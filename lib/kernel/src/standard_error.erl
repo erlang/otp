erlang/otp

commit ead74245e1dc632b8f20ee795092de04d2510e36

kernel: Update standard_error to use prim_tty nif
The primary reason that we do this is to make writing to standard_error
as synchronous as possible. When using the fd driver the write was
done on an async thread, which meant that there was no simple way
to get an acknowledgement when the write had completed. Now that we
use a NIF we can much more easily get a check when the write is done
so that we know when io:format(standard_error, ...) has returned we
have also written to that fd.

lib/kernel/src/standard_error.erl
@@ -23,7 +23,7 @@
 
 -include_lib("kernel/include/logger.hrl").
 
-%% Basic standard i/o server for user interface port.
+%% Basic standard i/o server for standard_error.
 -export([start_link/0, init/1, terminate/2]).
 
 -define(NAME, standard_error).
@@ -50,166 +50,164 @@ terminate(_Reason,Pid) ->
 -spec init([]) -> {'error','no_stderror'} | {'ok',pid(),pid()}.
 
 init([]) ->
-    case (catch start_port([out,binary])) of
-	Pid when is_pid(Pid) ->
-	    {ok,Pid,Pid};
-	_ ->
-	    {error,no_stderror}
+    case (catch start()) of
+        Pid when is_pid(Pid) ->
+            {ok,Pid,Pid};
+        _ ->
+            {error,no_stderror}
     end.
 
-start_port(PortSettings) ->
-    Id = spawn(fun () -> server({fd,2,2}, PortSettings) end),
+start() ->
+    Id = spawn(fun server/0),
     register(?NAME, Id),
     Id.
 
-server(PortName,PortSettings) ->
+server() ->
     process_flag(trap_exit, true),
-    Port = open_port(PortName,PortSettings),
-    run(Port).
+    TTY = prim_tty:init(#{ input => disabled,
+                           output => cooked,
+                           ofd => stderr}),
+    run(TTY).
 
-run(P) ->
-    put(encoding, latin1),
-    put(onlcr, false),
+run(TTY) ->
+    put(encoding, encoding(TTY)),
+    put(onlcr, prim_tty:isatty(stderr)),
     put(log, none),
-    server_loop(P).
+    server_loop(TTY).
 
-server_loop(Port) ->
+encoding(TTY) ->
+    case prim_tty:unicode(TTY) of
+        true -> unicode;
+        false -> latin1
+    end.
+
+server_loop(TTY) ->
     receive
         {io_request,From,ReplyAs,Request} = IoReq when is_pid(From) ->
             group:log_io_request(IoReq, get(log), ?MODULE),
-            _ = do_io_request(Request, From, ReplyAs, Port),
-            server_loop(Port);
-        {'EXIT',Port,badsig} ->         % Ignore badsig errors
-            server_loop(Port);
-        {'EXIT',Port,What} ->           % Port has exited
-            exit(What);
+            case io_request(Request, TTY) of
+                {stop, Reason} ->
+                    io_reply(From, ReplyAs, {error, Reason}),
+                    stop;
+                {reply, Reply} ->
+                    io_reply(From, ReplyAs, Reply),
+                    server_loop(TTY)
+            end;
         _Other ->               % Ignore other messages
-            server_loop(Port)
+            server_loop(TTY)
     end.
 
-get_fd_geometry(Port) ->
-    case (catch port_control(Port,?CTRL_OP_GET_WINSIZE,[])) of
-	List when length(List) =:= 8 ->
-	    <<W:32/native,H:32/native>> = list_to_binary(List),
-	    {W,H};
-	_ ->
-	    error
+get_fd_geometry(TTY) ->
+    case prim_tty:window_size(TTY) of
+        {ok, {W, H}} -> {W, H};
+        _ -> error
     end.
-
-%% NewSaveBuffer = io_request(Request, FromPid, ReplyAs, Port, SaveBuffer)
-
-do_io_request(Req, From, ReplyAs, Port) ->
-    {_Status,Reply}  = io_request(Req, Port),
-    io_reply(From, ReplyAs, Reply).
 
 %% New in R13B
 %% Encoding option (unicode/latin1)
-io_request({put_chars,unicode,Chars}, Port) ->
+io_request({put_chars,unicode,Chars}, TTY) ->
     case wrap_characters_to_binary(Chars, unicode, get(encoding)) of
         error ->
-            {error,{error,put_chars}};
+            {reply,{error,put_chars}};
         Bin ->
-            put_chars(Bin, Port)
+            put_chars(Bin, TTY)
     end;
-io_request({put_chars,unicode,Mod,Func,Args}, Port) ->
+io_request({put_chars,unicode,Mod,Func,Args}, TTY) ->
     case catch apply(Mod, Func, Args) of
         Data when is_list(Data); is_binary(Data) ->
             case wrap_characters_to_binary(Data, unicode, get(encoding)) of
                 Bin when is_binary(Bin) ->
-                    put_chars(Bin, Port);
+                    put_chars(Bin, TTY);
                 error ->
-                    {error,{error,put_chars}}
+                    {reply,{error,put_chars}}
             end;
         _ ->
-            {error,{error,put_chars}}
+            {reply,{error,put_chars}}
     end;
-io_request({put_chars,latin1,Chars}, Port) ->
+io_request({put_chars,latin1,Chars}, TTY) ->
     case catch unicode:characters_to_binary(Chars, latin1, get(encoding)) of
         Data when is_binary(Data) ->
-            put_chars(Data, Port);
+            put_chars(Data, TTY);
         _ ->
-            {error,{error,put_chars}}
+            {reply,{error,put_chars}}
     end;
-io_request({put_chars,latin1,Mod,Func,Args}, Port) ->
+io_request({put_chars,latin1,Mod,Func,Args}, TTY) ->
     case catch apply(Mod, Func, Args) of
         Data when is_list(Data); is_binary(Data) ->
             case
                 catch unicode:characters_to_binary(Data, latin1, get(encoding))
             of
                 Bin when is_binary(Bin) ->
-                    put_chars(Bin, Port);
+                    put_chars(Bin, TTY);
                 _ ->
-                    {error,{error,put_chars}}
+                    {reply,{error,put_chars}}
             end;
         _ ->
-            {error,{error,put_chars}}
+            {reply,{error,put_chars}}
     end;
 %% BC if called from pre-R13 node
-io_request({put_chars,Chars}, Port) -> 
-    io_request({put_chars,latin1,Chars}, Port); 
-io_request({put_chars,Mod,Func,Args}, Port) ->
-    io_request({put_chars,latin1,Mod,Func,Args}, Port);
+io_request({put_chars,Chars}, TTY) ->
+    io_request({put_chars,latin1,Chars}, TTY);
+io_request({put_chars,Mod,Func,Args}, TTY) ->
+    io_request({put_chars,latin1,Mod,Func,Args}, TTY);
 %% New in R12
-io_request({get_geometry,columns},Port) ->
-    case get_fd_geometry(Port) of
-	{W,_H} ->
-	    {ok,W};
-	_ ->
-	    {error,{error,enotsup}}
+io_request({get_geometry,columns},TTY) ->
+    case get_fd_geometry(TTY) of
+        {W,_H} ->
+            {reply,W};
+        _ ->
+            {reply,{error,enotsup}}
     end;
-io_request({get_geometry,rows},Port) ->
-    case get_fd_geometry(Port) of
-	{_W,H} ->
-	    {ok,H};
-	_ ->
-	    {error,{error,enotsup}}
+io_request({get_geometry,rows},TTY) ->
+    case get_fd_geometry(TTY) of
+        {_W,H} ->
+            {reply,H};
+        _ ->
+            {reply,{error,enotsup}}
     end;
-io_request(getopts, _Port) ->
+io_request(getopts, _TTY) ->
     getopts();
-io_request({setopts,Opts}, _Port) when is_list(Opts) ->
-    do_setopts(Opts);
-io_request({requests,Reqs}, Port) ->
-    io_requests(Reqs, {ok,ok}, Port);
-io_request(R, _Port) ->                      %Unknown request
-    {error,{error,{request,R}}}.		%Ignore but give error (?)
+io_request({setopts,Opts}, _TTY) when is_list(Opts) ->
+    setopts(Opts);
+io_request({requests,Reqs}, TTY) ->
+    io_requests(Reqs, {reply,ok}, TTY);
+io_request(R, _TTY) ->                      %Unknown request
+    {reply,{error,{request,R}}}.        %Ignore but give error (?)
 
-%% Status = io_requests(RequestList, PrevStat, Port)
+%% Status = io_requests(RequestList, PrevStat, TTY)
 %%  Process a list of output requests as long as the previous status is 'ok'.
-
-io_requests([R|Rs], {ok,_Res}, Port) ->
-    io_requests(Rs, io_request(R, Port), Port);
+io_requests([_|_], {reply,{error, _}} = Error, _TTY) ->
+    Error;
+io_requests([R|Rs], {reply,_Ok}, TTY) ->
+    io_requests(Rs, io_request(R, TTY), TTY);
 io_requests([_|_], Error, _) ->
     Error;
 io_requests([], Stat, _) ->
     Stat.
-
-%% put_port(DeepList, Port)
-%%  Take a deep list of characters, flatten and output them to the
-%%  port.
-
-put_port(List, Port) ->
-    send_port(Port, {command, List}).
-
-%% send_port(Port, Command)
-
-send_port(Port, Command) ->
-    Port ! {self(),Command}.
-
 
 %% io_reply(From, ReplyAs, Reply)
 %%  The function for sending i/o command acknowledgement.
 %%  The ACK contains the return value.
 
 io_reply(From, ReplyAs, Reply) ->
-    From ! {io_reply,ReplyAs,Reply}.
+    From ! {io_reply,ReplyAs,Reply},
+    ok.
 
 %% put_chars
-put_chars(Chars, Port) when is_binary(Chars) ->
-    _ = put_port(Chars, Port),
-    {ok,ok}.
+put_chars(Chars, TTY) when is_binary(Chars) ->
+    {ok, MonitorRef} = prim_tty:write(TTY, Chars, self()),
+    #{ write := WriteRef } = prim_tty:handles(TTY),
+    receive
+        {WriteRef, ok} ->
+            erlang:demonitor(MonitorRef, [flush]),
+            {reply, ok};
+        {'DOWN', MonitorRef, _, _, Reason} ->
+            ?LOG_INFO("Failed to write to standard error (~p)", [Reason]),
+            {stop, Reason}
+    end.
 
 %% setopts
-do_setopts(Opts0) ->
+setopts(Opts0) ->
     Opts = expand_encoding(Opts0),
     case check_valid_opts(Opts) of
         true ->
@@ -221,9 +219,9 @@ do_setopts(Opts0) ->
                  ({log, Bool}) ->
                       put(log, Bool)
               end, Opts),
-            {ok, ok};
+            {reply, ok};
         false ->
-            {error,{error,enotsup}}
+            {reply,{error,enotsup}}
     end.
 
 check_valid_opts([]) ->
@@ -258,16 +256,16 @@ getopts() ->
     Uni = {encoding,get(encoding)},
     Onlcr = {onlcr, get(onlcr)},
     Log = {log, get(log)},
-    {ok,[Uni, Onlcr, Log]}.
+    {reply,[Uni, Onlcr, Log]}.
 
 wrap_characters_to_binary(Chars,From,To) ->
     TrNl = get(onlcr),
     Limit = case To of
-		latin1 ->
-		    255;
-		_Else ->
-		    16#10ffff
-	    end,
+                latin1 ->
+                    255;
+                _Else ->
+                    16#10ffff
+            end,
     case catch unicode:characters_to_list(Chars, From) of
         L when is_list(L) ->
             unicode:characters_to_binary(

erts/emulator/nifs/common/prim_tty_nif.c
@@ -147,7 +147,7 @@ static ERL_NIF_TERM tty_tgoto_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM a
 
 static ErlNifFunc nif_funcs[] = {
     {"isatty", 1, isatty_nif},
-    {"tty_create", 0, tty_create_nif},
+    {"tty_create", 1, tty_create_nif},
     {"tty_init", 2, tty_init_nif},
     {"setlocale", 1, setlocale_nif},
     {"tty_is_open", 2, tty_is_open},
@@ -884,7 +884,9 @@ static ERL_NIF_TERM tty_create_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM 
 
 #ifndef __WIN32__
     tty->ifd = 0;
-    tty->ofd = 1;
+    tty->ofd = 0;
+    if (!tty_get_fd(env, argv[0], &tty->ofd) || tty->ofd == 0)
+        return enif_make_badarg(env);
 
 #ifdef HAVE_TERMCAP
     if (tcgetattr(tty->ofd, &tty->tty_rmode) >= 0) {
@@ -899,7 +901,10 @@ static ERL_NIF_TERM tty_create_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM 
         tty->ifd = CreateFile("nul", GENERIC_READ, 0,
                               NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     }
-    tty->ofd = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    if (enif_is_identical(argv[0], atom_stdin))
+        return enif_make_badarg(env);
+    tty->ofd = tty_get_handle(env, argv[0]);
     if (tty->ofd == INVALID_HANDLE_VALUE || tty->ofd == NULL) {
         tty->ofd = CreateFile("nul", GENERIC_WRITE, 0,
                               NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);

lib/kernel/src/prim_tty.erl
@@ -113,7 +113,7 @@
 -export([reader_stop/1, disable_reader/1, enable_reader/1, read/1, read/2,
          is_reader/2, is_writer/2, output_mode/1]).
 
--nifs([isatty/1, tty_create/0, tty_init/2, setlocale/1,
+-nifs([isatty/1, tty_create/1, tty_init/2, setlocale/1,
        tty_select/2, tty_window_size/1,
        tty_encoding/1, tty_is_open/2, write_nif/2, read_nif/3, isprint/1,
        wcwidth/1, wcswidth/1,
@@ -168,7 +168,8 @@
                }).
 
 -type options() :: #{ input := cooked | raw | disabled,
-                      output := raw | cooked }.
+                      output := raw | cooked,
+                      ofd => stdout | stderr }.
 -type request() ::
         {putc_raw, binary()} |
         {putc, unicode:unicode_binary()} |
@@ -252,7 +253,7 @@ init(UserOptions) when is_map(UserOptions) ->
     on_load(),
 
     Options = options(UserOptions),
-    {ok, TTY} = tty_create(),
+    {ok, TTY} = tty_create(maps:get(ofd, Options)),
 
     %% Initialize the locale to see if we support utf-8 or not
     UnicodeSupported =
@@ -295,7 +296,7 @@ init_term(State = #state{ tty = TTY, options = Options }) ->
 
     WriterState =
         if TTYState#state.writer =:= undefined ->
-                {ok, Writer} = proc_lib:start_link(?MODULE, writer, [State#state.tty]),
+                {ok, Writer} = proc_lib:start_link(?MODULE, writer, [[State#state.tty, self()]]),
                 TTYState#state{ writer = Writer };
            true ->
                 TTYState
@@ -356,7 +357,7 @@ reinit(State = #state{ options = OldOptions }, UserOptions) ->
     end.
 
 options(UserOptions) ->
-    maps:merge(#{ input => raw, output => cooked }, UserOptions).
+    maps:merge(#{ input => raw, output => cooked, ofd => stdout }, UserOptions).
 
 init(State, ssh) ->
     State#state{ xn = true };
@@ -541,7 +542,7 @@ call(Pid, Msg) ->
     end.
 
 reader([TTY, Encoding, Parent]) ->
-    register(user_drv_reader, self()),
+    set_name(Parent, "reader"),
     ReaderRef = make_ref(),
 
     ok = tty_select(TTY, ReaderRef),
@@ -638,8 +639,8 @@ flush_unicode_state(FromEnc) ->
             FromEnc
     end.
 
-writer(TTY) ->
-    register(user_drv_writer, self()),
+writer([TTY, Parent]) ->
+    set_name(Parent, "writer"),
     WriterRef = make_ref(),
     proc_lib:init_ack({ok, {self(), WriterRef}}),
     writer_loop(TTY, WriterRef).
@@ -672,6 +673,10 @@ writer_loop(TTY, WriterRef) ->
                     exit(self(), Reason)
             end
     end.
+
+set_name(Parent, Postfix) ->
+    {registered_name, Name} = erlang:process_info(Parent, registered_name),
+    register(list_to_atom(atom_to_list(Name) ++ "_" ++ Postfix), self()).
 
 -spec handle_request(state(), request()) -> {erlang:iovec(), state()}.
 handle_request(State = #state{ options = #{ output := raw } }, Request) ->
@@ -1467,8 +1472,8 @@ dbg(_) ->
 -spec isatty(stdin | stdout | stderr | tty()) -> boolean() | ebadf.
 isatty(_Fd) ->
     erlang:nif_error(undef).
--spec tty_create() -> {ok, tty()}.
-tty_create() ->
+-spec tty_create(stdout | stderr) -> {ok, tty()}.
+tty_create(_Fd) ->
     erlang:nif_error(undef).
 tty_init(_TTY, _Options) ->
     erlang:nif_error(undef).

commit 142867a13b0330cd6375c4038488c66e7fcf340b

Fix race condition in standard_error process registration
Fix a race condition where the standard_error process could call
prim_tty:init/1 before being registered, causing a crash when prim_tty's
writer and reader processes attempt to derive their own registered names
from the parent process.

The issue was introduced in commit
ead74245e1dc632b8f20ee795092de04d2510e36 (PR #9116), which changed
prim_tty's reader and writer processes to dynamically derive their
registered names from their parent process name (e.g., 'standard_error'
-> 'standard_error_reader', 'standard_error_writer'). This was done
to support multiple TTY instances (stdout and stderr) with
distinct process names.

However, the standard_error:start/0 function had a race condition:
1. spawn(fun server/0) - creates process, starts executing immediately
2. register(?NAME, Id) - parent registers the spawned process
3. server() calls prim_tty:init() - may execute before step 2

When prim_tty:init/1 spawns writer/reader processes, they call
set_name(Parent, Postfix) which does: erlang:process_info(Parent,
registered_name)

If this executes before the parent completes registration, it returns []
instead of {registered_name, standard_error}, causing a pattern match
failure: "no match of right hand side value: []" at prim_tty.erl:674.

The fix moves the register/2 call into the spawned process itself,
ensuring it completes before prim_tty:init/1 is called. This eliminates
the race condition entirely.

Closes #10174

lib/kernel/src/standard_error.erl
@@ -62,12 +62,11 @@ init([]) ->
     end.
 
 start() ->
-    Id = spawn(fun server/0),
-    register(?NAME, Id),
-    Id.
+    spawn(fun server/0).
 
 server() ->
     process_flag(trap_exit, true),
+    register(?NAME, self()),
     ok = prim_tty:load(),
     TTY = prim_tty:init(#{ input => disabled,
                            output => cooked,