e8713066bea5b0b12931a5fe1f0debe983602ff4
Richard Carlsson <carlsson.richard@gmail.com>
2025-11-30 10:24:45 +0100

    compiler: Enable warn_deprecated_catch by default

--- lib/stdlib/src/erl_lint.erl
+++ lib/stdlib/src/erl_lint.erl
@@ -831,7 +831,7 @@ bool_options() ->
      {deprecated_function,true},
      {deprecated_type,true},
      {deprecated_callback,true},
-     {deprecated_catch,false},
+     {deprecated_catch,true},
      {obsolete_guard,true},
      {untyped_record,false},
      {missing_spec,false},
