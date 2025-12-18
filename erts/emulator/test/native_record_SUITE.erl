45f335668a6c0027485b2a6dbd1f8befc044fc62
Björn Gustavsson <bjorn@erlang.org>
2025-12-18 06:26:39 +0100

    Add more tests for the external term format
    Note: This new test fails, because native records are not
    properly purged when unloading a module.

--- erts/emulator/test/native_record_SUITE.erl
+++ erts/emulator/test/native_record_SUITE.erl
@@ -24,7 +24,7 @@
 
 -export([all/0,suite/0,groups/0,init_per_suite/1,end_per_suite/1,
 	 init_per_group/2,end_per_group/2,
-         term_order/1,gc/1]).
+         term_order/1,gc/1,external_term_format/1]).
 
 -record #a{x=1, y=2}.
 -record #b{a=1, b=2}.
@@ -35,14 +35,14 @@ suite() ->
 
 all() ->
     [term_order,
-     gc].
+     gc,
+     external_term_format].
 
 groups() ->
     [].
 
 init_per_suite(Config) ->
     id(Config),
-    %% {module,ext_records} = code:ensure_loaded(ext_records),
     Config.
 
 end_per_suite(_Config) ->
@@ -75,7 +75,6 @@ term_order(_Config) ->
     true = id(#a{x=1}) < id(#a{x=2}),
     true = id(#a{x=10}) > id(#a{x=0}),
 
-
     ok.
 
 gc(_Config) ->
@@ -96,6 +95,57 @@ gc_add(I, #a{x=S0, y=#b{b=L0}=B0}=A0) ->
     L = [I|L0],
     A = A0#a{x=S, y=B0#b{b=L}},
     gc_add(I - 1, A).
+
+external_term_format(_Config) ->
+    RecA = #a{},
+    RecB = #b{a=RecA,b=lists:seq(1, 10)},
+
+    Local = ext_records:local([1,2,3], {RecA, RecB}),
+    Vector = #ext_records:vector{},
+
+    Records = [RecA, RecB, Vector, Local],
+    Bin = term_to_binary(Records),
+
+    erts_debug:set_internal_state(available_internal_state, true),
+
+    _ = [begin
+             io:format("~p\n", [R1]),
+             Def1 = record_def(R1),
+             Def2 = record_def(R2),
+
+             true = R1 =:= R2,
+             true = Def1 =:= Def2,
+             true = erts_debug:same(Def1, Def2)
+         end || R1 <- Records && R2 <- binary_to_term(Bin)],
+
+    true = code:delete(ext_records),
+    _ = code:purge(ext_records),
+
+    %% FIXME: This should fail.
+    records:create(ext_records, vector, #{}),
+
+    _ = [begin
+             io:format("~p\n", [R1]),
+             Def1 = record_def(R1),
+             Def2 = record_def(R2),
+
+             true = R1 =:= R2,
+             true = Def1 =:= Def2,
+             case records:get_module(R1) of
+                 ?MODULE ->
+                     true = erts_debug:same(Def1, Def2);
+                 ext_records ->
+                     %% The original definition has been unloaded.
+                     false = erts_debug:same(Def1, Def2)
+             end
+         end || R1 <- Records && R2 <- binary_to_term(Bin)],
+
+    erts_debug:set_internal_state(available_internal_state, false),
+
+    ok.
+
+record_def(R) ->
+    erts_debug:get_internal_state({native_record_def, R}).
 
 
 %%% Common utilities.
