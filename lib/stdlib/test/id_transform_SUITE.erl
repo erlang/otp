97f3abfbdc55322ea05fe0ca8635ac5807958ea0
Kiko Fernandez-Reyes <kiko@erlang.org>
2026-01-19 15:44:56 +0100

    stdlib: move examples to doc on release
    when making an Erlang release `make release`, the `examples` should not
    be part of the release. `make release_docs` will create a folder named
    `examples` in the released `doc` folder.

--- lib/stdlib/test/id_transform_SUITE.erl
+++ lib/stdlib/test/id_transform_SUITE.erl
@@ -59,7 +59,7 @@ end_per_group(_GroupName, Config) ->
 
 %% Test erl_id_trans.
 id_transform(Config) when is_list(Config) ->
-    File = filename:join([code:lib_dir(stdlib),"examples",
+    File = filename:join([code:lib_dir(stdlib),"doc", "examples",
 			"erl_id_trans.erl"]),
     {ok,erl_id_trans,Bin} = compile:file(File, [binary,report]),
     {module,erl_id_trans} = code:load_binary(erl_id_trans, File, Bin),
