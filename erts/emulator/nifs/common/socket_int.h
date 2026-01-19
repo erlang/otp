956733d4ecd45e84f614c508ff5e45a6cb7a104a
Nelson Vides <videsnelson@gmail.com>
2026-01-19 13:51:22 +0000

    Support only old enough timestamping flags on the erlang level

--- erts/emulator/nifs/common/socket_int.h
+++ erts/emulator/nifs/common/socket_int.h
@@ -711,12 +711,8 @@ ress-of-packed-member]
     GLOBAL_ATOM_DEF(timestamping_rx_software); \
     GLOBAL_ATOM_DEF(timestamping_tx_hardware); \
     GLOBAL_ATOM_DEF(timestamping_tx_software); \
-    GLOBAL_ATOM_DEF(timestamping_tx_sched);    \
-    GLOBAL_ATOM_DEF(timestamping_tx_ack);      \
-    GLOBAL_ATOM_DEF(timestamping_tx_completion);\
     GLOBAL_ATOM_DEF(timestamping_software);    \
     GLOBAL_ATOM_DEF(timestamping_raw_hardware);\
-    GLOBAL_ATOM_DEF(timestamping_opt_id);      \
     GLOBAL_ATOM_DEF(tos);                      \
     GLOBAL_ATOM_DEF(transparent);              \
     GLOBAL_ATOM_DEF(timeout);                  \
