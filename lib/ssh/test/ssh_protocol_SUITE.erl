87cfdbbdca179194ff4d39a079369515c19799c6
Michał Wąsowski <michal@erlang.org>
2026-01-27 21:58:02 +0100

    Add tests for each side may guess functionality

--- lib/ssh/test/ssh_protocol_SUITE.erl
+++ lib/ssh/test/ssh_protocol_SUITE.erl
@@ -38,6 +38,8 @@
          groups/0,
          init_per_suite/1,
          end_per_suite/1,
+         init_per_group/2,
+         end_per_group/2,
          init_per_testcase/2,
          end_per_testcase/2
         ]).
@@ -93,14 +95,24 @@
          service_name_length_too_short/1,
          client_close_after_hello/1,
          channel_close_timeout/1,
-         extra_ssh_msg_service_request/1
+         extra_ssh_msg_service_request/1,
+         client_guesses_correctly/1,
+         client_guesses_incorrectly/1
         ]).
-
--define(NEWLINE, <<"\r\n">>).
--define(REKEY_DATA_TMO, 65000).
 
 -define(DEFAULT_KEX, 'diffie-hellman-group14-sha256').
 -define(EXTRA_KEX, 'diffie-hellman-group1-sha1').
+
+-define(DH_KEX, ['diffie-hellman-group16-sha512',
+                 'diffie-hellman-group18-sha512',
+                 'diffie-hellman-group14-sha256',
+                 'diffie-hellman-group14-sha1',
+                 'diffie-hellman-group1-sha1']).
+-define(ECDH_KEX, ['ecdh-sha2-nistp521',
+                   'ecdh-sha2-nistp384',
+                   'ecdh-sha2-nistp256']).
+-define(HYBRID_KEX, ['mlkem768x25519-sha256']).
+
 
 -define(CIPHERS, ['aes256-ctr','aes192-ctr','aes128-ctr','aes128-cbc','3des-cbc']).
 -define(DEFAULT_CIPHERS, (fun() -> Ciphs = filter_supported(cipher, ?CIPHERS),
@@ -156,7 +168,10 @@ all() ->
      {group,ext_info},
      {group,preferred_algorithms},
      {group,client_close_early},
-     {group,channel_close}
+     {group,channel_close},
+     {group,dh},
+     {group,ecdh},
+     {group,hybrid}
     ].
 
 groups() ->
@@ -208,7 +223,12 @@ groups() ->
                                  modify_combo
                                 ]},
      {client_close_early, [], [client_close_after_hello]},
-     {channel_close, [], [channel_close_timeout]}
+     {channel_close, [], [channel_close_timeout]},
+     {guess, [], [client_guesses_correctly,
+                  client_guesses_incorrectly]},
+     {dh, [], [{group, guess}]},
+     {ecdh, [], [{group, guess}]},
+     {hybrid, [], [{group, guess}]}
     ].
 
 
@@ -217,6 +237,34 @@ init_per_suite(Config) ->
 
 end_per_suite(Config) ->
     stop_apps(Config).
+
+init_per_group(guess, Config) ->
+    case ssh_transport:supported_algorithms(public_key) of
+        PubKey when length(PubKey) >= 2 ->
+            ParentGroup = get_parent_group(Config),
+            KexInput =
+                case ParentGroup of
+                    dh -> ?DH_KEX;
+                    ecdh -> ?ECDH_KEX;
+                    hybrid -> ?HYBRID_KEX
+                end,
+            case {filter_supported(kex, KexInput), filter_supported(kex, ?ECDH_KEX)} of
+                {Kex, KexEcdh} when length(Kex) == 1, length(KexEcdh) >= 1, ParentGroup == hybrid ->
+                    %% There is only one hybrid kex supported, we have to fill algorithms with other
+                    [{kex, Kex ++ ?ECDH_KEX}, {public_key, PubKey} | Config];
+                {Kex, _} when length(Kex) >= 2 ->
+                    [{kex, Kex}, {public_key, PubKey} | Config];
+                _ ->
+                    {skip, "Not enough kex algorithms supported"}
+            end;
+        _ ->
+            {skip, "Not enough public key algorithms supported"}
+    end;
+init_per_group(_GroupName, Config) ->
+    Config.
+
+end_per_group(_GroupName, Config) ->
+    Config.
 
 init_per_testcase(Tc, Config) when Tc == no_common_alg_server_disconnects;
                                    Tc == custom_kexinit ->
@@ -1513,6 +1561,24 @@ extra_ssh_msg_service_request(Config) ->
 	  ], EndState),
     ok.
 
+%%% RFC 4253 section 7, client guesses correctly
+client_guesses_correctly(Config) ->
+    Algs = [proplists:lookup(kex, Config), proplists:lookup(public_key, Config)],
+    Helpers = [fun client_guess_test/4, fun client_guess_test_renegotiation/4],
+    [Helper(true, Config, Algs, Algs) || Helper <- Helpers].
+
+%%% RFC 4253 section 7, client guesses incorrectly
+client_guesses_incorrectly(Config) ->
+    {kex, KexAlgs} = Kex = proplists:lookup(kex, Config),
+    {public_key, PubKeyAlgs} = PubKey = proplists:lookup(public_key, Config),
+    ClientAlgs = [Kex, PubKey],
+    Helpers = [fun client_guess_test/4, fun client_guess_test_renegotiation/4],
+    ServerKexAlgs = [KexAlgs, lists:reverse(KexAlgs)],
+    ServerPubKeyAlgs = [PubKeyAlgs, lists:reverse(PubKeyAlgs)],
+    [Helper(false, Config, ClientAlgs, [{kex, K}, {public_key, P}])
+     || Helper <- Helpers, K <- ServerKexAlgs, P <- ServerPubKeyAlgs,
+        K /= KexAlgs orelse P /= PubKeyAlgs].
+
 %%%================================================================
 %%%==== Internal functions ========================================
 %%%================================================================
@@ -1553,9 +1619,12 @@ chk_pref_algs(Config,
 
 filter_supported(K, Algs) -> Algs -- (Algs--supported(K)).
 
-supported(_K) -> proplists:get_value(
-                   server2client,
-                   ssh_transport:supported_algorithms(cipher)).
+supported(cipher) ->
+    proplists:get_value(
+      server2client,
+      ssh_transport:supported_algorithms(cipher));
+supported(K) ->
+    ssh_transport:supported_algorithms(K).
 
 to_lists(L) -> lists:map(fun erlang:atom_to_list/1, L).
     
@@ -1653,28 +1722,39 @@ connect_and_kex(Config) ->
     connect_and_kex(Config, ssh_trpt_test_lib:exec([]) ).
 
 connect_and_kex(Config, InitialState) ->
+    ClientAlgs = [{kex,[?DEFAULT_KEX]}, {cipher,?DEFAULT_CIPHERS}],
+    connect_and_kex(Config, InitialState, ClientAlgs, dh).
+
+connect_and_kex(Config, InitialState, ClientAlgs, Variant) ->
     ssh_trpt_test_lib:exec(
       [{connect,
-	ssh_test_lib:server_host(Config),ssh_test_lib:server_port(Config),
-	[{preferred_algorithms,[{kex,[?DEFAULT_KEX]},
-                                {cipher,?DEFAULT_CIPHERS}
-                               ]},
+        ssh_test_lib:server_host(Config),ssh_test_lib:server_port(Config),
+        [{preferred_algorithms, ClientAlgs},
          {silently_accept_hosts, true},
          {recv_ext_info, false},
-	 {user_dir, user_dir(Config)},
-	 {user_interaction, false}
-         | proplists:get_value(extra_options,Config,[])
+         {user_dir, user_dir(Config)},
+         {user_interaction, false}
+        | proplists:get_value(extra_options,Config,[])
         ]},
        receive_hello,
        {send, hello},
        {send, ssh_msg_kexinit},
-       {match, #ssh_msg_kexinit{_='_'}, receive_msg},
-       {send, ssh_msg_kexdh_init},
-       {match,# ssh_msg_kexdh_reply{_='_'}, receive_msg},
-       {send, #ssh_msg_newkeys{}},
-       {match, #ssh_msg_newkeys{_='_'}, receive_msg}
-      ],
+       {match, #ssh_msg_kexinit{_='_'}, receive_msg}] ++
+          get_kex_variant_ops(Variant) ++
+          [{send, #ssh_msg_newkeys{}},
+           {match, #ssh_msg_newkeys{_='_'}, receive_msg}
+          ],
       InitialState).
+
+get_kex_variant_ops(dh) ->
+    [{send, ssh_msg_kexdh_init},
+     {match, #ssh_msg_kexdh_reply{_='_'}, receive_msg}];
+get_kex_variant_ops(ecdh) ->
+    [{send, ssh_msg_kex_ecdh_init},
+     {match, #ssh_msg_kex_ecdh_reply{_='_'}, receive_msg}];
+get_kex_variant_ops(hybrid) ->
+    [{send, ssh_msg_kex_hybrid_init},
+     {match, #ssh_msg_kex_hybrid_reply{_='_'}, receive_msg}].
 
 channel_close_timeout(Config) ->
     {User,_Pwd} = server_user_password(Config),
@@ -1806,3 +1886,92 @@ trpt_test_lib_send_disconnect(State) ->
                                  }},
        close_socket
       ], State).
+
+%%%----------------------------------------------------------------
+
+%%% Common part of test case for guessing functionality
+client_guess_test(GuessCorrect, Config0, ClientAlgs, ServerAlgs) ->
+    Variant = get_parent_group(Config0),
+    ?CT_LOG("~nGuessCorrect: ~p~nVariant: ~p~nClientAlgs: ~p~nServerAlgs: ~p~n",
+            [GuessCorrect, Variant, ClientAlgs, ServerAlgs]),
+    Config = start_std_daemon(Config0, [{preferred_algorithms, ServerAlgs}]),
+    try
+        {ok, _} =
+            ssh_trpt_test_lib:exec(
+              [{set_options, [print_ops, {print_messages,detail}]},
+               {connect,
+                ssh_test_lib:server_host(Config), ssh_test_lib:server_port(Config),
+                [{silently_accept_hosts, true},
+                 {user_dir, user_dir(Config)},
+                 {user_interaction, false},
+                 {preferred_algorithms, ClientAlgs}
+                ]},
+               receive_hello,
+               {send, hello},
+               {send, ssh_msg_kexinit_guess},
+               {match, #ssh_msg_kexinit{_='_'}, receive_msg}] ++
+                  get_specific_ops(GuessCorrect, Variant) ++
+                  [{send, #ssh_msg_newkeys{}},
+                   {match, #ssh_msg_newkeys{_='_'}, receive_msg}]),
+        ?CT_LOG("Finished~n")
+    after
+        stop_std_daemon(Config)
+    end.
+
+%%% Common part of test case for guessing in renegotiation
+client_guess_test_renegotiation(GuessCorrect, Config0, ClientAlgs, ServerAlgs) ->
+    Variant = get_parent_group(Config0),
+    ?CT_LOG("~nGuessCorrect: ~p~nVariant: ~p~nClientAlgs: ~p~nServerAlgs: ~p~n",
+            [GuessCorrect, Variant, ClientAlgs, ServerAlgs]),
+    Config = start_std_daemon(Config0, [{preferred_algorithms, ServerAlgs}]),
+    try
+        {ok, InitialState} = ssh_trpt_test_lib:exec(
+                               [{set_options, [print_ops, {print_messages,detail}]}]
+                              ),
+        {ok, AfterKexState} = connect_and_kex(Config, InitialState, ClientAlgs, Variant),
+        {User, Pwd} = server_user_password(Config),
+        {ok, AfterAuthState} =
+            ssh_trpt_test_lib:exec(
+              [{send, #ssh_msg_service_request{name = "ssh-userauth"}},
+               {match, #ssh_msg_service_accept{name = "ssh-userauth"}, receive_msg},
+               {send, #ssh_msg_userauth_request{user = User,
+                                                service = "ssh-connection",
+                                                method = "password",
+                                                data = <<?BOOLEAN(?FALSE),
+                                                         ?STRING(unicode:characters_to_binary(Pwd))>>
+                                               }},
+               {match, #ssh_msg_userauth_success{_='_'}, receive_msg}
+              ], AfterKexState),
+        {ok, _} =
+            ssh_trpt_test_lib:exec([{send, ssh_msg_kexinit_guess},
+                                    {match, #ssh_msg_kexinit{_='_'}, receive_msg}] ++
+                                       get_specific_ops(GuessCorrect, Variant) ++
+                                       [{send, #ssh_msg_newkeys{}},
+                                        {match, #ssh_msg_newkeys{_='_'}, receive_msg}],
+                                   AfterAuthState),
+        ?CT_LOG("Finished~n")
+    after
+        stop_std_daemon(Config)
+    end.
+
+%%%----------------------------------------------------------------
+get_parent_group(Config) ->
+    [[{name, Group}]] = proplists:get_value(tc_group_path, Config),
+    Group.
+
+%%%----------------------------------------------------------------
+
+%%% Specific operations for the guess and variant (dh, ecdh, ...)
+get_specific_ops(GuessCorrect, Variant) ->
+    {InitGuess, Init, Reply} = get_specific_ops(Variant),
+    [{send, InitGuess}] ++
+        [{send, Init} || GuessCorrect =:= false] ++
+        [{match, Reply, receive_msg}].
+
+%%% RFC 4253 section 7, each side may guess
+get_specific_ops(dh) ->
+    {ssh_msg_kexdh_init_guess, ssh_msg_kexdh_init, #ssh_msg_kexdh_reply{_='_'}};
+get_specific_ops(ecdh) ->
+    {ssh_msg_kex_ecdh_init_guess, ssh_msg_kex_ecdh_init, #ssh_msg_kex_ecdh_reply{_='_'}};
+get_specific_ops(hybrid) ->
+    {ssh_msg_kex_hybrid_init_guess, ssh_msg_kex_hybrid_init, #ssh_msg_kex_hybrid_reply{_='_'}}.
