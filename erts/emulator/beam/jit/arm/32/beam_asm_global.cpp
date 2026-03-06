a7913b9f157762f7e872cac822b7931794af6a7d
Luca Succi <luca.succi@stritzinger.com>
2026-03-06 10:24:07 +0100

    Fix GC division bug
    Divide by 4 instead of 8 on arm32

--- erts/emulator/beam/jit/arm/32/beam_asm_global.cpp
+++ erts/emulator/beam/jit/arm/32/beam_asm_global.cpp
@@ -102,7 +102,7 @@ void BeamGlobalAssembler::emit_garbage_collect() {
      * the GC routines handle that separately and we don't want it to be added
      * twice. */
     a.sub(ARG2, ARG3, HTOP);
-    a.lsr(ARG2, ARG2, imm(3));
+    a.lsr(ARG2, ARG2, imm(2));
     a.sub(ARG2, ARG2, imm(S_RESERVED));
 
     /* Save our return address in c_p->i so we can tell where we crashed if we
