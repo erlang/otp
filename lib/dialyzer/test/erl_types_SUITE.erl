06817a170b21cbcb630f8f0a4781ec4bfca537e1
williamthome <williamthome@hotmail.com>
2026-03-08 01:43:00 -0300

    Add license headers to files missing them

--- lib/dialyzer/test/erl_types_SUITE.erl
+++ lib/dialyzer/test/erl_types_SUITE.erl
@@ -1,5 +1,11 @@
 %% -*- erlang-indent-level: 4 -*-
 %%
+%% %CopyrightBegin%
+%%
+%% SPDX-License-Identifier: Apache-2.0
+%%
+%% Copyright Ericsson AB 2025-2026. All Rights Reserved.
+%%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
 %% You may obtain a copy of the License at
@@ -11,6 +17,8 @@
 %% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
+%%
+%% %CopyrightEnd%
 %%
 -module(erl_types_SUITE).
 
