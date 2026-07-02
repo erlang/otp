0fbc69bbe2ff39a517eca05de2d02f38047c359e
Micael Karlberg <bmk@erlang.org>
2026-07-02 14:20:08 +0200

    [kernel|test] Print socket info at (sctp) suite init

--- lib/kernel/test/socket_sctp_SUITE.erl
+++ lib/kernel/test/socket_sctp_SUITE.erl
@@ -289,8 +289,9 @@ init_per_suite(Config0) ->
        "~n      Nodes:  ~p", [Config0, erlang:nodes()]),
 
     try socket:info() of
-        #{load_nif_result := ok} ->
-	    ?P("~s -> socket nif loaded", [?FUNCTION_NAME]),
+        #{load_nif_result := ok} = SocketInfo ->
+	    ?P("~s -> socket nif loaded: "
+               "~n   ~p", [?FUNCTION_NAME, SocketInfo]),
             has_support_sctp(),
             case ?KLIB:init_per_suite(Config0) of
                 {skip, _} = SKIP ->
