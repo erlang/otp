778a9fb357518915040fa52707075d2ef5795f35
Michał Wąsowski <michal@erlang.org>
2026-07-22 23:22:12 +0200

    Implement openssh mitigation for CVE-2008-5161

--- lib/ssh/src/ssh_fsm.hrl
+++ lib/ssh/src/ssh_fsm.hrl
@@ -30,38 +30,27 @@
 %% Internal process state
 %%====================================================================
 -record(data, {
-	  starter                               :: pid()
-						 | undefined,
-	  auth_user                             :: string()
-						 | undefined,
-	  connection_state                      :: #connection{}
-						 | undefined,
-	  latest_channel_id         = 0         :: non_neg_integer()
-                                                 | undefined,
-	  transport_protocol                    :: atom()
-                                                 | undefined,	% ex: tcp
-	  transport_cb                          :: atom()
-                                                 | undefined,	% ex: gen_tcp
-	  transport_close_tag                   :: atom()
-                                                 | undefined,	% ex: tcp_closed
-	  ssh_params                            :: #ssh{}
-                                                 | undefined,
-	  socket                                :: gen_tcp:socket()
-                                                 | undefined,
-	  decrypted_data_buffer     = <<>>      :: binary()
-                                                 | undefined,
-	  encrypted_data_buffer     = <<>>      :: binary()
-                                                 | undefined,
-	  aead_data                 = <<>>      :: binary()
-                                                 | undefined,
-	  undecrypted_packet_length             :: undefined | non_neg_integer(),
-	  key_exchange_init_msg                 :: #ssh_msg_kexinit{}
-						 | undefined,
-	  last_size_rekey           = 0         :: non_neg_integer(),
-	  event_queue               = []        :: list(),
-	  inet_initial_buffer_size              :: pos_integer()
-						 | undefined
-	 }).
+               starter                               :: pid() | undefined,
+               auth_user                             :: string() | undefined,
+               connection_state                      :: #connection{} | undefined,
+               latest_channel_id         = 0         :: non_neg_integer() | undefined,
+               transport_protocol                    :: atom() | undefined,  % ex: tcp
+               transport_cb                          :: atom() | undefined,  % ex: gen_tcp
+               transport_close_tag                   :: atom() | undefined,  % ex: tcp_closed
+               ssh_params                            :: #ssh{} | undefined,
+               socket                                :: gen_tcp:socket() | undefined,
+               decrypted_data_buffer     = <<>>      :: binary() | undefined,
+               encrypted_data_buffer     = <<>>      :: binary() | undefined,
+               aead_data                 = <<>>      :: binary() | undefined,
+               undecrypted_packet_length             :: undefined | non_neg_integer(),
+               key_exchange_init_msg                 :: #ssh_msg_kexinit{} | undefined,
+               last_size_rekey           = 0         :: non_neg_integer(),
+               event_queue               = []        :: list(),
+               inet_initial_buffer_size              :: pos_integer() | undefined,
+               discard_bytes_left        = 0         :: integer(),
+               discard_mac_already       = 0         :: non_neg_integer(),
+               discard_reason                        :: term()
+              }).
 
 
 %%====================================================================
