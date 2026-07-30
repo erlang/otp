erlang/otp

commit e522e738e15910ad7678910ec0797b2444d39772

epmd: Fix a bug so that we can change default listen address later

erts/epmd/src/epmd_srv.c
@@ -292,16 +292,17 @@ void run(EpmdVars *g)
   if (g->addresses != NULL && /* String contains non-separator characters if: */
       g->addresses[strspn(g->addresses," ,")] != '\000')
     {
+      enum {
+        EPMD_SPECIAL_ADDRESS_V4_LOOPBACK = 1 << 0,
+        EPMD_SPECIAL_ADDRESS_V4_ANY = 1 << 1,
+#if defined(EPMD6)
+        EPMD_SPECIAL_ADDRESS_V6_LOOPBACK = 1 << 2,
+        EPMD_SPECIAL_ADDRESS_V6_ANY = 1 << 3,
+#endif
+      } special_addresses = 0;
+
       char *tmp = NULL;
       char *token = NULL;
-
-      /* Always listen on the loopback. */
-      SET_ADDR(iserv_addr[num_sockets],htonl(INADDR_LOOPBACK),sport);
-      num_sockets++;
-#if defined(EPMD6)
-      SET_ADDR6(iserv_addr[num_sockets],in6addr_loopback,sport);
-      num_sockets++;
-#endif
 
       /* IPv4 and/or IPv6 loopback may not be present, for example if
        * the protocol stack is explicitly disabled in the kernel.  If
@@ -348,23 +349,47 @@ void run(EpmdVars *g)
 	    }
 
 #if defined(EPMD6)
-	  if (sa->ss_family == AF_INET6 && IN6_IS_ADDR_LOOPBACK(&addr6))
-	      continue;
-
-	  if (sa->ss_family == AF_INET)
+          if (sa->ss_family == AF_INET6) {
+            if (IN6_IS_ADDR_LOOPBACK(&addr6)) {
+              special_addresses |= EPMD_SPECIAL_ADDRESS_V6_LOOPBACK;
+            } else if (IN6_ARE_ADDR_EQUAL(&addr6, &in6addr_any)) {
+              special_addresses |= EPMD_SPECIAL_ADDRESS_V6_ANY;
+            }
+          } else if (sa->ss_family == AF_INET)
 #endif
-	  if (IS_ADDR_LOOPBACK(addr))
-	    continue;
+          {
+            if (IS_ADDR_LOOPBACK(addr)) {
+                special_addresses |= EPMD_SPECIAL_ADDRESS_V4_LOOPBACK;
+            } else if (IS_ADDR_ANY(addr)) {
+                special_addresses |= EPMD_SPECIAL_ADDRESS_V4_ANY;
+            }
+          }
 
 	  num_sockets++;
 
-	  if (num_sockets >= MAX_LISTEN_SOCKETS)
+	  if (num_sockets >= (MAX_LISTEN_SOCKETS - 2))
 	    {
 	      dbg_tty_printf(g,0,"cannot listen on more than %d IP addresses",
-			     MAX_LISTEN_SOCKETS);
+			     (MAX_LISTEN_SOCKETS - 2));
 	      epmd_cleanup_exit(g,1);
 	    }
 	}
+
+        /* Always listen on the loopback address, taking care not to clash with
+         * INADDR_ANY if specified. */
+        if (!(special_addresses & (EPMD_SPECIAL_ADDRESS_V4_LOOPBACK |
+                                   EPMD_SPECIAL_ADDRESS_V4_ANY))) {
+          SET_ADDR(iserv_addr[num_sockets],htonl(INADDR_LOOPBACK),sport);
+          num_sockets++;
+        }
+
+#if defined(EPMD6)
+        if (!(special_addresses & (EPMD_SPECIAL_ADDRESS_V6_LOOPBACK |
+                                   EPMD_SPECIAL_ADDRESS_V6_ANY))) {
+          SET_ADDR6(iserv_addr[num_sockets],in6addr_loopback,sport);
+          num_sockets++;
+        }
+#endif
 
       free(tmp);
     }

erts/epmd/src/epmd_int.h
@@ -172,6 +172,7 @@ static const struct in6_addr in6addr_loopback =
 #endif /* HAVE_IN6 */
 
 #define IS_ADDR_LOOPBACK(addr) ((addr).s_addr == htonl(INADDR_LOOPBACK))
+#define IS_ADDR_ANY(addr) ((addr).s_addr == htonl(INADDR_ANY))
 
 #if defined(EPMD6)
 

commit e3d12c3a79f968f60b7d984c1f34b9ba774650d7

epmd: Fix regression introduced in e522e738e

erts/epmd/src/epmd_srv.c
@@ -304,14 +304,6 @@ void run(EpmdVars *g)
       char *tmp = NULL;
       char *token = NULL;
 
-      /* IPv4 and/or IPv6 loopback may not be present, for example if
-       * the protocol stack is explicitly disabled in the kernel.  If
-       * any sockets to this point fail, log the error but do not exit
-       * with failure.  If any socket after this (explicitly
-       * configured via addresses) fails, then consider the error
-       * fatal. */
-      nonfatal_sockets = num_sockets;
-
 	  if ((tmp = strdup(g->addresses)) == NULL)
 	{
 	  dbg_perror(g,"cannot allocate memory");
@@ -379,6 +371,13 @@ void run(EpmdVars *g)
          * INADDR_ANY if specified. */
         if (!(special_addresses & (EPMD_SPECIAL_ADDRESS_V4_LOOPBACK |
                                    EPMD_SPECIAL_ADDRESS_V4_ANY))) {
+          /* IPv4 and/or IPv6 loopback may not be present, for example if
+           * the protocol stack is explicitly disabled in the kernel. If
+           * any implicitly enabled sockets fail, log the error but do not exit
+           * with failure. */
+          ASSERT(MAX_LISTEN_SOCKETS <= 16);
+          nonfatal_sockets |= (1 << num_sockets);
+
           SET_ADDR(iserv_addr[num_sockets],htonl(INADDR_LOOPBACK),sport);
           num_sockets++;
         }
@@ -386,6 +385,9 @@ void run(EpmdVars *g)
 #if defined(EPMD6)
         if (!(special_addresses & (EPMD_SPECIAL_ADDRESS_V6_LOOPBACK |
                                    EPMD_SPECIAL_ADDRESS_V6_ANY))) {
+          ASSERT(MAX_LISTEN_SOCKETS <= 16);
+          nonfatal_sockets |= (1 << num_sockets);
+
           SET_ADDR6(iserv_addr[num_sockets],in6addr_loopback,sport);
           num_sockets++;
         }
@@ -508,10 +510,9 @@ void run(EpmdVars *g)
               dbg_perror(g,"failed to bind on ipaddr %s",
                          epmd_ntop(&iserv_addr[i],
                                    socknamebuf, sizeof(socknamebuf)));
-              if (i >= nonfatal_sockets)
+              if (!(nonfatal_sockets & (1 << i))) {
                   epmd_cleanup_exit(g,1);
-              else
-              {
+              } else {
                   close(listensock[i]);
                   continue;
               }