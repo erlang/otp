b2253b431e872dc6ee26f232dcede57da50bf968
Konrad Pietrzak <konrad@erlang.org>
2026-08-19 14:26:37 +0200

    inets: Adjust keep_alive_timeout effective measurement in httpd
        keep_alive_timeout would check the time between opening a connection,
        or right after sending a response, until the next request's headers
        are parsed.
        Now the timeout is cancelled as soon as new data arrives, before the
        parsing.
        Allow atom `infinity` as a value for keep_alive_timeout to disable the
        timeout entirely.

--- lib/inets/src/http_server/httpd_request_handler.erl
+++ lib/inets/src/http_server/httpd_request_handler.erl
@@ -118,9 +118,11 @@ init([Manager, ConfigDB, AcceptTimeout]) ->
 
             Peername = http_transport:peername(SocketType, Socket),
             Sockname = http_transport:sockname(SocketType, Socket),
-
             %%Timeout value is in seconds we want it in milliseconds
-            KeepAliveTimeOut = 1000 * httpd_util:lookup(ConfigDB, keep_alive_timeout, 150),
+            KeepAliveTimeOut = case Keepalive of
+                           true -> 1000 * httpd_util:lookup(ConfigDB, keep_alive_timeout, 150);
+                           _ -> infinity
+                       end,
 
             case http_transport:negotiate(SocketType, Socket, ?HANDSHAKE_TIMEOUT) of
                 {error, {tls_alert, {_, AlertDesc}} = Error} ->
@@ -191,7 +193,7 @@ continue_init(Manager, ConfigDB, SocketType, Socket, Peername, Sockname,
                            chunk                   = chunk_start(MaxChunk)},
             setopts(Socket, SocketType, [binary, {packet, 0}, {active, once}]),
             NewState =
-                data_receive_counter(activate_request_timeout(State),
+                data_receive_counter(activate_keepalive_timeout(State),
                                      httpd_util:lookup(ConfigDB, minimum_bytes_per_second, false)),
             gen_server:enter_loop(?MODULE, [], NewState)
     end.
@@ -242,7 +244,7 @@ handle_info({Proto, Socket, Data},
   when (((Proto =:= tcp) orelse 
 	 (Proto =:= ssl) orelse 
 	 (Proto =:= dummy)) andalso is_binary(Data)) ->
-
+    cancel_keepalive_timeout(State),
     PROCESSED = (catch Module:Function([Data | Args])),
     NewDataSize = case State#state.byte_limit of
 		      undefined ->
@@ -255,9 +257,9 @@ handle_info({Proto, Socket, Data},
         {ok, Result} ->
 	    NewState = case NewDataSize of
 			   undefined ->
-			       cancel_request_timeout(State);
+			       State;
 			   _ ->
-			       set_new_data_size(cancel_request_timeout(State), NewDataSize)
+			       set_new_data_size(State, NewDataSize)
 		       end,
             handle_msg(Result, NewState);
 	{error, {size_error, MaxSize, ErrCode, ErrStr}, Version} ->
@@ -357,7 +359,7 @@ terminate(_Reason, State) ->
     do_terminate(State).
 
 do_terminate(#state{mod = ModData} = State) ->
-    cancel_request_timeout(State),
+    cancel_keepalive_timeout(State),
     httpd_socket:close(ModData#mod.socket_type, ModData#mod.socket).
 
 format_status(normal, [_, State]) ->
@@ -707,7 +709,7 @@ handle_next_request(#state{mod = #mod{connection = true} = ModData,
                            chunk                  = chunk_start(MaxChunk),
 			   response_sent          = false},
     
-    NewState = activate_request_timeout(TmpState),
+    NewState = activate_keepalive_timeout(TmpState),
 
     case Data of
 	<<>> ->
@@ -720,9 +722,12 @@ handle_next_request(#state{mod = #mod{connection = true} = ModData,
 handle_next_request(State, _) ->
     {stop, normal, State}.
 
-activate_request_timeout(#state{timeout = Time} = State) ->
+activate_keepalive_timeout(#state{timeout = infinity} = State) ->
+    State;
+activate_keepalive_timeout(#state{timeout = Time} = State) ->
+    NewState = cancel_keepalive_timeout(State),
     Ref = erlang:send_after(Time, self(), timeout),
-    State#state{timer = Ref}.
+    NewState#state{timer = Ref}.
 data_receive_counter(State, Byte_limit) ->
     case Byte_limit of
 	false ->
@@ -731,9 +736,9 @@ data_receive_counter(State, Byte_limit) ->
 	    erlang:send_after(3000, self(), check_data_first),
 	    State#state{data = 0, byte_limit = Nr}
     end.
-cancel_request_timeout(#state{timer = undefined} = State) ->
+cancel_keepalive_timeout(#state{timer = undefined} = State) ->
     State;
-cancel_request_timeout(#state{timer = Timer} = State) ->
+cancel_keepalive_timeout(#state{timer = Timer} = State) ->
     erlang:cancel_timer(Timer),
     receive 
 	timeout ->
