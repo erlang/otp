fd1aeb168e19708643ec0c666af7f354866a6982
Lukas Backström <lukas@erlang.org>
2026-09-03 11:18:49 +0200

    stdlib: Fix ets documentation

--- lib/stdlib/src/ets.erl
+++ lib/stdlib/src/ets.erl
@@ -2698,9 +2698,9 @@ table by using `insert/2`.
 When called with argument `read`, the function `InitFun` is assumed to return
 `end_of_input` when there is no more input, or `{Objects, Fun}`, where `Objects`
 is a list of objects and `Fun` is a new input function. Any other value `Value`
-is returned as an error `{error, {init_fun, Value}}`. Each input function is
-called exactly once, and if an error occur, the last function is called with
-argument `close`, the reply of which is ignored.
+will cause an error to be raised with `Value` as the error reason. Each input
+function is called exactly once, and if an error occur, the last function is
+called with argument `close`, the reply of which is ignored.
 
 If the table type is `set` and more than one object exists with a given key, one
 of the objects is chosen. This is not necessarily the last object with the given
