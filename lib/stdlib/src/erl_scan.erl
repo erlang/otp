c3864d660047f4049a87c8661f3513d74b3537ae
Maria Scott <maria-12648430@hnc-agency.org>
2026-09-09 15:07:21 +0200

    Bugfix in erl_scan character class

--- lib/stdlib/src/erl_scan.erl
+++ lib/stdlib/src/erl_scan.erl
@@ -458,7 +458,7 @@ string_quote(_)    -> $". %"
           C =:= $_ orelse
           C >= $0 andalso C =< $9 orelse
           C =:= $@ orelse
-          C >= $ß andalso C =< $ÿ andalso C =/= $÷ andalso
+          C >= $ß andalso C =< $ÿ andalso C =/= $÷ orelse
           C >= $À andalso C =< $Þ andalso C =/= $×))).
 -define(CHAR(C), (is_integer(C) andalso 0 =< C andalso C < 16#110000)).
 -define(UNICODE(C),
