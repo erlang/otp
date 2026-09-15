088122631e4c4afb9ad738600b1ef85aff4a505b
Ingela Anderton Andin <ingela@erlang.org>
2026-09-15 15:35:18 +0200

    ssl: Fix versions/0 doc key app_vsn -> ssl_app
    The versions/0 -doc listed the version entry key as app_vsn, but both
    the -spec and the function body use ssl_app ({ssl_app, ?VSN}). A user
    following the documentation, e.g. lists:keyfind(app_vsn, 1,
    ssl:versions()), would get false. Correct the doc bullet to ssl_app.

--- lib/ssl/src/ssl.erl
+++ lib/ssl/src/ssl.erl
@@ -3391,7 +3391,7 @@ sockname(#sslsocket{pid = [Pid| _], fd = {Transport, Socket,_,_}}) when is_pid(P
 Lists information, mainly concerning TLS/DTLS versions, in runtime for debugging
 and testing purposes.
 
-- **`app_vsn`** - The application version of the SSL application.
+- **`ssl_app`** - The application version of the SSL application.
 
 - **`supported`** - TLS versions supported with current application environment
   and crypto library configuration. Overridden by a version option on
